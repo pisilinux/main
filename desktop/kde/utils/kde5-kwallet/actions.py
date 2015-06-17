cdc39d8abd111ca85d233c9761553329bbd55353
groni <groni3427@gmail.com>
2015-06-17 18:20:00 +0200

    add folder lookandfeel, utils and more packages

---
+++ desktop/kde/utils/kde5-kwallet/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import cmaketools
+
+def setup():
+    cmaketools.configure("-DCMAKE_BUILD_TYPE=Release \
+                          -DECM_MKSPECS_INSTALL_DIR=/usr/lib/qt5/mkspecs/modules \
+                          -DLIB_INSTALL_DIR=lib \
+                          -DBUILD_TESTING=OFF")
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.install()
+    
+    pisitools.dodoc("README.md","COPYING.LIB")
