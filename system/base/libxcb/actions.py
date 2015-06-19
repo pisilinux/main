25736b05e809da2494f52b0cd92f6b498d0dd8b3
ayhanyalcinsoy <ayhanyalcinsoy@gmail.com>
2015-06-19 13:39:55 +0300

    moved into main repo for pisi 2.0

---
+++ system/base/libxcb/actions.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    pisitools.flags.add("-DNDEBUG")
+
+    autotools.autoreconf("-vif")
+    autotools.configure("--disable-static \
+                         --enable-xevie \
+                         --enable-xprint \
+                         --enable-xinput \
+                         --enable-xkb \
+                         --without-launchd \
+                         --without-doxygen")
+
+    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ") 
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("-j1 DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("COPYING", "NEWS", "README")
