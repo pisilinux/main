a403ebbf3576e6061ecdb4aeb005fa855577dd20
Ertuğrul Erata <ertugrulerata@gmail.com>
2015-06-28 09:06:44 +0300

    works for mesa in blfs mesa not need llvm?

---
+++ programming/misc/libedit/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--disable-static \
+                         --enable-widec \
+                         --disable-dependency-tracking \
+                         --enable-fast-install")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("ChangeLog", "COPYING", "THANKS")
+    pisitools.remove("/usr/lib/libedit.la")
