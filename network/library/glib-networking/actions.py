0a5bc79cc4324e9f18b4f1b0854883b05d24f966
Ertuğrul Erata <ertugrulerata@gmail.com>
2015-07-13 15:15:56 +0300

    libsoup, glib-networking and gnutls fix deps.

---
+++ network/library/glib-networking/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure("--disable-static \
+                         --disable-installed-tests \
+                         --with-ca-certificates=/etc/ssl/certs/ca-certificates.crt \
+                         --without-libproxy \
+                         --with-gnutls \
+                         --with-pkcs11")
+
+def build():
+    autotools.make()
+
+def check():
+    autotools.make("check")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
