cbf6a0eb6c49ccc7cade7db79a6779072a30779b
Ertuğrul Erata <ertugrulerata@gmail.com>
2015-07-15 19:28:56 +0300

    move to pisi-2.0 main not builded.

---
+++ x11/driver/xorg-input-vmmouse/actions.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("\
+                         --without-hal-fdi-dir      \
+                         --without-hal-callouts-dir \
+                         --with-udev-rules-dir=/lib/udev/rules.d \
+                        ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
