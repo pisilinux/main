fc574a8180d66e1af70f67d5065b34493f10c165
Ertuğrul Erata <ertugrulerata@gmail.com>
2015-07-22 21:46:44 +0300

    mc in main

---
+++ util/misc/mc/actions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--with-screen=slang \
+                         --with-gpm-mouse \
+                         --with-vfs \
+                         --with-ext2undel \
+                         --with-edit \
+                         --with-x=yes \
+                         --enable-charset \
+                         --enable-nls \
+                         --without-samba \
+                         --with-configdir=/etc/samba \
+                         --with-codepagedir=/var/lib/samba/codepages \
+                         --with-privatedir=/etc/samba/private")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall('DESTDIR="%s"' % get.installDIR())
+
+    pisitools.dodoc("ABOUT*", "AUTHORS", "ChangeLog", "NEWS", "README*")
