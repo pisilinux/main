5967058f89529fc1959946253d7b53a85435d33d
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2015-07-23 13:50:56 +0300

    libv4l:moved into main for pisi 2.0

---
+++ multimedia/video/libv4l/actions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+        #autotools.autoreconf("-vfi")
+        autotools.configure("--disable-static \
+                             --disable-rpath")
+
+        pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    if get.buildTYPE() == "emul32": return
+
+    pisitools.dodoc("ChangeLog", "COPYING*", "README*", "TODO")
+    pisitools.insinto("/%s/%s/" % (get.docDIR(), get.srcNAME()), "contrib")
+
