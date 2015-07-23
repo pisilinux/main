98d52842959db414574e8475ceff0ba602fbd04d
Ertuğrul Erata <ertugrulerata@gmail.com>
2015-07-23 10:28:34 +0300

    exiv2 fix builddep.

---
+++ multimedia/misc/exiv2/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--disable-static \
+                         --disable-dependency-tracking \
+                         --disable-rpath")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("COPYING", "README", "doc/ChangeLog")
