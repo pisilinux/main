f722e3b8d6ef4476eb267edc5c32846cb628d7b4
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2015-07-28 12:41:19 +0300

    kde-gtk-config:fixed dep

--- desktop/kde/plasma/kde-gtk-config/actions.py
+++ desktop/kde/plasma/kde-gtk-config/actions.py
@@ -8,11 +8,18 @@ from pisi.actionsapi import kde5
 from pisi.actionsapi import pisitools
 
 def setup():
-    kde5.configure()
+    kde5.configure("-DCMAKE_BUILD_TYPE=Release \
+                   -DCMAKE_INSTALL_PREFIX=/usr \
+                   -DLIB_INSTALL_DIR=lib \
+                   -DINCLUDE_DIR=/include/gtk-3.0/gtk \
+                   -DLIBEXEC_INSTALL_DIR=lib \
+                   -DSYSCONF_INSTALL_DIR=/etc \
+                   -DKDE_INSTALL_USE_QT_SYS_PATHS=ON \
+                   -DBUILD_TESTING=OFF")
 
 def build():
     kde5.make()
 
 def install():
     kde5.install()
-    
+
