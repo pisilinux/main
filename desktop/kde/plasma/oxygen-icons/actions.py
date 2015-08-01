52f50912ec26e02e1cfb12cf04329a3a48df78e0
Ertuğrul Erata <ertugrulerata@gmail.com>
2015-08-02 01:27:39 +0300

    oxygen-icons in main

---
+++ desktop/kde/plasma/oxygen-icons/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    cmaketools.configure("-DCMAKE_BUILD_TYPE=Release \
+                         -DKDE4_BUILD_TESTS=OFF \
+                         -DCMAKE_INSTALL_PREFIX=/usr")
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
