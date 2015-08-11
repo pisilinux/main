c6cc27076e003a719e6e2dd7e2b7b8d99974d418
Ertuğrul Erata <ertugrulerata@gmail.com>
2015-08-11 16:22:17 +0300

    fix and remove unused in xorg-video-intel

--- x11/driver/xorg-video-intel/actions.py
+++ x11/driver/xorg-video-intel/actions.py
@@ -15,6 +15,8 @@ def setup():
                          --disable-dri3 \
                          --enable-glamor \
                         ")
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
     autotools.make()
