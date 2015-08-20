3d64c08135a1fbb026867e140962e03d379af26c
groni <groni3427@gmail.com>
2015-08-20 18:41:42 +0200

    change actions.py

--- desktop/kde/application/kruler/actions.py
+++ desktop/kde/application/kruler/actions.py
@@ -5,15 +5,15 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import kde4
+from pisi.actionsapi import kde5
 from pisi.actionsapi import get
 
 
 def setup():
-    kde4.configure()
+    kde5.configure()
 
 def build():
-    kde4.make()
+    kde5.make()
 
 def install():
-    kde4.install()
+    kde5.install()
