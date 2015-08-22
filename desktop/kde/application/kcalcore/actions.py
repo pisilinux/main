c459fd18b45a19b3e22a3932c0b5619262493839
Ertuğrul Erata <ertugrulerata@gmail.com>
2015-08-22 23:28:11 +0300

    prison-qt5 fixed in main

--- desktop/kde/application/kcalcore/actions.py
+++ desktop/kde/application/kcalcore/actions.py
@@ -4,16 +4,16 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import kde5
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import kde5
 
 def setup():
-    kde5.configure("-DQT5_BUILD=ON")
+    kde5.configure()
 
 def build():
     kde5.make()
 
 def install():
     kde5.install()
-
-    pisitools.dodoc("LICENSE")
+    
+    pisitools.dodoc("COPYING")
