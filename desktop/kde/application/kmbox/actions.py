860acfe3ef3419e5b64bac0efd1d86b6d02bdb8b
Ertuğrul Erata <ertugrulerata@gmail.com>
2015-08-23 00:33:52 +0300

    akonadi fix ..

--- desktop/kde/application/kmbox/actions.py
+++ desktop/kde/application/kmbox/actions.py
@@ -4,17 +4,16 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import kde5
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import kde5
 
 def setup():
-    kde5.configure("-DCMAKE_BUILD_TYPE=Release \
-                    -DINSTALL_QSQLITE_IN_QT_PREFIX=TRUE \
-                    -DWITH_SOPRANO=OFF")
+    kde5.configure()
 
 def build():
     kde5.make()
 
 def install():
     kde5.install()
-    pisitools.dodoc("AUTHORS", "NEWS", "README")
+    
+    pisitools.dodoc("COPYING.LIB")
