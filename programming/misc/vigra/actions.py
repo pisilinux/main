c7cafb93d74425ab98936a80c2cf54ffd9db6a1c
namso-01 <namso-01@hotmail.it>
2015-08-23 23:02:38 +0300

    vigra: depoya eklendi.

---
+++ programming/misc/vigra/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+#WorkDir = "%s-%s" % (get.srcNAME(), get.srcVERSION())
+NoStrip=["/usr/share/doc"]
+
+def setup():
+    cmaketools.configure("-DWITH_VIGRANUMPY=1 \
+                          -DWITH_OPENEXR=1 \
+                          -DDOXYGEN_FOUND=0")
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.install()
+
+    pisitools.dodoc("README*", "LICENSE*")
