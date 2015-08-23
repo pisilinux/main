64a12b2698e31ade47c853d97686892dffc75e77
namso-01 <namso-01@hotmail.it>
2015-08-23 23:00:47 +0300

    hugin: depoya eklendi.

---
+++ multimedia/graphics/hugin/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    cmaketools.configure("-DCMAKE_BUILD_TYPE=Release \
+                          -DENABLE_LAPACK=yes")
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall('DESTDIR="%s"' % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "README", "TODO")
