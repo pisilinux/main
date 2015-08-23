aa2d8265b2c6690a0609f208b112b98c0d9a135c
Ertuğrul Erata <ertugrulerata@gmail.com>
2015-08-23 18:37:53 +0300

    akonadi-search, kmailtransport in main, kpimtextedit fix build.

---
+++ desktop/kde/application/akonadi-search/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import kde5
+from pisi.actionsapi import pisitools
+
+def setup():
+    kde5.configure("-DCMAKE_BUILD_TYPE=Release \
+                    -DINSTALL_QSQLITE_IN_QT_PREFIX=TRUE \
+                    -DPLUGIN_INSTALL_DIR=/usr/lib/qt5/plugins \
+                    -DLIB_INSTALL_DIR=/usr/lib \
+                    -DWITH_SOPRANO=OFF")
+
+def build():
+    kde5.make()
+
+def install():
+    kde5.install()
+    
+    pisitools.dodoc("COPYING*")
