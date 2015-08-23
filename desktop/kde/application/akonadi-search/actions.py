9e630c3299c0783c7a7c2bfa9e755c53b2b4e23a
Ertuğrul Erata <ertugrulerata@gmail.com>
2015-08-23 18:46:53 +0300

    actions.py fix, remove unneded options.

--- desktop/kde/application/akonadi-search/actions.py
+++ desktop/kde/application/akonadi-search/actions.py
@@ -8,16 +8,12 @@ from pisi.actionsapi import kde5
 from pisi.actionsapi import pisitools
 
 def setup():
-    kde5.configure("-DCMAKE_BUILD_TYPE=Release \
-                    -DINSTALL_QSQLITE_IN_QT_PREFIX=TRUE \
-                    -DPLUGIN_INSTALL_DIR=/usr/lib/qt5/plugins \
-                    -DLIB_INSTALL_DIR=/usr/lib \
-                    -DWITH_SOPRANO=OFF")
+    kde5.configure()
 
 def build():
     kde5.make()
 
 def install():
     kde5.install()
-    
+
     pisitools.dodoc("COPYING*")
