b4c290dc1232ad1831319337c391ae372b011a1d
groni <groni3427@gmail.com>
2015-08-24 14:09:52 +0200

    correct actions.py

--- desktop/kde/application/ktnef/actions.py
+++ desktop/kde/application/ktnef/actions.py
@@ -8,16 +8,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import kde5
 
 def setup():
-    kde5.configure("-DCMAKE_BUILD_TYPE=Release \
-                          -DECM_MKSPECS_INSTALL_DIR=/usr/lib/qt5/mkspecs/modules \
-                          -DQT_PLUGIN_INSTALL_DIR=lib/qt5/plugins \
-                          -DLIB_INSTALL_DIR=lib \
-                          -DSYSCONF_INSTALL_DIR=/etc \
-                          -DKDE_INSTALL_USE_QT_SYS_PATHS=ON \
-                          -DCMAKE_SKIP_RPATH=ON \
-                          -DPYTHON_EXECUTABLE=/usr/bin/python3 \
-                          -DQML_INSTALL_DIR=/usr/lib/qt5/qml \
-                          -DBUILD_TESTING=OFF")
+    kde5.configure()
 
 def build():
     kde5.make()
