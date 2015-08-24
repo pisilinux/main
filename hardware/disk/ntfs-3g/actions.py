9e2b3314d6ed4383b4bd5050dd9ea46510fce6e2
Ertuğrul Erata <ertugrulerata@gmail.com>
2015-08-24 09:43:46 +0300

    ntfs-3g moved to main

---
+++ hardware/disk/ntfs-3g/actions.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.export("CFLAGS", "%s -D_FILE_OFFSET_BITS=64" % get.CFLAGS())
+    autotools.configure("--prefix=/usr \
+                        --sbin=/usr/bin \
+                        --mandir=/usr/share/man \
+                        --disable-ldconfig \
+                        --disable-static \
+                        --with-fuse=external \
+                        --enable-posix-acls \
+                        --enable-extras")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s rootbindir=/usr/bin rootsbindir=/usr/bin rootlibdir=/usr/lib" % get.installDIR())
+    
+    pisitools.domove("/usr/bin/ntfs-3g.*", "/bin")
+
+    # Create some compat symlinks
+    pisitools.dosym("/bin/ntfs-3g", "/sbin/mount.ntfs")
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "CREDITS", "NEWS", "README")
+    pisitools.dosym("/usr/share/doc/ntfs-3g", "/usr/share/doc/ntfsprogs")
