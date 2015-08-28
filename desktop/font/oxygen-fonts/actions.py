1d3dffd93e7d52c11c00fc86021c0962fabecbf3
Vedat Demir <vedat@pisilinux.org>
2015-08-28 23:10:51 +0300

    oxygen-fonts and fontforge

--- desktop/font/oxygen-fonts/actions.py
+++ desktop/font/oxygen-fonts/actions.py
@@ -2,12 +2,20 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/copyleft/gpl.txt
+# See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import kde5
+
+def setup():
+    kde5.configure()
+
+def build():
+    kde5.make()
 
 def install():
-    pisitools.insinto("/usr/share/fonts/oxygen", "*/*.ttf")
+    kde5.install()
 
-    pisitools.dodoc("COPYING-OFL", "README")
+
+    pisitools.dodoc("COPYING-OFL", "README.md")
