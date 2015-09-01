45514f493cb9744d43e326dba232e71ec016e3e6
alihanozturk <alihan@pisilinux.org>
2015-09-01 21:09:05 +0300

    soprano:ad d

---
+++ programming/misc/soprano/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+shelltools.export("JAVA_HOME","/usr/lib/jvm/java-7-openjdk")
+
+def setup():
+    cmaketools.configure("-DCMAKE_SKIP_RPATH=OFF")
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README")
