f9124720896cbf22e8d4cbecfc5140c8859bf4dd
groni <groni3427@gmail.com>
2015-09-14 14:44:57 +0200

    okular from playground/vedat

--- desktop/kde/application/okular/actions.py
+++ desktop/kde/application/okular/actions.py
@@ -4,7 +4,11 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import cmaketools
 from pisi.actionsapi import kde5
+from pisi.actionsapi import get
 
 
 def setup():
