bf11c551120a38f1df0c925bc797704c5b222828
Ertuğrul Erata <ertugerata@p2.0alfa5>
2015-10-01 22:46:01 +0300

    iputils update

--- network/misc/iputils/actions.py
+++ network/misc/iputils/actions.py
@@ -10,19 +10,16 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def build():
-    autotools.make("CCOPT='%s -fpie' CC=%s" % (get.CFLAGS(), get.CC()))
-
-    autotools.compile("ifenslave.c -o ifenslave")
+    autotools.make("USE_GNUTLS=no CCOPT='%s -fpie' CC=%s" % (get.CFLAGS(), get.CC()))
 
 def install():
-    for app in ["ping", "ping6"]:
-        pisitools.dobin(app)
+    pisitools.dobin("ping")
 
-    for app in ["clockdiff", "arping", "rdisc", "tracepath", "tracepath6", "traceroute6", "ifenslave"]:
+    for app in ["clockdiff", "rarpd", "tftpd", "arping", "rdisc", "tracepath", "tracepath6", "traceroute6"]:
         pisitools.dosbin(app)
 
     # We will not need these if we set cap on postInstall like Fedora
     shelltools.chmod("%s/usr/bin/ping" % get.installDIR(), 04711)
-    shelltools.chmod("%s/usr/bin/ping6" % get.installDIR(), 04711)
+    #shelltools.chmod("%s/usr/bin/ping6" % get.installDIR(), 04711)
 
     pisitools.dodoc("RELNOTES")
