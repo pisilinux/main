c5b66c36857260c0627f7a828205d56eba6545c7
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2015-10-03 21:46:26 +0300

    konversation:fixed dep

--- desktop/kde/application/konversation/actions.py
+++ desktop/kde/application/konversation/actions.py
@@ -8,7 +8,10 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import kde5
 
 def setup():
-    kde5.configure()
+    kde5.configure("-DCMAKE_BUILD_TYPE=Release \
+                    -DCMAKE_INSTALL_PREFIX=/usr \
+                    -DLIB_INSTALL_DIR=lib \
+                    -DKDE_INSTALL_USE_QT_SYS_PATHS=ON")
 
 def build():
     kde5.make()
