dbe03633c7022da50dc8992461655636aef33839
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2015-10-03 21:47:13 +0300

    kruler:fixed dep

--- desktop/kde/application/kruler/actions.py
+++ desktop/kde/application/kruler/actions.py
@@ -7,7 +7,11 @@
 from pisi.actionsapi import kde5
 
 def setup():
-    kde5.configure()
+    kde5.configure("-DCMAKE_BUILD_TYPE=Release \
+                    -DBUILD_TESTING=OFF \
+                    -DCMAKE_INSTALL_PREFIX=/usr \
+                    -DKDE_INSTALL_USE_QT_SYS_PATHS=ON \
+                    -DECM_MKSPECS_INSTALL_DIR=/usr/lib/qt5/mkspecs/modules")
 
 def build():
     kde5.make()
