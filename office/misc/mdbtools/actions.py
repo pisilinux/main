d184ddcd8978030b5edd872dfb0b7f19e36a2f1d
suvari <suvarice@gmail.com>
2015-10-14 16:59:08 +0300

    add mdbtools

---
+++ office/misc/mdbtools/actions.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir = "%s-%s" % (get.srcNAME(), get.srcVERSION().replace("_", ""))
+
+def setup():
+    #shelltools.unlink("acinclude.m4")
+    autotools.autoreconf("-fi")
+    autotools.configure("--disable-gtk-doc \
+                         --disable-static \
+                         --disable-man \
+                         --disable-gmdb2 \
+                         --with-unixodbc=/usr")
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+
+
+def check():
+    autotools.make("check")
+
+def build():
+    autotools.make("-j1")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "NEWS", "README", "TODO")
