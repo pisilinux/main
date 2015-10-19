fc5771734fb0ddd920473f9144670b38ac9e9587
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2015-10-19 14:35:52 +0300

    pyqt5:fixed. it works

--- programming/language/python/python-qt5/actions.py
+++ programming/language/python/python-qt5/actions.py
@@ -20,27 +20,6 @@ def setup():
                                     --qmake='/usr/bin/qmake-qt5' \
                                     --destdir='/usr/lib/python3.4/site-packages' \
                                     --sip-incdir='/usr/include/python3.4m' \
-                                    --enable=QtCore \
-                                    --enable=QtDBus \
-                                    --enable=QtDesigner \
-                                    --enable=QtGui \
-                                    --enable=QtHelp \
-                                    --enable=QtMultimedia\
-                                    --enable=QtMultimediaWidgets \
-                                    --enable=QtNetwork \
-                                    --enable=QtOpenGL \
-                                    --enable=QtPrintSupport \
-                                    --enable=QtQml \
-                                    --enable=QtQuick \
-                                    --enable=QtSensors \
-                                    --enable=QtSerialPort \
-                                    --enable=QtSql \
-                                    --enable=QtSvg \
-                                    --enable=QtTest \
-                                    --enable=QtWebKit \
-                                    --enable=QtWebKitWidgets \
-                                    --enable=QtWidgets \
-                                    --enable=QtXmlPatterns \
                                     CFLAGS='%s' CXXFLAGS='%s'" % (get.CFLAGS(), get.CXXFLAGS()), pyVer = "3")
     shelltools.system("find -name 'Makefile' | xargs sed -i 's|-Wl,-rpath,/usr/lib||g;s|-Wl,-rpath,.* ||g'")
 
@@ -50,28 +29,7 @@ def setup():
                                     --destdir='/usr/lib/python2.7/site-packages' \
                                     --sip-incdir='/usr/include/python2.7' \
                                     --sip /usr/bin/py2sip \
-                                    --qmake='/usr/bin/qmake-qt5'\
-                                    --enable=QtCore \
-                                    --enable=QtDBus \
-                                    --enable=QtDesigner \
-                                    --enable=QtGui \
-                                    --enable=QtHelp \
-                                    --enable=QtMultimedia\
-                                    --enable=QtMultimediaWidgets \
-                                    --enable=QtNetwork \
-                                    --enable=QtOpenGL \
-                                    --enable=QtPrintSupport \
-                                    --enable=QtQml \
-                                    --enable=QtQuick \
-                                    --enable=QtSensors \
-                                    --enable=QtSerialPort \
-                                    --enable=QtSql \
-                                    --enable=QtSvg \
-                                    --enable=QtTest \
-                                    --enable=QtWebKit \
-                                    --enable=QtWebKitWidgets \
-                                    --enable=QtWidgets \
-                                    --enable=QtXmlPatterns ")
+                                    --qmake='/usr/bin/qmake-qt5' ")
     shelltools.system("find -name 'Makefile' | xargs sed -i 's|-Wl,-rpath,/usr/lib||g;s|-Wl,-rpath,.* ||g'")
 
 def build():
@@ -81,14 +39,17 @@ def build():
 
 def install():
     shelltools.cd("%s/Py2Qt-gpl-5.5" % get.workDIR())
+    autotools.rawInstall("-C pyrcc DESTDIR=%(DESTDIR)s INSTALL_ROOT=%(DESTDIR)s" % {'DESTDIR':get.installDIR()})
+    autotools.rawInstall("-C pylupdate DESTDIR=%(DESTDIR)s INSTALL_ROOT=%(DESTDIR)s" % {'DESTDIR':get.installDIR()})
     autotools.rawInstall("DESTDIR=%(DESTDIR)s INSTALL_ROOT=%(DESTDIR)s" % {'DESTDIR':get.installDIR()})
     # Fix conflicts with python3-pyqt5
     pisitools.rename("/usr/bin/pylupdate5", "py2lupdate5")
     pisitools.rename("/usr/bin/pyrcc5", "py2rcc5")    
     pisitools.rename("/usr/lib/qt5/plugins/PyQt5/libpyqt5qmlplugin.so", "libpy2qt5qmlplugin.so")
     pisitools.rename("/usr/share/qt5/qsci/api/python/PyQt5.api", "Py2Qt5.api")
-    #pisitools.remove("/usr/lib/qt5/plugins/designer/libpyqt5.so")
-    pisitools.rename("/usr/bin/pyuic5", "python2-pyuic5")
+    pisitools.rename("/usr/bin/pyuic5", "py2uic5")    
+    pisitools.domove("/usr/share/sip/PyQt5/*", "/usr/share/sip/Py2Qt5")
+    pisitools.removeDir("/usr/share/sip/PyQt5")
     
     shelltools.cd("%s/PyQt-gpl-5.5" % get.workDIR())
     autotools.rawInstall("-C pyrcc DESTDIR=%(DESTDIR)s INSTALL_ROOT=%(DESTDIR)s" % {'DESTDIR':get.installDIR()})
