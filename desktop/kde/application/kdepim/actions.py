56f4fa76312fcbdea5146af50f470dba9398d9c5
yusuf <aydemir@users.noreply.github.com>
2015-10-26 05:26:45 +0200

    kdepim

--- desktop/kde/application/kdepim/actions.py
+++ desktop/kde/application/kdepim/actions.py
@@ -10,7 +10,6 @@ from pisi.actionsapi import kde5
 def setup():
     kde5.configure("-DCMAKE_INSTALL_PREFIX=/usr \
                     -DCMAKE_BUILD_TYPE=Release \
-                    -DCMAKE_SKIP_RPATH=ON \
                     -DLIB_INSTALL_DIR=lib \
                     -DSYSCONF_INSTALL_DIR=/etc \
                     -DQML_INSTALL_DIR=/usr/lib/qt5/qml \
