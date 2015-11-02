68f406119e24ef0b74131ac3dcdff64091d1ed29
aydemir <yusuf.aydemir@pisilinux.org>
2015-11-02 12:21:12 +0200

    libspectre add repo for okular

---
+++ programming/misc/libspectre/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure("--disable-static \
+                         --enable-test \
+                         --disable-dependency-tracking \
+                         --with-gnu-ld")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall('DESTDIR="%s"' % get.installDIR())
+
+    pisitools.dodoc("ChangeLog","COPYING","NEWS","README")
