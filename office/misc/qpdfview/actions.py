bc387dc5e0b469fedfdc0cdbbaa457b344b50825
groni <groni3427@gmail.com>
2015-11-04 22:19:51 +0100

    qpdfview is a tabbed document viewer

---
+++ office/misc/qpdfview/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import qt5
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+
+WorkDir="qpdfview-0.4.14"
+
+
+def setup():        
+    qt5.configure("qpdfview.pro")
+
+def build():
+    qt5.make()
+
+def install():
+    qt5.install()
+
+    pisitools.dodoc("CONTRIBUTORS",  "COPYING", "README")
+
+
