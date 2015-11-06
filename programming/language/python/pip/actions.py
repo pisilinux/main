c609f7827c5aa801099aa1ea06c76fd328cc5dec
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2015-11-06 15:00:39 +0200

    pip:added python3-pip

--- programming/language/python/pip/actions.py
+++ programming/language/python/pip/actions.py
@@ -5,6 +5,20 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
 
-def install():
+WorkDir="pip-%s" % get.srcVERSION()
+
+def setup():
+    shelltools.makedirs("python3")
+    shelltools.copytree("../pip-%s" % get.srcVERSION(), "%s/python3" % get.workDIR())
+    pythonmodules.compile()
+    shelltools.cd("%s/python3" % get.workDIR())
+    pythonmodules.compile(pyVer = "3")
+def install():    
     pythonmodules.install()
+    shelltools.cd("%s/python3" % get.workDIR())
+    pythonmodules.install(pyVer = "3")
+    pisitools.rename("/usr/bin/pip", "pip3")
