1bf42dcefc0ef69a6c4db61f05835b30597f1be6
alihanozturk <alihan@pisilinux.org>
2015-11-16 22:12:06 +0200

    options delete

--- desktop/kde/application/kcron/actions.py
+++ desktop/kde/application/kcron/actions.py
@@ -8,12 +8,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import kde5
 
 def setup():
-    kde5.configure("-DCMAKE_BUILD_TYPE=Release \
-                    -DCMAKE_INSTALL_PREFIX=/usr \
-                    -DLIB_INSTALL_DIR=lib \
-                    -DPLUGIN_INSTALL_DIR=/usr/lib/qt5/plugins \
-                    -DKDE_INSTALL_USE_QT_SYS_PATHS=ON \
-                    -DBUILD_TESTING=OFF")
+    kde5.configure()
 
 def build():
     kde5.make()
