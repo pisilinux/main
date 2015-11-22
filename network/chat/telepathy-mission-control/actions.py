88de0e21f9936937b5ba9d96909507b0d03f6084
alihanozturk <alihan@pisilinux.org>
2015-11-22 12:17:26 +0200

    telepathy-mission-control:add pisi-2.0 and v.bump

---
+++ network/chat/telepathy-mission-control/actions.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    #autotools.autoreconf("-vfi")
+    autotools.configure("--disable-static \
+                         --libexecdir=/usr/lib \
+                         --enable-gnome-keyring \
+                         --with-connectivity=nm \
+                         --enable-gtk-doc \
+                         --disable-upower \
+                         --disable-static \
+                         --disable-schemas-compile")
+    
+    #pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "COPYING", "ChangeLog", "README", "NEWS")
