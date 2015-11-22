e6741423f7dc512a17d70fd6312e042723283ba7
alihanozturk <alihan@pisilinux.org>
2015-11-22 14:27:57 +0200

    telepathy-salut:add pisi-2.0 and v.bump

---
+++ network/chat/telepathy-salut/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--prefix=/usr \
+                         --sysconfdir=/etc \
+                         --libexecdir=/usr/lib/telepathy \
+                         --disable-static \
+                         --with-tls=openssl \
+                         --enable-avahi-tests=no \
+                         --disable-Werror")
+    
+    #pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+
+def build():
+    autotools.make("-j1")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README", "NEWS")
