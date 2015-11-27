fff4918998867f21f177a98fe1baec7f8815bf92
groni <groni3427@gmail.com>
2015-11-27 23:15:04 +0100

    inkscape SVG vector graphics application

---
+++ multimedia/graphics/inkscape/actions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure(" \
+                         --enable-lcms \
+                         --disable-static \
+                         --enable-poppler-cairo \
+                         --disable-dependency-tracking \
+                         --without-gnome-vfs \
+                         --without-inkjar \
+                         --enable-dbusapi \
+                         --enable-nls")
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "COPYING", "COPYING.LIB", "ChangeLog", "NEWS", "README")
