85eaf054f43f10b45977ef77d3124fab341d66ac
groni <groni3427@gmail.com>
2015-11-27 16:36:51 +0100

    libgsf Gnome structured file library

---
+++ programming/misc/libgsf/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure("--disable-static \
+                         --enable-introspection \
+                         --with-pic")
+
+    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "BUGS", "COPY*", "HACKING", "NEWS", "README", "TODO")
