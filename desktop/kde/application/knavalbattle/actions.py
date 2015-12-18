eb1942f30cd89955628f2c8e4ae8ed035cc7e18f
alihanozturk <alihan@pisilinux.org>
2015-12-18 22:54:47 +0200

    knavalbattle:add pisi-2.0

---
+++ desktop/kde/application/knavalbattle/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import kde5
+from pisi.actionsapi import get
+
+NoStrip=["/usr/share/icons"]
+
+def setup():
+    kde5.configure()
+
+def build():
+    kde5.make()
+
+def install():
+    kde5.install()
+    
+    pisitools.dodoc("AUTHORS", "COPYING*", "README*")
