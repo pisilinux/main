77e5156cf8d6cd28a2dd6ee074659afc91936f7f
aydemir <yusuf.aydemir@pisilinux.org>
2016-01-12 19:11:25 +0200

    kozmetik

--- desktop/kde/framework/kactivities/actions.py
+++ desktop/kde/framework/kactivities/actions.py
@@ -4,24 +4,16 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import kde5
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import cmaketools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
 
 def setup():
-    shelltools.makedirs("%s/build" %get.workDIR())
+    kde5.configure()
 
-    cmaketools.configure("-DCMAKE_BUILD_TYPE=Release \
-                          -DCMAKE_INSTALL_PREFIX=/usr \
-                          -DLIB_INSTALL_DIR=lib \
-                          -DQML_INSTALL_DIR=lib/qt5/qml \
-                          -DKDE_INSTALL_USE_QT_SYS_PATHS=ON \
-                          -DBUILD_TESTING=OFF")
 def build():
-    cmaketools.make()
+    kde5.make()
 
 def install():
-    cmaketools.install()
+    kde5.install()
 
     pisitools.dodoc("README.md", "MAINTAINER", "TODO")
--- desktop/kde/framework/kxmlrpcclient/actions.py
+++ desktop/kde/framework/kxmlrpcclient/actions.py
@@ -4,25 +4,13 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import cmaketools
+from pisi.actionsapi import kde5
 
 def setup():
-    pisitools.ldflags.add("-Wl,-rpath,/usr/lib")
-    cmaketools.configure("-DBUILD_TESTING=OFF \
-                          -DLIB_INSTALL_DIR=lib \
-                          -DCMAKE_BUILD_TYPE=Release \
-                          -DSYSCONF_INSTALL_DIR=/etc \
-                          -DCMAKE_INSTALL_PREFIX=/usr \
-                          -DLIBEXEC_INSTALL_DIR=libexec \
-                          -DKDE_INSTALL_USE_QT_SYS_PATHS=ON \
-                          -DQML_INSTALL_DIR=/usr/lib/qt5/qml \
-                          -DPYTHON_EXECUTABLE=/usr/bin/python \
-                          -DQT_PLUGIN_INSTALL_DIR=lib/qt5/plugins \
-                          -DECM_MKSPECS_INSTALL_DIR=/usr/lib/qt5/mkspecs/modules ")
+    kde5.configure()
 
 def build():
-    cmaketools.make()
+    kde5.make()
 
 def install():
-    cmaketools.install()
+    kde5.install()
