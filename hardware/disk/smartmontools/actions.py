5b003fa80bece2866410843b0748273a61893fcb
groni <groni3427@gmail.com>
2016-01-26 17:54:20 +0100

    add smartmontools Programs to control and monitor storage systems using the Self-Monitoring

---
+++ hardware/disk/smartmontools/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.touch("ChangeLog")
+    autotools.autoreconf("-fi")
+    autotools.configure("--with-libcap-ng=yes \
+                         --enable-drivedb \
+                         --with-systemdsystemunitdir=no")
+
+def build():
+    autotools.make("CXXFLAGS='%s -fpie'" % get.CXXFLAGS())
+
+def install():
+    pisitools.dosed("smartd.service.in","sysconfig/smartmontools","conf.d/smartd")
+    pisitools.dosed("smartd.service.in","smartd_opts","SMARTD_ARGS")
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "NEWS", "README", "smartd.conf")
