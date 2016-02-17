6306a8c151819937f0f6612b70c5270b396af574
İbrahim KARAGÜZEL <karaguzelibrahim@gmail.com>
2016-02-17 18:47:20 +0200

    build with docker

---
+++ network/analyzer/asleap/actions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+
+def build():
+    autotools.make()
+
+def install():
+    pisitools.dosbin("asleap")
+    pisitools.dosbin("genkeys")
