af387c7b8dcbbacd2082da012b492e16c460ced2
İbrahim KARAGÜZEL <karaguzelibrahim@gmail.com>
2016-02-18 10:07:33 +0200

    Version bump

--- network/analyzer/aircrack-ng/actions.py
+++ network/analyzer/aircrack-ng/actions.py
@@ -5,32 +5,11 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import get
-#from pisi.actionsapi import pisitools
-
-# if pisi can't find source directory, see /var/pisi/aircrack-ng/work/ and:
-# WorkDir="aircrack-ng-"+ get.srcVERSION() +"/sub_project_dir/"
-
-def setup():
-    #autotools.configure()
-    pass
+from pisi.actionsapi import pisitools
 
 def build():
-    autotools.make()
+    autotools.make("sqlite=true experimental=true pcre=true")
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-# Take a look at the source folder for these file as documentation.
-#    pisitools.dodoc("AUTHORS", "BUGS", "ChangeLog", "COPYING", "README")
-
-# If there is no install rule for a runnable binary, you can 
-# install it to binary directory.
-#    pisitools.dobin("aircrack-ng")
-
-# You can use these as variables, they will replace GUI values before build.
-# Package Name : aircrack-ng
-# Version : 1.2
-# Summary : Aircrack-ng is a complete suite of tools to assess WiFi network security.
-
-# For more information, you can look at the Actions API
-# from the Help menu and toolbar.
+    autotools.install("sqlite=true experimental=true pcre=true")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "LICENSE", "README", "VERSION")
