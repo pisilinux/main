b60edf00f48a9f497d2907cc4cb0f9d21fd4e085
Ertuğrul Erata <ertugrulerata@gmail.com>
2016-02-25 16:30:37 +0200

    postgis fix build, verbump

--- science/gis/postgis/actions.py
+++ science/gis/postgis/actions.py
@@ -16,7 +16,7 @@ def setup():
     pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
-    autotools.make()
+    autotools.make("-j1")
 
 def install():
     autotools.rawInstall('DESTDIR=%s' % get.installDIR())
