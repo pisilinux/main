668e7c74ecc9882b0038e876f93820102ab9f146
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2016-03-17 08:35:00 +0200

    perl-Font-TTF:moved into main

---
+++ programming/language/perl/perl-Font-TTF/actions.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import perlmodules
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+
+WorkDir="Font-TTF-%s" % get.srcVERSION()
+
+def setup():
+    for d in ("README.TXT", "TODO", "Changes"):
+        shelltools.chmod(d, 0644)
+
+    perlmodules.configure()
+
+def build():
+    perlmodules.make()
+
+def check():
+    #perlmodules.make("test")
+
+def install():
+    perlmodules.install()
+
+    pisitools.dodoc("README*", "TODO", "Changes")
