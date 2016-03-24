c9af89c82b4ae16c65aea349b4d4e644ba36c3a4
Ertuğrul Erata <ertugrulerata@gmail.com>
2016-03-24 13:50:02 +0200

    update doxygen

--- office/misc/doxygen/actions.py
+++ office/misc/doxygen/actions.py
@@ -6,21 +6,19 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import cmaketools
 
 def setup():
     # --shared and --release are default parameters, however we just write them
     # to down to avoid confusing wheter it's a static/shared or release/debug
     # package at the first look :)
-    autotools.rawConfigure("--shared \
-                            --release \
-                            --dot dot \
-                            --prefix /usr")
-
+    cmaketools.configure()
+    
 def build():
-    autotools.make()
+    cmaketools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
     
     #pisitools.doman("doc/doxywizard.1")
 
