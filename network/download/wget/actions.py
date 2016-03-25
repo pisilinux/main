374749efdc9053f0ebde86cf7ddcda2fa74c3191
suvari <suvarice@gmail.com>
2016-03-25 17:44:44 +0200

    wget: added and index..

---
+++ network/download/wget/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--prefix=/usr --sysconfdir=/etc \
+                         --disable-rpath \
+                         --with-ssl=openssl")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    # default root certs location
+    shelltools.echo("%s/etc/wgetrc" % get.installDIR(), "ca_certificate=/etc/ssl/certs/ca-certificates.crt")
+
+    pisitools.dodoc("AUTHORS", "COPYING", "ChangeLog*", "NEWS", "README", "MAILING-LIST")
