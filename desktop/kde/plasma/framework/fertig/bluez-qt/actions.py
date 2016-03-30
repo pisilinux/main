dd9de1681fb019e9fe2f227327c5fd8ed5b1723e
groni <groni@pisilinux.org>
2016-03-30 16:35:56 +0200

    add plasma 5.6.1

---
+++ desktop/kde/plasma/framework/fertig/bluez-qt/actions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import kde5
+from pisi.actionsapi import pisitools
+
+def setup():
+    kde5.configure("-DUDEV_RULES_INSTALL_DIR=/usr/lib/udev/rules.d")
+
+def build():
+    kde5.make()
+
+def install():
+    kde5.install()
