a0d351f6b3886c974be144ef9d578b9d1ed28609
groni <groni@pisilinux.org>
2016-03-30 17:30:37 +0200

    reset history

--- desktop/kde/plasma/framework/fertig/kcoreaddons/comar/package.py
+++
@@ -1,8 +0,0 @@
- # -*- coding: utf-8 -*-
-
-from pisi.version import Version
-
-def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-    os.system("/usr/bin/update-mime-database /usr/share/mime > /dev/null")
-
-
