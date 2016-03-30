80061bd8055c0d4916a1371dee927128ed186446
alihanozturk <alihan@pisilinux.org>
2016-03-30 16:30:47 +0300

    libaccounts-glib:update

--- programming/library/libaccounts-glib/actions.py
+++ programming/library/libaccounts-glib/actions.py
@@ -11,21 +11,15 @@ from pisi.actionsapi import get
 
 
 def setup():
-    shelltools.system("PYTHON=/usr/bin/python2.7 HAVE_GCOV_FALSE='#' ./configure \
-                          --disable-static \
-                          --disable-gtk-doc")
+    shelltools.system("./autogen.sh")
+    autotools.configure("--prefix=/usr \
+                         --disable-static \
+                         --disable-gtk-doc")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    
-    pisitools.domove("/usr/local/include/libaccounts-glib/*", "/usr/include/libaccounts-glib/")
-    pisitools.domove("/usr/local/lib/*", "/usr/lib/")
-    pisitools.domove("/usr/local/bin/*", "/usr/bin/")
-    pisitools.domove("/usr/local/share/*", "/usr/share/")
-    
-    pisitools.removeDir("/usr/local")
     
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING")
