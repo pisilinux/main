1aeb3abf0fcb341a707187876c867c34bbe95a00
suvari <suvarice@gmail.com>
2016-04-02 02:26:13 +0300

    libdvbpsi: fix build, ver. bump to 1.3.0

--- multimedia/video/libdvbpsi/actions.py
+++ multimedia/video/libdvbpsi/actions.py
@@ -9,7 +9,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--enable-release \
+    autotools.configure("--prefix=/usr \
                          --disable-static")
 
 def build():
