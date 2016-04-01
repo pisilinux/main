bb7be7d0400b4e36b572fd6a1cbbb6dbf062a2ea
Ertuğrul Erata <ertugrulerata@gmail.com>
2016-04-01 19:10:43 +0300

    add openslp in main

---
+++ network/analyzer/openslp/actions.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.export("CFLAGS", "%s -fPIC -fno-strict-aliasing -fPIE -DPIE " % get.CFLAGS())
+    shelltools.export("LDFLAGS", "%s -pie -Wl,-z,now" % get.LDFLAGS())
+    
+    autotools.autoreconf("-fi")
+    
+    autotools.configure("--disable-static \
+                         --disable-dependency-tracking \
+                         --disable-rpath \
+                         --localstatedir=/var \
+                         --enable-slpv1 \
+                         --enable-slpv2-security")
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+
+def build():
+    autotools.make("all docs")
+
+def install():
+    autotools.install()
+    
+    pisitools.dohtml("doc/doc/html/*")
+    pisitools.dodoc("AUTHORS", "FAQ", "ChangeLog", "NEWS", "README", "THANKS")
