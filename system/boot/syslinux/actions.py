7fc168bae77048d4d7912f75de16043495c5d4a5
Ertuğrul Erata <ertugrulerata@gmail.com>
2016-04-03 09:51:04 +0300

    syslinux update

--- system/boot/syslinux/actions.py
+++ system/boot/syslinux/actions.py
@@ -14,24 +14,33 @@ datadir = "/usr/lib/syslinux"
 
 NoStrip = ["/sbin", "/usr/lib"]
 
-pisitools.flags.remove("-fPIC")
-
 def build():
-    # shelltools.export("CFLAGS", "-Werror -Wno-unused -finline-limit=2000")
     shelltools.export("CFLAGS", get.CFLAGS())
     shelltools.export("LDFLAGS", "")
+    # do not swallow efi compilation output to make debugging easier
+    shelltools.system("sed 's|> /dev/null 2>&1||' -i efi/check-gnu-efi.sh")
+      
+    # disable debug and development flags to reduce bootloader size
+    shelltools.system("truncate --size 0 mk/devel.mk")
 
-    autotools.make('DATE="PisiLinux" spotless')
-    autotools.make('DATE="PisiLinux"')
-    # autotools.make('DATE="PARDUS" installer')
-    # autotools.make('DATE="PARDUS" -C sample tidy')
+    autotools.make("-j1 PYTHON=python bios efi64 efi32")
 
 def install():
     autotools.rawInstall('INSTALLROOT=%s MANDIR="/usr/share/man" AUXDIR="/usr/lib/syslinux"' % get.installDIR())
-    for f in tools:
-        pisitools.insinto(datadir, "utils/"+f)
 
-    #pisitools.domove("/usr/lib/syslinux/libutil_com.c32","/usr/lib/syslinux/com32/libutil")
-    #pisitools.domove("/usr/lib/syslinux/libcom32.c32","/usr/lib/syslinux/com32/lib")
+    pisitools.removeDir("/usr/lib/syslinux/com32")
+    pisitools.removeDir("/usr/lib/syslinux/dosutil")
+    pisitools.remove("/usr/lib/syslinux/syslinux.com")
+    
+    #pisitools.dodir("/usr/lib/syslinux/bios")
+    
+    pisitools.domove("/usr/lib/syslinux/*.c32", "/usr/lib/syslinux/bios")
+    pisitools.domove("/usr/lib/syslinux/*.bin", "/usr/lib/syslinux/bios")
+    pisitools.domove("/usr/lib/syslinux/*.0", "/usr/lib/syslinux/bios")
+    pisitools.domove("/usr/lib/syslinux/memdisk", "/usr/lib/syslinux/bios")
+    
+    #for f in tools:
+    #    pisitools.insinto(datadir, "utils/"+f) 
+
     pisitools.dodoc("README", "NEWS", "doc/*.txt", "doc/logo/LICENSE")
     pisitools.remove("/usr/bin/gethostip")
