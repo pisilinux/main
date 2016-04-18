2c0fa0cf586fa6aa751b182b2fc26cdf2de0808e
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2016-04-18 04:25:07 +0300

    qt5-webit:v.bump to 5.6.0

--- desktop/toolkit/qt5/qt5-webkit/actions.py
+++ desktop/toolkit/qt5/qt5-webkit/actions.py
@@ -14,11 +14,10 @@ def setup():
     qt5.configure()
 
 def build():
-    qt5.make()
+    qt5.make("docs")
 
 def install():
-    #  autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    qt5.install("INSTALL_ROOT=%s" % get.installDIR())
+    qt5.install("INSTALL_ROOT=%s install_docs" % get.installDIR())
 
     #I hope qtchooser will manage this issue
     for bin in shelltools.ls("%s/usr/lib/qt5/bin" % get.installDIR()):
