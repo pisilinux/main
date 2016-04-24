af43c905352c1140aa1bf5e6ff051252c5e8437e
yusuf <yusuf.aydemir@pisilinux.org>
2016-04-24 21:06:22 +0300

    psd fix

--- desktop/pisilinux/pisilinux-desktop-services/actions.py
+++ desktop/pisilinux/pisilinux-desktop-services/actions.py
@@ -5,15 +5,9 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pythonmodules
-from pisi.actionsapi import get
-from pisi.actionsapi import pisitools
-
-#WorkDir = 'pds-%s' % get.srcVERSION()
 
 def build():
     pythonmodules.compile()
 
 def install():
     pythonmodules.install()
-    pisitools.domove("/usr/lib/python2.7/site-packages", "/usr/lib/python3.4")
-    pisitools.removeDir("/usr/lib/python2.7")
