ec8a49d64debcc295f35e3b3d78c910b2b55edb1
alihanozturk <alihan@pisilinux.org>
2016-05-03 17:30:19 +0300

    update phonon

--- desktop/kde/phonon/qt5-phonon/actions.py
+++ desktop/kde/phonon/qt5-phonon/actions.py
@@ -9,18 +9,13 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    kde5.configure("-DCMAKE_BUILD_TYPE=Release \
-                    -DCMAKE_SKIP_RPATH=ON \
-                    -DCMAKE_INSTALL_PREFIX=/usr \
-                    -DPHONON_INSTALL_QT_EXTENSIONS_INTO_SYSTEM_QT=ON \
-                    -DPHONON_BUILD_PHONON4QT5=ON \
-                    -D__KDE_HAVE_GCC_VISIBILITY=NO \
-                    -DCMAKE_INSTALL_LIBDIR=lib")
+    kde5.configure("-DPHONON_BUILD_PHONON4QT5=ON")
 
 def build():
     kde5.make()
 
 def install():
     kde5.install()
-
-    pisitools.dosym("/usr/include/KDE/Phonon", "/usr/include/Phonon")
+    
+    pisitools.domove("/usr/share/qt5/mkspecs/modules/qt_phonon4qt5.pri", "/usr/lib/qt5/mkspecs/modules/")
+    pisitools.removeDir("/usr/share/qt5")
