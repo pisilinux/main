710fe4f79faa27f5252e1a37da96dfb0f158b593
Ertuğrul Erata <ertugrulerata@gmail.com>
2016-05-04 21:06:17 +0300

    update

--- programming/language/go/actions.py
+++ programming/language/go/actions.py
@@ -9,14 +9,14 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-NoStrip=["/"]
+NoStrip = "/"
 
 def build():
     
     shelltools.export("GOROOT", "%s/go-go1.6.2" % get.workDIR()) #0
     shelltools.export("GOBIN", "$GOROOT/bin") #1
     shelltools.export("GOPATH", "%s" % get.workDIR())
-    #shelltools.export("GOROOT_FINAL", "/usr/lib/go")
+
     shelltools.export("GOROOT_BOOTSTRAP", "%s/go-go1.6.2/go-linux-amd64-bootstrap" % get.workDIR())  #2
 
     shelltools.export("GOOS","linux")
@@ -36,25 +36,27 @@ def build():
     #    shelltools.system("$GOROOT/bin/go build -v -x -o $GOROOT/pkg/tool/$GOOS\_$GOARCH/%s golang.org/x/tools/cmd/%s" % (tool, tool))
 
 
-def install():  
+def install():
+    shelltools.export("GOROOT_FINAL", "/usr/lib/go")
     shelltools.cd("%s/go-go1.6.2" % get.workDIR())
 
     pisitools.dodir("/usr/lib/go")
     shelltools.system("cp -rp api bin doc lib pkg src  %s/usr/lib/go" % get.installDIR())
-        
+    shelltools.system("chown -R  root:root %s/usr" % get.installDIR())
+
     pisitools.dosym("/usr/lib/go/bin/go", "/usr/bin/go")
     pisitools.dosym("/usr/lib/go/bin/gofmt", "/usr/bin/gofmt")
-    
-    shelltools.system("cp -R doc  %s/usr/lib/go" % get.installDIR())
-    shelltools.system("cp -R lib  %s/usr/lib/go" % get.installDIR())
-    shelltools.system("cp -R pkg  %s/usr/lib/go" % get.installDIR())
-    shelltools.system("cp -R src  %s/usr/lib/go" % get.installDIR())
-    
-#    shelltools.chmod("%s/usr/lib/go/bin" % get.installDIR(), 0755)
-#    shelltools.chmod("%s/usr/lib/go/pkg/tool" % get.installDIR(), 0755)
+
+    pisitools.dosym("usr/lib/go/doc", "/usr/share/doc/%s/doc" % get.srcNAME())
+    pisitools.dosym("usr/lib/go/api", "/usr/share/doc/%s/api" % get.srcNAME())
 
     shelltools.system("cp -a misc  %s/usr/lib/go" % get.installDIR())
-    
+
     pisitools.removeDir("/usr/lib/go/pkg/bootstrap")
- 
+
+    # remove testdata, which hit cave fix-linkage
+    pisitools.remove("/usr/lib/go/src/debug/elf/testdata/gcc-386-freebsd-exec")
+    pisitools.removeDir("/usr/lib/go/pkg/linux_amd64/internal/syscall/windows")
+    pisitools.removeDir("/usr/lib/go/pkg/obj")
+
     pisitools.dodoc("VERSION", "LICENSE", "PATENTS", "README*", "AUTHORS", "CONTRIBUTORS")
