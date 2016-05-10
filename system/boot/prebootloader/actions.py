e9e0a15b56bf0ac1792bf65034622027d0d9c234
bferturk <blacksea8393@gmail.com>
2016-05-10 17:17:22 +0300

    prebootloader add repo

---
+++ system/boot/prebootloader/actions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+
+def install():
+    pisitools.dodir("/usr/lib/prebootloader")
+    #pisitools.insinto("/usr/share/calamares/branding/pisilinux", "branding/pisi-kde/*")
+    pisitools.dolib("*.efi", "/usr/lib/prebootloader/")
