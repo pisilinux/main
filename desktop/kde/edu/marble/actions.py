4f6cc61736af901aa371d6d9d62f5d781116c728
alihanozturk <alihan@pisilinux.org>
2016-05-28 18:46:52 +0300

    marble:first relesae

---
+++ desktop/kde/edu/marble/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import kde5
+
+def setup():
+    kde5.configure("-DWITH_libshp=OFF \
+                    -DWITH_libgps=OFF \
+                    -DWITH_QextSerialPort=OFF \
+                    -DWITH_liblocation=OFF \
+                    -DBUILD_MARBLE_TOOLS=YES \
+                    -DBUILD_TESTING=OFF \
+                    -DBUILD_MARBLE_EXAMPLES=OFF \
+                    -DBUILD_MARBLE_TESTS=OFF \
+                    -DMOBILE=OFF")
+
+def build():
+    kde5.make()
+
+def install():
+    kde5.install()
+
+    pisitools.dodoc("BUGS", "ChangeLog", "CODING", "COPYING*", "CREDITS", "LICENSE*", "MANIFESTO.txt", "INSTALL", "USECASES")
