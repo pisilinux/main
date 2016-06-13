6e61900f03ff2d8d22fe94a33b12cd753da4a71f
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2016-06-13 20:18:22 +0300

    chromium-browser:version bump

--- network/web/chromium-browser/actions.py
+++ network/web/chromium-browser/actions.py
@@ -4,106 +4,109 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir = "chromium-%s" % get.srcVERSION()
+
+shelltools.export("HOME", get.workDIR())
+
+ARCH = "x64"
 
 def setup():
-
     shelltools.export("LC_ALL", "C")
-    shelltools.system("sed -i 's|icu)|icu-i18n)|g' build/linux/system.gyp")
     shelltools.system("export -n CFLAGS CXXFLAGS")
-    shelltools.touch(get.workDIR() + "/chromium-48.0.2564.97/chrome/test/data/webui/i18n_process_css_test.html")
+    shelltools.system("sed -i 's|icu)|icu-i18n)|g' build/linux/system.gyp")
+    shelltools.system("CFLAGS+=' -fno-delete-null-pointer-checks'")
+    shelltools.touch(get.workDIR() + "/chromium-*/chrome/test/data/webui/i18n_process_css_test.html")
+    
     
     options = "\
-               -Duse_system_expat=1 \
-               -Duse_system_flac=1 \
-               -Duse_system_harfbuzz=1 \
-               -Duse_system_icu=0 \
-               -Duse_system_jsoncpp=1 \
-               -Duse_system_libevent=1 \
-               -Duse_system_libjpeg=1 \
-               -Duse_system_libpng=1 \
-               -Duse_system_libvpx=0 \
-               -Duse_system_libwebp=1 \
-               -Duse_system_opus=1 \
-               -Duse_system_snappy=1 \
-               -Duse_system_speex=1 \
-               -Duse_system_zlib=1 \
-               -Duse_system_bzip2=1 \
-               -Dicu_use_data_file_flag=0 \
-               -Duse_gconf=0 \
-               -Dclang=0 \
-               -Dlinux_use_bundled_gold=0 \
-               -Dlinux_use_bundled_binutils=0 \
-               -Dlinux_use_gold_flags=0 \
-               -Dhost_clang=0 \
-               -Dclang_use_chrome_plugins=0 \
-               -Dlinux_link_gsettings=1 \
-               -Dlinux_link_kerberos=1 \
-               -Dlinux_link_libbrlapi=1 \
-               -Dlinux_link_libgps=1 \
-               -Dlinux_link_libpci=1 \
-               -Dlinux_link_libspeechd=1 \
-               -Dlinux_link_pulseaudio=1 \
-               -Dlibspeechd_h_prefix=speech-dispatcher/ \
-               -Dwerror= \
-               -Ddisable_fatal_linker_warnings=1 \
-               -Ddisable_nacl=1 \
-               -Ddisable_newlib_untar=1 \
-               -Ddisable_pnacl=1 \
-               -Ddisable_sse2=1 \
-               -Ddisable_glibc=1 \
-               -Dtarget_arch=x64 \
-               -Dffmpeg_branding=Chromium \
-               -Dproprietary_codecs=1 \
-               -Dpython_arch=x64 \
-               -Dlogging_like_official_build=1 \
-               -Dlinux_sandbox_chrome_path=/usr/lib/chromium-browser/chromium-browser \
-               -Dlinux_sandbox_path=/usr/lib/chromium-browser/chromium-sandbox \
-               -Dgoogle_api_key=AIzaSyBINKL31ZYd8W5byPuwTXYK6cEyoceGh6Y \
-               -Dgoogle_default_client_id=879512332529.apps.googleusercontent.com \
-               -Dgoogle_default_client_secret=RmQPJJeL1cNJ8iETnoVD4X17 "
-   
-    shelltools.system("build/linux/unbundle/replace_gyp_files.py  %s" % options)
-   
-    shelltools.export("GYP_GENERATORS","ninja")
-    shelltools.system("build/gyp_chromium build/all.gyp --depth=. %s" % options)
+                        -Dwerror= \
+                        -Dclang=0 \
+                        -Dpython_ver=2.7 \
+                        -Dlinux_link_gsettings=1 \
+                        -Dlinux_link_libpci=1 \
+                        -Dlinux_link_libspeechd=1 \
+                        -Dlinux_link_pulseaudio=1 \
+                        -Dlinux_strip_binary=1 \
+                        -Dlinux_use_bundled_binutils=0 \
+                        -Dlinux_use_bundled_gold=0 \
+                        -Dlinux_use_gold_flags=0 \
+                        -Dicu_use_data_file_flag=1 \
+                        -Dlogging_like_official_build=1 \
+                        -Dtracing_like_official_build=1 \
+                        -Dfieldtrial_testing_like_official_build=1 \
+                        -Dlibspeechd_h_prefix=speech-dispatcher/ \
+                        -Dffmpeg_branding=Chrome \
+                        -Dproprietary_codecs=1 \
+                        -Duse_gnome_keyring=0 \
+                        -Duse_system_bzip2=1 \
+                        -Duse_system_flac=1 \
+                        -Duse_system_ffmpeg=0 \
+                        -Duse_system_harfbuzz=1 \
+                        -Duse_system_icu=0 \
+                        -Duse_system_libevent=1 \
+                        -Duse_system_libjpeg=1 \
+                        -Duse_system_libpng=1 \
+                        -Duse_system_libvpx=1 \
+                        -Duse_system_libxml=0 \
+                        -Duse_system_snappy=1 \
+                        -Duse_system_xdg_utils=1 \
+                        -Duse_system_yasm=1 \
+                        -Duse_system_zlib=0 \
+                        -Dusb_ids_path=/usr/share/kcmusb/usb.ids \
+                        -Duse_mojo=0 \
+                        -Duse_gconf=0 \
+                        -Duse_sysroot=0 \
+                        -Denable_hangout_services_extension=1 \
+                        -Denable_widevine=1 \
+                        -Ddisable_fatal_linker_warnings=1 \
+                        -Ddisable_glibc=1 \
+                        -Ddisable_nacl=1 \
+                        -Ddisable_pnacl=1 \
+                        -Dlinux_sandbox_path=/usr/lib/chromium-browser/chromium-sandbox \
+                        -Dlinux_sandbox_chrome_path=/usr/lib/chromium-browser/chromium-browser \
+                        -Drelease_extra_cflags=-fno-ipa-cp \
+                        -Dgoogle_api_key=AIzaSyDwr302FpOSkGRpLlUpPThNTDPbXcIn_FM \
+                        -Dgoogle_default_client_id=413772536636.apps.googleusercontent.com \
+                        -Dgoogle_default_client_secret=0ZChLK6AxeA3Isu96MkwqDR4 "
     
-  
+    shelltools.system("build/linux/unbundle/replace_gyp_files.py  %s" % options)
+    shelltools.system("build/gyp_chromium build/all.gyp --depth=. %s" % options)
+    shelltools.export("GYP_GENERATORS","ninja")
+
 def build():
+    
+    #Sandbox for error must remain separate
     
     shelltools.system("ninja -C out/Release chrome")
     shelltools.system("ninja -C out/Release chrome_sandbox")
     shelltools.system("ninja -C out/Release chromedriver")
 
 def install():
-  
     shelltools.cd("out/Release")
 
-    shelltools.makedirs("%s/usr/lib/chromium-browser" % get.installDIR())
-    
-    binaries_for_inst=["chrome", "chrome_sandbox", "chromedriver", "natives_blob.bin", "snapshot_blob.bin"]
-    #libraries_for_inst=["libffmpegsumo.so", "icudtl.dat"]
-
-   
-    # install and strip binaries
-    for mybin in binaries_for_inst:
-        pisitools.insinto("/usr/lib/chromium-browser", mybin)
-       
-    # install and strip shared libs  
-    # for mylib in libraries_for_inst:
-        # pisitools.insinto("/usr/lib/chromium-browser", mylib)
-         
-    pisitools.dosym("/usr/lib/chromium-browser/chrome", "/usr/lib/chromium-browser/chromium-browser")
-    pisitools.rename("/usr/lib/chromium-browser/chrome_sandbox", "chrome-sandbox")
-    shelltools.chmod("%s/usr/lib/chromium-browser/chrome-sandbox" % get.installDIR(), 04755)
-    
-     # install rest of needed files
-    pisitools.insinto("/usr/lib/chromium-browser", "*.pak")
+    #should be checked should for the missing folder "out/Release"
+    pisitools.insinto("/usr/lib/chromium-browser", "chrome")
+    pisitools.insinto("/usr/lib/chromium-browser", "chromedriver")
+    pisitools.insinto("/usr/lib/chromium-browser", "chrome_sandbox", "chrome-sandbox")
     pisitools.insinto("/usr/lib/chromium-browser", "locales")
     pisitools.insinto("/usr/lib/chromium-browser", "resources")
+    pisitools.insinto("/usr/lib/chromium-browser", "natives_blob.bin")
+    pisitools.insinto("/usr/lib/chromium-browser", "snapshot_blob.bin")
+    pisitools.insinto("/usr/lib/chromium-browser", "icudtl.dat")
+    pisitools.insinto("/usr/lib/chromium-browser", "*.pak")
+    pisitools.insinto("/usr/lib/chromium-browser", "character_data_generator")
+    pisitools.insinto("/usr/lib/chromium-browser", "*.json")
+    pisitools.insinto("/usr/lib/chromium-browser", "mksnapshot")
+    pisitools.dosym("/usr/lib/chromium-browser/chrome", "/usr/bin/chromium-browser")
+    
+    shelltools.system("chmod -v 4755 %s/usr/lib/chromium-browser/chrome-sandbox" %get.installDIR())
+    
+
     
     pisitools.newman("chrome.1", "chromium-browser.1")
     
@@ -115,5 +118,3 @@ def install():
     
     pisitools.dodoc("LICENSE")
 
-
-    
