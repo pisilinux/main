4bada07e08d3f264f292054e9320ee4adc1c422a
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2016-06-14 22:24:16 +0300

    qt5-webengine:removed nss from build dep

--- desktop/toolkit/qt5/qt5-webengine/actions.py
+++ desktop/toolkit/qt5/qt5-webengine/actions.py
@@ -14,14 +14,10 @@ def setup():
     shelltools.export("QT5LINK", "/usr/lib/qt5/bin")
     shelltools.export("QT5DIR", "/usr/lib/qt5")
     shelltools.export("CFLAGS", "%s -I/usr/lib/sqlite3.11.0.0" % get.CFLAGS())
-    #qt5.configure()
     shelltools.system("qmake-qt5 WEBENGINE_CONFIG+=use_proprietary_codecs qtwebengine.pro")
-    
     
 def build():
     qt5.make()
-    #Drop QMAKE_PRL_BUILD_DIR because reference the build dir
-    #shelltools.system('find "$pkgdir/usr/lib" -type f -name '*.prl' -exec sed -i -e '/^QMAKE_PRL_BUILD_DIR/d' {} \;')
 
 def install():
     qt5.install("INSTALL_ROOT=%s" % get.installDIR())
