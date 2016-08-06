08abe4f002b8b10433b8818297d7df40acd735ab
metehan <mthnzbk@gmail.com>
2016-08-06 23:03:42 +0300

    pm güncellemesi paket oluşturulamıyordu halledildi.

--- desktop/pisilinux/package-manager/actions.py
+++ desktop/pisilinux/package-manager/actions.py
@@ -6,17 +6,17 @@
 
 from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import kde4
+from pisi.actionsapi import kde5
 
 def install():
-    pisitools.dosed("data/*.desktop", "Icon=application-x-pisi", "Icon=/usr/share/icons/breeze/apps/128/package-manager.png")
+    #pisitools.dosed("data/*.desktop", "Icon=application-x-pisi", "Icon=/usr/share/icons/breeze/apps/128/package-manager.png")
     pythonmodules.install()
 
     # Copy Notification Rc file for Kde 4
-    pisitools.insinto("%s/package-manager/" % kde4.appsdir, "src/package-manager.notifyrc")
+    pisitools.insinto("%s/package-manager/" % kde5.appsdir, "src/package-manager.notifyrc")
 
     for lang in ('de','en','es','fr','nl','sv','tr'):
-        pisitools.insinto("%s/html/%s/package-manager/" % (kde4.docdir, lang),
+        pisitools.insinto("%s/html/%s/package-manager/" % (kde5.docdir, lang),
                           "help/%s/main_help.html" % lang, "index.html")
 
 
