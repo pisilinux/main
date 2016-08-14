031e3d8d5f247fbe1e9948d1a5a49d265030bf84
alihanozturk <alihan@pisilinux.org>
2016-08-14 14:30:27 +0300

    ktp-accounts-kcm:first relase

---
+++ desktop/kde/applications/ktp-accounts-kcm/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import kde5
+
+def setup():
+    cmaketools.configure()
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.install()
+
+    pisitools.dodoc("COPYING", "INSTALL", "README")
