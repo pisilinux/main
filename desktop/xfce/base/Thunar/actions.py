ff9e7e04832914ce10d945875cdbe992628c8d0f
bferturk <blacksea8393@gmail.com>
2016-09-06 05:28:11 +0300

    xfce base

---
+++ desktop/xfce/base/Thunar/actions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--prefix=/usr \
+                         --disable-static \
+                         --enable-gio-unix \
+                         --enable-dbus \
+                         --enable-startup-notification \
+                         --enable-gudev \
+                         --enable-notifications \
+                         --enable-exif \
+                         --enable-pcre \
+                         --disable-debug")
+
+    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "FAQ", "HACKING", "NEWS", "README", "THANKS", "TODO")
---
+++ desktop/xfce/base/xfce4-panel/actions.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+def setup():
+    autotools.configure("--prefix=/usr \
+                         --sysconfdir=/etc \
+                         --libexecdir=/usr/lib \
+                         --localstatedir=/var \
+                         --disable-static \
+                         --enable-gio-unix \
+                         --enable-gtk-doc \
+                         --enable-gtk3 \
+                         --disable-debug")
+
+    #pisitools.dosed("libtool", "^(hardcode_libdir_flag_spec=).*", '\\1""')
+    #pisitools.dosed("libtool", "^(runpath_var=)LD_RUN_PATH", "\\1DIE_RPATH_DIE")
+    #pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
+
+    pisitools.dodoc("AUTHORS", "COPYING", "ChangeLog", "NEWS", "README", "TODO")
