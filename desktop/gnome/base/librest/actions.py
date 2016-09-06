be52d6787726132315111cc4055fdb52decc66ca
bferturk <blacksea8393@gmail.com>
2016-09-06 16:49:55 +0300

    librest

---
+++ desktop/gnome/base/librest/actions.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    options = "--prefix=/usr \
+               --sysconfdir=/etc"
+               
+    if get.buildTYPE() == "_emul32":
+        options += " --libdir=/usr/lib32 \
+                     --bindir=/_emul32/bin \
+                     --sbindir=/_emul32/sbin \
+                     --disable-dtrace"
+                     
+        shelltools.export("CC", "%s -m32" % get.CC())
+        shelltools.export("CXX", "%s -m32" % get.CXX())
+        shelltools.export("PKG_CONFIG_PATH", "/usr/lib32/pkgconfig")
+        
+    autotools.configure(options)
+
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "README")
