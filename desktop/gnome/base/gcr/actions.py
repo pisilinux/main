dfe5c9805f68d19b4057957902cc91b7bf7e80fd
bferturk <blacksea8393@gmail.com>
2016-09-06 16:31:43 +0300

    gcr

---
+++ desktop/gnome/base/gcr/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.system("""sed -i -r 's:"(/desktop):"/org/gnome\1:' schema/*.xml""")
+    autotools.configure("--prefix=/usr \
+                         --sysconfdir=/etc")
+
+    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog","COPYING", "README", "NEWS")
