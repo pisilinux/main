603a9b507a304f5e0eeb42448f12ba9d0fc57224
bferturk <blacksea8393@gmail.com>
2016-09-06 04:34:06 +0300

    fix dep

--- desktop/gnome/base/polkit-gnome/actions.py
+++ desktop/gnome/base/polkit-gnome/actions.py
@@ -9,7 +9,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.autoreconf("-fi")
+   # autotools.autoreconf("-fi")
     autotools.configure("--disable-static")
 
 def build():
