a9262b27e74cbc156613d81ef3458d1bdee56e89
aydemir <yusuf.aydemir@pisilinux.org>
2016-10-26 11:07:25 +0300

    applications

---
+++ desktop/kde/applications/kmix/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import kde5
+
+NoStrip=["/usr/share"]
+
+def setup():
+    kde5.configure("-DKMIX_KF5_BUILD=ON")
+
+def build():
+    kde5.make()
+
+def install():
+    kde5.install()
