28e8407b6e1f5444edd0eb8076bf4a3666bd30ef
groni <groni@pisilinux.org>
2016-11-07 16:38:29 +0100

    add discount build dependency for cutemarked

---
+++ editor/discount/actions.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+
+def install():
+    pisitools.insinto("/", "usr")    
