63523aa8e12adaf60e55283141371f3a6390ea70
groni <groni@pisilinux.org>
2016-11-11 22:19:03 +0100

    add digikam new kde 5 version

---
+++ desktop/kde/graphics/digikam/actions.py
@@ -0,0 +1,34 @@
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import kde5
+from pisi.actionsapi import get
+from pisi.actionsapi import cmaketools
+
+#shelltools.export("HOME", get.workDIR())
+
+def setup():
+    kde5.configure("-DFORCED_UNBUNDLE=ON \
+                    -DWITH_LQR=ON \
+                    -DWITH_LENSFUN=OFF \
+                    -DWITH_MarbleWidget=ON \
+                    -DENABLE_LCMS2=ON \
+                    -DDIGIKAMSC_COMPILE_KIPIPLUGINS=ON \
+                    -DDIGIKAMSC_USE_PRIVATE_SHAREDLIBS=ON \
+                    -DDIGIKAMSC_COMPILE_LIBKGEOMAP=ON \
+                    -DDIGIKAMSC_COMPILE_LIBKVKONTAKTE=ON \
+                    -DDIGIKAMSC_COMPILE_LIBMEDIAWIKI=ON \
+                    -DDIGIKAMSC_COMPILE_LIBKFACE=ON \
+                    -DDIGIKAMSC_COMPILE_LIBKIPI=ON ")
+
+def build():
+    kde5.make()
+
+def install():
+    kde5.install()
+
+    pisitools.dodoc("COPYING", "COPYING-CMAKE-SCRIPTS", "README.LOCAL", "README", "README.MACOS", "NEWS")
+    
