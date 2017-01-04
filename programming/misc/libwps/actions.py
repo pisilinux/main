1764cc22ad4e7c2026ce113ab7fbe0713be62530
alihanozturk <alihan@pisilinux.org>
2017-01-04 23:21:29 +0300

    add calligra and dep

---
+++ programming/misc/libwps/actions.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    for f in ("AUTHORS", "NEWS", "ChangeLog"):
+        shelltools.touch(f)
+
+    #autotools.autoreconf("-fi")
+    autotools.configure("--without-docs --disable-static \
+                         --disable-werror")
+
+    # for fix unused dependency
+    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("CREDITS", "README")
