162126c00b41cff3cac2b8fe91b5f8b8e1b99043
groni <groni@pisilinux.org>
2017-01-29 09:42:58 +0100

    change issues

--- hardware/disk/dosfstools/actions.py
+++ hardware/disk/dosfstools/actions.py
@@ -8,7 +8,7 @@ from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 
 def build():
-    autotools.make()
+    autotools.make("CFLAGS='%s -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -fno-strict-aliasing'" % get.CFLAGS())
 
 def install():
     autotools.rawInstall("DESTDIR=%s install-bin install-man PREFIX=%s SBINDIR=/sbin" % (get.installDIR(), get.defaultprefixDIR()))
