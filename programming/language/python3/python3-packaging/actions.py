0c48ecf4a5528e24a47dfb073115cf6437d7176e
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2017-02-01 13:19:21 +0300

    python3-six:v.bump to 1.10.0

--- programming/language/python3/python3-packaging/actions.py
+++ programming/language/python3/python3-packaging/actions.py
@@ -14,4 +14,4 @@ def install():
     pythonmodules.install(pyVer="3")
 
     #pisitools.removeDir("/usr/lib/*/site-packages/six-1.9.0*egg-info*")
-    pisitools.remove("/usr/lib/*/site-packages/*egg-info*")
+    #pisitools.remove("/usr/lib/*/site-packages/*egg-info*")
