8c4f70f9c2ce757dcbbd32b026bd8142a456dff3
alihanozturk <alihan@pisilinux.org>
2017-02-17 09:50:05 +0300

    wavpack:fix actions

--- multimedia/sound/wavpack/actions.py
+++ multimedia/sound/wavpack/actions.py
@@ -4,12 +4,13 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.autoreconf("-vfi")
+    shelltools.system("./autogen.sh")
     autotools.configure("--prefix=/usr \
                          --libdir=/usr/lib \
                          --disable-static \
