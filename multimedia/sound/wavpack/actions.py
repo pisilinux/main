cfe2a8e472c8b29a3ea5230d8d5cc1b601e25fd0
alihanozturk <alihan@pisilinux.org>
2017-02-17 09:48:18 +0300

    wavpack:update

--- multimedia/sound/wavpack/actions.py
+++ multimedia/sound/wavpack/actions.py
@@ -9,6 +9,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
+    autotools.autoreconf("-vfi")
     autotools.configure("--prefix=/usr \
                          --libdir=/usr/lib \
                          --disable-static \
