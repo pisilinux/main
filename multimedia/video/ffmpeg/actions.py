193346fc5a2b98dd85c93bfa884a1235ae82954d
alihanozturk <alihan@pisilinux.org>
2017-02-17 10:53:14 +0300

    ffmpeg: update

--- multimedia/video/ffmpeg/actions.py
+++ multimedia/video/ffmpeg/actions.py
@@ -7,10 +7,56 @@
 from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 
 def setup():
-    autotools.rawConfigure("--prefix=/usr \
-                            --enable-shared ")
+    shelltools.system("./configure --prefix=/usr \
+                            --mandir=/usr/share/man \
+                            --disable-debug \
+                            --disable-static \
+                            --disable-stripping \
+                            --enable-avfilter \
+                            --enable-avresample \
+                            --enable-dxva2 \
+                            --enable-fontconfig \
+                            --enable-gnutls \
+                            --enable-gpl \
+                            --enable-libass \
+                            --enable-libbluray \
+                            --enable-libfreetype \
+                            --enable-libgsm \
+                            --enable-libmodplug \
+                            --enable-libmp3lame \
+                            --enable-libopencore_amrnb \
+                            --enable-libopencore_amrwb \
+                            --enable-libopenjpeg \
+                            --enable-libopus \
+                            --enable-libpulse \
+                            --enable-librtmp \
+                            --enable-libschroedinger \
+                            --enable-libspeex \
+                            --enable-libtheora \
+                            --enable-libv4l2 \
+                            --enable-libvorbis \
+                            --enable-libvpx \
+                            --enable-libx264 \
+                            --enable-libx265 \
+                            --enable-libxvid \
+                            --enable-pic \
+                            --enable-postproc \
+                            --enable-runtime-cpudetect \
+                            --enable-shared \
+                            --enable-swresample \
+                            --enable-vdpau \
+                            --enable-version3 \
+                            --enable-x11grab \
+                            --enable-libdc1394 \
+                            --enable-libnut \
+                            --enable-libcelt \
+                            --enable-frei0r \
+                            --enable-libcdio \
+                            --enable-libvo-amrwbenc \
+                            --enable-nonfree")
 
 def build():
     autotools.make()
