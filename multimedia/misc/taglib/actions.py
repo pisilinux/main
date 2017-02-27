19111cc98b7c4a17305d356b2dc776b5552d569c
groni <groni@pisilinux.org>
2017-02-27 23:05:17 +0100

    taglib change actions.py and pspec.xml

--- multimedia/misc/taglib/actions.py
+++ multimedia/misc/taglib/actions.py
@@ -9,7 +9,9 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    cmaketools.configure("-DWITH_ASF=On -DWITH_MP4=On")
+    cmaketools.configure("-DBUILD_SHARED_LIBS=ON \
+                        -DWITH_MP4=ON \
+                        -DWITH_ASF=ON")
 
 def build():
     cmaketools.make()
