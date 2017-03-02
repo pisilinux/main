300d45ac19607fdf77d0b27ff629ff6df87437c6
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2017-03-02 14:08:39 +0300

    libXfont2:new package for xorg-server

---
+++ x11/library/libXfont2/actions.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.autoreconf("-vif")
+    autotools.configure("--disable-static \
+                         --disable-devel-docs")
+
+    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README")
