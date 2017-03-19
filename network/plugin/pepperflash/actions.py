b9e7e9e9ef17179aabbe8b802ec804d313246809
alihanozturk <alihan@pisilinux.org>
2017-03-19 20:57:02 +0300

    pepperflash:update

--- network/plugin/pepperflash/actions.py
+++ network/plugin/pepperflash/actions.py
@@ -5,12 +5,7 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
-
-WorkDir = "."
 
 def install():
-    pisitools.insinto("usr/lib/chromium-browser/PepperFlash/", "./PepperFlash/manifest.json")
-    pisitools.insinto("usr/lib/chromium-browser/PepperFlash/", "./PepperFlash/libpepflashplayer.so")
-    pisitools.insinto("usr/lib/chromium-browser/", "./chromium-browser/libwidevinecdmadapter.so")
-    pisitools.insinto("usr/lib/chromium-browser/", "./chromium-browser/libwidevinecdm.so")
+    pisitools.insinto("usr/lib/chromium-browser/PepperFlash/", "manifest.json")
+    pisitools.insinto("usr/lib/chromium-browser/PepperFlash/", "libpepflashplayer.so")
