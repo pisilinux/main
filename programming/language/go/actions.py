0efb7f18377a8a717c724a616b5730969c4b6de1
Ertuğrul Erata <ertugrulerata@gmail.com>
2017-03-20 19:48:31 +0300

    go update, docker, runc and containerd rebuild

--- programming/language/go/actions.py
+++ programming/language/go/actions.py
@@ -16,12 +16,12 @@ def build():
     shelltools.export("go_platform","linux-amd64")
     shelltools.export("go_linker","/lib/ld-linux-x86-64.so.2")
 
-    shelltools.export("GOROOT", "%s/go-go1.6.3" % get.workDIR()) #0
+    shelltools.export("GOROOT", "%s/go-go1.7.5" % get.workDIR()) #0
 
     shelltools.export("GOBIN", "$GOROOT/bin") #1
     shelltools.export("GOPATH", "%s" % get.workDIR())
     shelltools.export("GOROOT_FINAL", "/usr/lib/go")
-    shelltools.export("GOROOT_BOOTSTRAP", "%s/go-go1.6.3/go-linux-amd64-bootstrap" % get.workDIR())  #2
+    shelltools.export("GOROOT_BOOTSTRAP", "%s/go-go1.7.5/go-linux-amd64-bootstrap" % get.workDIR())  #2
 
     shelltools.export("GOOS","linux")
     shelltools.export("GOARCH","amd64")
@@ -42,10 +42,10 @@ def build():
 
 def install():
     shelltools.export("GOROOT_FINAL", "/usr/lib/go")
-    shelltools.cd("%s/go-go1.6.3" % get.workDIR())
+    shelltools.cd("%s/go-go1.7.5" % get.workDIR())
 
     pisitools.dodir("/usr/lib/go")
-    shelltools.system("cp -rp api bin doc lib pkg src  %s/usr/lib/go" % get.installDIR())
+    shelltools.system("cp -r api bin doc lib pkg src  %s/usr/lib/go" % get.installDIR())
     shelltools.system("chown -R  root:root %s/usr" % get.installDIR())
 
     pisitools.dosym("/usr/lib/go/bin/go", "/usr/bin/go")
@@ -54,7 +54,7 @@ def install():
     pisitools.dosym("/usr/lib/go/doc", "/usr/share/doc/%s/doc" % get.srcNAME())
     pisitools.dosym("/usr/lib/go/api", "/usr/share/doc/%s/api" % get.srcNAME())
 
-    shelltools.system("cp -a misc  %s/usr/lib/go" % get.installDIR())
+    shelltools.system("cp -r misc  %s/usr/lib/go" % get.installDIR())
 
     pisitools.removeDir("/usr/lib/go/pkg/bootstrap")
 
