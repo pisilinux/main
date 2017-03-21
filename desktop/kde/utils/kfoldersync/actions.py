79fe37b3555de5a4ba2e23c34d97168b0676b03f
groni <groni@pisilinux.org>
2017-03-21 14:02:36 +0100

    kfoldersync change actions.py

--- desktop/kde/utils/kfoldersync/actions.py
+++ desktop/kde/utils/kfoldersync/actions.py
@@ -6,8 +6,7 @@
 from pisi.actionsapi import kde5
 from pisi.actionsapi import pisitools
 
-# if pisi can't find source directory, see /var/pisi/kfoldersync/work/ and:
-# WorkDir="kfoldersync-"+ get.srcVERSION() +"/sub_project_dir/"
+
 
 def setup():
     kde5.configure()
@@ -18,17 +17,7 @@ def build():
 def install():
     kde5.install()
 
-# Take a look at the source folder for these file as documentation.
-    pisitools.dodoc("COPYING.txt", "README.md")
 
-# If there is no install rule for a runnable binary, you can 
-# install it to binary directory.
-#    pisitools.dobin("kfoldersync")
+    pisitools.dodoc("COPYING.GPL3", "README.md")
 
-# You can use these as variables, they will replace GUI values before build.
-# Package Name : kfoldersync
-# Version : 3.0.0
-# Summary : Folder synchronization and backup tool for KDE
 
-# For more information, you can look at the Actions API
-# from the Help menu and toolbar.
