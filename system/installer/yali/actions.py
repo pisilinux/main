91d0dece420bce8fd9cbea5b02f1383944f087ba
Ertuğrul Erata <ertugrulerata@gmail.com>
2017-03-22 11:25:15 +0300

    rebuild and update system.installer

--- system/installer/yali/actions.py
+++ system/installer/yali/actions.py
@@ -8,9 +8,9 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import pythonmodules
 
 def setup():
-    repo_uri = "http://packages.pisilinux.org/repositories/1.0/stable/x86_64/pisi-index.xml.xz" # FIXME
+    repo_uri = "http://ciftlik.pisilinux.org/2.0-Beta.1/pisi-index.xml.xz" # FIXME
     pisitools.dosed("yali/constants.py", "@REPO_URI@", repo_uri)
-    pisitools.dosed("yali/constants.py", "@REPO_NAME@", "pisi") # FIXME
+    pisitools.dosed("yali/constants.py", "@REPO_NAME@", "Beta") # FIXME
 
     pisitools.dosed("conf/yali.conf", "@INSTALL_TYPE@", "default")
 
