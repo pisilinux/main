796b7d62c633a9f5df7c9cebb65fb2bada97f2e0
Rmys <muscnsl@hotmail.com>
2017-06-03 02:02:44 +0300

    eric6-17.05:Version Bump

--- programming/environment/eric6/actions.py
+++ programming/environment/eric6/actions.py
@@ -10,7 +10,7 @@ from pisi.actionsapi import get
 NoStrip = ["/usr/bin", "/usr/lib", "/usr/qt5/qsci/api"]
 conf = {"bindir": "/usr/bin",
         "installdir": get.installDIR(),
-        "site-packages": "/usr/lib/python3.4/site-packages"}
+        "site-packages": "/usr/lib/python3.6/site-packages"}
 def install():
    pythonmodules.run("install.py -z \
                                   -b %(bindir)s \
