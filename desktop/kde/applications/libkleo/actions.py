67be7397e11074157c3df26559280a77780aa3c6
suvari <suvarice@gmail.com>
2017-07-03 01:57:14 +0300

    libkleo

--- desktop/kde/applications/libkleo/actions.py
+++ desktop/kde/applications/libkleo/actions.py
@@ -5,8 +5,10 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import kde5
+from pisi.actionsapi import shelltools
 
 def setup():
+    shelltools.system("sed -i -e '/find_package.*QGpgme/d' CMakeLists.txt")
     kde5.configure()
 
 def build():
