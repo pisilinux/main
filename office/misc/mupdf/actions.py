7f811a44cb8a8c320b156c28d8a7c33cd11a4e23
suvari <suvarice@gmail.com>
2017-07-13 19:07:27 +0300

    mupdf: new package for cups-filters 1.14.1

---
+++ office/misc/mupdf/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+
+def build():
+    autotools.make("prefix=/usr")
+
+    # remove bundled packages, use our system libraries
+    shelltools.system("rm -rf thirdparty/{curl,freetype,glfw,harfbuzz,jbig2dec,jpeg,openjpeg,zlib}")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s prefix=/usr" % get.installDIR())
+    pisitools.remove("/usr/lib/*.a")
+
+    pisitools.dodoc("README", "COPYING")
