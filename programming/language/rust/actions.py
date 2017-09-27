e5422239ef86fd508c9e4caedd475c0738483ba3
Rmys <muscnsl@hotmail.com>
2017-09-27 18:51:25 +0300

    rust-1.20.0:First release

---
+++ programming/language/rust/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+
+def build():
+    shelltools.system("python ./x.py --verbose")
+    
+
+def install():
+    pisitools.insinto("/", "build/x86_64-unknown-linux-gnu/stage0/etc")
+    pisitools.insinto("/usr", "build/x86_64-unknown-linux-gnu/stage0/bin")
+    pisitools.insinto("/usr", "build/x86_64-unknown-linux-gnu/stage0/lib")
+    pisitools.insinto("/usr", "build/x86_64-unknown-linux-gnu/stage0/share")
+    #pisitools.insinto("/usr", "build/x86_64-unknown-linux-gnu/stage0/manifest.in")
+        
+    #pisitools.dodoc("LICENSE","AUTHORS","README*")
