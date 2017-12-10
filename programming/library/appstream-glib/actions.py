0175e99b770046ca8af7db76eec5bd2c06f452be
Ertuğrul Erata <ertugrulerata@gmail.com>
2017-12-10 20:29:14 +0300

    appstream-glib and gcap

---
+++ programming/library/appstream-glib/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--disable-rpm \
+                         --disable-stemmer \
+                         --disable-dep11 \
+                         --enable-fonts \
+                         --disable-static \
+                         --enable-builder \
+                         --enable-firmware")
+    
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    #pisitools.dodoc("AUTHORS", "ChangeLog", "README*", "NEWS")
