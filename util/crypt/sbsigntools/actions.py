731d625ffd7c7ec031f3f09456a1a434b0efb43b
Ertuğrul Erata <ertugrulerata@gmail.com>
2017-12-14 18:45:51 +0300

    sbsigntools ad

---
+++ util/crypt/sbsigntools/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+# WorkDir = ""
+# NoStrip = "/"
+
+def setup():
+    #shelltools.move("lib/ccan-6ac981d","lib/ccan")
+    
+    shelltools.system("sh autogen.sh")
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "README*", "NEWS")
