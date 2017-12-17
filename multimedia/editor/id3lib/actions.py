9bc95a55c66f17cce865c53d6b67f0630d0dc7ff
sss <sss@pisi_linux2.0rc>
2017-12-17 19:52:51 +0300

    id3lib-3.8.3:First release

---
+++ multimedia/editor/id3lib/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import libtools
+from pisi.actionsapi import get
+
+def setup():
+    libtools.libtoolize("-fc")
+    autotools.aclocal()
+    autotools.autoconf()
+    autotools.automake("--add-missing --copy")
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "README")
