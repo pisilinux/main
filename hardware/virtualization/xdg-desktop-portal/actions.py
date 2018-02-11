4f7d7785a80c7b7ee476b5666bfd427cb6617910
Ertuğrul Erata <ertugrulerata@gmail.com>
2018-02-11 22:19:50 +0300

    add xdg-desktop-portal

---
+++ hardware/virtualization/xdg-desktop-portal/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2018 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.system("sh autogen.sh")
+    
+    autotools.configure("--with-systemduserunitdir=none")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("COPYING", "README*", "NEWS")
