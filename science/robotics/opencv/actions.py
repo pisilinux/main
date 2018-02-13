a6e5de2a6bf31932765c5b59983cf09736f61777
Rmys <muscnsl@hotmail.com>
2018-02-14 01:29:11 +0300

    opencv:rebuild

--- science/robotics/opencv/actions.py
+++ science/robotics/opencv/actions.py
@@ -9,7 +9,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-shelltools.export("PYTHONDONTWRITEBYTECODE", "")
+#shelltools.export("PYTHONDONTWRITEBYTECODE", "")
 
 def setup():
 
