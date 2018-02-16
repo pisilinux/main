7a02782d9c158f266c378e386ce8cb948b6a7bfe
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2018-02-16 07:29:10 +0300

    qupzilla:v.bump to 2.2.5

--- network/web/qupzilla/actions.py
+++ network/web/qupzilla/actions.py
@@ -12,14 +12,16 @@ from pisi.actionsapi import get
 def setup():
     qt5.configure(parameters="USE_WEBGL=true \
                               QUPZILLA_PREFIX=/usr/ \
+                              DISABLE_UPDATES_CHECK=true \
                               GNOME_INTEGRATION=true \
-                              KDE_INTEGRATION=true \
-                              NO_X11=true")
+                              KDE_INTEGRATION=true ")
 
 def build():
     qt5.make()
 
 def install():
     qt5.install("INSTALL_ROOT=%s" % get.installDIR())
+    #zsh
+    pisitools.insinto("/usr/share/zsh/site-functions", "linux/completion/*")
 
     pisitools.dodoc("AUTHORS", "BUILDING*", "CHANGELOG", "COPYRIGHT", "FAQ", "README*")
