453b063ba5f6bb3df96c0ef844887345380c9128
Rmys <muscnsl@hotmail.com>
2018-02-28 22:10:28 +0300

    cups-filters-1.20.1:ver.bump

--- hardware/printer/cups-filters/actions.py
+++ hardware/printer/cups-filters/actions.py
@@ -33,4 +33,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README")
+    #pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README")
