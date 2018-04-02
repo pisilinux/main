aa6e234cc160e9686b9373ac9d52d13968a965f9
groni <groni@pisilinux.org>
2018-04-02 17:47:50 +0200

    add avidemux video converter

---
+++ multimedia/converter/avidemux/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def build():
+    shelltools.system("bash ./bootStrap.bash")
+
+def install():
+    autotools.install()
+
+    pisitools.insinto("/", "install/usr")
+    pisitools.insinto("/usr/share/pixmaps", "avidemux_icon.png", "avidemux.png")
+
+    pisitools.dodoc("COPYING", "AUTHORS", "License*")
+    pisitools.doman("man/*")
