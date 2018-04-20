3a18f31a24090b0a6d11652e567ede718e02e9e7
Rmys <muscnsl@hotmail.com>
2018-04-20 16:10:22 +0300

    at-spi2-core-2.28.0:ver.bump

--- desktop/misc/at-spi2-core/actions.py
+++ desktop/misc/at-spi2-core/actions.py
@@ -4,41 +4,36 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
-if get.buildTYPE() == "emul32":
-    libexec = "/tmp"
-    sysconf = "--sysconfdir=/tmp"
-else:
-    libexec = "/usr/libexec/at-spi2"
-    sysconf = ""
 
 def setup():
-    autotools.autoreconf("-vif")
-    autotools.configure("--disable-static \
-                         --disable-silent-rules \
-                         --disable-xevie \
-                         --libexecdir=%s\
-                         --with-dbus-daemondir=/usr/bin \
-                         %s \
-                        " % (libexec, sysconf))
-
-    #pisitools.dosed("libtool", "^(hardcode_libdir_flag_spec=).*", '\\1""')
-    pisitools.dosed("libtool", "^(runpath_var=)LD_RUN_PATH", "\\1DIE_RPATH_DIE")
-    pisitools.dosed("libtool", "( -shared )", " -Wl,--as-needed\\1")
-
-
-def build():
-    autotools.make()
-
-def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    #pisitools.removeDir("/etc")
+    shelltools.makedirs("build")
+    shelltools.cd("build")
+    options = "meson .. --prefix=/usr -D enable_docs=true"
+    
     if get.buildTYPE() == "emul32":
-        pisitools.dosed("%s/usr/share/dbus-1/services" % get.installDIR(), "^(Exec=)\/tmp", r"\1/usr/libexec/at-spi2")
-        pisitools.removeDir("/tmp")
+        options += "meson --prefix=/usr --libexec=tmp --sysconfdir=tmp --libdir=lib32 -D enable_docs=false"
+        
+        
+    shelltools.system(options)
+    
+def build():
+    shelltools.cd("build")
+    shelltools.system("ninja")
+    
+def install():
+    shelltools.cd("build")
+    shelltools.system("DESTDIR=%s ninja install" % get.installDIR())
+    
+    if get.buildTYPE() == "emul32":
+        #pisitools.dosed("%s/usr/share/dbus-1/services" % get.installDIR(), "^(Exec=)\/tmp", r"\1/usr/libexec/at-spi2")
+        pisitools.removeDir("/usr/tmp")
         return
+    
+    shelltools.cd("..")
+    pisitools.dodoc("COPYING", "NEWS", "README")
 
-    pisitools.dodoc("AUTHORS", "COPYING", "README", "NEWS")
