c0a18b6a5f6ad884a9153d1d933d546efced939b
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2018-04-26 01:30:40 +0300

    obconf-qt

---
+++ desktop/lxqt/obconf-qt/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
+
+def setup():
+    shelltools.makedirs("build")
+    shelltools.cd("build")
+    cmaketools.configure("-DCMAKE_BUILD_TYPE=release \
+                          -DCMAKE_INSTALL_PREFIX=/usr \
+                          -DPULL_TRANSLATIONS=no", sourceDir="..")
+
+def build():
+    shelltools.cd("build")
+    cmaketools.make()
+
+def install():
+    shelltools.cd("build")
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
