d3ccdf197ccf3d9508303a6ac291ab1232e5b6b4
Rmys <muscnsl@gmail.com>
2018-04-25 14:36:03 +0300

    libzip-1.5.1

--- util/archive/libzip/actions.py
+++ util/archive/libzip/actions.py
@@ -4,26 +4,20 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import autotools
+from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
+
 def setup():
-    # fix test return state
-    #pisitools.dosed("regress/open_nonarchive.test", "19/2", "19/0")
-    autotools.autoreconf("-fi")
-    autotools.configure()
+    cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr \
+                          -DCMAKE_BUILD_TYPE=Release \
+                          -DCMAKE_INSTALL_LIBDIR=lib")
 
 def build():
-    autotools.make()
-
-def check():
-    autotools.make("check")
+    cmaketools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "NEWS.md", "README.md")
-
-    # preserve old header path for compatibility
-    #pisitools.dosym("/usr/lib/libzip/include/zipconf.h", "/usr/include/zipconf.h")
+    pisitools.dodoc("AUTHORS*", "API*", "TODO*", "NEWS*", "LICENSE", "README*")
