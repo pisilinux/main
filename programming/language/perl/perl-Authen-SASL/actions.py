e047d80cedd35bfb1b7d46f73237317661e4ddd4
Rmys <muscnsl@gmail.com>
2018-07-26 18:27:40 +0300

    perl-5.26

--- programming/language/perl/perl-Authen-SASL/actions.py
+++ programming/language/perl/perl-Authen-SASL/actions.py
@@ -6,11 +6,14 @@
 
 from pisi.actionsapi import perlmodules
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 WorkDir = "%s-%s" % (get.srcNAME()[5:], get.srcVERSION())
 
 def setup():
+    shelltools.system("sed -i -e 's/use inc::Module::Install;/use lib q[.]; use inc::Module::Install;/' Makefile.PL || die")
+    
     perlmodules.configure()
 
 def build():
