0c868ee5daee00fa93612225b51b3bece8dc10c0
Rmys <muscnsl@hotmail.com>
2018-08-04 14:29:44 +0300

    frei0r:ver.bump

--- multimedia/misc/frei0r-plugins/actions.py
+++ multimedia/misc/frei0r-plugins/actions.py
@@ -20,4 +20,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README", "NEWS")
+    pisitools.dodoc("AUTHORS*", "ChangeLog*", "COPYING*", "README*", "NEWS")
