67083f53abbc27591d0826d8ab960763a777ebc5
Rmys <muscnsl@hotmail.com>
2018-08-09 13:23:53 +0300

    openjpeg2:32 bit

--- multimedia/graphics/openjpeg2/actions.py
+++ multimedia/graphics/openjpeg2/actions.py
@@ -7,16 +7,45 @@
 from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
+
 
 def setup():
-    cmaketools.configure("-DCMAKE_BUILD_TYPE=Release \
-                          -DCMAKE_INSTALL_PREFIX=/usr \
-                          -DBUILD_DOC=on")
+    #shelltools.makedirs("build")
+    #shelltools.cd("build")
+
+    options = "-DCMAKE_BUILD_TYPE=Release \
+               -DCMAKE_INSTALL_LIBDIR=/usr/lib \
+               -DCMAKE_INSTALL_PREFIX=/usr \
+               -DBUILD_DOC=on"
+               
+    if get.buildTYPE() == "emul32":
+        shelltools.export("CC", "gcc -m32")
+        shelltools.export("CXX", "g++ -m32")
+        shelltools.export("PKG_CONFIG_PATH","/usr/lib32/pkgconfig")
+        
+        options = "-DOPENJPEG_INSTALL_LIB_DIR=lib32 \
+                   -DCMAKE_INSTALL_PREFIX=/usr \
+                   -DOPENJPEG_INSTALL_BIN_DIR=emul32 \
+                   -DBUILD_DOC=OFF"
+    
+    cmaketools.configure(options)
 
 def build():
+    #shelltools.cd("build")
     cmaketools.make()
 
 def install():
+    #shelltools.cd("build")
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-    pisitools.dodoc("AUTHORS*", "CHANGELOG*", "NEWS*", "README*")
+    
+    if get.buildTYPE() == "emul32":
+        pisitools.insinto("/usr/bin/","%s/usr/emul32/opj_compress" % get.installDIR(),"opj_compress_32")
+        pisitools.insinto("/usr/bin/","%s/usr/emul32/opj_decompress" % get.installDIR(),"opj_decompress_32")
+        pisitools.insinto("/usr/bin/","%s/usr/emul32/opj_dump" % get.installDIR(),"opj_dump_32")
+        pisitools.dosed("%s/usr/lib32/pkgconfig/libopenjp2.pc" % get.installDIR(), "emul32", "bin")
+        pisitools.removeDir("/usr/emul32")
+        
+        
+        pisitools.dodoc("AUTHORS*", "CHANGELOG*", "NEWS*", "README*")
+        
