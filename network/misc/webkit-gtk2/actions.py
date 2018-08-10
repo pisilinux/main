6f2fb75427c227a75eb64457dea44f38ecfb8a12
Rmys <muscnsl@gmail.com>
2018-08-10 08:48:35 +0300

    webkit-gtk2

--- network/misc/webkit-gtk2/actions.py
+++ network/misc/webkit-gtk2/actions.py
@@ -45,7 +45,7 @@ def setup():
 def build():
     shelltools.export("CFLAGS", cflags)
     shelltools.export("CXXFLAGS", cxxflags)
-    autotools.make("all stamp-po")
+    autotools.make("-j1 all stamp-po")
 
 def install():
     shelltools.export("CFLAGS", cflags)
