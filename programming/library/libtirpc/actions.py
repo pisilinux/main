b0bc46ee256b9601f816a6f5e672d0c096737a0b
Rmys <muscnsl@hotmail.com>
2018-09-02 11:58:25 +0300

    libnsl:First release

---
+++ programming/library/libtirpc/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.system("sed '/stdlib.h/a#include <stdint.h>' -i src/xdr_sizeof.c")
+    shelltools.system("sed '/key_secret_is/s/secret/secretkey/' -i src/libtirpc.map")
+    autotools.configure("--disable-static")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
