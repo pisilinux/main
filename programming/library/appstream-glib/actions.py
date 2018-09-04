0d9b4c0cb4a461202452e74d14ef860e5683b3da
Rmys <muscnsl@hotmail.com>
2018-09-04 13:18:12 +0300

    appstream-glib:ver.bump

--- programming/library/appstream-glib/actions.py
+++ programming/library/appstream-glib/actions.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-#
-# Copyright 2017 TUBITAK/UEKAE
-# Licensed under the GNU General Public License, version 2.
-# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
@@ -11,18 +10,17 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--disable-rpm \
-                         --disable-stemmer \
-                         --disable-dep11 \
-                         --enable-fonts \
-                         --disable-static \
-                         --enable-builder \
-                         --enable-firmware")
+    shelltools.makedirs("build")
+    shelltools.cd("build")
+    shelltools.system("meson --prefix=/usr -Dstemmer=false -Drpm=false ..")
     
 def build():
-    autotools.make()
-
+    shelltools.cd("build")
+    shelltools.system("ninja")
+    
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-    #pisitools.dodoc("AUTHORS", "ChangeLog", "README*", "NEWS")
+    shelltools.cd("build")
+    shelltools.system("DESTDIR=%s ninja install" % get.installDIR())
+    
+    shelltools.cd("..")
+    pisitools.dodoc("README*", "NEWS", "COPYING", "AUTHORS")
