c49d37bdace754f5c06d5e4ce51e4051ee3de33a
suvari <suvarice@gmail.com>
2018-09-23 18:13:35 +0300

    prison-qt5-devel obsoleted, replaces fixed, accessibility moved to main

---
+++ accessibility/brltty/actions.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+def setup():
+    pisitools.flags.add(" -lspeechd")
+    autotools.configure(" \
+                --with-tables-directory=/usr/share/brltty \
+                --with-screen-driver=lx \
+                --enable-gpm \
+                --with-install-root='%s'  \
+                --with-writable-directory='/run/brltty' \
+                --disable-java-bindings" % get.installDIR())
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    
+    pisitools.removeDir("/run")
+    pisitools.removeDir("/var")
+    pisitools.dodoc("LICENSE*", "README")
+	
---
+++ accessibility/liblouis/actions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pythonmodules
+
+def setup():
+ #   autotools.autoreconf("-vfi")
+    autotools.configure("--disable-static")
+
+def build():
+    autotools.make("-j1")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dodoc("AUTHORS", "COPYING", "ChangeLog")
+    
+
+    shelltools.export("LD_PRELOAD","%s/liblouis/.libs/liblouis.so.12" % get.curDIR())
+    shelltools.cd("python")
+
+    pythonmodules.install("--prefix=/usr --optimize=1")
+    pythonmodules.install("--prefix=/usr --optimize=1",pyVer = "3")
+ 
