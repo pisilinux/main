c77f3f6a6920332bcbcdc44aace9de979d475dd3
Rmys <muscnsl@gmail.com>
2018-10-13 01:23:37 +0300

    qt5-5.10

--- desktop/kde/plasma/plasma-vault/actions.py
+++ desktop/kde/plasma/plasma-vault/actions.py
@@ -1,13 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import kde5
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 
 def setup():
+    shelltools.system("sed -i 's|5.11.0|5.10.0|g' CMakeLists.txt")
     kde5.configure()
 
 def build():
@@ -15,5 +17,5 @@ def build():
 
 def install():
     kde5.install()
-
+    
     pisitools.dodoc("COPYING*")
