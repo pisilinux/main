57f6579567dd161ca5f71db46205da152aed5742
Rmys <muscnsl@gmail.com>
2018-10-12 18:42:39 +0300

    qt5-5.10

--- desktop/kde/plasma/kactivitymanagerd/actions.py
+++ desktop/kde/plasma/kactivitymanagerd/actions.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+#
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/copyleft/gpl.txt
+# See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import kde5
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 
 def setup():
+    shelltools.system("sed -i 's|5.11.0|5.10.0|g' CMakeLists.txt")
     kde5.configure()
 
 def build():
