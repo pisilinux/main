12a36902aeec9698a76022e673861aeebe6d975a
Rmys <muscnsl@hotmail.com>
2018-10-18 01:46:11 +0300

    framework-5.51.0:ver.bump

--- desktop/kde/plasma/plasma-workspace-wallpapers/actions.py
+++ desktop/kde/plasma/plasma-workspace-wallpapers/actions.py
@@ -9,7 +9,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 
 def setup():
-    shelltools.system("sed -i 's|5.11.0|5.10.0|g' CMakeLists.txt")
+    #shelltools.system("sed -i 's|5.11.0|5.10.0|g' CMakeLists.txt")
     kde5.configure()
 
 def build():
