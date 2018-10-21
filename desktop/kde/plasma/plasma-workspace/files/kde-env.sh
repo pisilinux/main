e434d98df261c355c4953d45dad16cba303e8f86
Ertuğrul Erata <ertugrulerata@gmail.com>
2018-10-21 22:00:16 +0300

    Update kde-env.sh

--- desktop/kde/plasma/plasma-workspace/files/kde-env.sh
+++ desktop/kde/plasma/plasma-workspace/files/kde-env.sh
@@ -9,9 +9,9 @@ export QML_IMPORT_PATH=$QML2_IMPORT_PATH
 export KDE_SESSION_VERSION=5
 export KDE_FULL_SESSION=true
 #separete
-export XDG_DATA_HOME=$HOME/.local5/share
-export XDG_CONFIG_HOME=$HOME/.config5
-export XDG_CACHE_HOME=$HOME/.cache5
+export XDG_DATA_HOME=$HOME/.local/share
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_CACHE_HOME=$HOME/.cache
 #export XDG_RUNTIME_DIR=/tmp/runtime-$USER
 #build
 export CMAKE_PREFIX_PATH=$KF5:$CMAKE_PREFIX_PATH
