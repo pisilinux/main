4051e8e07843b119332b711535c9dfddc435e145
Rmys <muscnsl@hotmail.com>
2018-11-16 14:02:38 +0300

    freecell-solver:First release kpat

---
+++ programming/library/freecell-solver/actions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.makedirs("build")
+    shelltools.cd("build")
+    cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr \
+                          -DFCS_WITH_TEST_SUITE=OFF \
+                          -DBUILD_STATIC_LIBRARY=OFF", sourceDir="..")
+
+def build():
+    shelltools.cd("build")
+    cmaketools.make()
+
+def install():
+    shelltools.cd("build")
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    
+    shelltools.cd("..")
+
+    pisitools.dodoc("AUTHORS.asciidoc", "COPYING.asciidoc", "NEWS.asciidoc", "README.asciidoc")
