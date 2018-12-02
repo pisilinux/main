e3c2db080320a9e23dbeeeff96c2c5b75a2a9d50
alick01 <alicengizkurt@gmail.com>
2018-12-02 18:12:41 +0300

    clamav and gui add

---
+++ antivirus/clamav-gui/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import qt5
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+
+
+WorkDir = "ClamAV-GUI/"
+
+def setup():
+    shelltools.system("qmake-qt5 -r PREFIX=/usr clamav-gui.pro")
+    
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("INSTALL_ROOT=%s" % get.installDIR())
