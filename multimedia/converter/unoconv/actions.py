50647d141c57b4a26ba83fc12a26fe705191fabc
Rmys <muscnsl@hotmail.com>
2018-12-04 11:44:27 +0300

    unoconv:make python3

--- multimedia/converter/unoconv/actions.py
+++ multimedia/converter/unoconv/actions.py
@@ -4,14 +4,13 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
-from pisi.actionsapi import autotools
+from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
 def build():
-    autotools.make()
+    pythonmodules.compile(pyVer = "3")
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pythonmodules.install(pyVer = "3")
 
-    pisitools.dodoc("AUTHORS", "ChangeLog*", "COPYING", "README*")
+    pisitools.dodoc("ChangeLog", "COPYING", "README*")
