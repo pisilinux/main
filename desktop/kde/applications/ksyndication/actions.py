5662036ed25cb615606574a953958142207073c5
Rmys <muscnsl@gmail.com>
2018-12-27 13:13:32 +0300

    kblog

--- desktop/kde/applications/ksyndication/actions.py
+++ desktop/kde/applications/ksyndication/actions.py
@@ -5,9 +5,11 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import kde5
 
 def setup():
+    shelltools.system("sed -i 's|5.10.0|5.9.3|g' CMakeLists.txt")
     kde5.configure()
 
 def build():
@@ -16,4 +18,4 @@ def build():
 def install():
     kde5.install()
 
-    pisitools.dodoc("COPYING.LIB", "COPYING.BSD")
+    pisitools.dodoc("COPYING.LIB")
