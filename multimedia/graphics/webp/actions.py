df6bce5f08a2525808fb7f3951d255640ae88f97
Rmys <muscnsl@hotmail.com>
2019-01-18 15:55:54 +0300

    webp-1.0.1:ver.bump

--- multimedia/graphics/webp/actions.py
+++ multimedia/graphics/webp/actions.py
@@ -10,6 +10,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 
 def setup():
+    autotools.autoreconf("-fi")
     autotools.configure("\
                          --disable-static \
                          --enable-libwebpdecoder \
