a77234f36d951192ab8f12f39033c35837d58d7c
Rmys <muscnsl@hotmail.com>
2019-02-02 11:06:37 +0300

    mindforger:First release

---
+++ office/mindforger/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import qt5
+from pisi.actionsapi import pisitools
+
+def setup():
+    qt5.configure()
+
+def build():
+    qt5.make("QMAKE=/usr/bin/qmake")
+
+def install():
+    qt5.install()
+
+
+    pisitools.dodoc("LICENSE", "README*")
