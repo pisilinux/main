ed218ce8a41749d51fb887dfa5d838e87b64277c
Rmys <muscnsl@hotmail.com>
2019-02-07 14:35:53 +0300

    ogdi:rebuild

--- science/gis/ogdi/actions.py
+++ science/gis/ogdi/actions.py
@@ -23,7 +23,8 @@ def build():
     shelltools.export("TARGET", "Linux")
     shelltools.export("CFG", "release")
     autotools.make("-j1")
-
+    autotools.make("-C contrib/gdal")
+    
 def install():
     pisitools.dolib_so("bin/Linux/*.so")
     pisitools.dosym("/usr/lib/libogdi.so", "/usr/lib/libogdi.so.3")
