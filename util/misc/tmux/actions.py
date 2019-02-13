0e6911b50065c14f2a6dc30c7f4527abc44d8bd5
Hasan Basri <hbasria@gmail.com>
2019-02-14 01:52:36 +0300

    zsh package added

--- util/misc/tmux/actions.py
+++ util/misc/tmux/actions.py
@@ -17,12 +17,3 @@ def build():
 def install():
     autotools.rawInstall("PREFIX=/%s DESTDIR=%s" % (get.defaultprefixDIR(), get.installDIR()))
 
-    # we love vim
-    #pisitools.insinto("/usr/share/vim/vimfiles/syntax/", "examples/tmux.vim", "tmux.vim")
-    #pisitools.insinto("/usr/share/doc/tmux/examples", "examples/*")
-    #pisitools.remove("/usr/share/doc/tmux/examples/tmux.vim")
-
-    # Bash completion
-    #pisitools.domove("/usr/share/doc/tmux/examples/bash_completion_tmux.sh", "/etc/bash_completion.d", "tmux")
-
-    #pisitools.dodoc("CHANGES", "FAQ")
