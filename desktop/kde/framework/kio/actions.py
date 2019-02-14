c6ab71d83ee1fa5be182f0a1ee44764abf1b961d
Rmys <muscnsl@hotmail.com>
2019-02-14 23:10:06 +0300

    kio:rebuild

--- desktop/kde/framework/kio/actions.py
+++ desktop/kde/framework/kio/actions.py
@@ -9,7 +9,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 
 def setup():
-    #shelltools.system("sed -i 's|5.52.0|5.55.0|g' CMakeLists.txt")
+    shelltools.system("sed -i 's|5.52.0|5.55.0|g' CMakeLists.txt")
     kde5.configure()
 
 def build():
