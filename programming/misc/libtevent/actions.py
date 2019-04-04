e8551866f511a0b757e2112f10bd0fd98429a116
Rmys <muscnsl@hotmail.com>
2019-04-04 23:24:29 +0300

    libtevent:ver.bump

--- programming/misc/libtevent/actions.py
+++ programming/misc/libtevent/actions.py
@@ -14,6 +14,7 @@ def setup():
     autotools.configure("\
                          --builtin-libraries=replace \
                          --bundled-libraries=NONE \
+                         --extra-python=/usr/bin/python \
                          --disable-rpath \
                         ")
 
