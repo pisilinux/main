720ee263526297bc8e4027fc242f42fe0b32fccd
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2019-04-10 17:34:58 +0300

    new package:rkward

---
+++ science/mathematics/rkward/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+from pisi.actionsapi import kde5
+from pisi.actionsapi import pisitools
+
+
+def setup():
+    kde5.configure("-DR_HOME=/usr/lib/R")
+
+def build():
+    kde5.make()
+
+def install():
+    #for installing rbackend mkdir needed directory(for R-2.5.0)
+    pisitools.dodir("/usr/lib/R/library")
+
+    kde5.install()
+
+    # TODO: this one seems better than the one in kdelibs
+    #pisitools.remove("/usr/share/kde4/apps/katepart/syntax/r.xml")
+
