d7c8fe38e8d332da92d2190adf47cc685fce80fd
Rmys <muscnsl@hotmail.com>
2019-04-13 21:37:30 +0300

    avidemux:ver.bump

--- multimedia/converter/avidemux/actions.py
+++ multimedia/converter/avidemux/actions.py
@@ -13,10 +13,16 @@ def build():
     shelltools.system("bash ./bootStrap.bash")
 
 def install():
-    autotools.install()
-
-    pisitools.insinto("/", "install/usr")
-    pisitools.dobin("install/usr/lib/libADM*", "/usr/lib")
+    autotools.install("-C buildCore DESTDIR=%s install" % get.installDIR())
+    autotools.install("-C buildQt5 DESTDIR=%s install" % get.installDIR())
+    autotools.install("-C buildCli DESTDIR=%s install" % get.installDIR())
+    autotools.install("-C buildPluginsCommon DESTDIR=%s install" % get.installDIR())
+    autotools.install("-C buildPluginsQt5 DESTDIR=%s install" % get.installDIR())
+    autotools.install("-C buildPluginsCLI DESTDIR=%s install" % get.installDIR())
+    autotools.install("-C buildPluginsSettings DESTDIR=%s install" % get.installDIR())
+    
+    shelltools.chmod(get.installDIR() + "/usr/lib/libADM6*")
+    
     pisitools.insinto("/usr/share/pixmaps", "avidemux_icon.png", "avidemux.png")
 
     pisitools.dodoc("COPYING", "AUTHORS", "License*")
