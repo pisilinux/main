c0faaa3076c482647abb6ed0a072af841a3c715b
4fury <lilian2water@gmail.com>
2019-04-14 19:47:00 +0300

    deadbeef version bump + new dependency

---
+++ multimedia/misc/opusfile/actions.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    options = "\
+                --disable-static \
+                --enable-fixed-point \
+              "
+
+    if get.buildTYPE() == "_emul32":
+        options += "  --libdir=/usr/lib32 \
+                  "
+    shelltools.export("PKG_CONFIG_PATH", "/usr/lib32/pkgconfig")
+
+    autotools.configure(options)
+
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", \
+    "COPYING", \
+    "README.md")
+
+#    if get.buildTYPE() == "_emul32":
+#        pisitools.dodoc("README", "NEWS", "AUTHORS")
