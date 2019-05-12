1a43aba103a4dcbc130c8682d342e0d5c927a3dd
Ertuğrul Erata <ertugrulerata@gmail.com>
2019-05-12 12:46:15 +0300

    xdg-destop-portal-gtk added in main

---
+++ hardware/virtualization/xdg-desktop-portal-gtk/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2018 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    #shelltools.system("NOCONFIGURE=1 ./autogen.sh")
+    
+    autotools.configure("--with-systemduserunitdir=none \
+                         --disable-geoclue \
+                         --disable-pipewire")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("COPYING", "NEWS")
