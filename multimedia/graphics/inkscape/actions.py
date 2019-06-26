434565fc08e6fe7122bbb4ac3939220521dfdb0b
Rmys <muscnsl@hotmail.com>
2019-06-26 11:48:18 +0300

    inkscape:rebuild cmake build

--- multimedia/graphics/inkscape/actions.py
+++ multimedia/graphics/inkscape/actions.py
@@ -2,31 +2,23 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import cmaketools
 from pisi.actionsapi import get
-from pisi.actionsapi import shelltools
-def setup():
-    shelltools.system("./autogen.sh")
-    autotools.configure(" \
-                         --enable-lcms \
-                         --disable-static \
-                         --enable-poppler-cairo \
-                         --disable-dependency-tracking \
-                         --without-gnome-vfs \
-                         --without-inkjar \
-                         --enable-dbusapi \
-                         --enable-nls")
-    
-    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
+def setup():
+    cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr \
+                          -DWITH_DBUS=ON \
+                          -DCMAKE_BUILD_TYPE=Release")
 
 def build():
-    autotools.make()
+    cmaketools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    
+    pisitools.remove("/usr/lib/inkscape/*.a")
+    
     pisitools.dodoc("AUTHORS", "COPYING", "ChangeLog", "NEWS", "README")
