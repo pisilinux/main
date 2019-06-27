8e5078121764f2486049c9ac7bf11293dda1dbe2
mathias-freire <mathiasfreire45@gmail.com>
2019-06-28 01:53:42 +0300

    libgudev 32 bit

--- programming/library/libgudev/actions.py
+++ programming/library/libgudev/actions.py
@@ -10,12 +10,24 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import autotools
 
 def setup():
-    autotools.configure("--prefix=/usr \
+	autotools.configure("--prefix=/usr \
                         --enable-introspection \
                         --enable-gtk-doc \
                         --disable-umockdev")
+	
+	if get.buildTYPE() == "_emul32" :
+		
+		shelltools.export("CC", "gcc -m32")
+		shelltools.export("CXX", "g++ -m32")
+		shelltools.export("PKG_CONFIG_PATH", "/usr/lib32/pkgconfig")
+		
+		autotools.configure("--prefix=/usr \
+							 --libdir=/usr/lib32 \
+                             --disable-introspection \
+                             --disable-gtk-doc \
+                             --disable-umockdev")
     
-    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+	pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
     
 def build():
     autotools.make()
