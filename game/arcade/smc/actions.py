670a332d80d9ff202bb66084dda420e0cb357e7d
mathias-freire <mathiasfreire45@gmail.com>
2019-07-04 21:36:35 +0300

    smc add repo

---
+++ game/arcade/smc/actions.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+import os
+
+def fixperms(d):
+    for root, dirs, files in os.walk(d):
+        for name in dirs:
+            shelltools.chmod(os.path.join(root, name), 0755)
+        for name in files:
+            shelltools.chmod(os.path.join(root, name), 0644)
+
+def setup():
+    shelltools.export("LDFLAGS","-lX11 -lboost_system")
+    fixperms("data")
+    shelltools.system("./autogen.sh")
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.insinto("/usr/share/smc/music", "data/music/*")
+
+    pisitools.insinto("/usr/share/pixmaps", "makefiles/unix/idr_main_48.png", "smc.png")
+
+
