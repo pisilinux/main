91c0c305237c4d894fd90525ea2c261a7f4de61e
mathias-freire <mathiasfreire45@gmail.com>
2019-07-05 19:09:28 +0300

    supertux add repo

---
+++ game/arcade/supertux/actions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.export("CFLAGS", "%s  -fPIC" % get.CFLAGS())
+    shelltools.export("CXXFLAGS", "%s  -fPIC" % get.CXXFLAGS())
+    pisitools.dosed("src/addon/addon_manager.cpp", "#  include <curl/types.h>", "")
+    pisitools.dosed("src/supertux/screen_manager.hpp", "#include <cstddef>", "")
+    shelltools.makedirs("build")
+    shelltools.cd("build")
+    cmaketools.configure("-DINSTALL_SUBDIR_SHARE=share/supertux2 -DINSTALL_SUBDIR_BIN=bin", sourceDir="..")
+
+def build():
+    shelltools.cd("build")
+    cmaketools.make()
+
+def install():
+    shelltools.cd("build")
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    shelltools.cd("..")
+    #pisitools.remove("%s/%s2/INSTALL" % (get.docDIR(), get.srcNAME()))
+    #pisitools.dodoc("docs/*")
