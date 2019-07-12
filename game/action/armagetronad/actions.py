aa66be671be3563faff0df3b0cfd18d8672e1790
mathias-freire <mathiasfreire45@gmail.com>
2019-07-12 21:58:28 +0300

    armagetronad fix

--- game/action/armagetronad/actions.py
+++ game/action/armagetronad/actions.py
@@ -11,16 +11,13 @@ from pisi.actionsapi import get
 
 def setup():
     pisitools.dosed("configure.ac", "png_check_sig", "png_sig_cmp")
+    pisitools.dosed("configure.ac", "FTGL.h", "ftgl.h")
     autotools.autoreconf("-vfi")
-    autotools.configure("--enable-glout \
-                         --enable-master \
-                         --enable-main \
-                         --disable-uninstall \
-                         --with-x \
-                         --disable-etc \
-                         --disable-initscripts \
-                         --disable-sysinstall \
-                         --disable-games")
+    autotools.configure("--sysconfdir=/etc \
+		                 --mandir=/usr/share/man \
+		                 --disable-games \
+		                 --enable-automakedefaults \
+		                 --disable-uninstall")
 
 def build():
     autotools.make()
@@ -33,3 +30,9 @@ def install():
     #We better cleanup
     pisitools.removeDir("/usr/share/armagetronad/desktop")
     shelltools.chmod("%s/etc/armagetronad/rc.config" % get.installDIR(), 0644)
+    
+    pisitools.dodir("/usr/share/armagetronad/moviepack")
+    shelltools.copy("moviepack/*", "%s/usr/share/armagetronad/moviepack" % get.installDIR())
+    
+    pisitools.dodir("/usr/share/armagetronad/moviesounds")
+    shelltools.copy("moviesounds/*", "%s/usr/share/armagetronad/moviesounds" % get.installDIR())
