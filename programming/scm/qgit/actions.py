1961ff91e60fbbbcce4e7049ff3ad2279de2964d
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2019-08-28 15:37:00 +0300

    git add pisi-index*
    git add programming
    
    Signed-off-by: ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>

---
+++ programming/scm/qgit/actions.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import qt5
+
+#WorkDir="qgit"
+
+def setup():
+    qt5.configure()
+
+def build():
+    qt5.make()
+
+def install():
+    qt5.install()
+
+    pisitools.dodoc("README*", "COPYING*")
