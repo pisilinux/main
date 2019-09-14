76ee51213ca1a14604048c4742a3b29a24960711
Rmys <muscnsl@hotmail.com>
2019-09-14 20:41:14 +0300

    python3-pygobject3:ver. bump

--- programming/language/python3/python3-pygobject3/actions.py
+++ programming/language/python3/python3-pygobject3/actions.py
@@ -1,29 +1,30 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
 
 def setup():
-    pisitools.dosed("configure", "-Werror=format", "#-Werror=format")
-    shelltools.export("PYTHON", "/usr/bin/python3.6")
-    shelltools.system("./configure --prefix=/usr \
-                       --localstatedir=/var \
-                       --disable-static")
-
+    shelltools.makedirs("build")
+    shelltools.cd("build") 
+    shelltools.system("meson .. --prefix=/usr -Dpython=python3")
+    
+   
 def build():
-    autotools.make()
-
+    shelltools.cd("build")
+    shelltools.system("ninja")
+    
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    shelltools.cd("build")
+    shelltools.system("DESTDIR=%s ninja install" % get.installDIR())
     pisitools.rename("/usr/lib/pkgconfig/pygobject-3.0.pc", "py3gobject-3.0.pc")
     pisitools.rename("/usr/include/pygobject-3.0/pygobject.h", "py3gobject.h") 
     
-    pisitools.dodoc("ChangeLog", "README*")
-
     
+    shelltools.cd("..")
+    pisitools.dodoc("COP*", "NEWS", "README*")
