4bee2a0810da18c56da4f91a7afc6e642d2538ff
4fury <lilian2water@gmail.com>
2019-09-28 15:13:52 +0300

    version bump.

--- office/misc/mupdf/actions.py
+++ office/misc/mupdf/actions.py
@@ -9,17 +9,16 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 
-
 def build():
     shelltools.system("sed '/TOFU_CJK /c #define TOFU_CJK 1/' -i include/mupdf/fitz/config.h")
     shelltools.system("sed -i '/ttc/s/^/#/' Makefile")
     autotools.make("prefix=/usr")
 
     # remove bundled packages, use our system libraries
-#    shelltools.system("rm -rf thirdparty/{curl,freeglut,freetype,harfbuzz,jpeg,lcms2,libjpeg,openjpeg,zlib}")
+#    shelltools.system("rm -rf thirdparty/{curl,freeglut,freetype,harfbuzz,jbig2dec,lcms2,libjpeg,mujs,openjpeg,zlib}")
 
 def install():
     autotools.rawInstall("DESTDIR=%s prefix=/usr" % get.installDIR())
-#    pisitools.remove("/usr/lib/*.a")
+    pisitools.removeDir("/usr/lib")
 
     pisitools.dodoc("CHANGES", "README", "COPYING")
