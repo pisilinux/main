131a5d0b4abe24d11871fb89091787feacd992cb
İdris Kalp <idriskalp@gmail.com>
2019-10-27 19:21:41 +0300

    libcdio ver bump

--- hardware/optical/libcdio/actions.py
+++ hardware/optical/libcdio/actions.py
@@ -2,28 +2,26 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file http://www.gnu.org/copyleft/gpl.txt 
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
 def setup():
-    autotools.autoreconf("-vif")
-    autotools.configure("--disable-static \
-                         --with-cd-paranoia-name=libcdio-paranoia \
-                         --disable-vcd-info \
-                         --enable-cpp-progs \
-                         --disable-rpath")
+	autotools.autoreconf("-vfi")
+	autotools.configure("--enable-static='no' \
+		                 --enable-cddb")
 
+	
 def build():
-    autotools.make()
+	
+	autotools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % (get.installDIR()))
+	autotools.rawInstall("DESTDIR=%s" %get.installDIR())
+	
+	pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS.md", "README")
 
-    #pisitools.dosed("%s/usr/include/cdio/version.h" % get.installDIR(), '%s[^"]+' % get.srcVERSION(), get.srcVERSION())
-    pisitools.dosed("%s/usr/include/cdio/cdio_config.h" % get.installDIR(), "/define CDIO_LIBCDIO_SOURCE_PATH/s|.*|/* #undef CDIO_LIBCDIO_SOURCE_PATH */|")
-
-    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README", "THANKS")
