ec9c2d52992b8408012e1aae7c8f2c513fe6550c
İdris Kalp <idriskalp@gmail.com>
2019-10-27 17:36:34 +0300

    vmaf add to repo

--- multimedia/video/vmaf/actions.py
+++ multimedia/video/vmaf/actions.py
@@ -9,12 +9,16 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
+NoStrip=["/usr/lib"]
 
 def build():
 	autotools.make()
 
 def install():
+	shelltools.system("g++ -shared -o wrapper/libvmaf.so wrapper/obj/*.o")
+	pisitools.insinto("/usr/lib", "wrapper/libvmaf.so")
 	autotools.rawInstall("DESTDIR=%s INSTALL_PREFIX=/usr" % get.installDIR())
 	pisitools.insinto("/usr/bin", "wrapper/vmafossexec")
+	pisitools.remove("/usr/lib/libvmaf.a")
 	pisitools.dodoc("CHANGELOG.md", "LICENSE", "NOTICE.md", "FAQ.md", "README.md")
 
