d809f34744dabf1233a70b0ad4fc983479d33d10
İdris Kalp <idriskalp@gmail.com>
2019-10-27 15:17:29 +0300

    libopenmpt add to repo

---
+++ multimedia/sound/libopenmpt/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt 
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+	autotools.autoreconf("-vfi")
+	autotools.configure("--disable-static \
+		                 --without-portaudiocpp")
+
+	
+def build():
+	
+	autotools.make()
+
+def install():
+	autotools.rawInstall("DESTDIR=%s" %get.installDIR())
+	
+	pisitools.dodoc("LICENSE", "README.md")
+
