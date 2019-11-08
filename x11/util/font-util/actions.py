8438c7539d587d348a54fa2d3504ccfd684f22d5
Rmys <muscnsl@hotmail.com>
2019-11-08 22:14:51 +0300

    font-util:ver.bump

--- x11/util/font-util/actions.py
+++ x11/util/font-util/actions.py
@@ -16,4 +16,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("ChangeLog", "COPYING", "README")
+    pisitools.dodoc("ChangeLog", "COPYING", "README*")
