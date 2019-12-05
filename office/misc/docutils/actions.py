ece7095bd08fc8319067c538c75ea4bc80f1afa4
blue-devil <bluedevil@sctzine.com>
2019-12-05 21:03:57 +0000

    docutils : rebuilt with python3 bindings

--- office/misc/docutils/actions.py
+++ office/misc/docutils/actions.py
@@ -9,10 +9,37 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
+WorkDir="docutils-%s" % get.srcVERSION()
+
+def setup():
+    shelltools.cd("..")
+    shelltools.makedirs("build_python")
+    shelltools.copytree("./%s" % WorkDir,  "build_python")
+    shelltools.cd(WorkDir)
+    
+    shelltools.cd("..")
+    shelltools.makedirs("build_python3")
+    shelltools.copytree("./%s" % WorkDir,  "build_python3")
+    shelltools.cd(WorkDir)
+
+def build():
+    pythonmodules.compile()
+    
+    shelltools.cd("../build_python/%s" % WorkDir)
+    pythonmodules.compile()
+    
+    shelltools.system("pwd")
+    shelltools.cd("../../build_python3/%s" % WorkDir)
+    pythonmodules.compile(pyVer="3")
+
 def install():
     pythonmodules.install()
-
-    pisitools.dodoc("COPYING.txt", "PKG-INFO", "README.txt")
+    
+    shelltools.cd("../build_python/%s" % WorkDir)
+    pythonmodules.install()
+    
+    shelltools.cd("../../build_python3/%s" % WorkDir)
+    pythonmodules.install(pyVer="3")
 
     #Remove .py extensions from scripts in /usr/bin
     for f in shelltools.ls("%s/usr/bin" % get.installDIR()):
