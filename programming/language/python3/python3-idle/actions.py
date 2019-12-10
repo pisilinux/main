6fa77c165461b0237fa5d315fb35e81e54548328
IdrisKalp <idriskalp@gmail.com>
2019-12-10 04:36:56 +0300

    python3-idle add to main

--- programming/language/python3/python3-idle/actions.py
+++ programming/language/python3/python3-idle/actions.py
@@ -14,15 +14,15 @@ def setup():
     pisitools.dosed("Lib/cgi.py","^#.* /usr/local/bin/python","#!/usr/bin/python")
 
     shelltools.unlinkDir("Modules/expat")
-    shelltools.unlinkDir("Modules/zlib")
+    #shelltools.unlinkDir("Modules/zlib")
     shelltools.unlinkDir("Modules/_ctypes/darwin")
-    shelltools.unlinkDir("Modules/_ctypes/libffi")
-    shelltools.unlinkDir("Modules/_ctypes/libffi_msvc")
+    #shelltools.unlinkDir("Modules/_ctypes/libffi")
+    #shelltools.unlinkDir("Modules/_ctypes/libffi_msvc")
     shelltools.unlinkDir("Modules/_ctypes/libffi_osx")
-    shelltools.unlinkDir("Modules/_decimal/libmpdec")
+    #shelltools.unlinkDir("Modules/_decimal/libmpdec")
 
     autotools.rawConfigure("\
-                            --prefix=/usr \
+                            --prefix=/temp \
                             --enable-shared \
                             --with-threads \
                             --with-computed-gotos \
@@ -39,5 +39,15 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.remove("/usr/bin/2to3")
-    pisitools.dodoc("LICENSE", "README.*")
+    
+    shelltools.mkdir("%s/usr" % get.installDIR())
+    
+    pisitools.domove("/temp/lib/python3*/tkinter", "/usr/lib/python3*/tkinter")
+    pisitools.domove("/temp/lib/python3*/lib-dynload/_tkinter.cpython-38m-x86_64-linux-gnu.so", "/usr/lib/python3*/lib-dynload/_tkinter.cpython-38m-x86_64-linux-gnu.so")
+    pisitools.domove("/temp/lib/python3.8/turtledemo", "/usr/lib/python3.8/turtledemo")
+    pisitools.domove("/temp/bin/idle3", "/usr/bin/idle3")
+    pisitools.domove("/temp/lib/python*/idlelib", "/usr/lib/python*/idlelib")
+    
+    pisitools.removeDir("/temp")
+    #pisitools.remove("/usr/bin/2to3")
+    #pisitools.dodoc("LICENSE", "README.*")
