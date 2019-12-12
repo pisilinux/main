c093568b5d70d99dd442487a9dbf33c846da9262
blue-devil <bluedevil@sctzine.com>
2019-12-12 01:30:48 +0000

    lapack : version bump

--- science/mathematics/lapack/actions.py
+++ science/mathematics/lapack/actions.py
@@ -12,23 +12,28 @@ from pisi.actionsapi import get
 
 
 def setup():
+    pisitools.dosed("INSTALL/make.inc.gfortran", "-O2 -frecursive", "")
+    pisitools.dosed("INSTALL/make.inc.gfortran", "-O0 -frecursive", "")
+    '''
     shelltools.copy("INSTALL/make.inc.gfortran", "make.inc")
-
     if get.ARCH() == "x86_64":
         pisitools.dosed("make.inc", "-O2", "%s -fPIC -m64 -funroll-all-loops" % get.CFLAGS())
         pisitools.dosed("make.inc", "NOOPT    =", "NOOPT    =-m64 -fPIC ")
     else:
         pisitools.dosed("make.inc", "-O2", "%s -fPIC -funroll-all-loops" % get.CFLAGS())
+    '''
 
     shelltools.makedirs("build")
     shelltools.cd("build")
     options = "-DCMAKE_INSTALL_LIBDIR=lib \
                -DBUILD_SHARED_LIBS=ON \
+               -DCBLAS=ON \
                -DBUILD_TESTING=OFF"
 
     if get.buildTYPE() == "static":
         options = "-DCMAKE_INSTALL_LIBDIR=lib \
                    -DBUILD_SHARED_LIBS=OFF \
+                   -DCBLAS=ON \
                    -DBUILD_TESTING=OFF"
 
     cmaketools.configure(options, sourceDir="..")
