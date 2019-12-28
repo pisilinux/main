235ab2a95f869353333f328cbf18a56ecacb747c
4fury <lilian2water@gmail.com>
2019-12-28 19:31:15 +0300

    simpleburn update.

--- hardware/optical/simpleburn/actions.py
+++ hardware/optical/simpleburn/actions.py
@@ -2,24 +2,18 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/copyleft/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import cmaketools
 
-
-
-def setup():    
-    cmaketools.configure("-DCMAKE_BUILD_TYPE=Release  \
-      -DCMAKE_INSTALL_PREFIX=/usr \
-      -DGTK3=ON \
-      -DBURNING=LIBBURNIA ")
+def setup():  
+    pisitools.dosed("CMakeLists.txt", "doc/\$", "share/doc/$")
+    cmaketools.configure("-DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr -DBURNING=LIBBURNIA")
 
 def build():
     cmaketools.make()
 
 def install():
-    cmaketools.install()    
-    
-#pisitools.dodoc("AUTHORS", "ChangeLog", "INSTALL", "LICENSE", "TODO")
-   
+    cmaketools.install()
+
