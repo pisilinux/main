b7203436a95325954270adaa56d576d19b3a5a23
blue-devil <bluedevil@sctzine.com>
2019-12-30 17:57:22 +0000

    python3-sphinx-alabaster-theme : version bump

--- programming/language/python3/python3-sphinx-alabaster-theme/actions.py
+++ programming/language/python3/python3-sphinx-alabaster-theme/actions.py
@@ -4,16 +4,10 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import pythonmodules
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
-
 
 def build():
     pythonmodules.compile(pyVer="3")
 
 def install():
     pythonmodules.install(pyVer="3")
-
-    pisitools.dodoc("LICENSE*", "README*")
