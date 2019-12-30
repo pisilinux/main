ed504c7cd2e4b460247a16ec39bcc82286f108e7
blue-devil <bluedevil@sctzine.com>
2019-12-30 18:31:23 +0000

    python3-Pygments:version bump

--- programming/language/python3/python3-Pygments/actions.py
+++ programming/language/python3/python3-Pygments/actions.py
@@ -4,15 +4,12 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir="%s-%s" % (get.srcNAME().split("-")[1], get.srcVERSION())
-
 def build():
-    shelltools.system('sed -i "s:pygmentize:&3:g" setup.py')
     pythonmodules.compile(pyVer="3")
 
 def install():
@@ -20,5 +17,5 @@ def install():
 
     #pisitools.dohtml("docs/build/*")
     #pisitools.insinto("/usr/share/doc/%s/src/" % get.srcNAME(),"docs/src/*")
-    pisitools.insinto("/usr/share/man", "doc/pygmentize.1", "pygmentize3.1")
+    pisitools.insinto("/usr/share/man/man1", "doc/pygmentize.1", "pygmentize.1")
     
