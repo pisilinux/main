5f07d26f67208c5c6e04261c125288025c581b21
blue-devil <bluedevil@sctzine.com>
2019-12-30 18:07:48 +0000

    python3-sphinxcontrib-websupport:moved to main

---
+++ programming/language/python3/python3-sphinxcontrib-websupport/actions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import pythonmodules
+
+def build():
+    pythonmodules.compile(pyVer="3")
+    
+#def check():
+#    pythonmodules.compile("test", pyVer="3")
+    
+def install():
+    pythonmodules.install("-O1", pyVer="3")
