88639b71b2d4244709325a8b1a87afc94ebbbdf5
blue-devil <bluedevil@sctzine.com>
2020-01-02 21:21:39 +0000

    python3-PyYaml:ver. bump

--- programming/language/python3/python3-PyYAML/actions.py
+++ programming/language/python3/python3-PyYAML/actions.py
@@ -5,16 +5,21 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 WorkDir = "PyYAML-%s" % get.srcVERSION()
 
 def build():
+    pisitools.cflags.add("-Wno-pointer-sign -Wno-incompatible-pointer-types -Wno-sign-compare -Wno-discarded-qualifiers")
+    shelltools.export("LDSHARED", "x86_64-pc-linux-gnu-gcc -Wl,-O1,--as-needed -shared -lpthread -Wl,-O1 -Wl,-z,relro -Wl,--hash-style=gnu -Wl,--as-needed -Wl,--sort-common")
     pythonmodules.compile(pyVer="3")
+    
+def check():
+    pythonmodules.compile("test", pyVer="3")
 
 def install():
     pythonmodules.install(pyVer="3")
 
     pisitools.insinto("%s/%s" % (get.docDIR(), get.srcNAME()), "examples")
-    pisitools.dodoc("LICENSE", "PKG-INFO", "README")
