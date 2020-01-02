bbf473c3dabdf480cd5de4403e920b374a00026b
4fury <lilian2water@gmail.com>
2020-01-02 19:59:45 +0300

    papirus-icon-theme update.

--- desktop/lookandfeel/papirus-icon-theme/actions.py
+++ desktop/lookandfeel/papirus-icon-theme/actions.py
@@ -17,4 +17,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "LICENSE", "README*")
+    pisitools.dodoc("AUTHORS", "LICENSE", "README.md")
