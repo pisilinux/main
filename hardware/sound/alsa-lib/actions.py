cd8191acc7f90efc40c4749822ab463503e52eb5
blue-devil <bluedevil@sctzine.com>
2020-01-06 21:00:00 +0000

    alsa-lib:version bump

--- hardware/sound/alsa-lib/actions.py
+++ hardware/sound/alsa-lib/actions.py
@@ -10,27 +10,29 @@ from pisi.actionsapi import get
 from pisi.actionsapi import shelltools
 
 def setup():
-    options = "--disable-aload"
+    options = "--disable-aload --without-debug"
 
-    if get.buildTYPE() == "emul32":
-        options += " --disable-python"
-        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+    #if get.buildTYPE() == "emul32":
+    #    options += " --disable-python"
+    #    shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
 
-    autotools.autoreconf("-fi")
-
+    #autotools.autoreconf("-fi")
+    pisitools.cflags.add("-Wno-address-of-packed-member -Wno-unused-result")
     autotools.configure(options)
     pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
     # rpath fix
-    pisitools.dosed("libtool", "^hardcode_libdir_flag_spec=.*", "hardcode_libdir_flag_spec=\"\"")
-    pisitools.dosed("libtool", "^runpath_var=LD_RUN_PATH", "runpath_var=DIE_RPATH_DIE")
+    #pisitools.dosed("libtool", "^hardcode_libdir_flag_spec=.*", "hardcode_libdir_flag_spec=\"\"")
+    #pisitools.dosed("libtool", "^runpath_var=LD_RUN_PATH", "runpath_var=DIE_RPATH_DIE")
 
 def build():
     autotools.make()
+    autotools.make("doc")
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    if get.buildTYPE() == "emul32": return
+    #if get.buildTYPE() == "emul32": return
 
-    pisitools.dodoc("ChangeLog", "TODO", "COPYING", "doc/*.txt")
+    pisitools.dodoc("COPYING", "MEMORY-LEAK", "NOTES")
+    pisitools.dohtml("doc/doxygen/html/*")
