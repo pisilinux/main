54f762e14b81871cb5b0e8d9a5dd3b025edf6c1e
Marcin Bojara <marcin.bojara@gmail.com>
2020-01-07 23:14:09 +0100

    libdrm: use mesontools

--- x11/library/libdrm/actions.py
+++ x11/library/libdrm/actions.py
@@ -4,28 +4,15 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import get
-from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import mesontools
 from pisi.actionsapi import shelltools
 
 def setup():
-    #pisitools.dosed("configure.ac", "pthread-stubs", deleteLine=True)
-    autotools.autoreconf("-fi")
-    options = " --enable-udev"
-     
-    if get.buildTYPE() == "_emul32":
-        options += " --libdir=/usr/lib32"
-    
-    autotools.configure(options)
-    
-    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+    mesontools.configure("-Dudev=true -Dvalgrind=true")
 
 def build():
-    autotools.make()
+    mesontools.build()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    if get.buildTYPE() == "_emul32":
-        return
-        
-    #pisitools.dodoc("README")
+    mesontools.install()
