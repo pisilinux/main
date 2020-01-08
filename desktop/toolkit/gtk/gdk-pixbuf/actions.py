ca6fd8acac47e7ba0ccc56966943445462507496
idriskalp <idriskalp@gmail.com>
2020-01-08 19:17:51 +0300

    gdk-pixbuf 2.40.0

--- desktop/toolkit/gtk/gdk-pixbuf/actions.py
+++ desktop/toolkit/gtk/gdk-pixbuf/actions.py
@@ -7,35 +7,50 @@
 from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import mesontools
 
 def setup():
-    options = "\
-                 --disable-static \
-                 --disable-silent-rules \
-                 --with-libjasper \
-                 --with-x11 \
-                 --with-included-loaders=png \
-              "
+    #options = "\
+                 #--disable-static \
+                 #--disable-silent-rules \
+                 #--with-libjasper \
+                 #--with-x11 \
+                 #--with-included-loaders=png \
+              #"
 
-    options += "\
-                 --bindir=/_emul32/bin \
-                 --disable-introspection \
-               " if get.buildTYPE() == "emul32" else \
-               "\
-                 --enable-introspection \
-               "
-    autotools.configure(options)
+    #options += "\
+                 #--bindir=/_emul32/bin \
+                 #--disable-introspection \
+               #" if get.buildTYPE() == "emul32" else \
+               #"\
+                 #--enable-introspection \
+               #"
+    #autotools.configure(options)
 
-    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+    #pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+    
+	options = "-Dgtk_doc=true \
+	           -Dx11=true \
+	           -Dbuiltin_loaders=png \
+	           -Djasper=true \
+			"
+	
+	if get.buildTYPE()=="emul32" :
+		options += "-Dintrospection=false \
+		            --bindir=/usr/bin32"
+	else:
+		options += "-Dintrospection=true"
+		
+	mesontools.configure(options)
 
 def build():
-    autotools.make()
+     mesontools.build()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    mesontools.install()
     if get.buildTYPE() == "emul32":
-        pisitools.domove("/_emul32/bin/gdk-pixbuf-query-loaders", "/usr/bin", "gdk-pixbuf-query-loaders-32")
-        pisitools.removeDir("/_emul32")
+        pisitools.domove("/usr/bin32/gdk-pixbuf-query-loaders", "/usr/bin", "gdk-pixbuf-query-loaders-32")
+        pisitools.removeDir("/usr/bin32")
         return
     pisitools.dosym("/usr/bin/gdk-pixbuf-query-loaders", "/usr/bin/gdk-pixbuf-query-loaders-64")
-    pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README")
+    pisitools.dodoc("COPYING", "README.md")
