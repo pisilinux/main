5d1a52ea58ebc4ac46680e87e570dd8610806619
blue-devil <blue-devil@sctzine.com>
2020-01-09 22:05:12 +0000

    python3-idna:version bump

--- programming/language/python3/python3-idna/actions.py
+++ programming/language/python3/python3-idna/actions.py
@@ -5,12 +5,9 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pythonmodules
-from pisi.actionsapi import pisitools
 
 def build():
     pythonmodules.compile(pyVer="3")
 
 def install():
     pythonmodules.install(pyVer="3")
-
-    #pisitools.dodoc("AUTHORS", "BUGS", "ChangeLog", "COPYING", "NEWS", "README")
