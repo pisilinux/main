433dc0625e3b22b921138086ddb3719d017222a6
blue-devil <blue-devil@sctzine.com>
2020-01-09 22:14:57 +0000

    python3-psutil:version bump

--- programming/language/python3/python3-psutil/actions.py
+++ programming/language/python3/python3-psutil/actions.py
@@ -5,12 +5,13 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 
 def build():
+    pisitools.cflags.add("-Wno-stringop-truncation")
+    shelltools.export("LDSHARED", "x86_64-pc-linux-gnu-gcc -Wl,-O1,--as-needed -shared -L/var/pisi/python3-3.8.1-8/work/temp/lib -lbz2 -lpthread -ldl")
     pythonmodules.compile(pyVer="3")
 
 def install():
     pythonmodules.install(pyVer="3")
-
-    pisitools.dodoc("LICENSE", "README.rst", "INSTALL.rst")
