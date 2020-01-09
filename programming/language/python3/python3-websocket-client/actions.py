7930c40d38090d25f297c6e0671a4fff1c7ec832
blue-devil <blue-devil@sctzine.com>
2020-01-09 22:25:27 +0000

    python3-web-socket-client:version bump

--- programming/language/python3/python3-websocket-client/actions.py
+++ programming/language/python3/python3-websocket-client/actions.py
@@ -4,12 +4,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pythonmodules
-from pisi.actionsapi import pisitools
 
 def build():
     pythonmodules.compile(pyVer="3")
 
 def install():
     pythonmodules.install(pyVer="3")
-
-    pisitools.dodoc("MANIFEST*", "ChangeLog", "README*", "LICENSE")
