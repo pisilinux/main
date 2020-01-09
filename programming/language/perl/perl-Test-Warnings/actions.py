31a06195e78f02a0537b28267f683d30d1da7939
blue-devil <blue-devil@sctzine.com>
2020-01-09 21:44:43 +0000

    perl-Test-Warnings:first pisi release

---
+++ programming/language/perl/perl-Test-Warnings/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import perlmodules
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+examples = "%s/%s/examples" % (get.docDIR(), get.srcNAME())
+
+def setup():
+    perlmodules.configure()
+
+def build():
+    perlmodules.make()
+
+def check():
+    perlmodules.make("test")
+
+def install():
+    perlmodules.install()
+
+    pisitools.dodoc("README", "LICENCE")
+    shelltools.chmod("examples/*", 0644)
+    pisitools.insinto(examples, "examples/*")
