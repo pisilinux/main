20f38d7dff97b5eed21358121ad3720b257fc83a
blue-devil <blue-devil@sctzine.com>
2020-01-11 13:13:00 +0000

    cython3:moved from contrib, dep for numpy

---
+++ programming/language/python3/cython3/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def build():
+    shelltools.export("LDSHARED", "x86_64-pc-linux-gnu-gcc -Wl,-O1,--as-needed -shared -lpthread -Wl,-O1 -Wl,-z,relro -Wl,--hash-style=gnu -Wl,--as-needed -Wl,--sort-common")
+    shelltools.export("CFLAGS", "-fno-strict-aliasing -mtune=generic -march=x86-64 -O2 -pipe -fstack-protector -D_FORTIFY_SOURCE=2 -g -fPIC -fwrapv -DNDEBUG -g -fwrapv -O3 -Wno-strict-aliasing")
+
+    pythonmodules.compile(pyVer="3")
+
+# it takes hours to run test, run with caution!
+#def check():
+#    pythonmodules.run("runtests.py -vv --no-pyregr", pyVer="3")
+
+def install():
+    pythonmodules.install(pyVer="3")
+   
+    #for dirs in ["Doc", "Demos", "tests"]:
+    #    pisitools.insinto("%s/%s" % (get.docDIR(), get.srcNAME()), dirs)
+       
+    #pisitools.dodoc("COPYING*", "README*", "USAGE*", "LICENSE*")
