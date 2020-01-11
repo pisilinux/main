54f057ac716bae4fa6d2f6bdceae663b6cf6b760
blue-devil <blue-devil@sctzine.com>
2020-01-11 12:12:24 +0000

    python3-PyYAML:version bump

--- programming/language/python/python-PyYAML/actions.py
+++ programming/language/python/python-PyYAML/actions.py
@@ -5,13 +5,19 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 WorkDir = "PyYAML-%s" % get.srcVERSION()
 
 def build():
+    pisitools.cflags.add("-Wno-pointer-sign -Wno-incompatible-pointer-types -Wno-sign-compare -Wno-discarded-qualifiers")
+    shelltools.export("LDSHARED", "x86_64-pc-linux-gnu-gcc -Wl,-O1,--as-needed -shared -lpthread -Wl,-O1 -Wl,-z,relro -Wl,--hash-style=gnu -Wl,--as-needed -Wl,--sort-common")
     pythonmodules.compile()
+    
+def check():
+    pythonmodules.compile("test")
 
 def install():
     pythonmodules.install()
