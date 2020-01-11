9d05fe88726459377798b185fa82657029a77bf1
blue-devil <blue-devil@sctzine.com>
2020-01-11 11:28:57 +0000

    python-mutagen:version bump, python3-mutagen:first pisi release

--- programming/language/python/python-mutagen/actions.py
+++ programming/language/python/python-mutagen/actions.py
@@ -6,11 +6,21 @@
 
 from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
-WorkDir = "mutagen-%s" % get.srcVERSION()
+def build():
+    pythonmodules.compile()
 
 def install():
     pythonmodules.install()
-
-    pisitools.dodoc("NEWS", "COPYING", "README.rst")
+    pisitools.rename("/usr/bin/mid3cp","mid3cp-py2")
+    pisitools.rename("/usr/bin/mid3v2","mid3v2-py2")
+    pisitools.rename("/usr/bin/moggsplit","moggsplit-py2")
+    pisitools.rename("/usr/bin/mid3iconv","mid3iconv-py2")
+    pisitools.rename("/usr/bin/mutagen-pony","mutagen-pony-py2")
+    pisitools.rename("/usr/bin/mutagen-inspect","mutagen-inspect-py2")
+    pisitools.rename("/usr/share/man/man1/mid3cp.1","mid3cp-py2.1")
+    pisitools.rename("/usr/share/man/man1/mid3v2.1","mid3v2-py2.1")
+    pisitools.rename("/usr/share/man/man1/mid3iconv.1","mid3iconv-py2.1")
+    pisitools.rename("/usr/share/man/man1/moggsplit.1","moggsplit-py2.1")
+    pisitools.rename("/usr/share/man/man1/mutagen-pony.1","mutagen-pony-py2.1")
+    pisitools.rename("/usr/share/man/man1/mutagen-inspect.1","mutagen-inspect-py2.1")
