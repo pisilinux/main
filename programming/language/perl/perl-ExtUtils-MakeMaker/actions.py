876b5bd812f594f1018fe7a0fa112bb75a260e25
blue-devil <bluedevil@sctzine.com>
2020-01-14 21:11:54 +0000

    perl-ExtUtils-MakeMaker:moved from contrib

---
+++ programming/language/perl/perl-ExtUtils-MakeMaker/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import perlmodules
+from pisi.actionsapi import pisitools
+
+def setup():
+    perlmodules.configure()
+
+def build():
+    perlmodules.make()
+
+# tests take long time
+#def check():
+#    perlmodules.make("test")
+
+def install():
+    perlmodules.install()
+    # conflict: perl has a binary with the same name
+    pisitools.remove("/usr/bin/instmodsh")
+    perlmodules.removePodfiles()
