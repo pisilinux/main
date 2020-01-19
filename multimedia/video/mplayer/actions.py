271bd66ebb99dd7d53bd81d4fd88ef6edb4ec970
idriskalp <idriskalp@gmail.com>
2020-01-19 14:58:45 +0300

    mplayer r38161

--- multimedia/video/mplayer/actions.py
+++ multimedia/video/mplayer/actions.py
@@ -13,21 +13,16 @@ from pisi.actionsapi import pythonmodules
 pisitools.flags.sub("-O[\ds]+", "-O3")
 
 def setup():
-    shelltools.system('sed -E "s|(#include <samba-4.0/libsmbclient.h>)|#include <time.h>\n\1|g" -i stream/stream_smb.c ./version.sh')
+    shelltools.system("mv ffmpeg-4.2.2 ffmpeg")
+	
     autotools.rawConfigure('\
                         --prefix=/usr \
-                        --libdir=/usr/lib \
                         --confdir=/usr/share/mplayer   \
                         --enable-dynamic-plugins \
                         --enable-menu   \
-                        --enable-x11 \
                         --datadir=/usr/share/mplayer \
                         --charset=UTF-8 \
-                        --enable-xvmc \
-                        --enable-smb \
                         --language=en \
-                        --enable-radio \
-                        --enable-radio-capture \
                         --enable-gui')
     
 def build():
@@ -42,8 +37,7 @@ def install():
                        MANDIR=%(D)s/usr/share/man" % {"D": get.installDIR()})
 
     # set the default skin for gui
-    #shelltools.copytree("mplayer-1.2_pre37124/Blue-multilingual", "%s/usr/share/mplayer/skins/default" % get.installDIR())
-    shelltools.copytree("Clearlooks-multilingual", "%s/usr/share/mplayer/skins/default" % get.installDIR())
+    shelltools.copytree("mplayer-1.2_pre37124/Blue-multilingual", "%s/usr/share/mplayer/skins/default" % get.installDIR())
     
     # codecs conf, not something user will interact with
     pisitools.insinto("/etc/mplayer", "etc/*.conf")
