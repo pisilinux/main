e09f142ff537a97c55dc4c7dd79553e0ec3c88bf
idriskalp <idriskalp@gmail.com>
2020-01-19 12:37:08 +0300

    libtevent 0.10.2

--- programming/misc/libtevent/actions.py
+++ programming/misc/libtevent/actions.py
@@ -14,7 +14,6 @@ def setup():
     autotools.configure("\
                          --builtin-libraries=replace \
                          --bundled-libraries=NONE \
-                         --extra-python=/usr/bin/python \
                          --disable-rpath \
                         ")
 
