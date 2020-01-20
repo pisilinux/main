a57a8fc13e93481cb5ed5cbbcf86be73cb969ee7
blue-devil <bluedevil@sctzine.com>
2020-01-21 01:23:10 +0300

    python-imaging:cleand and rebuilt

--- programming/language/python/python-imaging/actions.py
+++ programming/language/python/python-imaging/actions.py
@@ -4,16 +4,20 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 WorkDir="Imaging-%s" % get.srcVERSION()
+# fix unused direct dependency warning
+shelltools.export("LDSHARED", "x86_64-pc-linux-gnu-gcc -Wl,-O1,--as-needed -shared -lpthread")
 
 def install():
-    pisitools.dosed("_imagingft.c", "<freetype/freetype.h>", "<freetype2/freetype.h>")
-    pisitools.dosed("_imagingft.c", "<freetype/fterrors.h>", "<freetype2/fterrors.h>")
+    # suppress compiler warnings
+    pisitools.cflags.add("-Wno-incompatible-pointer-types -Wno-unused-but-set-variable -Wno-int-to-pointer-cast")
+    #pisitools.dosed("_imagingft.c", "<freetype/freetype.h>", "<freetype2/freetype.h>")
+    #pisitools.dosed("_imagingft.c", "<freetype/fterrors.h>", "<freetype2/fterrors.h>")
     pythonmodules.install()
 
     shelltools.cd("Sane")
@@ -24,4 +28,3 @@ def install():
         pisitools.insinto("/usr/include/%s" % get.curPYTHON(), "libImaging/%s" % header)
 
     pisitools.dodoc("README")
-
