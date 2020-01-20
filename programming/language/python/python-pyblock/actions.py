80d09ee7f2c412ff001a3a81e61c64b9bee867cc
blue-devil <bluedevil@sctzine.com>
2020-01-21 01:47:33 +0300

    python-pyblock:rebuilt

--- programming/language/python/python-pyblock/actions.py
+++ programming/language/python/python-pyblock/actions.py
@@ -4,8 +4,8 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import autotools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 
 
