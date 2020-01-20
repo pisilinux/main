e95c8c22246ce27c051e216bb155bdceba8bb3fd
blue-devil <bluedevil@sctzine.com>
2020-01-21 01:19:41 +0300

    python-enum34:rebuilt

--- programming/language/python/python-enum34/actions.py
+++ programming/language/python/python-enum34/actions.py
@@ -5,12 +5,9 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pythonmodules
-from pisi.actionsapi import pisitools
 
 def build():
     pythonmodules.compile()
 
 def install():
     pythonmodules.install()
-
-    #pisitools.dodoc("AUTHORS", "BUGS", "ChangeLog", "COPYING", "NEWS", "README")
