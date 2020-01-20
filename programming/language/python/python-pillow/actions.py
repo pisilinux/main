96aa080e0b4aac9f27c949b52dbeb4de0076dd85
blue-devil <bluedevil@sctzine.com>
2020-01-21 01:40:46 +0300

    python-pillow:ver. bump

--- programming/language/python/python-pillow/actions.py
+++ programming/language/python/python-pillow/actions.py
@@ -4,24 +4,20 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 #WorkDir="Imaging-%s" % get.srcVERSION()
 
+def build():
+    # fix unused direct dependency analysis
+    shelltools.export("LDSHARED", "x86_64-pc-linux-gnu-gcc -Wl,-O1,--as-needed -shared -lpthread")
+    pythonmodules.compile()
+
 def install():
-    #pisitools.dosed("_imagingft.c", "<freetype/freetype.h>", "<freetype2/freetype.h>")
-    #pisitools.dosed("_imagingft.c", "<freetype/fterrors.h>", "<freetype2/fterrors.h>")
     pythonmodules.install()
 
-    #shelltools.cd("Sane")
-    #pythonmodules.install()
-    #shelltools.cd("..")
-
-    for header in ["Imaging.h","ImPlatform.h"]:
-        pisitools.insinto("/usr/include/%s" % get.curPYTHON(), "src/libImaging/%s" % header)
-
-    pisitools.dodoc("README.rst")
-
+    # install header files
+    pisitools.insinto("/usr/include/python3.8/Imaging/", "src/libImaging/*.h")
