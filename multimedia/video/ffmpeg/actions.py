be71dddb0f2b2694adc8db6ebe16595a29531368
Rmys <muscnsl@hotmail.com>
2020-01-21 23:11:52 +0300

    ffmpeg

--- multimedia/video/ffmpeg/actions.py
+++ multimedia/video/ffmpeg/actions.py
@@ -50,7 +50,6 @@ def setup():
                             --enable-libopencore_amrnb \
                             --enable-libopencore_amrwb \
                             --enable-libopenjpeg \
-                            --enable-libopenmpt \
                             --enable-libopus \
                             --enable-libpulse \
                             --enable-librsvg \
