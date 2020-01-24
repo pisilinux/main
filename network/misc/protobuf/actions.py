b07c9fcfa867218c0fcd90181aefa147a64bb6a6
blue-devil <bluedevil@sctzine.com>
2020-01-24 23:56:24 +0300

    protobuf:ver.bump and split py packages

--- network/misc/protobuf/actions.py
+++ network/misc/protobuf/actions.py
@@ -4,18 +4,26 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
+WorkDir="%s" % get.srcDIR()
+
 def setup():
+    # create a folder for python3 build
+    shelltools.copy("python/",  "python3/")
+
+    # suppress compiler warnings
+    pisitools.cxxflags.add("-Wno-stringop-overflow -Wno-unused-function -Wno-deprecated-declarations")
     shelltools.export("PTHREAD_LIBS", "-lpthread")
-    shelltools.copy("../googletest-release-1.8.0", "gtest")
+    shelltools.system("./autogen.sh")
     autotools.autoreconf("-vif")
     autotools.configure("--disable-static")
     
+    # fix unused direct dependency analysis
     pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
@@ -23,11 +31,27 @@ def build():
     shelltools.cd("python")
     pythonmodules.compile()
     shelltools.cd("..")
+    shelltools.cd("python3")
+    pythonmodules.compile(pyVer="3")
+    shelltools.cd("..")
+
+# tests successfully completed but takes to much time!
+#def check():
+#    autotools.make("check")
+#    shelltools.cd("python")
+#    pythonmodules.compile("test")
+#    shelltools.cd("..")
+#    shelltools.cd("python3")
+#    pythonmodules.compile("test", pyVer="3")
+#    shelltools.cd("..")
 
 def install():
     autotools.install()
     shelltools.cd("python")
     pythonmodules.install()
+    shelltools.cd("..")
+    shelltools.cd("python3")
+    pythonmodules.install(pyVer="3")
     shelltools.cd("..")
 
     pisitools.insinto("/usr/share/vim/vimfiles/syntax/", "editors/proto.vim")
