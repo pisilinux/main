74001c3d88a6c65562ccb810f48fcaa79649b426
Rmys <muscnsl@hotmail.com>
2020-01-31 23:39:22 +0300

    libgusb ver. bump

--- hardware/misc/libgusb/actions.py
+++ hardware/misc/libgusb/actions.py
@@ -4,24 +4,19 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import mesontools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    shelltools.makedirs("build")
-    shelltools.cd("build")
-    shelltools.system("meson .. --prefix=/usr -Ddocs=false")
-    
+    mesontools.configure()
+
 def build():
-    shelltools.cd("build")
-    shelltools.system("ninja")
+    mesontools.build()
     
 def install():
-    shelltools.cd("build")
-    shelltools.system("DESTDIR=%s ninja install" % get.installDIR())
-    
-    shelltools.cd("..")
+    mesontools.install()
+
     pisitools.dodoc("COPYING", "NEWS", "README*")
 
