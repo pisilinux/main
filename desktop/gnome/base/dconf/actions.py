56f4b5883abc5a0519c7837326b508201279c081
Rmys <muscnsl@hotmail.com>
2020-01-31 22:47:15 +0300

    dconf ver. bump

--- desktop/gnome/base/dconf/actions.py
+++ desktop/gnome/base/dconf/actions.py
@@ -1,33 +1,22 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import mesontools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-	
-    shelltools.system(""" sed -i "s|link_whole: libdconf_common,|link_with: libdconf_common,|g" common/meson.build """)
-	
-    shelltools.makedirs("build")
-    shelltools.cd("build")
-    shelltools.system("meson .. --prefix=/usr --sysconfdir=/etc -Dwith-dbus-service-dir=/usr/share/dbus-1/services \
-        -Dwith-dbus-system-service-dir=/usr/share/dbus-1/system-services -Dwith-gio-modules-dir=/usr/lib/gio/modules \
-        -Denable-gtk-doc=false -Denable-man=true")
-    
-    
+    mesontools.configure("-Dgtk_doc=true")
+
 def build():
-    shelltools.cd("build")
-    shelltools.system("ninja")
+    mesontools.build()
     
 def install():
-    shelltools.cd("build")
-    shelltools.system("DESTDIR=%s ninja install" % get.installDIR())
-    
-    shelltools.cd("..")
+    mesontools.install()
+
     pisitools.dodoc("COPYING", "NEWS", "README")
 
