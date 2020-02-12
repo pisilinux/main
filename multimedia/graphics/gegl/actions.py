731222b8d36d16f0da83d92b907ce54fc64e2660
Rmys <muscnsl@hotmail.com>
2020-02-12 20:44:14 +0300

    gegl ver. bump

--- multimedia/graphics/gegl/actions.py
+++ multimedia/graphics/gegl/actions.py
@@ -4,51 +4,22 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
+
 from pisi.actionsapi import get
+from pisi.actionsapi import mesontools
+from pisi.actionsapi import pisitools
+
 
 def setup():
-    #autotools.autoreconf("-vfi")
-    autotools.configure("--enable-mmx \
-                         --enable-sse \
-                         --includedir=/usr/include \
-                         --datadir=/usr/share \
-                         --with-pic \
-                         --with-cairo \
-                         --with-pango \
-                         --with-pangocairo \
-                         --with-gdk-pixbuf \
-                         --with-lensfun \
-                         --with-openexr \
-                         --with-sdl \
-                         --with-jasper \
-                         --with-lua \
-                         --without-libavformat \
-                         --with-libv4l \
-                         --with-libspiro \
-                         --with-exiv2 \
-                         --with-umfpack \
-                         --disable-static \
-                         --disable-gtk-doc \
-                         --enable-gtk-doc-html=no \
-                         --disable-docs \
-                         --disable-workshop")
-                         #--with-librsvg \
-                         #--with-graphviz \
-
-    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+    mesontools.configure("-Ddocs=false \
+                          -Dexiv2=enabled \
+                          -Dgdk-pixbuf=enabled")
 
 def build():
-    autotools.make()
-
-# disabled due sandbox violations
-#def check():
-#    autotools.make("check")
-
+    mesontools.build()
+    
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "COPYING.LESSER", "NEWS", "README")
+    mesontools.install()
+    
+    pisitools.dodoc("AUTHORS", "COPYING", "COPYING.LESSER")
 
