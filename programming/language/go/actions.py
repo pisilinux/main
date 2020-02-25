c878a79665858cd6669c1c941d7f55357f986c6b
blue-devil <bluedevil@sctzine.com>
2020-02-25 04:06:16 +0300

    go:version bump

--- programming/language/go/actions.py
+++ programming/language/go/actions.py
@@ -4,9 +4,9 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import autotools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 NoStrip = ["/"]
@@ -15,12 +15,12 @@ def build():
     shelltools.export("go_platform","linux-amd64")
     shelltools.export("go_linker","/lib/ld-linux-x86-64.so.2")
 
-    shelltools.export("GOROOT", "%s/go-go1.11.9" % get.workDIR()) #0
+    shelltools.export("GOROOT", "%s/go-go1.13.8" % get.workDIR()) #0
 
     shelltools.export("GOBIN", "$GOROOT/bin") #1
     shelltools.export("GOPATH", "%s" % get.workDIR())
     shelltools.export("GOROOT_FINAL", "/usr/lib/go")
-    shelltools.export("GOROOT_BOOTSTRAP", "%s/go-go1.11.9/go-linux-amd64-bootstrap" % get.workDIR())  #2
+    shelltools.export("GOROOT_BOOTSTRAP", "%s/go-go1.13.8/go-linux-amd64-bootstrap" % get.workDIR())  #2
 
     shelltools.export("GOOS","linux")
     shelltools.export("GOARCH","amd64")
@@ -31,7 +31,7 @@ def build():
 
 def install():
     shelltools.export("GOROOT_FINAL", "/usr/lib/go")
-    shelltools.cd("%s/go-go1.11.9" % get.workDIR())
+    shelltools.cd("%s/go-go1.13.8" % get.workDIR())
 
     pisitools.dodir("/usr/lib/go")
     shelltools.system("cp -r api bin doc lib pkg src  %s/usr/lib/go" % get.installDIR())
@@ -45,7 +45,7 @@ def install():
 
     shelltools.system("cp -r misc  %s/usr/lib/go" % get.installDIR())
 
-    pisitools.removeDir("/usr/lib/go/pkg/bootstrap")
+    #pisitools.removeDir("/usr/lib/go/pkg/bootstrap")
 
     # remove testdata, which hit cave fix-linkage
     pisitools.remove("/usr/lib/go/src/debug/elf/testdata/gcc-386-freebsd-exec")
@@ -55,4 +55,4 @@ def install():
     pisitools.removeDir("/usr/lib/go/pkg/linux_amd64")
     
 
-    pisitools.dodoc("VERSION", "LICENSE", "PATENTS", "README*", "AUTHORS", "CONTRIBUTORS")
+    pisitools.dodoc("LICENSE", "PATENTS", "README*")
