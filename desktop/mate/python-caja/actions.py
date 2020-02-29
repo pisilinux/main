2e2a176290ba6fda366faec0969a2b00e37f6742
Rmys <muscnsl@gmail.com>
2020-02-29 23:00:52 +0300

    python3.8

--- desktop/mate/python-caja/actions.py
+++ desktop/mate/python-caja/actions.py
@@ -10,7 +10,7 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    shelltools.export("PYTHON","python3.6")
+    shelltools.export("PYTHON","python3.8")
     autotools.configure("--prefix=/usr \
                         --disable-scrollkeeper \
                         --disable-schemas-install")
