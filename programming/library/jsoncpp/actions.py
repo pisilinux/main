c2492e733622c0106424d70525d658b2d22fd939
suvari <suvarice@gmail.com>
2020-03-01 15:24:22 +0300

    jsoncpp: ver. bump to 1.9.2

--- programming/library/jsoncpp/actions.py
+++ programming/library/jsoncpp/actions.py
@@ -1,25 +1,23 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import cmaketools
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
+from pisi.actionsapi import get
+from pisi.actionsapi import mesontools
+from pisi.actionsapi import pisitools
 def setup():
-    cmaketools.configure("-DCMAKE_BUILD_TYPE=release \
-                          -DCMAKE_INSTALL_PREFIX=/usr \
-                          -DJSONCPP_WITH_CMAKE_PACKAGE=ON \
-                          -DBUILD_SHARED_LIBS=ON \
-                          -DBUILD_STATIC_LIBS=OFF \
-                          -DCMAKE_INSTALL_LIBDIR=/usr/lib")
+    mesontools.configure()
 
 def build():
-    cmaketools.make()
+    mesontools.build()
+    
+def check():
+    mesontools.build("test")
 
 def install():
-    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    mesontools.install()
 
     pisitools.dodoc("AUTHORS", "LICENSE", "README*")
