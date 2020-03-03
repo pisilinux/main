4d01429019da8294964093042aa76d58db34749b
4fury <lilian2water@gmail.com>
2020-03-03 12:22:54 +0300

    rebuild.

--- multimedia/sound/faad2/actions.py
+++ multimedia/sound/faad2/actions.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
@@ -14,7 +14,7 @@ def setup():
     autotools.autoreconf("-vfi")
 
     autotools.configure("--without-xmms \
-                         --with-drm \
+                         --without-drm \
                          --disable-static")
 
 def build():
