c4c705ebdbeee3b1a1429060ff94ff62bc0af391
blue-devil <bluedevil@sctzine.com>
2020-03-05 04:06:09 +0300

    decorator : ver.bump

--- programming/language/python/python-decorator/actions.py
+++ programming/language/python/python-decorator/actions.py
@@ -6,11 +6,9 @@
 
 from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import pisitools
 
 def check():
     shelltools.system("nosetests --with-doctest -e documentation")
 
 def install():
     pythonmodules.install()
-
