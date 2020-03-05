002c9ee4e0dd33ce164bace718541712d0449fd4
Rmys <muscnsl@gmail.com>
2020-03-05 23:36:32 +0300

    gegl02

--- multimedia/graphics/gegl02/actions.py
+++ multimedia/graphics/gegl02/actions.py
@@ -10,7 +10,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    #autotools.autoreconf("-vfi")
+    autotools.autoreconf("-vfi")
     autotools.configure("--enable-mmx \
                          --enable-sse \
                          --includedir=/usr/include \
