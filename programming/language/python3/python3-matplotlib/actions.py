ad47b7ee62a0ca6487ad86653adfd728907dd0a9
blue-devil <bluedevil@sctzine.com>
2020-03-07 04:50:58 +0300

    python3-matplotlib:first pisi rel

---
+++ programming/language/python3/python3-matplotlib/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    # dodoc method cannot move LICENSE named dir, so i delete the directory
+    shelltools.unlinkDir("LICENSE")
+
+def build():
+    # suppress compiler warnings:
+    pisitools.cflags.add("-Wno-unused-but-set-variable -Wno-misleading-indentation -Wno-stringop-truncation -Wno-unused-function")
+    # fix unused direct dependency analysis
+    shelltools.export("LDSHARED", "x86_64-pc-linux-gnu-gcc -Wl,-O1,--as-needed -shared -lpthread -lbz2")
+    pythonmodules.compile(pyVer="3")
+
+def install():
+    pythonmodules.install(pyVer="3")
