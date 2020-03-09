a5c1f2a00f55da5969fe668ab5087dd1d0f8ab33
4fury <lilian2water@gmail.com>
2020-03-09 17:13:04 +0300

    libburn.

--- hardware/optical/libisoburn/actions.py
+++ hardware/optical/libisoburn/actions.py
@@ -7,8 +7,6 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
-
-
 def setup():
     autotools.configure("--prefix=/usr   \
                          --enable-static  \
