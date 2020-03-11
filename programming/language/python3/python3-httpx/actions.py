c824c3eb7a116e588a8df0a98b7ee582cf679e55
blue-devil <bluedevil@sctzine.com>
2020-03-12 00:58:51 +0300

    python3-httpx:ver bump

--- programming/language/python3/python3-httpx/actions.py
+++ programming/language/python3/python3-httpx/actions.py
@@ -5,9 +5,11 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
 
 def build():
     pythonmodules.compile(pyVer="3")
 
 def install():
     pythonmodules.install(pyVer="3")
+    pisitools.dodoc("LICENSE*")
