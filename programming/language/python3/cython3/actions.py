490559d4d1f26212891aab8745d150164348920e
blue-devil <bluedevil@sctzine.com>
2020-03-27 22:59:25 +0300

    cython3: ver. bump

--- programming/language/python3/cython3/actions.py
+++ programming/language/python3/cython3/actions.py
@@ -10,21 +10,16 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def build():
-    # fix unused dependency analysis
+    # fix unused direct dependency analysis
     shelltools.export("LDSHARED", "x86_64-pc-linux-gnu-gcc -Wl,-O1,--as-needed -shared -lpthread -Wl,-O1 -Wl,-z,relro -Wl,--hash-style=gnu -Wl,--as-needed -Wl,--sort-common")
-    # compress compiler warnings
+    # suppress compiler warnings
     shelltools.export("CFLAGS", "-fno-strict-aliasing -mtune=generic -march=x86-64 -O2 -pipe -fstack-protector -D_FORTIFY_SOURCE=2 -g -fPIC -fwrapv -DNDEBUG -g -fwrapv -O3 -Wno-strict-aliasing")
 
     pythonmodules.compile(pyVer="3")
 
-# it takes hours to run test, run with caution!
+# Blue DeviL Note: it takes hours to run test, run with caution!
 #def check():
 #    pythonmodules.run("runtests.py -vv --no-pyregr", pyVer="3")
 
 def install():
     pythonmodules.install(pyVer="3")
-   
-    #for dirs in ["Doc", "Demos", "tests"]:
-    #    pisitools.insinto("%s/%s" % (get.docDIR(), get.srcNAME()), dirs)
-       
-    #pisitools.dodoc("COPYING*", "README*", "USAGE*", "LICENSE*")
