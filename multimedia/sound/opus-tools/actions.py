1636b47d0eb84d2f40a72cd8f9d655ad24fbf26a
blue-devil <bluedevil@sctzine.com>
2020-04-08 23:25:05 +0300

    opus-tools:moved from contrib

---
+++ multimedia/sound/opus-tools/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--enable-sse --enable-assertions \
+                        --disable-static \
+                        --disable-oggtest \
+                        --disable-opustest \
+                        --disable-opusfiletest \
+                        --disable-libopusenctest")
+
+    # fix unused direct dependency analysis
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "COPYING")
