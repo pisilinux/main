f861caec4e20ad7b1f9878c38b1922f284f1fc67
blue-devil <bluedevil@sctzine.com>
2020-04-09 17:24:19 +0300

    python-sphinxcontrib-websupport:moved from contrib

---
+++ programming/language/python/python-sphinxcontrib-websupport/actions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import pythonmodules
+
+def build():
+    pythonmodules.compile()
+    
+def check():
+    pythonmodules.compile("test")
+
+def install():
+    pythonmodules.install("-O1")
