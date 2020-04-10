c6c040dc5e49a7a6da5883651831e715f9f25c01
blue-devil <bluedevil@sctzine.com>
2020-04-10 18:39:09 +0300

    perl-XML-LibXML : first pisi rel

--- programming/language/perl/perl-XML-LibXML/actions.py
+++ programming/language/perl/perl-XML-LibXML/actions.py
@@ -4,14 +4,20 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import get
 from pisi.actionsapi import perlmodules
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
 WorkDir = "%s-%s" % (get.srcNAME()[5:], get.srcVERSION())
 
 def setup():
     perlmodules.configure()
+    # suppress warning messages
+    pisitools.dosed("Makefile", "CCFLAGS = ", "CCFLAGS = -Wno-stringop-overflow -Wno-incompatible-pointer-types ")
+    # fix unused direct dependency analysis
+    pisitools.dosed("Makefile","LDDLFLAGS =","LDDLFLAGS = -Wl,-O1,--as-needed")
+    # fix runpath analysis
+    pisitools.dosed("Makefile","LD_RUN_PATH=\"\$\(LD_RUN_PATH\)\"", "")
 
 def build():
     perlmodules.make()
