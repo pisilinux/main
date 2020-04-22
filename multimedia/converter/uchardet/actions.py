8496b5927a2a466d6a0bf8eaab05ebf9bae031cc
Erkan IŞIK <erkanisik@yahoo.com>
2020-04-22 13:49:22 +0300

    add packages
    flacon
    uchardet
    azpainter
    azpainterb and index

---
+++ multimedia/converter/uchardet/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr \
+                          -DCMAKE_INSTALL_LIBDIR=lib \
+                          -DCMAKE_BUILD_TYPE=Release")
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "COPYING", "README*")
