5faf41c31bdafbba1dd5fd0b1b03c2d793e71bb8
4fury <lilian2water@gmail.com>
2020-04-29 07:23:42 +0300

    version fix.

--- util/archive/xarchiver/actions.py
+++ util/archive/xarchiver/actions.py
@@ -2,24 +2,22 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
+t = 'PLUGINDIR=/usr/lib/xfce4/thunar-archive-plugin'
+
 def setup():
-	autotools.configure("--prefix=/usr \
-	\
-	--enable-gtk3 --enable-plugin \
-	\
-	--disable-dependency-tracking")
+	autotools.configure("--disable-gtk2 --enable-plugin --disable-dependency-tracking")
 
 def build():
-	autotools.make("PLUGINDIR=/usr/lib/xfce4/thunar-archive-plugin")
+	autotools.make(t)
 
 def install():
-	autotools.rawInstall("DESTDIR=%s PLUGINDIR=/usr/lib/xfce4/thunar-archive-plugin" % get.installDIR())
+	autotools.rawInstall("DESTDIR=%s %s" % (get.installDIR(), t))
 
 	pisitools.dodoc("COPYING", "README")
 
