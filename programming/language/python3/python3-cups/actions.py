98c971233362c46511c7b86b8c71cb316017b0cc
blue-devil <bluedevil@sctzine.com>
2020-05-17 01:38:37 +0300

    python3-cups:ver bump

--- programming/language/python3/python3-cups/actions.py
+++ programming/language/python3/python3-cups/actions.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-#
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import autotools
-from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pythonmodules
 
 def build():
-    pisitools.dosed("Makefile", "python", "python3")
-    autotools.make()
+    # suppress compiler warnings
+    pisitools.cflags.add("-Wno-deprecated-declarations")
+    # fix unused direct dependency analysis
+    shelltools.export("LDSHARED", "x86_64-pc-linux-gnu-gcc -Wl,-O1,--as-needed -shared -lbz2 -ldl")
+    pythonmodules.compile(pyVer="3")
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-    pisitools.dodoc("COPYING", "NEWS", "README", "TODO", "examples/cupstree.py")
+    pythonmodules.install(pyVer="3")
