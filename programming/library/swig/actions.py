a8d9e23466ddd6464d4503f0299a595736260c5b
blue-devil <bluedevil@sctzine.com>
2020-05-19 00:30:35 +0300

    swig rebuild

--- programming/library/swig/actions.py
+++ programming/library/swig/actions.py
@@ -4,21 +4,26 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
 
 def setup():
     shelltools.system("sed -i 's/\$(PERL5_SCRIPT/-I. &/' Examples/Makefile.in")
     shelltools.system("sed -i 's/\$command 2/-I. &/' Examples/test-suite/perl5/run-perl-test.pl")
     autotools.configure("--without-clisp \
                          --without-maximum-compile-warnings")
+    #autotools.configure("--prefix=/usr")
 
 def build():
     autotools.make()
+
+# too much dependency to test
+#def check():
+#    shelltools.system("make check || warning 'Tests failed'")
     
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("ANNOUNCE", "CHANGES*", "LICENSE", "README", "TODO")
+    pisitools.dodoc("LICENSE*", "README")
