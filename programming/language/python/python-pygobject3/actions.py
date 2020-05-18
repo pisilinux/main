b7e560ed85eaff14e1c09c8e7c9b11974c04ba59
blue-devil <bluedevil@sctzine.com>
2020-05-19 01:23:10 +0300

    python-pygobject3:ver bump

--- programming/language/python/python-pygobject3/actions.py
+++ programming/language/python/python-pygobject3/actions.py
@@ -4,16 +4,15 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 
 def setup():
     shelltools.makedirs("build")
     shelltools.cd("build") 
-    shelltools.system("meson .. --prefix=/usr -Dpython=python2.7")
-    
+    shelltools.system("meson .. --prefix=/usr -Dpython=python2.7")   
    
 def build():
     shelltools.cd("build")
