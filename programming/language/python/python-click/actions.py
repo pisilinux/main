c483b00be9f51fe5fb9d470f80bad79ead53acd8
blue-devil <bluedevil@sctzine.com>
2020-05-25 00:44:04 +0300

    python-click: ver bump

--- programming/language/python/python-click/actions.py
+++ programming/language/python/python-click/actions.py
@@ -4,9 +4,9 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import pythonmodules
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import pythonmodules
 
 def build():
     pythonmodules.compile()
