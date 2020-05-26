c61e2459ffbc2eb3d9de51dacc6e7a5a0f08edb9
blue-devil <bluedevil@sctzine.com>
2020-05-27 01:11:53 +0300

    libqalculate:ver bump

--- science/mathematics/libqalculate/actions.py
+++ science/mathematics/libqalculate/actions.py
@@ -4,17 +4,17 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
 
 def setup():
-    autotools.autoreconf("-fiv")
+    #autotools.autoreconf("-fiv")
     shelltools.system("./autogen.sh")
     autotools.configure("--disable-static")
     
-    
+    # fix unused direct dependency analysis
     pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
