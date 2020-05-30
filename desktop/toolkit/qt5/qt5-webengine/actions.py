974945c60f80b11f7b58b57d2ef76ee827901bc7
Rmys <muscnsl@gmail.com>
2020-05-30 21:50:44 +0300

    qt5-webengine rebuild

--- desktop/toolkit/qt5/qt5-webengine/actions.py
+++ desktop/toolkit/qt5/qt5-webengine/actions.py
@@ -11,21 +11,31 @@ from pisi.actionsapi import qt5
 from pisi.actionsapi import get
 
 def setup():
+    shelltools.makedirs("build")
+    shelltools.cd("build")
     #shelltools.export("QT5LINK", "/usr/lib/qt5/bin")
     #shelltools.export("QT5DIR", "/usr/lib/qt5")
     #shelltools.export("CFLAGS", "%s -I/usr/lib/sqlite3.16.2" % get.CFLAGS())
-    shelltools.system("qmake WEBENGINE_CONFIG+=use_proprietary_codecs WEBENGINE_CONFIG+=use_system_icu WEBENGINE_CONFIG+=use_system_protobuf WEBENGINE_CONFIG+=use_system_ffmpeg qtwebengine.pro")
+    #shelltools.system("qmake WEBENGINE_CONFIG+=use_proprietary_codecs WEBENGINE_CONFIG+=use_system_icu WEBENGINE_CONFIG+=use_system_protobuf WEBENGINE_CONFIG+=use_system_ffmpeg qtwebengine.pro")
     #pisitools.ldflags.add("-Wl,--no-keep-memory ")
-    qt5.configure()
+    shelltools.system("qmake .. -- -proprietary-codecs \
+                   -system-ffmpeg \
+                   -system-opus \
+                   -system-webp \
+                   -webengine-icu \
+                   -webengine-kerberos")
     
 def build():
+    shelltools.cd("build")
     qt5.make()
 
 def install():
+    shelltools.cd("build")
     qt5.install("INSTALL_ROOT=%s" % get.installDIR())
 
     #I hope qtchooser will manage this issue
     #for bin in shelltools.ls("%s/usr/lib/qt5/bin" % get.installDIR()):
        #pisitools.dosym("/usr/lib/qt5/bin/%s" % bin, "/usr/bin/%s-qt5" % bin)
-
+    
+    shelltools.cd("..")
     pisitools.dodoc("LICENSE*")
