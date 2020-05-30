612ba38d06bdf23c20d3b383da65c7cbab5010e2
blue-devil <bluedevil@sctzine.com>
2020-05-31 01:50:07 +0300

    qhull:first pisi rel

---
+++ science/misc/qhull/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import cmaketools
+
+def setup():
+    cmaketools.configure("-DCMAKE_INSTALL_LIBDIR=lib \
+                          -DCMAKE_SKIP_RPATH=ON")
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("README*", "COPYING*")
