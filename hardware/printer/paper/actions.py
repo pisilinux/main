d41680541bcd52d61067566ab7ce64ad087062d4
blue-devil <bluedevil@sctzine.com>
2020-06-04 00:55:06 +0300

    paper: first pisi rel

---
+++ hardware/printer/paper/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+def setup():
+    # extract gnulib from archive:
+    shelltools.system("cd .. && tar -xzvf gnulib-d279bc6.tar.gz")
+
+    shelltools.system("./bootstrap --gnulib-srcdir=%s/gnulib-d279bc6 --skip-git --copy" % get.workDIR())
+    autotools.configure("--libexecdir=/usr/lib")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("COPYING", "README")
