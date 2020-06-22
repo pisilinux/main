1e0a932713cb62ad798ce58b104205ff802137db
erkanisik1 <erkanisik@yahoo.com>
2020-06-22 13:44:53 +0300

    add game/misc/plib library

---
+++ game/misc/plib/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    pisitools.cxxflags.add("-fPIC")
+    autotools.autoreconf("-vif")
+    autotools.configure("--disable-static")
+
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "KNOWN_BUGS", "NOTICE", "README*", "TODO*", "NEWS")
