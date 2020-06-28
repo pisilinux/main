e5f957c3211b8232400511770a28c49652a35848
Rmys <muscnsl@gmail.com>
2020-06-28 16:30:16 +0300

    pulseaudio-qt rebuild cmake

--- multimedia/sound/pulseaudio-qt/actions.py
+++ multimedia/sound/pulseaudio-qt/actions.py
@@ -1,19 +1,32 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import kde5
+from pisi.actionsapi import get
 
 def setup():
-    kde5.configure()
+    shelltools.makedirs("build")
+    shelltools.cd("build")
+    cmaketools.configure("-DBUILD_QCH=ON \
+                          -DCMAKE_INSTALL_PREFIX=/usr \
+                          -DCMAKE_INSTALL_LIBDIR=lib \
+                          -DBUILD_TESTING=OFF", sourceDir="..")
 
 def build():
-    kde5.make()
+    shelltools.cd("build")
+    cmaketools.make()
+    #cmaketools.make("-C build doc")
 
 def install():
-    kde5.install()
+    pisitools.dodoc("README*", "COPYING*")
 
-    pisitools.dodoc("COPYING*")
+    shelltools.cd("build")
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    #pisitools.dohtml("doc/html/*")
+
+    #pisitools.remove("/usr/bin/lit2epub")
