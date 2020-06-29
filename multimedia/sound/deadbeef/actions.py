fd5082470cf5865eb9d4532142e94988478131e0
4fury <lilian2water@gmail.com>
2020-06-29 19:37:27 +0300

    deadbeef: ver. bump.

--- multimedia/sound/deadbeef/actions.py
+++ multimedia/sound/deadbeef/actions.py
@@ -8,13 +8,25 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-w = "-Wno-unused-but-set-variable -Wno-deprecated-declarations \
--Wno-incompatible-pointer-types -Wno-multichar -Wno-unused-result \
--Wno-misleading-indentation -Wno-dangling-else -Wno-parentheses \
--Wno-unused-variable -Wno-unused-value -Wno-vla"
+j = "-Wno-multichar \
+     -Wno-parentheses \
+     -Wno-unused-value \
+     -Wno-unused-result \
+     -Wno-dangling-else \
+     -Wno-unused-variable \
+     -Wno-stringop-overflow \
+     -Wno-misleading-indentation \
+     -Wno-unused-but-set-variable \
+     -Wno-unused-but-set-variable \
+     -Wno-deprecated-declarations \
+     -Wno-incompatible-pointer-types \
+    "
 
 def setup():
-    pisitools.cflags.add(w)
+    pisitools.cflags.add(j)
+    pisitools.cxxflags.add("-Wno-deprecated-declarations -Wno-vla")
+
+    autotools.autoreconf("-fiv")
     autotools.configure("--disable-oss")
 
     pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
