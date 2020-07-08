2d0c79917117226d1903af3c4ddc8fa4c33f8ffc
Rmys <muscnsl@gmail.com>
2020-07-08 17:39:39 +0000

    smtube rebuild

--- multimedia/video/smtube/actions.py
+++ multimedia/video/smtube/actions.py
@@ -6,14 +6,20 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
+from pisi.actionsapi import qt5
 from pisi.actionsapi import pisitools
 
 def setup():
-    pisitools.dosed("Makefile", "^DOC_PATH=.*$", "DOC_PATH=$(PREFIX)/share/doc/smplayer")
+    #pisitools.dosed("Makefile", "^DOC_PATH=.*$", "DOC_PATH=$(PREFIX)/share/doc/smplayer")
+    pisitools.dosed("Makefile", "PREFIX=/usr/local", "PREFIX=/usr")
 
 def build():
-    autotools.make("PREFIX=/usr QMAKE=/usr/bin/qmake LRELEASE=/usr/bin/lrelease")
+    qt5.make()
+    #autotools.make("PREFIX=/usr QMAKE=/usr/bin/qmake LRELEASE=/usr/bin/lrelease")
 
 def install():
-    autotools.rawInstall("PREFIX=/usr DESTDIR=%s DOC_PATH=/usr/share/doc/%s" % (get.installDIR(),get.srcNAME()))
-    pisitools.dodoc("Readme.txt","Release_notes.txt","Changelog","Copying.txt") 
+    qt5.install()
+    #autotools.rawInstall("PREFIX=/usr DESTDIR=%s DOC_PATH=/usr/share/doc/%s" % (get.installDIR(),get.srcNAME()))
+    pisitools.dodoc("Readme.txt","Release_notes.txt","Changelog","Copying.txt")
+    
+    pisitools.dosed("%s/usr/share/applications/smtube.desktop" % get.installDIR(), "Icon=smtube", "Icon=smtube.png")
