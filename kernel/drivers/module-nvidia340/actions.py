53ab124aab3a679d2be0fb3a2bfdcb0fdf602f86
Rmys <muscnsl@hotmail.com>
2020-08-06 18:39:21 +0300

    nvidia rebuild

--- kernel/drivers/module-nvidia340/actions.py
+++ kernel/drivers/module-nvidia340/actions.py
@@ -29,9 +29,10 @@ def setup():
     
     #5.5 patch icin
     shelltools.move("tmp/.manifest", ".")
-    shelltools.system("patch -p1 < kernel-5.5.patch")
-    shelltools.system("patch -p1 < kernel-5.6.patch")
-    shelltools.system("patch -p1 < unfuck-340.108-build-fix.patch")
+    #shelltools.system("patch -p1 < kernel-5.5.patch")
+    #shelltools.system("patch -p1 < kernel-5.6.patch")
+    #shelltools.system("patch -p1 < unfuck-340.108-build-fix.patch")
+    shelltools.system("patch -p1 < kernel-5.7.patch")
     
 
     # Our libc is TLS enabled so use TLS library
