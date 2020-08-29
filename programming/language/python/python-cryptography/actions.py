7dd49cc36cfa309e6bb923e2be4e1f0db95cb2f2
blue-devil <bluedevil@sctzine.com>
2020-08-30 01:20:08 +0200

    python-cryptography:ver bump

--- programming/language/python/python-cryptography/actions.py
+++ programming/language/python/python-cryptography/actions.py
@@ -4,14 +4,14 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import pythonmodules
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pythonmodules
 
 def build():
     # suppress compiler warnings
-    pisitools.cflags.add("-Wno-stringop-truncation -Wno-deprecated-declarations -Wno-incompatible-pointer-types -Wno-discarded-qualifiers -Wno-sign-conversion -Wno-sign-compare")
+    pisitools.cflags.add("-Wno-stringop-truncation -Wno-deprecated-declarations -Wno-incompatible-pointer-types -Wno-discarded-qualifiers -Wno-sign-conversion -Wno-sign-compare -Wno-unused-function")
     # fix unused direct dependency analysis
     shelltools.export("LDSHARED", "x86_64-pc-linux-gnu-gcc -Wl,-O1,--as-needed -shared -lpthread -Wl,-O1 -Wl,-z,relro -Wl,--hash-style=gnu -Wl,--as-needed -Wl,--sort-common")
     pythonmodules.compile()
