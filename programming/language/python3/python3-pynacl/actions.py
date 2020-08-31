5aea14ab1b8282f490c287010eda038092c1fb58
blue-devil <bluedevil@sctzine.com>
2020-08-31 23:47:15 +0200

    python3-pynacl:ver bump

--- programming/language/python3/python3-pynacl/actions.py
+++ programming/language/python3/python3-pynacl/actions.py
@@ -4,8 +4,8 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import pythonmodules
 
 def build():
     # fix unused direct dependency analysis
