d01a876c1b530ac4e0fe3c2c92abb0aa306d2a76
blue-devil <bluedevil@sctzine.com>
2020-08-31 23:46:13 +0200

    python3-py:ver bump

--- programming/language/python3/python3-py/actions.py
+++ programming/language/python3/python3-py/actions.py
@@ -4,10 +4,10 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import pythonmodules
 
-shelltools.export("SETUPTOOLS_SCM_PRETEND_VERSION","1.8.1")
+shelltools.export("SETUPTOOLS_SCM_PRETEND_VERSION","1.9.0")
 
 def build():
     pythonmodules.compile(pyVer="3")
