d7bb210184d4f9472adff5b00eef35aa5715939f
Erkan IŞIK <erkanisik@yahoo.com>
2020-09-12 12:59:46 +0300

    yali version bump

--- system/installer/yali/actions.py
+++ system/installer/yali/actions.py
@@ -8,9 +8,9 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import pythonmodules
 
 def setup():
-    repo_uri = "https://ciftlik.pisilinux.org/2.0/pisi-index.xml.xz" # FIXME
+    repo_uri = "https://ciftlik.pisilinux.org/2.0-Beta.1/pisi-index.xml.xz" # FIXME
     pisitools.dosed("yali/constants.py", "@REPO_URI@", repo_uri)
-    pisitools.dosed("yali/constants.py", "@REPO_NAME@", "Stable") # FIXME
+    pisitools.dosed("yali/constants.py", "@REPO_NAME@", "Beta") # FIXME
 
     pisitools.dosed("conf/yali.conf", "@INSTALL_TYPE@", "default")
 
