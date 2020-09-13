844343d4a4fb5aac3e617367785affb0e9a59cd7
4fury-c3440d8 <icu7c1@gmail.com>
2020-09-13 21:34:00 +0300

    musepack, libmpcdec SV8, removed SV7.

--- multimedia/sound/libreplaygain/actions.py
+++ multimedia/sound/libreplaygain/actions.py
@@ -2,16 +2,17 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import autotools
-import os
+from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--disable-static")
+	autotools.configure()
 
 def build():
-    autotools.make()
+	autotools.make()
 
 def install():
-    autotools.install()
+	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
