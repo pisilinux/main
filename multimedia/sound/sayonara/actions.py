c3a170040146210b23058a52fe5a6c09568eed17
4fury-c3440d8 <icu7c1@gmail.com>
2020-09-15 10:58:55 +0300

    yarock, taglib, sayonara.

--- multimedia/sound/sayonara/actions.py
+++ multimedia/sound/sayonara/actions.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import kde5
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import kde5
 from pisi.actionsapi import get
 
 def setup():
-    kde5.configure()
+    kde5.configure("-DWITH_SYSTEM_TAGLIB=ON")
 
 def build():
     kde5.make()
@@ -19,7 +19,7 @@ def install():
 
     pisitools.remove("usr/share/icons/sayonara.png")
     pisitools.dodoc("CHANGES", \
-    "COPYING", \
     "LICENSE", \
     "MANUAL", \
-    "README.txt")
+    "README.md")
+
