2e1ec8b246c2fa158626d24ee53637066572d895
blue-devil <bluedevil@sctzine.com>
2020-09-16 02:17:28 +0200

    darktable:verbump

--- multimedia/graphics/darktable/actions.py
+++ multimedia/graphics/darktable/actions.py
@@ -4,24 +4,26 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from pisi.actionsapi import cmaketools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import cmaketools
 
 def setup():
     pisitools.ldflags.add("-lgs")
     shelltools.makedirs("build")
     shelltools.cd("build")
-    cmaketools.configure("-DCMAKE_INSTALL_LIBDIR=lib \
-                          -DDONT_INSTALL_GCONF_SCHEMAS=1 \
-                          -DCMAKE_BUILD_TYPE=Release \
+    cmaketools.configure("-DUSE_COLORD=On \
+                          -DUSE_OPENJPEG=On \
+                          -DUSE_LIBSECRET=On \
+                          -DBUILD_USERMANUAL=0 \
                           -DBINARY_PACKAGE_BUILD=1 \
                           -DBUILD_USERMANUAL=False \
-                          -DUSE_LIBSECRET=On \
-                          -DUSE_OPENJPEG=On \
-                          -DUSE_COLORD=On \
-                          -DBUILD_USERMANUAL=0", sourceDir="..")
+                          -DCMAKE_BUILD_TYPE=Release \
+                          -DCMAKE_SKIP_BUILD_RPATH=ON \
+                          -DCMAKE_INSTALL_LIBDIR=/usr/lib \
+                          -DCMAKE_INSTALL_LIBEXECDIR=/usr/lib \
+                          -DDONT_INSTALL_GCONF_SCHEMAS=1", sourceDir="..")
 
 def build():
     shelltools.cd("build")
@@ -31,3 +33,6 @@ def build():
 def install():
     shelltools.cd("build")
     cmaketools.install()
+    
+    # fix libdarktable.so path under /usr/lib
+    pisitools.dosym("/usr/lib/darktable/libdarktable.so", "/usr/lib/libdarktable.so")
