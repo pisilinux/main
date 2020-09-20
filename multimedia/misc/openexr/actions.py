0b78a9f8cfa83c2679543752d0f0e0b84b2a4ad7
Rmys <muscnsl@gmail.com>
2020-09-20 18:44:33 +0300

    openexr-2.5.3

--- multimedia/misc/openexr/actions.py
+++ multimedia/misc/openexr/actions.py
@@ -12,6 +12,8 @@ from pisi.actionsapi import get
 def setup():
     pisitools.flags.add("-pthread -I/usr/include/OpenEXR -I/usr/include/libdrm")
     pisitools.ldflags.add("-lImath -lHalf -lIex -lIexMath -lIlmThread -lpthread")
+    
+    shelltools.cd("OpenEXR")
     shelltools.system("./bootstrap")
     autotools.configure("--enable-shared \
                          --disable-static")
@@ -19,12 +21,15 @@ def setup():
     pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
+    shelltools.cd("OpenEXR")
     autotools.make()
 
 def install():
+    shelltools.cd("OpenEXR")
     # documents and examples go to "/usr/share/OpenEXR" without these parameters
     docdir = "/usr/share/doc/%s" % get.srcNAME()
     examplesdir = "%s/examples" % docdir
     autotools.rawInstall("DESTDIR=%s docdir=%s examplesdir=%s" % (get.installDIR(), docdir, examplesdir))
-
-    pisitools.dodoc("AUTHORS", "ChangeLog","NEWS", "README","LICENSE")
+    
+    shelltools.cd("..")
+    pisitools.dodoc("README*","LICENSE*")
