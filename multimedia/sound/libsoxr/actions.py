647cd353454edaade75a8ac152e952d414ea514f
4fury-c3440d8 <icu7c1@gmail.com>
2020-09-22 17:35:17 +0300

    libsoxr.

--- multimedia/sound/libsoxr/actions.py
+++ multimedia/sound/libsoxr/actions.py
@@ -2,37 +2,36 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/copyleft/gpl.txt 
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-	shelltools.system("mkdir build_")
-	shelltools.cd("build_")
+    shelltools.system("mkdir build_")
+    shelltools.cd("build_")
 
-	shelltools.system("cmake .. \
-		              -DCMAKE_INSTALL_PREFIX=/usr \
-		              -DCMAKE_BUILD_TYPE=Release \
-		              -DCMAKE_INSTALL_LIBDIR=lib \
-		              -DBUILD_EXAMPLES='OFF' \
-		              -DBUILD_SHARED_LIBS='ON' \
-		              -DWITH_AVFFT='OFF' \
-		              -DWITH_LSR_BINDINGS='ON' \
-		              -DWITH_PFFFT='ON' \
-		              -DWITH_OPENMP='ON'")
+    shelltools.system("cmake .. \
+                      -DCMAKE_INSTALL_PREFIX=/usr \
+                      -DCMAKE_BUILD_TYPE=Release \
+                      -DCMAKE_INSTALL_LIBDIR=lib \
+                      -DBUILD_EXAMPLES='OFF' \
+                      -DBUILD_SHARED_LIBS='ON' \
+                      -DWITH_AVFFT='OFF' \
+                      -DWITH_LSR_BINDINGS='ON' \
+                      -DWITH_PFFFT='ON' \
+                      -DWITH_OPENMP='ON'")
 
 def build():
-	shelltools.cd("build_")
-	cmaketools.make()
+    shelltools.cd("build_")
+    cmaketools.make()
 
 def install():
-	shelltools.cd("build_")
-	autotools.rawInstall("DESTDIR=%s" %get.installDIR())
-	
-	shelltools.cd("..")
-	pisitools.dodoc("AUTHORS", "COPYING.LGPL", "LICENCE", "NEWS", "README")
+    shelltools.cd("build_")
+    autotools.rawInstall("DESTDIR=%s" %get.installDIR())
+
+    shelltools.cd("..")
+    pisitools.dodoc("AUTHORS", "COPYING.LGPL", "LICENCE", "NEWS", "README")
 
