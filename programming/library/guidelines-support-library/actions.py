4d3969051079b3aa5200968c831282b883f5ed1c
bluedevil <bluedevil@sctzine.com>
2020-09-24 00:02:05 +0200

    guidelines-support-library: major version bump

--- programming/library/guidelines-support-library/actions.py
+++ programming/library/guidelines-support-library/actions.py
@@ -4,28 +4,27 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import cmaketools, shelltools
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import shelltools
 
 
 def setup():
     parameters = ' '.join([
-        '-B build',
-        '-DGSL_TEST=OFF',
-        '-DCMAKE_EXE_LINKER_FLAGS="%s"' % get.LDFLAGS(),
-        '-DCMAKE_BUILD_TYPE=Release'
-    ])
+                 '-B build',
+                 '-DGSL_TEST=OFF',
+                 '-DCMAKE_BUILD_TYPE=Release'
+                 '-DCMAKE_EXE_LINKER_FLAGS="%s"' % get.LDFLAGS(),
+                 ])
     cmaketools.configure(parameters)
 
-
 def build():
-    shelltools.cd('build')
+    shelltools.cd("build")
     cmaketools.make()
 
-
 def install():
-    shelltools.cd('build')
+    shelltools.cd("build")
     cmaketools.install()
 
-    pisitools.dodoc('../LICENSE')
+    pisitools.dodoc("../LICENSE", "../README*")
