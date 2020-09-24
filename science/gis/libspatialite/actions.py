627fd50d081449be9ec1b6d93737c8a000d172e3
bluedevil <bluedevil@sctzine.com>
2020-09-24 23:43:47 +0200

    libspatialite: ver bump

--- science/gis/libspatialite/actions.py
+++ science/gis/libspatialite/actions.py
@@ -3,21 +3,23 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
-from pisi.actionsapi import autotools
 from pisi.actionsapi import get
+from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
     autotools.configure("--disable-static \
+                         --enable-libxml2 \
                          --disable-geosadvanced")
     
+    # fix unused direct dependency analysis
     pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+
 
 def build():
     autotools.make()
 
+
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.dodoc("AUTHORS", "COPYING", "README", "README.coverage")
-
-# By PiSiDo 2.0.0
+    pisitools.dodoc("COPYING", "README", "README.coverage")
