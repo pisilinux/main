74623096f7c531dae2332b23785dac7610e93da0
bluedevil <bluedevil@sctzine.com>
2020-09-24 20:45:38 +0200

    python3-pytest-runner: first rel

---
+++ programming/language/python3/python3-pytest-runner/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pythonmodules
+
+
+shelltools.export("SETUPTOOLS_SCM_PRETEND_VERSION","5.2")
+
+
+def build():
+    pythonmodules.compile(pyVer="3")
+
+
+def install():
+    pythonmodules.install(pyVer="3")
