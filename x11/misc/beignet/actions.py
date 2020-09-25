2ffc37c8146d89974b966f8695eda44d32ef6066
4fury-c3440d8 <icu7c1@gmail.com>
2020-09-25 16:01:30 +0300

    homepages.

--- x11/misc/beignet/actions.py
+++ x11/misc/beignet/actions.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 TUBITAK/UEKAE
-# Licensed under the GNU General Public License, version 2.
-# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
@@ -12,19 +11,18 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-	shelltools.export("CC", "clang")
-	shelltools.export("CXX", "clang++")
-    
-	cmaketools.configure("-DCMAKE_INSTALL_LIBDIR=lib \
-						  -DOCLICD_COMPAT=1")
-    
+    shelltools.export("CC", "clang")
+    shelltools.export("CXX", "clang++")
+
+    cmaketools.configure("-DCMAKE_INSTALL_LIBDIR=lib \
+                          -DOCLICD_COMPAT=1")
+
 def build():
     cmaketools.make()
 
 def install():
-    
+
     autotools.rawInstall("DESTDIR=%s" %get.installDIR())
     pisitools.remove("/usr/include/CL/*")
     pisitools.removeDir("/usr/include")
-    
-     
+
