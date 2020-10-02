d75b014bb1d308b971977686a11bb0fbfa96433d
bluedevil <bluedevil@sctzine.com>
2020-10-03 00:16:42 +0200

    opencascade:first rel

---
+++ science/misc/opencascade/actions.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import cmaketools
+
+def setup():
+    options_cfg = ''.join([
+                  '-DUSE_TBB=ON ',
+                  '-DUSE_VTK=OFF ',
+                  '-DUSE_GL2PS=ON ',
+                  '-DUSE_FFMPEG=ON ',
+                  '-DUSE_FREEIMAGE=OFF ',
+                  'BUILD_TYPE=Release ',
+                  '-DCMAKE_INSTALL_PREFIX=/usr',
+                  ])
+    shelltools.makedirs("build")
+    shelltools.cd("build")
+    cmaketools.configure("%s" % options_cfg, sourceDir="..")
+
+def build():
+    shelltools.cd("build")
+    cmaketools.make()
+
+def install():
+    shelltools.cd("build")
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    
+    shelltools.cd("..")
+
+    pisitools.dodoc("LICENSE*", "OCCT_LGPL*")
