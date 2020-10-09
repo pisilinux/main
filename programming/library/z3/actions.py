226159bd5bee539c52ce159c7ce9b01d58ba24f0
4fury-c3440d8 <icu7c1@gmail.com>
2020-10-09 03:43:53 +0300

    cppcheck: version bump, add z3.

---
+++ programming/library/z3/actions.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+j = "-DCMAKE_INSTALL_PREFIX=/usr \
+     -DCMAKE_INSTALL_LIBDIR=lib \
+     -DPYTHON_EXECUTABLE=/usr/bin/python3 \
+     -DZ3_USE_LIB_GMP=True \
+     -DZ3_LINK_TIME_OPTIMIZATION=True \
+     -DZ3_BUILD_LIBZ3_SHARED=True \
+     -DZ3_BUILD_PYTHON_BINDINGS=True \
+     -DZ3_INSTALL_PYTHON_BINDINGS=True \
+    "
+
+shelltools.export("PYTHON", "/usr/bin/python3")
+
+def setup():
+	shelltools.makedirs("build")
+	shelltools.cd("build")
+	cmaketools.configure("%s -G 'Ninja'" % j, sourceDir = '..')
+
+def build():
+	shelltools.system("ninja -C build")
+
+def install():
+	shelltools.system("DESTDIR=%s ninja -C build install" % get.installDIR())
+
+	pisitools.dodoc("LICENSE.txt", "README*", "RELEASE_NOTES")
+
--- programming/tool/cppcheck/actions.py
+++ programming/tool/cppcheck/actions.py
@@ -9,27 +9,32 @@ from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-j = "-DHAVE_RULES=ON \
+j = "-DUSE_Z3=ON \
+     -DHAVE_RULES=ON \
      -DBUILD_GUI=ON \
-     -DWITH_QCHART=ON \
-     -DENABLE_CHECK_INTERNAL=ON -L \
+     -DBUILD_SHARED_LIBS=ON \
+     -DBUILD_TESTS=OFF \
+     -DCMAKE_BUILD_TYPE=Release \
+     -DCMAKE_DISABLE_PRECOMPILE_HEADERS=OFF \
+     -DENABLE_CHECK_INTERNAL=ON \
+     -DWITH_QCHART=ON -L \
     "
 
 def setup():
 	shelltools.makedirs("build")
 	shelltools.cd("build")
-	cmaketools.configure("-DUSE_Z3=OFF -DBUILD_TESTS=OFF %s" % j, sourceDir = '..')
+	cmaketools.configure(j, sourceDir = '..')
 
 def build():
 	shelltools.cd("build")
 	cmaketools.make()
 
-#def check():
-#	cmaketools.make("test")
+def check():
+	pass
 
 def install():
 	shelltools.cd("build")
 	cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-	pisitools.dodoc("../AUTHORS", "../COPYING", "../readme*")
+	pisitools.dodoc("../AUTHORS", "../COPYING", "philosophy.md", "../readme.md")
 
