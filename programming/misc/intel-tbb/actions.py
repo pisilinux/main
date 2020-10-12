189ec90f01751f5dd196d1a0278edc9a3ed9a5ac
Rmys <muscnsl@gmail.com>
2020-10-12 13:25:59 +0300

    intel-tbb

--- programming/misc/intel-tbb/actions.py
+++ programming/misc/intel-tbb/actions.py
@@ -11,7 +11,7 @@ from pisi.actionsapi import shelltools
 
 
 #WorkDir="tbb%soss" % (get.srcVERSION().replace(".", ""))
-WorkDir="tbb-%s" % get.srcVERSION()
+WorkDir="oneTBB-%s" % get.srcVERSION()
 
 
 def build():
