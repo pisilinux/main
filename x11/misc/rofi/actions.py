78215047cabc90d9982deef3d69f8350607f4120
4fury-c3440d8 <unsetmeintheworld@gmail.com>
2020-11-20 00:38:57 +0300

    rofi.

--- x11/misc/rofi/actions.py
+++ x11/misc/rofi/actions.py
@@ -2,11 +2,10 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
