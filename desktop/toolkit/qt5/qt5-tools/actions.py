aae696afd4206206db8dfaf454fc4d2f822a5597
Rmys <muscnsl@gmail.com>
2020-11-27 15:18:27 +0300

    qt5-tools rebuild

--- desktop/toolkit/qt5/qt5-tools/actions.py
+++ desktop/toolkit/qt5/qt5-tools/actions.py
@@ -11,8 +11,6 @@ from pisi.actionsapi import qt5
 from pisi.actionsapi import get
 
 def setup():
-    #shelltools.export("CC", "clang -fuse-ld=lld")
-    #shelltools.export("CXX", "clang++ -fuse-ld=lld")
     qt5.configure()
 
 def build():
