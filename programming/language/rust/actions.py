438c7c562f0c837d8d6a56d64287a5ead0411fe1
Idris Kalp <idriskalp@gmail.com>
2020-12-01 03:31:34 +0300

    rust 1.48.0, phase 1

--- programming/language/rust/actions.py
+++ programming/language/rust/actions.py
@@ -11,13 +11,35 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 WorkDir = "rustc-%s-src" %get.srcVERSION()
-crt_ver = "11.0.0"
+
+### ÖNEMLİ NOT: ###
+#LLVM yükseltmelerinden sonra, ilk aşama olarak rust, manuel indirdiğimiz binary rust paketi ile derlenecek. Bu birinci aşama olacak.
+#İkinci aşamada ise derlediğimiz bu rust ile rust tekrar derlenecek.
+#İkinci aşamada aşağıdaki satırlar yoruma çevrilecek.
+
+### IMPORTANT NOTE ###
+#After upgrading LLVM, rust will be built via manually downloaded binary rust distro. This will be phase 1.
+#At phase 2, rust will be built again with this newly built rust.
+#Before phase 2, the lines below will be commented out.
+
+RustBinDir = "rust-%s-x86_64-unknown-linux-gnu" %get.srcVERSION()
+RustcDir = "%s/rustbin" %get.workDIR()
+
+#################################################################
 
 def build():
+    #İkinci aşamada bu satırlar da yoruma çevrilecek.
+    #At phase 2, these lines also will be commented out.
+    shelltools.cd(RustBinDir)
+    shelltools.system("./install.sh --prefix=%s" %RustcDir)
+    shelltools.cd("..")
+    pisitools.dosed("config.toml", "/usr/bin/cargo", RustcDir+"/bin/cargo")
+    pisitools.dosed("config.toml", "/usr/bin/rustc", RustcDir+"/bin/rustc")
+    ####################################################
+    
     shelltools.export("LC_ALL", "en_US.UTF-8")
     shelltools.export("RUST_BACKTRACE", "1")
-    shelltools.export("RUST_COMPILER_RT_ROOT", "%s/%s/compiler-rt-%s.src" % (get.workDIR(), WorkDir, crt_ver))
-    shelltools.system("python ./x.py build")
+    shelltools.system("python ./x.py dist")
     
 
 def install():
