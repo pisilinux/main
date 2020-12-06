690583ecd2b9dc09514806c46c5306b46f98fbeb
Kamil ATLI <suvarice@gmail.com>
2020-12-06 12:52:58 +0300

    soundconverter: fix pyver

--- multimedia/converter/soundconverter/actions.py
+++ multimedia/converter/soundconverter/actions.py
@@ -8,10 +8,10 @@ from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import pisitools
 
 def build():
-	pythonmodules.compile(pyVer = '3')
+	pythonmodules.compile(pyVer="3")
 
 def install():
-	pythonmodules.install(pyVer = '3')
+	pythonmodules.install(pyVer="3")
 
 	pisitools.dodoc("AUTHORS", "CHANGELOG", "README.md")
 
