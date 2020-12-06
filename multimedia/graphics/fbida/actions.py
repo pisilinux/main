afa6620dfefed6e669fc719b946d2054455cf4fd
4fury-c3440d8 <unsetmeintheworld@gmail.com>
2020-12-06 18:03:00 +0000

    fbida.

--- multimedia/graphics/fbida/actions.py
+++ multimedia/graphics/fbida/actions.py
@@ -4,7 +4,7 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import mesontools
+#from pisi.actionsapi import mesontools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
