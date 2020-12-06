a7b58d467edbd7f51ae4cc2abb592a0a7b8d0a50
Rmys <muscnsl@gmail.com>
2020-12-06 21:25:06 +0300

    xfce4-datetime-plugin

--- desktop/xfce/addon/xfce4-datetime-plugin/actions.py
+++ desktop/xfce/addon/xfce4-datetime-plugin/actions.py
@@ -9,9 +9,15 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-	autotools.configure("--disable-static")
-
-	pisitools.dosed("libtool", " -shared ", " -Wl,--as-needed -shared ")
+    # fix build failure w/ xfce4-panel-4.15.0
+    pisitools.dosed("panel-plugin/datetime.c", "<libxfce4panel/xfce-panel-plugin.h>", "<libxfce4panel/libxfce4panel.h>")
+    pisitools.dosed("panel-plugin/datetime.c", "xfce_create_panel_toggle_button", "xfce_panel_create_toggle_button")
+    pisitools.dosed("panel-plugin/datetime.c", "panel_slice", "g_slice")
+    pisitools.dosed("panel-plugin/datetime-dialog.c", "<libxfce4panel/xfce-panel-plugin.h>", "<libxfce4panel/libxfce4panel.h>")
+    autotools.configure("--disable-static")
+    
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,--as-needed -shared ")
 
 def build():
 	autotools.make()
