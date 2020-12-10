b35f0b6f635a61597a0f8ac69f8fea4f1d1cf6a4
alick01 <alicengizkurt@gmail.com>
2020-12-10 11:45:42 +0300

    clamav actions correction

--- util/antivirus/clamav/actions.py
+++ util/antivirus/clamav/actions.py
@@ -12,7 +12,7 @@ from pisi.actionsapi import shelltools
 def setup():
     #autotools.autoreconf("-fiv")
     autotools.configure("--prefix=/usr \
-                         -sbindir=/usr/bin \
+                         --sbindir=/usr/bin \
                          --enable-id-check \
                          --enable-clamdtop \
                          --disable-static \
