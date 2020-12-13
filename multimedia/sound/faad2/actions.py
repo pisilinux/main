c3e440a4113ebcc71effbeedb3fd0bb8e3d355d7
4fury-c3440d8 <unsetmeintheworld@gmail.com>
2020-12-13 08:50:58 +0000

    music player daemon.

--- multimedia/sound/faad2/actions.py
+++ multimedia/sound/faad2/actions.py
@@ -4,21 +4,20 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    #pisitools.dosed("libfaad/Makefile.am", "iquote ", "I")
-    autotools.autoreconf("-vfi")
-
-    autotools.configure("--without-xmms \
-                         --disable-static")
+	shelltools.system("./bootstrap")
+	autotools.configure("--without-xmms --disable-static")
 
 def build():
-    autotools.make()
+	autotools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README", "TODO")
+	pisitools.dodoc("AUTHORS", "ChangeLog", "README*")
+
---
+++ multimedia/stream/mpd/actions.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
+
+from pisi.actionsapi import mesontools
+from pisi.actionsapi import pisitools
+
+def setup():
+#	pisitools.dosed("doc/meson.build", "sphinx-build", "sphinx-build3")
+	pisitools.ldflags.add("-lbsd")
+	pisitools.cxxflags.add("-lbsd")
+	mesontools.configure("-Ddocumentation=enabled -Dfifo=false \
+	\
+	-Dtcp=true \
+	-Ddsd=true \
+	-Dcue=true \
+	-Dpipe=true \
+	-Dhttpd=true \
+	-Depoll=true \
+	-Ddaemon=true \
+	-Dinotify=true \
+	-Deventfd=true \
+	-Dsignalfd=true \
+	-Drecorder=true \
+	-Ddatabase=true \
+	-Dneighbor=true \
+	-Dwave_encoder=true \
+	-Dlocal_socket=true \
+	\
+	-Dao=enabled \
+	-Dgme=enabled \
+	-Dmad=enabled \
+	-Dmms=enabled \
+	-Dnfs=enabled \
+	-Dicu=enabled \
+	-Doss=enabled \
+	-Dfaad=enabled \
+	-Dflac=enabled \
+	-Dipv6=enabled \
+	-Dupnp=enabled \
+	-Dcurl=enabled \
+	-Dalsa=enabled \
+	-Dopus=enabled \
+	-Dlame=enabled \
+	-Djack=enabled \
+	-Dsoxr=enabled \
+	-Ddbus=enabled \
+	-Dpcre=enabled \
+	-Dyajl=enabled \
+	-Dzlib=enabled \
+	-Dzzip=enabled \
+	-Dqobuz=enabled \
+	-Dtidal=enabled \
+	-Diconv=enabled \
+	-Dshout=enabled \
+	-Dbzip2=enabled \
+	-Dshine=enabled \
+	-Dpulse=enabled \
+	-Dexpat=enabled \
+	-Dopenal=enabled \
+	-Dmpg123=enabled \
+	-Dvorbis=enabled \
+	-Dudisks=enabled \
+	-Dwebdav=enabled \
+	-Dmpcdec=enabled \
+	-Dffmpeg=enabled \
+	-Dmikmod=enabled \
+	-Did3tag=enabled \
+	-Dsqlite=enabled \
+	-Dmodplug=enabled \
+	-Dwavpack=enabled \
+	-Dtwolame=enabled \
+	-Diso9660=enabled \
+	-Dsndfile=enabled \
+	-Dsmbclient=enabled \
+	-Daudiofile=enabled \
+	-Dvorbisenc=enabled \
+	-Dfluidsynth=enabled \
+	-Dsoundcloud=enabled \
+	-Dchromaprint=enabled \
+	-Dlibmpdclient=enabled \
+	-Dlibsamplerate=enabled \
+	-Dcdio_paranoia=enabled \
+	-Dsolaris_output=enabled \
+	\
+	-Dzeroconf=auto \
+	\
+	-Dsndio=disabled \
+	-Dadplug=disabled \
+	-Dtremor=disabled \
+	-Dsidplay=disabled \
+	-Dsystemd=disabled \
+	-Dwildmidi=disabled")
+
+def build():
+	mesontools.build()
+
+def install():
+	mesontools.install()
+	pisitools.insinto("/etc", "doc/mpdconf.example", "mpd.conf")
+
+	pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README.md")
+
