1023d5517a92b5b7cf2a62209fdd226247ddbf63
4fury-c3440d8 <icu7c1@gmail.com>
2021-01-02 09:35:59 +0000

    texlive-core.

--- editor/tex/texlive-core/actions.py
+++ editor/tex/texlive-core/actions.py
@@ -4,9 +4,9 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 
 WorkDir = "."
 revnr = get.srcVERSION().split(".")[1]
@@ -25,11 +25,15 @@ def install():
     # prepare and install installed packs list
     pisitools.dodir("/var/lib/texmf/pisilinux/installedpacks")
     pisitools.dosed("CONTENTS", "^#", deleteLine=True)
-    pisitools.insinto("/var/lib/texmf/pisilinux/installedpkgs", "CONTENTS", "%s_%s.list" % (get.srcNAME(), revnr))
+    pisitools.insinto("/var/lib/texmf/pisilinux/installedpacks", "CONTENTS", "%s_%s.list" % (get.srcNAME(), revnr))
 
     for i in shelltools.ls("texmf-dist"):
         shelltools.copytree("texmf-dist/%s/" % i, "%s/usr/share/texmf-dist/" % get.installDIR())
     shelltools.system("find texmf-dist -type f -executable -exec chmod 755 %s/usr/share/{} \;" % get.installDIR())
+
+#    for i in shelltools.ls("."):
+#        if shelltools.isDirectory(i) and not i.startswith("texmf"):
+#            shelltools.copytree(i, "%s/usr/share/texmf-dist/" % get.installDIR())
 
     for i in ["tlpkg"]:
         shelltools.copytree("%s/" % i, "%s/usr/share/" % get.installDIR())
