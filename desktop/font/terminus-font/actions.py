16b10e66e6abf92890d9d0087396401589d275e4
4fury-c3440d8 <icu7c1@gmail.com>
2021-01-02 13:24:49 +0000

    no otb.

--- desktop/font/terminus-font/actions.py
+++ desktop/font/terminus-font/actions.py
@@ -11,16 +11,14 @@ from pisi.actionsapi import get
 
 def setup():
 	pisitools.dosed("75-yes-terminus.conf", "Term", "xos4 Term")
-	autotools.configure("--x11dir=/usr/share/fonts/misc --otbdir=/usr/share/fonts/misc --psfdir=/usr/share/kbd/consolefonts")
+	autotools.configure("--x11dir=/usr/share/fonts/misc --psfdir=/usr/share/kbd/consolefonts")
 
 def build():
-	autotools.make("all otb")
+	autotools.make()
 
 def install():
 	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 	pisitools.insinto("/etc/fonts/conf.avail", "75-yes-terminus.conf", "75-yes-terminus.conf")
-	for t in shelltools.ls("*.otb"):
-		pisitools.insinto("/usr/share/fonts/misc", t)
 	pisitools.dosym("../conf.avail/75-yes-terminus.conf", "/etc/fonts/conf.d/75-yes-terminus.conf")
 
 	pisitools.dodoc("AUTHORS", "CHANGES", "README")
