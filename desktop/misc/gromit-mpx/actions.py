9e827701cd8dca9926feb55f441be473cb6b37d6
4fury-c3440d8 <icu7c1@gmail.com>
2021-01-02 12:28:52 +0000

    gromit-mpx.

---
+++ desktop/misc/gromit-mpx/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+	shelltools.makedirs("build")
+	shelltools.cd("build")
+	cmaketools.configure("-DCMAKE_INSTALL_SYSCONFDIR=/etc -L", sourceDir = '..')
+
+def build():
+	shelltools.cd("build")
+	cmaketools.make()
+
+def install():
+	shelltools.cd("build")
+	cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+	pisitools.dodoc("../AUTHORS", "../ChangeLog", "../NEWS.md", "../README.md")
+
---
+++ programming/library/libappindicator/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
+
+from pisi.actionsapi import autotools, pisitools, get
+
+def setup():
+	pisitools.dosed("src/Makefile.am", "-Werror", "")
+	autotools.autoreconf("-fiv")
+	autotools.configure("--with-gtk=3 --disable-static")
+
+	pisitools.dosed("libtool", " -shared ", " -Wl,--as-needed -shared ")
+
+def build():
+	autotools.make()
+
+def install():
+	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+	pisitools.dodoc("AUTHORS", "ChangeLog")
+
