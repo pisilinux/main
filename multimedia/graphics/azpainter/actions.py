530e8435e9db9af01cd74a782bb95bca16b17de2
4fury-c3440d8 <icu7c1@gmail.com>
2021-01-02 12:11:05 +0000

    azpainter.

--- multimedia/graphics/azpainter/actions.py
+++ multimedia/graphics/azpainter/actions.py
@@ -17,5 +17,5 @@ def build():
 def install():
 	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-	pisitools.dodoc("AUTHORS", "ChangeLog", "README")
+	pisitools.dodoc("AUTHORS", "ChangeLog", "ReadMe*")
 
