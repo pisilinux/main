48317ce33b54300e8fc2b0d07bb27e81e02f32ea
Rmys <muscnsl@gmail.com>
2021-01-07 15:35:14 +0300

    xorg-video-radeon rebuild

--- x11/driver/xorg-video-radeon/actions.py
+++ x11/driver/xorg-video-radeon/actions.py
@@ -6,8 +6,13 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
 def setup():
+    pisitools.cflags.add("-fcommon -fno-plt")
+    shelltools.export("CXXFLAGS", "-fno-plt")
+    pisitools.ldflags.add("-z,now")
     autotools.configure("--enable-glamor")
     
     pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
