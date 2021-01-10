baa9881b557af1a3b026476b3ad75d32e9ab81fb
4fury-c3440d8 <icu7c1@gmail.com>
2021-01-10 00:04:21 +0000

    deadbeef.

--- multimedia/sound/deadbeef/actions.py
+++ multimedia/sound/deadbeef/actions.py
@@ -4,32 +4,16 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-j = "-Wno-multichar \
-     -Wno-parentheses \
-     -Wno-unused-value \
-     -Wno-unused-result \
-     -Wno-dangling-else \
-     -Wno-unused-variable \
-     -Wno-stringop-overflow \
-     -Wno-misleading-indentation \
-     -Wno-unused-but-set-variable \
-     -Wno-unused-but-set-variable \
-     -Wno-deprecated-declarations \
-     -Wno-incompatible-pointer-types \
-    "
-
 def setup():
-    pisitools.cflags.add(j)
-    pisitools.cxxflags.add("-Wno-deprecated-declarations -Wno-vla")
-
-    autotools.autoreconf("-fiv")
-    autotools.configure("--disable-oss")
-
-    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+    shelltools.export("CC", "/usr/bin/clang")
+    shelltools.export("CXX", "/usr/bin/clang++")
+    shelltools.export("LDFLAGS", "%s -fuse-ld=lld -ldl " % get.LDFLAGS())
+    autotools.configure("--disable-oss --disable-coreaudio")
 
 def build():
     autotools.make()
@@ -37,5 +21,5 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "README")
+#    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "README")
 
