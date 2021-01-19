3ecaa4a18c9fa4191b98987deb7a1648e543f7ba
4fury-c3440d8 <icu7c1@gmail.com>
2021-01-19 07:41:34 +0000

    mupodf

--- office/misc/mupdf/actions.py
+++ office/misc/mupdf/actions.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+#
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/copyleft/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 
+i = ''.join([
+    'USE_SYSTEM_LIBS=yes ',
+    'USE_SYSTEM_GLUT=no ',
+    'USE_SYSTEM_LCMS2=no ',
+    'USE_SYSTEM_GUMBO=no ',
+    'USE_SYSTEM_JBIG2DEC=no '])
+
 def build():
+    shelltools.export("CFLAGS", "%s -fcommon" % get.CFLAGS())
     shelltools.system("sed '/TOFU_CJK /c #define TOFU_CJK 1/' -i include/mupdf/fitz/config.h")
     shelltools.system("sed -i '/ttc/s/^/#/' Makefile")
-    autotools.make("prefix=/usr")
+    autotools.make("prefix=/usr %s" % i)
 
     # remove bundled packages, use our system libraries
-#    shelltools.system("rm -rf thirdparty/{curl,freeglut,freetype,harfbuzz,jbig2dec,lcms2,libjpeg,mujs,openjpeg,zlib}")
+    shelltools.system("rm -rf thirdparty/{curl,freetype,harfbuzz,libjpeg,openjpeg,zlib}")
 
 def install():
-    autotools.rawInstall("DESTDIR=%s prefix=/usr" % get.installDIR())
-    pisitools.removeDir("/usr/lib")
+    autotools.rawInstall("DESTDIR=%s prefix=/usr %s" % (get.installDIR(), i))
 
-    pisitools.dodoc("CHANGES", "README", "COPYING")
+    pisitools.dodoc("CHANGES", "COPYING", "README")
