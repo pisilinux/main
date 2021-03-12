2e3d50b7026c7fefddbd87c09fd614d5c6076ef8
Idris Kalp <idriskalp@gmail.com>
2021-03-13 01:42:57 +0300

    digikam switch to ninja

--- desktop/kde/graphics/digikam/actions.py
+++ desktop/kde/graphics/digikam/actions.py
@@ -11,20 +11,25 @@ from pisi.actionsapi import cmaketools
 #shelltools.export("HOME", get.workDIR())
 
 def setup():
-    kde5.configure("-DBUILD_TESTING=OFF \
-                    -DENABLE_KFILEMETADATASUPPORT=ON \
-                    -DENABLE_MEDIAPLAYER=ON \
-                    -DENABLE_AKONADICONTACTSUPPORT=ON \
-                    -DENABLE_MYSQLSUPPORT=ON \
-                    -DENABLE_APPSTYLES=ON \
-                    -DENABLE_QWEBENGINE=ON \
-                    -DOpenGL_GL_PREFERENCE=GLVND")
+    shelltools.makedirs("build")
+    shelltools.cd("build")
+    cmaketools.configure("-G 'Ninja' \
+                          -DBUILD_TESTING=OFF \
+                          -DENABLE_KFILEMETADATASUPPORT=ON \
+                          -DENABLE_MEDIAPLAYER=ON \
+                          -DENABLE_AKONADICONTACTSUPPORT=ON \
+                          -DENABLE_MYSQLSUPPORT=ON \
+                          -DENABLE_APPSTYLES=ON \
+                          -DENABLE_QWEBENGINE=ON \
+                          -DOpenGL_GL_PREFERENCE=GLVND" \
+                          sourceDir = '..')
 
 def build():
-    kde5.make()
+    shelltools.cd("build")
+    shelltools.system("ninja")
 
 def install():
-    kde5.install()
+    shelltools.system("DESTDIR=%s ninja install" %get.installDIR())
 
     pisitools.dodoc("COPYING", "COPYING-CMAKE-SCRIPTS", "README*", "NEWS")
     
