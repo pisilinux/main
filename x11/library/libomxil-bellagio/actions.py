d3b4e4d8a36116ef03327f193ab543d8f484084c
Rmys <muscnsl@gmail.com>
2021-03-19 18:00:46 +0300

    libomxil-bellagio

--- x11/library/libomxil-bellagio/actions.py
+++ x11/library/libomxil-bellagio/actions.py
@@ -6,11 +6,17 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
+    shelltools.system("sed -i -e 's/ -Werror//' configure.ac")
+    pisitools.cflags.add("-fcommon")
     autotools.autoreconf("-fiv")
     autotools.configure("--disable-static")
+    
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
     autotools.make()
