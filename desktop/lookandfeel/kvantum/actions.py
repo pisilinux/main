8234fa9d5cd4022299d3dc1f8f0d74ef708625ff
4fury-c3440d8 <icu7c1@gmail.com>
2021-03-20 16:33:25 +0300

    kvantum.

--- desktop/lookandfeel/kvantum/actions.py
+++ desktop/lookandfeel/kvantum/actions.py
@@ -1,12 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import qt5
 
 def setup():
