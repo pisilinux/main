be6816381f36d0c80f852378a66004bee04c7edb
Rmys <muscnsl@gmail.com>
2021-03-29 18:00:34 +0300

    plank First release

---
+++ util/dock/plank/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.system("sed 's/0.19.6/0.20/' -i configure.ac")
+    shelltools.system("env NOCONFIGURE=1 ./autogen.sh")
+    autotools.configure("--disable-appor")
+
+
+
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "COPYING*", "NEWS", "README")
