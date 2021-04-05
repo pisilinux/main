185f4e48faef5d619a76575ea065905225475d79
Ayhan Yalçınsoy <ayhanyalcinsoy@pisilinux.org>
2021-04-05 13:47:33 +0300

    glade:new package for designing gtk-app

---
+++ desktop/toolkit/glade/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import mesontools
+from pisi.actionsapi import pisitools
+
+
+def setup():
+    mesontools.configure("--prefix=/usr -Dgtk_doc=true -Dgladeui=true -Dgjs=auto")
+
+def build():
+    mesontools.build()
+
+def install():
+    mesontools.install()
+
+    pisitools.dodoc("AUTHORS", "COPYING*", "NEWS", "README*", "TODO")
