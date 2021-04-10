a75e30f47682968c8f49756649fa91289bcba71c
Rmys <muscnsl@gmail.com>
2021-04-10 14:21:21 +0300

    xfce4 add* ver. bump

--- desktop/xfce/addon/gigolo/actions.py
+++ desktop/xfce/addon/gigolo/actions.py
@@ -17,5 +17,5 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README", "THANKS", "TODO")
+    pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "THANKS", "TODO")
 
