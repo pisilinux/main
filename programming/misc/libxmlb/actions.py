106410bad8a45739c85774706d815badb42b0ffe
Rmys <muscnsl@hotmail.com>
2021-04-21 18:24:45 +0300

    imdex libxmlb libgme

--- programming/misc/libxmlb/actions.py
+++ programming/misc/libxmlb/actions.py
@@ -1,25 +1,22 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-#
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-
-from pisi.actionsapi import get
-from pisi.actionsapi import mesontools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import mesontools
+
 def setup():
-    mesontools.configure("--prefix=/usr -Dstemming=false -Dqt=true -Dvapi=true")
+    mesontools.configure("-Dgtkdoc=true \
+                         --libexecdir=/usr/lib")
 
 def build():
     mesontools.build()
-    
-##def check():
-   # mesontools.build("test")
 
 def install():
     mesontools.install()
-    pisitools.dodoc("LICENSE", "README.md")
+    pisitools.dodoc("LICENSE", "README*")
+    # remove installed tests
+    pisitools.removeDir("/usr/lib/installed-tests/")
+    pisitools.removeDir("/usr/share/installed-tests/")
