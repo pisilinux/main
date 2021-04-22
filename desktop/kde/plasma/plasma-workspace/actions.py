0ff0c692eb2737ee8a16f06eda7d84f9487a3777
Rmys <muscnsl@gmail.com>
2021-04-22 22:19:47 +0300

    plasma-workspace rebuild

--- desktop/kde/plasma/plasma-workspace/actions.py
+++ desktop/kde/plasma/plasma-workspace/actions.py
@@ -5,6 +5,7 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import kde5
+from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 
@@ -20,5 +21,8 @@ def install():
     #pisitools.dosym("/usr/lib/drkonqi", "/usr/lib/libexec/drkonqi")
     
     pisitools.dosym("/usr/bin/startplasma-x11", "usr/bin/startkde")
+    
+    #set pisi-crocus-ancyrensis photos as default wallpapers
+    pisitools.dosed("%s/usr/share/plasma/look-and-feel/org.kde.breeze.desktop/contents/defaults" % get.installDIR(), "Next", "pisi-crocus-ancyrensis")
 
     pisitools.dodoc("COPYING", "HACKING")
