09645336f0398d09fd973683f03e1c54df608c3e
Rmys <muscnsl@gmail.com>
2021-04-22 22:22:02 +0300

    breeze rebuild

--- desktop/kde/plasma/breeze/actions.py
+++ desktop/kde/plasma/breeze/actions.py
@@ -5,6 +5,7 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import kde5
+from pisi.actionsapi import kde5
 from pisi.actionsapi import pisitools
 
 def setup():
@@ -15,3 +16,7 @@ def build():
 
 def install():
     kde5.install()
+    
+    #set pisi-crocus-ancyrensis photos as default wallpapers
+    pisitools.dosed("%s/usr/share/plasma/look-and-feel/org.kde.breezedark.desktop/contents/defaults" % get.installDIR(), "Next", "pisi-crocus-ancyrensis")
+    pisitools.dosed("%s/usr/share/plasma/look-and-feel/org.kde.breezetwilight.desktop/contents/defaults" % get.installDIR(), "Next", "pisi-crocus-ancyrensis")
