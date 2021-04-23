f0a4d2fb284a9848f9022830e26c56ec15e21db9
Berk Çakar <berk2238@hotmail.com>
2021-04-24 01:12:41 +0300

    cogl: Update actions.py

--- desktop/toolkit/gtk/cogl/actions.py
+++ desktop/toolkit/gtk/cogl/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
