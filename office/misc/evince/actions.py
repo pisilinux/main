0cbcdf580480465cc43b856895a134acf796632e
Rmys <muscnsl@gmail.com>
2021-04-23 15:41:17 +0300

    evince-3.38.2

--- office/misc/evince/actions.py
+++ office/misc/evince/actions.py
@@ -1,38 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+#
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/copyleft/gpl.txt
+# See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import shelltools
+from pisi.actionsapi import mesontools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
-
-shelltools.export('HOME', get.workDIR())
 
 def setup():
-    autotools.configure("--prefix=/usr \
-                        --sysconfdir=/etc \
-                        --disable-static \
-                        --enable-pdf \
-                        --enable-tiff \
-                        --enable-djvu \
-                        --enable-dvi \
-                        --enable-t1lib \
-                        --disable-nautilus \
-                        --enable-comics \
-                        --disable-scrollkeeper \
-                        --disable-schemas-compile \
-                        --enable-introspection")
-                        #--enable-nautilus \ 
-    
-    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+    mesontools.configure("-Dnautilus=false")
 
 def build():
-    autotools.make()
-
-def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    mesontools.build()
     
-    pisitools.dodoc("AUTHORS", "ChangeLog", "README", "COPYING", "TODO")
+def install():
+    mesontools.install()
+    
+    pisitools.dodoc("AUTHORS", "NEWS*", "README*", "COPYING", "TODO")
