5fded14a0ce4cfaac5c52595b74041ed3241b91b
Ayhan Yalçınsoy <ayhanyalcinsoy@pisilinux.org>
2021-05-01 18:39:25 +0300

    xsettingsd:new package

---
+++ desktop/toolkit/xsettingsd/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+# WorkDir = ""
+
+def setup():
+    shelltools.makedirs("build")
+    shelltools.cd("build")
+    cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr", sourceDir="..")
+
+def build():
+    shelltools.cd("build")
+    cmaketools.make()
+
+def install():
+    shelltools.cd("build")
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    shelltools.cd("..")
+    pisitools.dodoc("README*", "COPYING")
