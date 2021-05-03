0e41d2b0d531a89cf7b9dbde7139528cf47aa6f3
Rmys <muscnsl@gmail.com>
2021-05-03 22:05:33 +0300

    atk-2.36.0 rebuild

--- desktop/toolkit/gtk/atk/actions.py
+++ desktop/toolkit/gtk/atk/actions.py
@@ -2,19 +2,29 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file https://www.gnu.org/licenses/gpl-3.0.txt
+# See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import mesontools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import mesontools
+from pisi.actionsapi import get
+
 
 def setup():
-    mesontools.configure("-Dintrospection=true")
+    
+    options = "-Ddocs=false"
+    
+    
+    if get.buildTYPE() == "emul32":
+        options += " -Dintrospection=false "
+        
+        
+    mesontools.configure(options)
 
 def build():
+	
     mesontools.build()
 
 def install():
     mesontools.install()
-
+    
     pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README*")
-
