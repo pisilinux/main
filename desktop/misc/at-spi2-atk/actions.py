fe6e5056c0370b5f780b9cfb32fad294ad78e7a1
Rmys <muscnsl@gmail.com>
2021-05-04 17:39:41 +0300

    at-spi2-* ver. bump

--- desktop/misc/at-spi2-atk/actions.py
+++ desktop/misc/at-spi2-atk/actions.py
@@ -7,28 +7,24 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import mesontools
 from pisi.actionsapi import get
 
 def setup():
-    shelltools.makedirs("build")
-    shelltools.cd("build")
-    options = "meson --prefix=/usr --sysconfdir=/etc \
+    options = "--prefix=/usr --sysconfdir=/etc \
               "
                      
     if get.buildTYPE() == "emul32":
-        options += "--libdir=lib32 .."
+        options += " --libdir=lib32"
                 
-    shelltools.system(options)
+    mesontools.configure(options)
 
 def build():
-    shelltools.cd("build")
-    shelltools.system("ninja")
+    mesontools.build()
 
 def install():
-    shelltools.cd("build")
-    shelltools.system("DESTDIR=%s ninja install" % get.installDIR())
+    mesontools.install()
     if get.buildTYPE() == "emul32":
         return
     
-        shelltools.cd("build")
-        pisitools.dodoc("AUTHORS", "COPYING", "README", "NEWS")
+    pisitools.dodoc("AUTHORS", "COPYING", "README", "NEWS")
