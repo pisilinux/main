da2d9bd98644ecb6001871b23b05591807054ab2
Kamil ATLI <suvarice@gmail.com>
2021-05-10 10:11:05 +0300

    kolourpaint

--- desktop/kde/applications/kolourpaint/actions.py
+++ desktop/kde/applications/kolourpaint/actions.py
@@ -18,4 +18,4 @@ def build():
 def install():
     kde5.install()
 
-    pisitools.dodoc("AUTHORS", "BUGS", "ChangeLog", "COPYING*", "NEWS", "README")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "NEWS", "README.md")
