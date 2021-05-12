05062eb117ced49999b5a7106c61b95f2c4218e1
Rmys <muscnsl@gmail.com>
2021-05-12 14:45:02 +0300

    vokoscreenNG rebuild

--- multimedia/videoplayer/vokoscreenNG/actions.py
+++ multimedia/videoplayer/vokoscreenNG/actions.py
@@ -5,22 +5,24 @@
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import qt5
 from pisi.actionsapi import get
 
-WorkDir = "vokoscreenNG-%s/src" % get.srcVERSION()
+#WorkDir = "vokoscreenNG-%s/src" % get.srcVERSION()
 
 def setup():
-	pisitools.cxxflags.add("-Wno-deprecated-declarations")
-	qt5.configure()
+    pisitools.cxxflags.add("-Wno-deprecated-declarations")
+    qt5.configure("src/vokoscreenNG.pro")
 
 def build():
-	qt5.make()
+    qt5.make()
 
 def install():
-	pisitools.dobin("vokoscreenNG")
-	pisitools.insinto("/usr/share/applications", "applications/vokoscreenNG.desktop", "vokoscreenNG.desktop")
-	pisitools.insinto("/usr/share/pixmaps", "applications/vokoscreenNG.png", "vokoscreenNG.png")
+    pisitools.dobin("vokoscreenNG")
+    pisitools.insinto("/usr/share/applications", "src/applications/vokoscreenNG.desktop", "vokoscreenNG.desktop")
+    pisitools.insinto("/usr/share/pixmaps", "src/applications/vokoscreenNG.png", "vokoscreenNG.png")
+    
+    pisitools.dodoc("COPYING", "README.md")
 
-	pisitools.dodoc("../COPYING", "../README.md")
 
