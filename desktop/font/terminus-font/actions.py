9f3e8e2d9b75b30a3928e224f2fc3d7d93e615d6
4fury-c3440d8 <uglyside@yandex.ru>
2021-05-22 22:45:04 +0300

    revert no otb.

--- desktop/font/terminus-font/actions.py
+++ desktop/font/terminus-font/actions.py
@@ -11,14 +11,16 @@ from pisi.actionsapi import get
 
 def setup():
 	pisitools.dosed("75-yes-terminus.conf", "Term", "xos4 Term")
-	autotools.configure("--x11dir=/usr/share/fonts/misc --psfdir=/usr/share/kbd/consolefonts")
+	autotools.configure("--x11dir=/usr/share/fonts/misc --otbdir=/usr/share/fonts/misc --psfdir=/usr/share/kbd/consolefonts")
 
 def build():
-	autotools.make()
+	autotools.make("all otb")
 
 def install():
 	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 	pisitools.insinto("/etc/fonts/conf.avail", "75-yes-terminus.conf", "75-yes-terminus.conf")
+	for t in shelltools.ls("*.otb"):
+		pisitools.insinto("/usr/share/fonts/misc", t)
 	pisitools.dosym("../conf.avail/75-yes-terminus.conf", "/etc/fonts/conf.d/75-yes-terminus.conf")
 
 	pisitools.dodoc("AUTHORS", "CHANGES", "README")
