d218637409a853e64ae873918679d1334a69c918
Rmys <muscnsl@gmail.com>
2021-05-25 23:16:49 +0300

    atkmm-2.28.2

--- desktop/toolkit/gtk/atkmm/actions.py
+++ desktop/toolkit/gtk/atkmm/actions.py
@@ -4,23 +4,20 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import autotools
+from pisi.actionsapi import mesontools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--disable-static")
-    
-    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+    mesontools.configure()
 
 def build():
-    autotools.make()
+    mesontools.build()
 
 def install():
-    autotools.install()
+    mesontools.install()
 
-    pisitools.domove("/usr/share/doc/%s-1.6/*" % get.srcNAME(), "/usr/share/gtk-doc/html/atkmm")
-    pisitools.removeDir("/usr/share/doc/%s-1.6" % get.srcNAME())
-    pisitools.removeDir("/usr/share/devhelp")
+    #pisitools.domove("/usr/share/doc/%s-1.6/*" % get.srcNAME(), "/usr/share/gtk-doc/html/atkmm")
+    #pisitools.removeDir("/usr/share/doc/%s-1.6" % get.srcNAME())
+    #pisitools.removeDir("/usr/share/devhelp")
 
     pisitools.dodoc("ChangeLog", "COPYING", "NEWS", "README")
