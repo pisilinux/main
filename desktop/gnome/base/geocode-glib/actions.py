98d9a7ce624637b5ef918bfcb7825d3e69898610
Berk Çakar <berk.cakar@ug.bilkent.edu.tr>
2021-05-27 19:28:12 +0300

    gnome-settings-daemon, geocode-glib, libgweather, mutter, libdazzle packaged

---
+++ desktop/gnome/base/geocode-glib/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import mesontools
+
+def setup():
+    mesontools.configure()
+
+def build():
+    mesontools.build
+
+def install():
+    mesontools.install()
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING.LIB", "NEWS", "README")
---
+++ desktop/gnome/base/libgweather/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import mesontools
+
+def setup():
+    mesontools.configure("-Dselinux=false \
+                          -Dpackagekit=false")
+
+def build():
+    mesontools.build
+
+def install():
+    mesontools.install()
+    pisitools.dodoc("AUTHORS", "HACKING*", "COPYING", "NEWS", "README*")
