381a0bdc65a7b41a2162061f66c75c9e074ed9ec
Berk Çakar <berk.cakar@ug.bilkent.edu.tr>
2021-05-28 16:21:01 +0300

    gnome-online-accounts, grilo, totem-pl-parser, liboauth packaged

---
+++ programming/misc/liboauth/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--disable-static \
+                         --enable-nss \
+                         --enable-curl")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "LICENSE.OpenSSL", "README")
