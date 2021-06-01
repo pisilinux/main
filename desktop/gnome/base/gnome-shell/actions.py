1546f14a6cf654bc2ea0cc56ffa50e679ea4ca0c
Berk Çakar <berk.cakar@ug.bilkent.edu.tr>
2021-06-01 17:52:31 +0300

    gmime3, gnome-backgrounds, gnome-shell, gnome-shell-extensions, gnome-keyring, sushi packaged

---
+++ desktop/gnome/base/gnome-shell/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import mesontools
+from pisi.actionsapi import pisitools
+
+def setup():
+    mesontools.configure("-Dsystemd=false \
+                          -Dextensions_tool=true \
+                          -Dextensions_app=true \
+                          -Dman=true \
+                          -Dnetworkmanager=true")
+
+def build():
+    mesontools.build()
+
+def install():
+    mesontools.install()
+
+    pisitools.dodoc("COPYING", "HACKING.md", "NEWS", "README.md")
