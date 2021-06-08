280093a18ba6d84fb5e0c4c464999801312c0608
Rmys <muscnsl@gmail.com>
2021-06-08 15:24:53 +0300

    sysprof new package

---
+++ desktop/gnome/apps/sysprof/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import mesontools
+from pisi.actionsapi import get
+
+def setup():
+    #pisitools.cflags.add("-ffat-lto-objects")
+    mesontools.configure("-Dsystemdunitdir=/tmp")
+
+def build():
+    mesontools.build()
+
+def install():
+    mesontools.install()
+
+    pisitools.removeDir("/tmp")
+    
+    pisitools.dodoc("AUTHORS", "COPYING*", "NEWS", "README*")
