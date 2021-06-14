c7b4b3734ac42c72e3922e3fd34102555c3f430e
Berk Çakar <berk.cakar@ug.bilkent.edu.tr>
2021-06-15 00:54:58 +0300

    gnome-builder, gnome-contacts, gnome-disk-utility, gnome-maps, gnome-recipes, gnome-weather, libchamplain, jsonrpc-glib, template-glib packaged; gitg, libgit2-glib, libgit2, gd rebuild

--- desktop/gnome/apps/gitg/actions.py
+++ desktop/gnome/apps/gitg/actions.py
@@ -4,7 +4,8 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from pisi.actionsapi import mesontools, pisitools
+from pisi.actionsapi import mesontools
+from pisi.actionsapi import pisitools
 
 def setup():
     mesontools.configure()
---
+++ desktop/gnome/apps/gnome-builder/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import mesontools
+from pisi.actionsapi import pisitools
+
+def setup():
+    mesontools.configure("-Dwith_webkit=true \
+                          -Dwith_sysprof=true \
+                          -Dhelp=true \
+                          -Dnetwork_tests=false")
+
+def build():
+    mesontools.build()
+
+def install():
+    mesontools.install()
+
+    pisitools.dodoc("AUTHORS", "NEWS", "README.md", "COPYING")
