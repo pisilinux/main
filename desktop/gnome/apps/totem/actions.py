77fe0259ef5519127d5eebd505af7a46f45e4399
Ayhan Yalçınsoy <ayhanyalcinsoy@pisilinux.org>
2021-06-15 13:17:19 +0300

    totem:new package

---
+++ desktop/gnome/apps/totem/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools, get, mesontools, pisitools
+
+def setup():
+    mesontools.configure("--libexecdir=/usr/lib \
+                          -Denable-gtk-doc=true \
+                          -Dgst-inspect=false \
+                          -Dwith-plugins=all \
+                          -Denable-python=yes")
+
+def build():
+    mesontools.build()
+
+def install():
+    mesontools.install()
+
+    pisitools.dodoc("AUTHORS", "COPYING", "README*", "NEWS")
