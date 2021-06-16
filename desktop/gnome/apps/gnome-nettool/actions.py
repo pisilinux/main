fc4388efa21feb52639e27c5338790c687c008c6
Ayhan Yalçınsoy <ayhanyalcinsoy@pisilinux.org>
2021-06-16 16:15:51 +0300

    gnome-nettool:new package

---
+++ desktop/gnome/apps/gnome-nettool/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools, get, pisitools
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "README*", "NEWS")
