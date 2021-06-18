ff0342f26889e38d797b8e70f1e599743101e13c
Rmys <muscnsl@hotmail.com>
2021-06-18 13:20:09 +0300

    xorg-xwayland new package

---
+++ x11/library/eglexternalplatform/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+#def setup():
+    #autotools.configure()
+
+#def build():
+    #autotools.make()
+
+def install():
+    pisitools.insinto("/usr/include/EGL", "interface/*")
+    pisitools.insinto("/usr/lib/pkgconfig", "*.pc")
+    #autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("COPYING", "README*")
