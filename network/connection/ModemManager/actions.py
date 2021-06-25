e907bcddad965da39db582e0f524e4eaefac3261
Rmys <muscnsl@gmail.com>
2021-06-25 15:11:49 +0300

    ModemManager

--- network/connection/ModemManager/actions.py
+++ network/connection/ModemManager/actions.py
@@ -15,7 +15,6 @@ def setup():
                          --enable-more-warnings=yes \
                          --with-udev-base-dir=/lib/udev \
                          --enable-gtk-doc \
-                         --enable-more-warnings=no \
                          --with-systemd-journal=no \
                          --with-systemd-suspend-resume=no \
                          --with-polkit=permissive")
