925b667d4e2107a190d61215f3eed1799ab2f54d
Ayhan Yalçınsoy <ayhanyalcinsoy@pisilinux.org>
2021-06-28 00:42:31 +0300

    rygel:new package

---
+++ desktop/gnome/apps/rygel/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import mesontools, pisitools
+
+def setup():
+    # Disable tracker 2.0 plugin
+    pisitools.dosed("meson_options.txt", "'tracker',", " ")
+    mesontools.configure("-Dapi-docs=true \
+                          -Dexamples=false")
+
+def build():
+    mesontools.build()
+
+def install():
+    mesontools.install()
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "README*", "NEWS")
