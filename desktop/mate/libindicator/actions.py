18ac0c249016c9f406d50b9207bf14c236ee03dc
Rmys <muscnsl@gmail.com>
2021-07-05 18:44:17 +0300

    libindicator rebuild

--- desktop/mate/libindicator/actions.py
+++ desktop/mate/libindicator/actions.py
@@ -9,17 +9,39 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 
 def setup():
+    shelltools.copytree("../libindicator-%s" % (get.srcVERSION().replace("_", "~")), "../libindicator-%s-gtk3" % get.srcVERSION())
+
     pisitools.cflags.add("-Wno-error=deprecated-declarations")
     autotools.configure("--libexecdir=/usr/lib \
+                         --disable-static \
+                         --disable-introspection \
+                         --with-gtk=2")
+
+    # for fix unused dependency
+    pisitools.dosed("libtool"," -shared ", " -Wl,-O1,--as-needed -shared ")
+
+    shelltools.cd("../libindicator-%s-gtk3" % get.srcVERSION())
+    pisitools.cflags.add("-Wno-error=deprecated-declarations")
+    autotools.configure("--libexecdir=/usr/lib \
+                         --disable-static \
                          --with-gtk=3")
 
     # for fix unused dependency
     pisitools.dosed("libtool"," -shared ", " -Wl,-O1,--as-needed -shared ")
 
+
+
+
 def build():
     autotools.make()
 
+    shelltools.cd("../libindicator-%s-gtk3" % get.srcVERSION())
+    autotools.make()
+
 def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    shelltools.cd("../libindicator-%s-gtk3" % get.srcVERSION())
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("AUTHORS", "NEWS", "ChangeLog", "COPYING", "README")
