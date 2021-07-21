121f493a4d4e72b053340972cc90bb812697332d
4fury-c3440d8 <uglyside@yandex.ru>
2021-07-21 17:00:26 +0300

    portaudio ver. bump

--- multimedia/sound/portaudio/actions.py
+++ multimedia/sound/portaudio/actions.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 WorkDir = "portaudio"
@@ -20,4 +19,4 @@ def build():
 def install():
     autotools.rawInstall('DESTDIR="%s" libdir=/usr/lib' % get.installDIR())
 
-    pisitools.dodoc("LICENSE.txt", "README.txt")
+    pisitools.dodoc("LICENSE.txt", "README.md")
