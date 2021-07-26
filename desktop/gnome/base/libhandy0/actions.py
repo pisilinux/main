c30c95b595d6a810b21c092564d0d9e015ee3b57
Berk Çakar <berk.cakar@ug.bilkent.edu.tr>
2021-07-26 14:41:29 +0300

    libhandy0 rebuild

--- desktop/gnome/base/libhandy0/actions.py
+++ desktop/gnome/base/libhandy0/actions.py
@@ -3,21 +3,18 @@
 #
 # Licensed under the GNU General Public License, version 2
 # See the file http://www.gnu.org/copyleft/gpl.txt
-# TODO: glade catalog fails the build, needs to be fixed
-# (it is probably because of some missing dependencies on a fresh install ??gladeui??)
 
-from pisi.actionsapi import get
-from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import mesontools
 
 def setup():
-    mesontools.configure("-Dglade_catalog=disabled")
+    mesontools.configure("-Dglade_catalog=disabled \
+                          -Dexamples=false")
 
 def build():
-    mesontools.build
+    mesontools.build()
 
 def install():
     mesontools.install()
-    pisitools.dodoc("AUTHORS", "COPYING", "HACKING.md", "README.md")
+
+    pisitools.dodoc("AUTHORS", "COPYING", "README.md")
