01c27b1da842b6d74dd5fa9624f31570afdfa57c
Rmys <muscnsl@gmail.com>
2021-08-21 13:13:26 +0300

    yali rebuild stable repo

--- system/installer/yali/actions.py
+++ system/installer/yali/actions.py
@@ -8,11 +8,11 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import pythonmodules
 
 def setup():
-    repo_uri = "https://ciftlik.pisilinux.org/2.0-Beta.1/pisi-index.xml.xz" # FIXME
+    repo_uri = "https://stable2.pisilinux.org/pisi-index.xml.xz" # FIXME
     #pisitools.dosed("yali/constants.py", "@REPO_URI@", repo_uri)
     #pisitools.dosed("yali/constants.py", "@REPO_NAME@", "Beta") # FIXME
     pisitools.dosed("conf/yali.conf", "@REPO_URI@", repo_uri)
-    pisitools.dosed("conf/yali.conf", "@REPO_NAME@", "Beta") # FIXME
+    pisitools.dosed("conf/yali.conf", "@REPO_NAME@", "Stable2") # FIXME
 
     pisitools.dosed("conf/yali.conf", "@INSTALL_TYPE@", "default")
 
