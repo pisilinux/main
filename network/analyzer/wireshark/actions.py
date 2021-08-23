1f7d60a5f374a2f7e685700b2b996a96e9778d0a
Berk Çakar <berk.cakar@ug.bilkent.edu.tr>
2021-08-24 00:47:20 +0300

    wireshark, gnome-multi-writer packaged

---
+++ network/analyzer/wireshark/actions.py
@@ -0,0 +1,38 @@
+
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import mesontools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+def setup():
+    shelltools.makedirs("build")
+    shelltools.cd("build")
+    cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr \
+                          -DCMAKE_BUILD_TYPE=Release \
+                          -DCMAKE_INSTALL_DOCDIR=/usr/share/doc/wireshark \
+                          -G Ninja", sourceDir="..")
+
+def build():
+    mesontools.build()
+
+def install():
+    mesontools.install()
+
+    for f in ["epan/register.h", "ws_diag_control.h", "file.h", "cfile.h", "ws_symbol_export.h"]:
+        pisitools.insinto("/usr/include/wireshark/", f)
+
+    pisitools.insinto("/usr/include/wireshark/epan/", "epan/*.h")
+    pisitools.insinto("/usr/include/wireshark/wsutil/", "wsutil/*.h")
+    pisitools.insinto("/usr/include/wireshark/wiretap/", "wiretap/*.h")
+    pisitools.insinto("/usr/include/wireshark/epan/crypt/", "epan/crypt/*.h")
+    pisitools.insinto("/usr/include/wireshark/epan/ftypes/", "epan/ftypes/*.h")
+    pisitools.insinto("/usr/include/wireshark/epan/dfilter/", "epan/dfilter/*.h")
+    pisitools.insinto("/usr/include/wireshark/epan/dissectors/", "epan/dissectors/*.h")
+    pisitools.insinto("/usr/include/wireshark/epan/wmem/", "epan/wmem/*.h")
+
+    pisitools.dodoc("AUTHORS", "COPYING", "ChangeLog", "NEWS", "README*")
