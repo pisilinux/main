a0444bdad843207feff436a5b8a9a514c9501785
Rmys <muscnsl@hotmail.com>
2021-08-30 16:43:21 +0300

    flowblade new package

---
+++ multimedia/converter/flowblade/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+
+def build():
+    shelltools.system("sed -i -e 's|#!/usr/bin/env python|#!/usr/bin/python3|g' flowblade-trunk/Flowblade/launch/*")
+    shelltools.system("sed -i -e 's|#!/usr/bin/env python|#!/usr/bin/python3|g' flowblade-trunk/Flowblade/tools/clapperless.py")
+    shelltools.cd("flowblade-trunk")
+    pythonmodules.compile(pyVer="3")
+
+def install():
+    shelltools.cd("flowblade-trunk")
+    pythonmodules.install(pyVer="3")
+
+    shelltools.chmod(get.installDIR() + "/usr/lib/python3.8/site-packages/Flowblade/launch/*")
+    shelltools.chmod(get.installDIR() + "/usr/lib/python3.8/site-packages/Flowblade/tools/clapperless.py")
+
+    pisitools.dodoc("AUTHORS", "COPYING", "README*")
