cc9b587543f67bf3a725d0859ccf0946b911dc32
4fury-c3440d8 <uglyside@yandex.ru>
2021-09-14 06:13:40 +0300

    aria2.

--- network/download/aria2/actions.py
+++ network/download/aria2/actions.py
@@ -8,37 +8,33 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
+j = ''.join([
+    ' --prefix=/usr',
+    ' --enable-bittorrent',
+    ' --enable-libaria2',
+    ' --enable-metalink',
+    ' --enable-epoll',
+    ' --enable-nls',
+    ' --disable-rpath',
+    ' --disable-static',
+    ' --with-libz',
+    ' --with-gnutls',
+    ' --with-sqlite3',
+    ' --with-libxml2',
+    ' --with-libssh2',
+    ' --with-libcares',
+    ' --with-ca-bundle=/etc/ssl/certs/ca-certificates.crt',
+    ' --with-bashcompletiondir=/usr/share/bash-completion/completions '
+    ])
+
 def setup():
-	autotools.configure("--prefix=/usr \
-	--enable-bittorrent \
-	--enable-libaria2 \
-	--enable-metalink \
-	--enable-epoll \
-	--enable-nls \
-	\
-	--disable-rpath \
-	--disable-static \
-	\
-	--with-libz \
-	--with-gnutls \
-	--with-sqlite3 \
-	--with-libxml2 \
-	--with-libssh2 \
-	--with-libcares \
-	--with-ca-bundle=/etc/ssl/certs/ca-certificates.crt \
-	--with-bashcompletiondir=/usr/share/bash-completion/completions")
+	autotools.configure(j)
 
 def build():
-	autotools.make("-C po update-gmo")
 	autotools.make()
 
 def install():
 	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-	pisitools.dodoc("AUTHORS", \
-	"COPYING", \
-	"INSTALL", \
-	"LICENSE.OpenSSL", \
-	"NEWS", \
-	"README*")
+	pisitools.dodoc("AUTHORS", "ChangeLog", "README*")
 
