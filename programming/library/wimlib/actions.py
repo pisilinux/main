20daf48a3c36cd35e1dc3e545251eae83e974cac
Rmys <muscnsl@hotmail.com>
2021-09-27 15:59:32 +0300

    wimlib new package

---
+++ programming/library/wimlib/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--with-libcrypto \
+                         --disable-static \
+                         --enable-multithreaded-compression \
+                         --with-fuse \
+                         --with-ntfs-3g")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("COPYING*", "NEWS", "README")
