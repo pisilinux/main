4c01549aa249203e48e9f8ada126b7bf6d549c1c
4fury-c3440d8 <uglyside@yandex.ru>
2021-10-13 18:24:27 +0300

    claws-mail.

---
+++ network/mail/claws-mail/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
+
+from pisi.actionsapi import shelltools, autotools, pisitools, get
+
+i = ''.join([
+    ' --enable-ldap',
+    ' --enable-startup-notification',
+    ' --enable-alternate-addressbook',
+    ' --disable-static '
+    ])
+def setup():
+	autotools.configure(i)
+
+#	pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+
+def build():
+	autotools.make()
+
+def install():
+	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+	pisitools.dodoc("AUTHORS", "README")
+
