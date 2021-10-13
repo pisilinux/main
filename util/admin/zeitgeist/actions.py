ed7356e4c3fba76214dc4b613b409623d2ee466a
4fury-c3440d8 <uglyside@yandex.ru>
2021-10-13 19:14:55 +0300

    zeitgeist logging framework.

---
+++ util/admin/zeitgeist/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
+
+from pisi.actionsapi import shelltools, autotools, get
+
+def setup():
+	shelltools.system("NOCONFIGURE=1 ./autogen.sh")
+	autotools.configure()
+
+def build():
+	autotools.make()
+
+def install():
+	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+#	pisitools.dodoc("AUTHORS", "NEWS")
