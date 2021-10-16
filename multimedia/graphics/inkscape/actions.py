80ebe184470889ff49600c5ea9bac1bbb4cea85a
4fury-c3440d8 <uglyside@yandex.ru>
2021-10-16 22:55:45 +0300

    inkscape.

--- multimedia/graphics/inkscape/actions.py
+++ multimedia/graphics/inkscape/actions.py
@@ -10,22 +10,24 @@ from pisi.actionsapi import cmaketools
 from pisi.actionsapi import shelltools
 
 j = ''.join([
-    '-DWITH_DBUS=ON  ',
-    '-DBUILD_TESTING=OFF -L ',
-    '-DWITH_GSPELL=OFF ',
-    '-DBUILD_SHARED_LIBS=ON ',
-    '-DWITH_IMAGE_MAGICK=OFF ',
-    '-DBUILD_STATIC_LIBS=OFF ',
-    '-DCMAKE_INSTALL_LIBDIR=lib ',
-    '-DCYTHON_EXECUTABLE=/usr/bin/cython3 ',
-    '-DCMAKE_BUILD_TYPE=Release ',
-    '-DCMAKE_INSTALL_PREFIX=/usr '
+    ' -DWITH_DBUS=ON',
+    ' -DBUILD_TESTING=OFF',
+    ' -DWITH_GSPELL=OFF',
+    ' -DBUILD_SHARED_LIBS=ON',
+    ' -DWITH_IMAGE_MAGICK=OFF',
+    ' -DBUILD_STATIC_LIBS=OFF',
+    ' -DCMAKE_INSTALL_LIBDIR=lib',
+    ' -DCYTHON_EXECUTABLE=/usr/bin/cython3',
+    ' -D2GEOM_BOOST_PYTHON=ON',
+    ' -D2GEOM_CYTHON_BINDINGS=ON',
+    ' -DCMAKE_BUILD_TYPE=Release',
+    ' -DCMAKE_INSTALL_PREFIX=/usr -L '
     ])
 
 def setup():
-    # fix shebang
+    #fix shebang
 #    shelltools.system("find share/extensions -iname '*.py' -exec sed -i 's|env\ python$|env python3|g' {} \;")
-    # if with imagemagick build.
+    #if with imagemagick build.
 #    shelltools.export("PKG_CONFIG_PATH", "${PKG_CONFIG_PATH}:/usr/lib/imagemagick6/pkgconfig")
     shelltools.makedirs("build")
     shelltools.cd("build")
@@ -39,4 +41,4 @@ def install():
     shelltools.cd("build")
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("../COPYING", "../README.md")
+    pisitools.dodoc("../AUTHORS", "../NEWS.md", "../README.md")
