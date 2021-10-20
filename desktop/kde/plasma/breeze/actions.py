45ef9e446138affb2a26b1b4dd91330451e61c64
Rmys <muscnsl@gmail.com>
2021-10-20 12:34:32 +0300

    breeze rebuild

--- desktop/kde/plasma/breeze/actions.py
+++ desktop/kde/plasma/breeze/actions.py
@@ -7,8 +7,10 @@
 from pisi.actionsapi import kde5
 from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 
 def setup():
+    shelltools.system("sed -i 's|5.23.0|5.23.1|g' CMakeLists.txt")
     kde5.configure()
 
 def build():
