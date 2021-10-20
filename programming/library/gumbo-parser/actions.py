9519b6c0134d44a6978aad9e781c30dbdb48d18c
4fury-c3440d8 <uglyside@yandex.ru>
2021-10-20 23:15:39 +0300

    claws-mail rebuild.

---
+++ programming/library/gumbo-parser/actions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
+
+from pisi.actionsapi import shelltools, autotools, get
+
+def setup():
+	shelltools.system("NOCONFIGURE=1 ./autogen.sh")
+	autotools.configure("--disable-static")
+
+def build():
+	autotools.make()
+
+def install():
+	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
