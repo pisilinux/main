2c34d2250fa06c12380462a00878ad00cfc640be
4fury-c3440d8 <uglyside@yandex.ru>
2021-11-06 21:55:34 +0300

    zimg version bump.

--- multimedia/graphics/libzimg/actions.py
+++ multimedia/graphics/libzimg/actions.py
@@ -2,25 +2,21 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/copyleft/gpl.txt 
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
 	autotools.autoreconf("-vfi")
 	autotools.configure("--enable-static='no'")
 
-	
 def build():
-	
 	autotools.make()
 
 def install():
 	autotools.rawInstall("DESTDIR=%s" %get.installDIR())
-	
+
 	pisitools.dodoc("COPYING", "README.md", "ChangeLog")
 
