883e50fee37ccf9b6b8251adc04b216c76b57a48
Rmys <muscnsl@gmail.com>
2021-11-12 13:11:35 +0200

    xorg-xwayland-21.1.3

--- x11/wayland/xorg-xwayland/actions.py
+++ x11/wayland/xorg-xwayland/actions.py
@@ -11,25 +11,23 @@ from pisi.actionsapi import mesontools
 from pisi.actionsapi import get
 
 def setup():
-    mesontools.configure("-D ipv6=true \
-                            -D glx=true \
-                            -D dpms=true \
-                            -D xace=true \
-                            -D xvfb=true \
-                            -D xdmcp=true \
-                            -D xinerama=true \
-                            -D xcsecurity=true \
-                            -D screensaver=true \
-                            -D dri3=true \
-                            -D xwayland_eglstream=true \
-                            -D glamor=true \
-                            -D xkb_dir=/usr/share/X11/xkb \
-                            -D xkb_output_dir=/var/lib/xkb")
+    mesontools.configure("-Dipv6=true \
+                          -Dxvfb=false \
+                          -Dxdmcp=false \
+                          -Dxcsecurity=true \
+                          -Ddri3=true \
+                          -Dxwayland_eglstream=true \
+                          -Dglamor=true \
+                          -Dxkb_dir=/usr/share/X11/xkb \
+                          -Dxkb_output_dir=/var/lib/xkb")
 
 def build():
     mesontools.build()
 
 def install():
     mesontools.install()
+
+    pisitools.remove("/usr/lib/xorg/protocol.txt")
+    pisitools.remove("/usr/share/man/man1/Xserver.1")
     
     pisitools.dodoc("COPYING", "README*")
