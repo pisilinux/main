04b85690a8ff9ac75b3ddc9109a1ac388faa43da
Rmys <muscnsl@gmail.com>
2021-11-21 10:38:45 +0200

    qt5-3d kde patch

--- desktop/toolkit/qt5/qt5-3d/actions.py
+++ desktop/toolkit/qt5/qt5-3d/actions.py
@@ -4,11 +4,15 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import qt5
+from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 
 
 
 def setup():
+    shelltools.system("mkdir .git")
+    pisitools.dosed(".qmake.conf", "5.15.3", "5.15.2")
     qt5.configure()
 
 def build():
