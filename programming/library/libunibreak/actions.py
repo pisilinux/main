769962b9fe743aa856003bc8eea6f8e8874f1926
4fury-c3440d8 <uglyside@yandex.ru>
2021-11-22 17:34:36 +0300

    add missing dep. libunibreak.

---
+++ programming/library/libunibreak/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+	autotools.configure("--disable-static")
+
+def build():
+	autotools.make()
+
+def install():
+	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+	pisitools.dodoc("AUTHORS", "LICENCE", "NEWS", "README.md")
+
