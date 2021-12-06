558f5b79e57363f493d3427d0badf799e71898df
Rmys <muscnsl@gmail.com>
2021-12-06 17:11:37 +0300

    zeromq ver. bump

--- network/library/zeromq/actions.py
+++ network/library/zeromq/actions.py
@@ -5,10 +5,15 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
     autotools.autoreconf("-vfi")
+
+    shelltools.system("""sed -i 's/libzmq_werror="yes"/libzmq_werror="no"/' configure""")
+    shelltools.system("sed -i 's/openpgm-5.1/openpgm-5.2/' configure configure.ac")
+
     autotools.configure("--with-pgm --with-libsodium --disable-static")
     # fix unused dependency analysis
     pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
@@ -18,3 +23,5 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.insinto("/usr/include", "cppzmq-4.8.0/*.hpp")
