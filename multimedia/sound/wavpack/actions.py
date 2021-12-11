ec580c5d2fccd738f2f5447840a4c871d1d5f1ed
4fury-c3440d8 <uglyside@yandex.ru>
2021-12-11 17:38:43 +0300

    wavpack.

--- multimedia/sound/wavpack/actions.py
+++ multimedia/sound/wavpack/actions.py
@@ -2,19 +2,14 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    shelltools.system("./autogen.sh")
-    autotools.configure("--prefix=/usr \
-                         --libdir=/usr/lib \
-                         --disable-static \
-                         --enable-mmx")
+    autotools.configure("--disable-static --enable-asm")
 
 def build():
     autotools.make()
@@ -22,4 +17,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README")
+    pisitools.dodoc("AUTHORS", "NEWS")
