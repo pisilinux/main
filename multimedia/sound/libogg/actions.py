2e90e22cd8fd3de5624562be341f9728ab345441
4fury-c3440d8 <uglyside@yandex.ru>
2021-12-11 17:32:37 +0300

    libogg.

--- multimedia/sound/libogg/actions.py
+++ multimedia/sound/libogg/actions.py
@@ -2,12 +2,11 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import get
-
 
 def setup():
     autotools.configure("--disable-static")
