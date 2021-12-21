ca6f250b50099b1114e6a465b51a4e34700cf744
4fury-c3440d8 <uglyside@yandex.ru>
2021-12-21 07:16:49 +0300

    add libptytty.

---
+++ programming/library/libptytty/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
+
+from pisi.actionsapi import shelltools, cmaketools, pisitools, get
+
+def setup():
+	cmaketools.configure("-B_build")
+
+def build():
+	shelltools.cd("_build")
+	cmaketools.make()
+
+def install():
+	shelltools.cd("_build")
+	cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+	pisitools.dodoc("../Changes")
