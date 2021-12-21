d42abb6fdb6159b3e2800ba791fa39325314ba9c
4fury-c3440d8 <uglyside@yandex.ru>
2021-12-21 20:56:23 +0300

    goggles music manager( for fox toolkit fans ).

---
+++ multimedia/sound/gogglesmm/actions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+j = ''.join([
+    ' -DWITH_ALSA=ON',
+    ' -DWITH_NATIVE=ON -L '
+    ])
+
+def setup():
+	cmaketools.configure("-B_build %s" % j)
+
+def build():
+	shelltools.cd("_build")
+	cmaketools.make()
+
+def install():
+	shelltools.cd("_build")
+	cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+	pisitools.dodoc("../AUTHORS", "../ChangeLog")
+
