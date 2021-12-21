b6f48e86d872d0930fe7960df6982a566004eba4
4fury-c3440d8 <uglyside@yandex.ru>
2021-12-21 21:23:29 +0300

    remove unneeded import.

--- programming/language/perl/perl-ExtUtils-PkgConfig/actions.py
+++ programming/language/perl/perl-ExtUtils-PkgConfig/actions.py
@@ -5,7 +5,6 @@
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import perlmodules
-from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 
 def setup():
