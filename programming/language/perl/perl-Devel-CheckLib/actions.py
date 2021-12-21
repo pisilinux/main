76b22662984b169104cf396bd0f4011806838544
4fury-c3440d8 <uglyside@yandex.ru>
2021-12-21 20:36:11 +0300

    add perl modules.

---
+++ programming/language/perl/perl-Devel-CheckLib/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
+
+from pisi.actionsapi import perlmodules
+from pisi.actionsapi import pisitools
+
+def setup():
+	perlmodules.configure()
+
+def build():
+	perlmodules.make()
+
+def check():
+	perlmodules.make("test")
+
+def install():
+	perlmodules.install()
+
+	pisitools.dodoc("CHANGES")
+
---
+++ programming/language/perl/perl-Gtk2/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
+
+from pisi.actionsapi import perlmodules
+
+def setup():
+	perlmodules.configure()
+
+def build():
+	perlmodules.make()
+
+def check():
+	pass
+
+def install():
+	perlmodules.install()
+
