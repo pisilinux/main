79f3e607aaee043c9006fcf1c125e4eda91d7229
4fury-c3440d8 <uglyside@yandex.ru>
2022-01-17 11:10:55 +0300

    geeqie version bump.

--- multimedia/graphics/geeqie/actions.py
+++ multimedia/graphics/geeqie/actions.py
@@ -4,20 +4,26 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-j = "--disable-map \
-     --disable-pdf \
-     --disable-djvu \
-     --disable-heif \
-     --disable-gtk3 \
-     --disable-gpu-accel \
-     --disable-debug-log \
-    "
+j = ''.join([
+    ' --disable-map',
+    ' --disable-pdf',
+    ' --disable-djvu',
+    ' --disable-gtk3',
+    ' --disable-gpu-accel',
+    ' --disable-debug-log '
+    ])
 
 def setup():
+	pisitools.cflags.add("-Wno-deprecated-declarations")
+	# Fix instalation step
+	pisitools.dosed("Makefile.am", "\ ChangeLog\ ", " ")
+	pisitools.dosed("Makefile.am", "\ ChangeLog.html$", "")
+
 	autotools.autoreconf("-vif")
 	autotools.configure(j)
 
