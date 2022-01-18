fd2b77959ccdadac91910134ddda1d8125a5dc55
4fury-c3440d8 <uglyside@yandex.ru>
2022-01-18 05:57:05 +0300

    geeqie rebuild.

--- multimedia/graphics/geeqie/actions.py
+++ multimedia/graphics/geeqie/actions.py
@@ -4,7 +4,6 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
@@ -33,5 +32,4 @@ def build():
 def install():
 	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-#	pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README*")
-
+#	pisitools.dodoc("AUTHORS", "NEWS")
