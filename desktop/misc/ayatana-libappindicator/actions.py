43e95be34effb1fc37a09dd64d65c29a45a0d058
4fury-c3440d8 <uglyside@yandex.ru>
2022-01-18 07:35:24 +0300

    ayatana-libappindicator.

--- desktop/misc/ayatana-libappindicator/actions.py
+++ desktop/misc/ayatana-libappindicator/actions.py
@@ -4,20 +4,23 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import shelltools, autotools, pisitools, get
+from pisi.actionsapi import shelltools, cmaketools, pisitools, get
+
+j = ''.join([
+    ' -B_build',
+    ' -DCMAKE_BUILD_TYPE=Release',
+    ' -DENABLE_BINDINGS_MONO=OFF -L '
+    ])
 
 def setup():
-	shelltools.system("gtkdocize")
-	autotools.autoreconf("-fiv")
-	autotools.configure("--enable-gtk-doc --with-gtk=3 --disable-static")
-
-	pisitools.dosed("libtool", " -shared ", " -Wl,--as-needed -shared ")
+	cmaketools.configure(j)
 
 def build():
-	autotools.make()
+	shelltools.cd("_build")
+	cmaketools.make()
 
 def install():
-	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+	shelltools.cd("_build")
+	cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-	pisitools.dodoc("AUTHORS", "ChangeLog")
-
+	pisitools.dodoc("../AUTHORS", "../NEWS")
