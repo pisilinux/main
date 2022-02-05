5744ff166193f71db7832b07399488e345cb3323
4fury-c3440d8 <uglyside@yandex.ru>
2022-02-05 22:02:51 +0300

    cppcheck.

--- programming/tool/cppcheck/actions.py
+++ programming/tool/cppcheck/actions.py
@@ -9,23 +9,22 @@ from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-j = "-DUSE_Z3=ON \
-     -DHAVE_RULES=ON \
-     -DPYTHON_EXECUTABLE=/usr/bin/python3 \
-     -DBUILD_GUI=ON \
-     -DBUILD_SHARED_LIBS=ON \
-     -DBUILD_TESTS=OFF \
-     -DCMAKE_BUILD_TYPE=Release \
-     -DUSE_BUNDLED_TINYXML2=OFF \
-     -DCMAKE_DISABLE_PRECOMPILE_HEADERS=ON \
-     -DENABLE_CHECK_INTERNAL=ON \
-     -DWITH_QCHART=ON -L \
-    "
+j = ''.join([
+    ' -DUSE_Z3=ON',
+    ' -DHAVE_RULES=ON',
+    ' -DPYTHON_EXECUTABLE=/usr/bin/python3',
+    ' -DBUILD_GUI=ON',
+    ' -DBUILD_SHARED_LIBS=ON',
+    ' -DBUILD_TESTS=OFF',
+    ' -DCMAKE_BUILD_TYPE=Release',
+    ' -DUSE_BUNDLED_TINYXML2=OFF',
+    ' -DCMAKE_DISABLE_PRECOMPILE_HEADERS=ON',
+    ' -DENABLE_CHECK_INTERNAL=ON',
+    ' -DWITH_QCHART=ON -L '
+    ])
 
 def setup():
-	shelltools.makedirs("build")
-	shelltools.cd("build")
-	cmaketools.configure(j, sourceDir = '..')
+	cmaketools.configure("-Bbuild %s" % j)
 
 def build():
 	shelltools.cd("build")
@@ -38,5 +37,4 @@ def install():
 	shelltools.cd("build")
 	cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-	pisitools.dodoc("../AUTHORS", "../COPYING", "../philosophy.md", "../readme.md")
-
+	pisitools.dodoc("../AUTHORS")
