eae96d8186a4335f95fb53c13ae30d6c0ba19891
suvari <suvarice@gmail.com>
2022-02-05 23:47:55 +0300

    new packages: dtc, glusterfs, libiscsi for qemu

---
+++ hardware/misc/dtc/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.makedirs("build")
+    shelltools.cd("build")
+    shelltools.system("meson --prefix=/usr ..")
+
+def build():
+    shelltools.cd("build")
+    shelltools.system("ninja")
+
+def install():
+    shelltools.cd("build")
+    shelltools.system("DESTDIR=%s ninja install" % get.installDIR())
+
+    shelltools.cd("..")
+    pisitools.dodoc("GPL", "README*")
