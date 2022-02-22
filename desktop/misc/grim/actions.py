ae8ba03f7e7fb146e0982c6045bce9604b451c80
suvari <suvarice@gmail.com>
2022-02-22 16:49:13 +0300

    grim: fix build

--- desktop/misc/grim/actions.py
+++ desktop/misc/grim/actions.py
@@ -4,9 +4,7 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import mesontools
 from pisi.actionsapi import get
 
