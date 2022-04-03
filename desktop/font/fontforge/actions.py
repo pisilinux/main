248f091d83b1f17f0ba84d292ffc3645c2b0beb1
erkanisik1 <erkanisik@yahoo.com>
2022-04-03 23:59:02 +0300

    add new package and index
    new package ghostwriter

--- desktop/font/fontforge/actions.py
+++ desktop/font/fontforge/actions.py
@@ -1,33 +1,31 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import autotools
+from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import libtools
 from pisi.actionsapi import get
 
-#WorkDir = "fontforge-%s-b" % get.srcVERSION().split('_')[-1]
-
 def setup():
-    shelltools.export("PYTHON", "/usr/bin/python3")
-    #pisitools.dosed("configure.ac", "fontforge_package_name", "fontforge")
-    shelltools.system("""sed -e 's|python-"${PYTHON_VERSION}"|python-"${PYTHON_VERSION}"-embed|g' -i m4/fontforge_arg_enable.m4""")
-    shelltools.system("./bootstrap --force")
+    shelltools.makedirs("build")
+    shelltools.cd("build")
 
-    autotools.configure("--without-libuninameslist \
-                         --disable-static")
-    
-    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+    cmaketools.configure("-DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX=/usr \
+        -DENABLE_MAINTAINER_TOOLS=TRUE \
+        -DENABLE_FONTFORGE_EXTRAS=TRUE \
+        -DUNIX=TRUE", sourceDir="..")
 
 def build():
-    autotools.make()
+    cmaketools.make("-C build")
+    cmaketools.make("-C build doc")
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.dodoc("AUTHORS", "LICENSE")
+    pisitools.dodoc("AUTHORS", "LICENSE", "README.md")
+
+    shelltools.cd("build")
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
 
