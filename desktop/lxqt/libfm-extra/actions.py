3e509f3a119c30dddc53188f90749467fa65df53
4fury-c3440d8 <uglyside@yandex.ru>
2022-04-16 02:21:14 +0300

    libfm rebuild with gtk+-3

--- desktop/lxqt/libfm-extra/actions.py
+++ desktop/lxqt/libfm-extra/actions.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+#
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/copyleft/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
+from pisi.actionsapi import autotools, pisitools, get
 
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import autotools
+i = ''.join([
+    ' --enable-udisks',
+    ' --disable-static',
+    ' --disable-old-actions',
+    ' --with-gtk=3 '
+    ])
 
 def setup():
-    shelltools.system("./autogen.sh")
-    autotools.configure("--sysconfdir=/etc \
-                         --with-extra-only \
-                         --with-gtk=no     \
-                         --disable-static")
+	pisitools.cflags.add("-Wno-deprecated-declarations -Wno-incompatible-pointer-types")
+	autotools.configure(i)
+
+	pisitools.dosed("libtool", " -shared ", " -Wl,-O2,--as-needed -shared ")
 
 def build():
-    autotools.make()
+	autotools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.remove("/usr/include/libfm")
-    pisitools.dodir("/usr/include/libfm")
-    pisitools.domove("/usr/include/libfm-1.0/*", "/usr/include/libfm")
-    pisitools.removeDir("/usr/include/libfm-1.0")
-    pisitools.dodoc("AUTHORS", "COPYING")
+	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
