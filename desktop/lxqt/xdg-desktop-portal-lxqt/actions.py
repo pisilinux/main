b1d441998fb1dd1e32d987cd98cb229e7e10dae8
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2022-04-26 10:22:20 +0300

    xdg-desktop-portal-lxqt:new package

---
+++ desktop/lxqt/xdg-desktop-portal-lxqt/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import shelltools, get, cmaketools
+#from pisi.actionsapi import get
+#from pisi.actionsapi import cmaketools
+
+def setup():
+    shelltools.makedirs("build")
+    shelltools.cd("build")
+    cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr \
+                          -DCMAKE_INSTALL_LIBEXECDIR=lib", sourceDir="..")
+
+
+def build():
+    shelltools.cd("build")
+    cmaketools.make()
+
+def install():
+    shelltools.cd("build")
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
