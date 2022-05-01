0218b2262ea30d35922cd881dcfc58ff88edd158
4fury-c3440d8 <uglyside@yandex.ru>
2022-05-01 20:55:46 +0300

    inkscape.

--- multimedia/graphics/inkscape/actions.py
+++ multimedia/graphics/inkscape/actions.py
@@ -4,24 +4,23 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import get
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import cmaketools
-from pisi.actionsapi import shelltools
+from pisi.actionsapi import shelltools, cmaketools, pisitools, get
 
 j = ''.join([
-    ' -DWITH_DBUS=ON',
-    ' -DBUILD_TESTING=OFF',
-    ' -DWITH_GSPELL=OFF',
+    ' -DCMAKE_INSTALL_PREFIX=/usr'
+    ' -DCMAKE_BUILD_TYPE=None',
     ' -DBUILD_SHARED_LIBS=ON',
-    ' -DWITH_IMAGE_MAGICK=OFF',
-    ' -DBUILD_STATIC_LIBS=OFF',
     ' -DCMAKE_INSTALL_LIBDIR=lib',
     ' -DCYTHON_EXECUTABLE=/usr/bin/cython3',
-    ' -D2GEOM_BOOST_PYTHON=ON',
-    ' -D2GEOM_CYTHON_BINDINGS=ON',
-    ' -DCMAKE_BUILD_TYPE=Release',
-    ' -DCMAKE_INSTALL_PREFIX=/usr -L '
+    ' -DWITH_DBUS=OFF',
+    ' -DWITH_GSPELL=OFF',
+    ' -DWITH_IMAGE_MAGICK=OFF',
+    ' -DBUILD_STATIC_LIBS=OFF',
+    ' -D2GEOM_BOOST_PYTHON=OFF',
+    ' -D2GEOM_CYTHON_BUILD_SHARED=OFF',
+    ' -D2GEOM_CYTHON_BINDINGS=OFF',
+    ' -DBUILD_TESTING=OFF',
+    ' -D2GEOM_PERFOMANCE_TESTS=OFF -L ',
     ])
 
 def setup():
@@ -29,16 +28,15 @@ def setup():
 #    shelltools.system("find share/extensions -iname '*.py' -exec sed -i 's|env\ python$|env python3|g' {} \;")
     #if with imagemagick build.
 #    shelltools.export("PKG_CONFIG_PATH", "${PKG_CONFIG_PATH}:/usr/lib/imagemagick6/pkgconfig")
-    shelltools.makedirs("build")
-    shelltools.cd("build")
-    cmaketools.configure(j, sourceDir = '..')
+#    shelltools.export("JOBS", "-j5")
+    cmaketools.configure("-B_build %s" % j)
 
 def build():
-    shelltools.cd("build")
+    shelltools.cd("_build")
     cmaketools.make()
 
 def install():
-    shelltools.cd("build")
+    shelltools.cd("_build")
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("../AUTHORS", "../NEWS.md", "../README.md")
