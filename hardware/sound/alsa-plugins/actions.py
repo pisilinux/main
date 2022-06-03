45de3fe5b56e5230a0b9d6642a155356a3351f71
Rmys <muscnsl@gmail.com>
2022-06-03 17:20:43 +0300

    alsa-plugins rebuild

--- hardware/sound/alsa-plugins/actions.py
+++ hardware/sound/alsa-plugins/actions.py
@@ -20,8 +20,12 @@ def build():
     
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    shelltools.system("rm -v %s/etc/alsa/conf.d/98-maemo.conf" % get.installDIR())
-    shelltools.cd("%s" % get.installDIR())
+    pisitools.remove("/etc/alsa/conf.d/98-maemo.conf")
+    pisitools.domove("/etc/alsa/conf.d/99-pulseaudio-default.conf.example", "/usr/share/alsa/alsa.conf.d", "99-pulseaudio-default.conf")
+    pisitools.dosym("/usr/share/alsa/alsa.conf.d/99-pulseaudio-default.conf", "/etc/alsa/conf.d/99-pulseaudio-default.conf")
+    
+    #shelltools.system("rm -v %s/etc/alsa/conf.d/98-maemo.conf" % get.installDIR())
+    #shelltools.cd("%s" % get.installDIR())
     #shelltools.system("cp -r /etc/alsa/conf.d/99-pulseaudio-default.conf.example %s/usr/share/alsa/alsa.conf.d/99-pulseaudio-default.conf" % get.installDIR())
-    shelltools.system("ln -st  %s/etc/alsa/conf.d/ /usr/share/alsa/alsa.conf.d/99-pulseaudio-default.conf" % get.installDIR())
+    #shelltools.system("ln -st  %s/etc/alsa/conf.d/ /usr/share/alsa/alsa.conf.d/99-pulseaudio-default.conf" % get.installDIR())
 
