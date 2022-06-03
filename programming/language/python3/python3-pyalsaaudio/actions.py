dbc18ec1c079faec1a29830a51d5edb80efdd39c
Rmys <muscnsl@hotmail.com>
2022-06-03 15:14:09 +0300

    alsa-lib ver. bump

--- programming/language/python3/python3-pyalsaaudio/actions.py
+++ programming/language/python3/python3-pyalsaaudio/actions.py
@@ -5,11 +5,24 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
 #WorkDir="pyalsaaudio-%s" % get.srcVERSION()
 
 def build():
+    shelltools.copytree("../pyalsaaudio-%s" % (get.srcVERSION().replace("_", "~")), "../pyalsaaudio-%s-py2" % get.srcVERSION())
+
+
+    shelltools.cd("../pyalsaaudio-%s-py2" % get.srcVERSION())
+    pythonmodules.compile()
+
+    shelltools.cd("../pyalsaaudio-%s" % get.srcVERSION())
     pythonmodules.compile(pyVer="3")
 
 def install():
+    shelltools.cd("../pyalsaaudio-%s-py2" % get.srcVERSION())
+    pythonmodules.install()
+
+    shelltools.cd("../pyalsaaudio-%s" % get.srcVERSION())
     pythonmodules.install(pyVer="3")
