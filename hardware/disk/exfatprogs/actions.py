354df73e1201bf9ef812f22b1f7050c98a106d97
Rmys <muscnsl@gmail.com>
2022-06-05 12:39:35 +0300

    exfatprogs fix

--- hardware/disk/exfatprogs/actions.py
+++ hardware/disk/exfatprogs/actions.py
@@ -2,37 +2,18 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
+from pisi.actionsapi import autotools, pisitools, get
 
 def setup():
-    shelltools.export("OPTIMIZER", "%s" % get.CFLAGS())
-    shelltools.export("DEBUG", "-DNDEBUG")
-
-    autotools.configure("--enable-readline=yes \
-                         --enable-blkid=yes")
+	autotools.configure("--disable-static")
 
 def build():
-    autotools.make()
+	autotools.make()
 
 def install():
-    autotools.rawInstall("DIST_ROOT=%s" % get.installDIR())
-    autotools.rawInstall("DIST_ROOT=%s" % get.installDIR(), "install-dev")
-    # Needed for building the QA testsuite
-    #autotools.rawInstall("DIST_ROOT=%s" % get.installDIR(), "install-qa")
+	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    # Nuke static libraries
-    #pisitools.remove("/lib/libhandle.a")
-    #pisitools.remove("/lib/libhandle.la")
-    #pisitools.remove("/usr/lib/*.a")
+	pisitools.dodoc("NEWS")
 
-    # Fix the symlink
-    #pisitools.remove("/usr/lib/libhandle.so")
-    #pisitools.dosym("/lib/libhandle.so.1", "/usr/lib/libhandle.so")
-
-    # Set +x bit for the library
-    shelltools.chmod("%s/lib/libhandle.so.*.*.*" % get.installDIR(), 0755)
