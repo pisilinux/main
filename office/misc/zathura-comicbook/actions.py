6a8344d53d77a018e9faadaed853635bd2ce77a4
4fury-c3440d8 <uglyside@yandex.ru>
2022-06-11 09:52:47 +0300

    new applications: add zathura.

---
+++ office/misc/zathura-comicbook/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
+
+from pisi.actionsapi import mesontools, pisitools
+
+def setup():
+	mesontools.configure()
+
+def build():
+	mesontools.build()
+
+def install():
+	mesontools.install()
+
+	pisitools.dodoc("AUTHORS")
