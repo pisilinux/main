d4d83a4095462e501822c2d0b9c7b981d482f4e3
Rmys <muscnsl@gmail.com>
2022-06-19 18:26:15 +0300

    librest-0.9.1

--- desktop/gnome/base/librest/actions.py
+++ desktop/gnome/base/librest/actions.py
@@ -7,30 +7,34 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import mesontools
 from pisi.actionsapi import get
 
 def setup():
     options = "--prefix=/usr \
-               --sysconfdir=/etc"
+               -Dgtk_doc=false \
+               -Dsoup2=true \
+               --sysconfdir=/etc \
+              "
                
     if get.buildTYPE() == "_emul32":
         options += " --libdir=/usr/lib32 \
                      --bindir=/_emul32/bin \
                      --sbindir=/_emul32/sbin \
-                     --disable-dtrace"
+                   "
                      
         shelltools.export("CC", "%s -m32" % get.CC())
         shelltools.export("CXX", "%s -m32" % get.CXX())
         shelltools.export("PKG_CONFIG_PATH", "/usr/lib32/pkgconfig")
         
-    autotools.configure(options)
+    mesontools.configure(options)
 
-    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+    #pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
-    autotools.make()
+    mesontools.build()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    mesontools.install()
     
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "README")
+    pisitools.dodoc("AUTHORS", "COPYING*", "README*")
