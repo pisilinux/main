ab2431a9ad785075119561c6031c9796ef0f9035
Rmys <muscnsl@gmail.com>
2022-06-25 12:51:56 +0300

    sudo-1.9.11_p3

--- util/admin/sudo/actions.py
+++ util/admin/sudo/actions.py
@@ -40,4 +40,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("README.LDAP", "README")
+    pisitools.dodoc("README.LDAP*", "LICENSE*", "README*")
