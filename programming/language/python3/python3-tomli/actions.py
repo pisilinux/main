292eb3f05fdd2ef13b546c206665df04cff88a41
Rmys <muscnsl@hotmail.com>
2022-07-02 15:25:37 +0300

    python3 tomli.pep517.build

---
+++ programming/language/python3/python3-tomli/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+
+def build():
+    shelltools.system("python3 -m build -wn")
+
+def install():
+    shelltools.system("PIP_CONFIG_FILE=/dev/null pip3 install --isolated --root=%s --ignore-installed --no-deps dist/*.whl" % get.installDIR())
+
+    pisitools.dodoc("LICENSE", "README*")
