ffb6b539a7025b67c1cc9ed551eff7ac4de3e1f6
Rmys <muscnsl@hotmail.com>
2022-07-03 15:37:53 +0300

    zenity-3.42.0

--- desktop/gnome/base/zenity/actions.py
+++ desktop/gnome/base/zenity/actions.py
@@ -18,4 +18,4 @@ def build():
 def install():
     mesontools.install()
 
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README*")
