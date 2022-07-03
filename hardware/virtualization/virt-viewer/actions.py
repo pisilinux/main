6c70bdb38675d48a2b6cfbcd561fec040ece71ee
suvari <suvarice@gmail.com>
2022-07-03 18:51:41 +0300

    virt-viewer: new package

---
+++ hardware/virtualization/virt-viewer/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import mesontools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    mesontools.configure()
+
+def build():
+    mesontools.build()
+    #shelltools.system("ninja -C build")
+
+def check():
+    mesontools.build("test")
+
+def install():
+    mesontools.install()
+    pisitools.dodoc("COPYING", "README.md")
