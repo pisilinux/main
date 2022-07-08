8b57cedc84ef5a06b014f705f741ca43a9d6b026
Rmys <muscnsl@gmail.com>
2022-07-08 16:02:29 +0300

    virt-manager-4.0.0

--- hardware/virtualization/virt-manager/actions.py
+++ hardware/virtualization/virt-manager/actions.py
@@ -4,16 +4,19 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
 def setup():
-    shelltools.system("sed -i 's|python2|python2.7|g' {setup.py,virt-convert,virt-clone,virt-install,virt-manager,virt-xml}")
-    
-    pythonmodules.configure()
-    pythonmodules.compile()
+    pythonmodules.configure("--default-graphics=spice --default-hvs qemu,xen,lxc", pyVer="3")
+    pythonmodules.compile(pyVer="3")
 
 def install():
-    pythonmodules.install()
+    shelltools.system("""touch --no-create %s/usr/share/icons/hicolor >/dev/null 2>&1 || :
+                                /usr/bin/update-desktop-database > /dev/null 2>&1 || :""" % get.installDIR())
+
+    shelltools.system("python3 setup.py --no-update-icon-cache --no-compile-schemas install --root=%s --no-compile -O0" % get.installDIR())
+    #pythonmodules.install(pyVer="3")
     #pisitools.dodoc("COPYING", "NEWS", "README*", "PKG-INFO")
