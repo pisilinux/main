2937ef2ad57b2b26317a574625a879595b57e662
suvari <suvarice@gmail.com>
2022-07-10 21:23:30 +0300

    new packages: ufw, isoimagewriter for kde

---
+++ network/filter/ufw/6/evince/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import mesontools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    #shelltools.system("sed -i -e 's:T1_initLib:T1_InitLib:' meson.build || die")
+    mesontools.configure("-Dps=enabled -Ddvi=disabled -Dsystemduserunitdir=no -Dplatform=gnome")
+
+def build():
+    mesontools.build()
+    
+def install():
+    mesontools.install()
+    
+    pisitools.dodoc("AUTHORS", "NEWS*", "README*", "COPYING", "TODO")
---
+++ network/filter/ufw/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+
+def build():
+    pythonmodules.compile(pyVer="3")
+    shelltools.system("python3 setup.py build")
+
+def check():
+    pass
+    #pythonmodules.run("setup.py test", pyVer="3")
+    #pythonmodules.run("test/run_all_unittests.py",pyVer="3")
+    #pythonmodules.run("test/run_examples_test.py",pyVer="3")
+
+def install():
+    pythonmodules.install(pyVer="3")
+
+
+    pisitools.dodoc("COPYING", "README*")
