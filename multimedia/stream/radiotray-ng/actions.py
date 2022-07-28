ff374f4a0fb5403b6c4461155315d8b6d02eec06
Rmys <muscnsl@gmail.com>
2022-07-28 15:05:19 +0300

    radiotray-ng

--- multimedia/stream/radiotray-ng/actions.py
+++ multimedia/stream/radiotray-ng/actions.py
@@ -4,9 +4,10 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import cmaketools, pisitools, get
+from pisi.actionsapi import cmaketools, pisitools, shelltools, get
 
 def setup():
+	shelltools.system("sed -i 's:-Werror::' CMakeLists.txt")
 	cmaketools.configure("-DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF -L")
 
 def build():
