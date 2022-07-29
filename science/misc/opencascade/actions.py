1070d342f6198d92908d288d2eaac215758127da
Rmys <muscnsl@gmail.com>
2022-07-30 01:47:45 +0300

    opencascade ver. bump

--- science/misc/opencascade/actions.py
+++ science/misc/opencascade/actions.py
@@ -12,14 +12,14 @@ def setup():
     options_cfg = ''.join([
                   '-DUSE_TBB=ON ',
                   '-DUSE_VTK=True ',
-                  '-DINSTALL_VTK=False',
+                  '-DINSTALL_VTK=False ',
                   '-DUSE_GL2PS=ON ',
                   '-DUSE_FFMPEG=ON ',
                   '-DUSE_FREEIMAGE=OFF ',
                   '-DINSTALL_DIR_LIB=/usr/lib ',
-                  '-DCMAKE_INSTALL_PREFIX=/usr',
-                  '-D3RDPARTY_VTK_LIBRARY_DIR=/usr/lib',
-                  '-D3RDPARTY_VTK_INCLUDE_DIR=/usr/include',
+                  '-DCMAKE_INSTALL_PREFIX=/usr ',
+                  '-D3RDPARTY_VTK_LIBRARY_DIR=/usr/lib ',
+                  '-D3RDPARTY_VTK_INCLUDE_DIR=/usr/include ',
                   '-DINSTALL_DIR_CMAKE=/usr/lib/cmake/opencascade',
                   ])
     shelltools.makedirs("build")
