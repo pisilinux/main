2f29546edc4d1c34f97e5e045a607dedb986e461
4fury-c3440d8 <uglyside@yandex.ru>
2022-07-30 10:56:05 +0300

    wxPython python2.

--- programming/language/python/wxPython/actions.py
+++ programming/language/python/wxPython/actions.py
@@ -1,23 +1,22 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import autotools
 from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-options="WXPORT=gtk3 UNICODE=1 WX_CONFIG=/usr/bin/wx-config-gtk3"
+shelltools.export("JOBS", get.makeJOBS().replace("-j5", "5"))
+options="WXPORT=gtk3 WX_CONFIG=/usr/bin/wx-config-gtk3"
 
 def build():
-    shelltools.cd("wxPython")
     pythonmodules.compile(options)
 
 def install():
-    shelltools.cd("wxPython")
     pythonmodules.install(options)
-
-    pisitools.dohtml("docs/*")
-    pisitools.dodoc("docs/*.txt")
+    # install only python libraries
+    pisitools.removeDir("/usr/bin")
+    pisitools.removeDir("/usr/share")
