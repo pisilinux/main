7cae73bbf27ed765d8ab0fdd06b580c004348f2c
4fury-c3440d8 <uglyside@yandex.ru>
2022-07-30 10:57:31 +0300

    wxPython rebuild python3.

---
+++ programming/language/python3/wxPython3/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+shelltools.export("JOBS", get.makeJOBS().replace("-j5", "5"))
+options="WXPORT=gtk3 WX_CONFIG=/usr/bin/wx-config-gtk3"
+
+def build():
+    pythonmodules.compile(pyVer = '3', parameters = options)
+
+def install():
+    pythonmodules.install(pyVer = '3', parameters = options)
