41501650f7444cb4a326c40a0b6122a22601e18a
Rmys <muscnsl@hotmail.com>
2022-08-11 14:07:40 +0300

    dleyna-core,server,dbus,renderer new package

---
+++ network/library/dleyna-renderer/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import mesontools
+from pisi.actionsapi import get
+
+def setup():
+    pisitools.dosed("libdleyna/renderer/meson.build", "gupnp-1.2", "gupnp-1.6")
+    mesontools.configure()
+
+def build():
+    mesontools.build()
+
+def install():
+    mesontools.install()
+    
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README*")
