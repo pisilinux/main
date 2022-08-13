17eaa299c839512465e521f5caaca92d96ccb4cb
4fury-c3440d8 <uglyside@yandex.ru>
2022-08-13 18:57:11 +0300

    geeqie major version bump.

--- multimedia/graphics/geeqie/actions.py
+++ multimedia/graphics/geeqie/actions.py
@@ -4,32 +4,15 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
-
-j = ''.join([
-    ' --disable-map',
-    ' --disable-pdf',
-    ' --disable-djvu',
-    ' --disable-gtk3',
-    ' --disable-gpu-accel',
-    ' --disable-debug-log '
-    ])
+from pisi.actionsapi import mesontools, pisitools
 
 def setup():
-	pisitools.cflags.add("-Wno-deprecated-declarations")
-	shelltools.system("NOCONFIGURE=1 ./autogen.sh")
-
-	autotools.autoreconf("-vif")
-	autotools.configure(j)
+	mesontools.configure()
 
 def build():
-	autotools.make()
+	mesontools.build()
 
 def install():
-	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+	mesontools.install()
 
 	pisitools.dodoc("AUTHORS", "NEWS")
-	pisitools.removeDir("/usr/share/doc/geeqie-%s" % get.srcVERSION())
