b0545d4f9e445bf2b7cde04d8f7f93364c291b84
Rmys <muscnsl@gmail.com>
2022-08-17 22:44:11 +0300

    libgnomekbd-3.28.0

--- desktop/gnome/base/libgnomekbd/actions.py
+++ desktop/gnome/base/libgnomekbd/actions.py
@@ -10,7 +10,7 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    #autotools.autoreconf("-fi")
+    autotools.autoreconf("-fi")
     autotools.configure("--disable-static \
                          --enable-introspection")
 
