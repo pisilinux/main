7db67358d8ebff13a0f7a87900da68a94ed753ec
Rmys <muscnsl@gmail.com>
2022-08-22 13:05:30 +0300

    gnome-session rebuild

--- desktop/gnome/base/gnome-session/actions.py
+++ desktop/gnome/base/gnome-session/actions.py
@@ -6,15 +6,22 @@
 
 from pisi.actionsapi import mesontools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
 def setup():
-    mesontools.configure("-Dsystemd=false \
+    shelltools.system("sed 's@/bin/sh@/bin/sh -l@' -i gnome-session/gnome-session.in")
+    shelltools.system("""sed -i "/  systemd_dep/,+3d;/if enable_systemd/a \ " meson.build""")
+
+    mesontools.configure("--buildtype=release \
+                          -Dsystemd=false \
                           -Dsystemd_session=disable \
-                          -Dsystemd_journal=false \
-                          -Delogind=true")
+                          -Dsystemd_journal=false")
 
 def build():
     mesontools.build()
 
 def install():
     mesontools.install()
+
+    #shelltools.system("rm -rv %s/tmp/{*.d,*.target,*.service}" % get.installDIR())
