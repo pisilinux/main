8af8bb753a948739626d3cde82f3c58aad4d03f4
Rmys <muscnsl@hotmail.com>
2022-09-24 21:00:16 +0300

    kcolorpicker, kimageannotator new package

---
+++ programming/library/kcolorpicker/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    cmaketools.configure("-DBUILD_SHARED_LIBS=ON")
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("LICENSE", "README*")
