5b4fe7128e76d3ff1e5249cc71633872992e9b39
Rmys <muscnsl@gmail.com>
2022-09-26 12:29:17 +0300

    libgit2-1.5.0

--- programming/scm/libgit2/actions.py
+++ programming/scm/libgit2/actions.py
@@ -14,6 +14,7 @@ from pisi.actionsapi import get
 
 def setup():
     cmaketools.configure("-DTHREADSAFE:BOOL=ON \
+                          -DREGEX_BACKEND=pcre2 \
                           -DUSE_HTTP_PARSER=system")
 
 def build():
