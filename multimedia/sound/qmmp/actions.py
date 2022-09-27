cc871c90799cbba621771291257961d82bf7a5eb
4fury-c3440d8 <uglyside@yandex.ru>
2022-09-27 21:24:20 +0300

    qmmp.

--- multimedia/sound/qmmp/actions.py
+++ multimedia/sound/qmmp/actions.py
@@ -2,22 +2,25 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import cmaketools
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
+from pisi.actionsapi import shelltools, cmaketools, pisitools, get
+
+j = ''.join([
+    ' -Bbuild',
+    ' -DCMAKE_INSTALL_PREFIX=/usr',
+    ' -DCMAKE_INSTALL_LIBDIR=lib',
+    ' -L '
+    ])
 
 def setup():
-    pisitools.dosed("src/plugins/Input/cdaudio/decoder_cdaudio.cpp", "cdio\/cdda\.h", r"cdio/paranoia/cdda.h")
-    cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr \
-                          -DCMAKE_INSTALL_LIBDIR=lib \
-                          -DUSE_HAL:BOOL=FALSE")
+#    pisitools.dosed("src/plugins/Input/cdaudio/decoder_cdaudio.cpp", "cdio\/cdda\.h", r"cdio/paranoia/cdda.h")
+    cmaketools.configure(j)
 
 def build():
+    shelltools.cd("build")
     cmaketools.make()
 
 def install():
+    shelltools.cd("build")
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-    pisitools.dodoc("AUTHORS", "COPYING", "ChangeLog*", "README*")
