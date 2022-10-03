4aa7d01c056e6de49f762d33a35b36875263aa4f
Rmys <muscnsl@hotmail.com>
2022-10-03 14:41:37 +0300

    thunderbird ver. bump

--- network/mail/thunderbird/actions.py
+++ network/mail/thunderbird/actions.py
@@ -14,6 +14,7 @@ ObjDir = "build"
 MOZAPPDIR= "/usr/lib/thunderbird"
 shelltools.export("SHELL", "/bin/sh")
 shelltools.export("MACH_USE_SYSTEM_PYTHON", "1")
+shelltools.export("MOZBUILD_STATE_PATH", "mozbuild")
 
 locales = "be  ca  da  de  el  en-GB en-US  es-AR  es-ES  fi  fr  hr  hu  it  lt nl  pl  pt-BR  pt-PT  ro  ru  sr  sv-SE  tr  uk".split()
 xpidir = "%s/xpi" % get.workDIR()
