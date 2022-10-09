d8fbd7d94748dcf4724a27372eca359f3b70dbb2
Rmys <muscnsl@gmail.com>
2022-10-09 19:23:55 +0300

    QT6

--- desktop/toolkit/qt6/qt6-imageformats/actions.py
+++ desktop/toolkit/qt6/qt6-imageformats/actions.py
@@ -11,37 +11,20 @@ from pisi.actionsapi import cmaketools
 from pisi.actionsapi import qt6
 from pisi.actionsapi import get
 
-bindirQt6="/usr/lib/qt6/bin"
+# bindirQt6="/usr/lib/qt6/bin"
 
 def setup():
-    options = " -DCMAKE_INSTALL_PREFIX=%s \
-                    -DCMAKE_BUILD_TYPE=Release \
-                    -DINSTALL_BINDIR=%s \
-                    -DINSTALL_INCLUDEDIR=%s \
-                    -DINSTALL_ARCHDATADIR=%s \
-                    -DINSTALL_DOCDIR=%s \
-                    -DINSTALL_DATADIR=%s \
-                    -DINSTALL_MKSPECSDIR=%s \
-                    -DINSTALL_EXAMPLESDIR=%s \
-                    -DQT_FEATURE_libproxy=ON \
-                    -DQT_FEATURE_vulkan=ON \
-                    -DQT_FEATURE_system_freetype=ON \
-                    -DQT_FEATURE_system_harfbuzz=ON \
-                    -DQT_FEATURE_system_sqlite=ON \
-                    -DQT_FEATURE_dbus_linked=ON \
-                    -DQT_FEATURE_openssl_linked=ON" % (qt6.prefix, bindirQt6, qt6.headerdir, qt6.archdatadir, qt6.docdir, qt6.datadir, qt6.mkspecsdir, qt6.examplesdir)
-    
-    cmaketools.configure(options)
+    qt6.configure()
 
 def build():
     qt6.make()
     #qt6.make("docs")
 
 def install():
-    qt6.install("INSTALL_ROOT=%s" % get.installDIR())
+    qt6.install()
 
     #I hope qtchooser will manage this issue
-    #for bin in shelltools.ls("%s/usr/lib/qt6/bin" % get.installDIR()):
-        #pisitools.dosym("/usr/lib/qt6/bin/%s" % bin, "/usr/bin/%s-qt6" % bin)
+    for bin in shelltools.ls("%s/usr/lib/qt6/bin" % get.installDIR()):
+        pisitools.dosym("/usr/lib/qt6/bin/%s" % bin, "/usr/bin/%s-qt6" % bin)
 
-    pisitools.dodoc("LICENSE*")
+    pisitools.dodoc("LICENSES/*")
