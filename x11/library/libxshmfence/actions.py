8fdcfa92ee9d9c16ae3efff6f329d7853b746db7
Rmys <muscnsl@gmail.com>
2022-10-19 12:38:24 +0300

    libxshmfence ver. bump

--- x11/library/libxshmfence/actions.py
+++ x11/library/libxshmfence/actions.py
@@ -20,4 +20,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("ChangeLog", "COPYING", "README")
+    pisitools.dodoc("ChangeLog", "COPYING", "README*")
