ac66950f339d2d8efa8a75cdd821f29247bbe4cd
suvari <suvarice@gmail.com>
2022-10-22 14:27:54 +0300

    plasma-workspace: rebuild for libqalculate, new packages: libcpuid and cpu-x

---
+++ hardware/cpu/libcpuid/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.makedirs("build")
+    shelltools.cd("build")
+
+    cmaketools.configure(sourceDir="..")
+
+def build():
+    cmaketools.make("-C build")
+
+def install():
+    shelltools.cd("build")
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
