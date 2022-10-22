a84247ed404c28cbae9d27b7c93b8b166b6c8113
Rmys <muscnsl@gmail.com>
2022-10-22 18:44:53 +0300

    c-ares ver. bump

--- network/misc/c-ares/actions.py
+++ network/misc/c-ares/actions.py
@@ -4,21 +4,21 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
+
 
 def setup():
     shelltools.export("CXXFLAGS", get.CXXFLAGS().replace("-D_FORTIFY_SOURCE=2", ""))
     shelltools.export("CFLAGS", get.CFLAGS().replace("-D_FORTIFY_SOURCE=2", ""))
-    autotools.configure("--enable-shared \
-                         --disable-static")
+    cmaketools.configure()
 
 def build():
-    autotools.make()
+    cmaketools.make()
 
 def install():
-    autotools.install()
+    cmaketools.install()
 
     pisitools.dodoc("AUTHORS", "README*", "NEWS", "CHANGES", "RELEASE-NOTES", "LICENSE.md")
