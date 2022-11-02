8366bd193c5eb0aa6bd02d2b26ef578a0d7d8790
Rmys <muscnsl@hotmail.com>
2022-11-02 19:49:09 +0300

    gnome ver. bump

--- desktop/gnome/apps/nautilus-python/actions.py
+++ desktop/gnome/apps/nautilus-python/actions.py
@@ -1,28 +1,22 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
+from pisi.actionsapi import mesontools, pisitools, shelltools, get
 
 def setup():
     shelltools.export("PYTHON","/usr/bin/python3")
-
-    autotools.configure("PYTHON=python3 \
-                         --enable-gtk-doc")
-
-
-
-    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+    mesontools.configure()
 
 def build():
-    autotools.make()
+    mesontools.build()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    mesontools.install()
 
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README*")
+    shelltools.copytree("%s/usr/share/pkgconfig" % get.installDIR(), "%s/usr/lib/pkgconfig" % get.installDIR())
+    pisitools.removeDir("/usr/share/pkgconfig")
+
+    pisitools.dodoc("AUTHORS", "COPYING", "README*")
