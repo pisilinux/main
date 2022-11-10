15e563aa73065fe92088258b422f63629cbe2120
4fury-c3440d8 <uglyside@yandex.ru>
2022-11-10 20:25:21 +0300

    xfce4-battery-plugin.

--- desktop/xfce/addon/xfce4-battery-plugin/actions.py
+++ desktop/xfce/addon/xfce4-battery-plugin/actions.py
@@ -4,13 +4,10 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
+from pisi.actionsapi import autotools, pisitools, get
 
 def setup():
     autotools.configure()
-    
     # for fix unused dependency
     pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
 
@@ -20,5 +17,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "NEWS", "README")
-
+    pisitools.dodoc("AUTHORS", "NEWS")
