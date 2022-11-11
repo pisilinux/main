7d92297d0612385f27e415273e38cf809fc51d06
4fury-c3440d8 <uglyside@yandex.ru>
2022-11-11 13:51:57 +0300

    djvulibre.

---
+++ office/misc/djvu/comar/pakhandler.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+
+def updateData(filepath):
+    os.system("rm /usr/share/mime/version")
+#    os.system("update-mime-database /usr/share/mime")
