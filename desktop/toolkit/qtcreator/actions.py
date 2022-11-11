14dc5ce1825c32d2424ec423e2f31632a4743abd
Rmys <muscnsl@hotmail.com>
2022-11-11 18:09:00 +0300

    qtcreator-8.0.2

--- desktop/toolkit/qtcreator/actions.py
+++ desktop/toolkit/qtcreator/actions.py
@@ -3,23 +3,25 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt
 from pisi.actionsapi import autotools
-from pisi.actionsapi import qt5
-from pisi.actionsapi import shelltools
+from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import qt5
 from pisi.actionsapi import get
 
 #WorkDir="qt-creator-opensource-src-%s" % get.srcVERSION()
 def setup():
     pisitools.dosed("qtcreator.pri", "libexec/qtcreator", "lib/qtcreator")
-    pisitools.dosed("src/tools/tools.pro", "libexec", "lib")
-
-    shelltools.system("qmake LLVM_INSTALL_DIR=/usr QBS_INSTALL_DIR=/usr \
-                             CONFIG+=release QMAKE_CFLAGS_ISYSTEM=-I \
-                             DEFINES+=QBS_ENABLE_PROJECT_FILE_UPDATES \
-                             QTC_PREFIX=/usr qtcreator.pro")
-
+    # pisitools.dosed("src/tools/tools.pro", "libexec", "lib")
+    cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr \
+                            -DCMAKE_INSTALL_LIBEXECDIR=lib \
+                            -DWITH_DOCS=ON \
+                            -DBUILD_DEVELOPER_DOCS=ON \
+                            -DBUILD_QBS=OFF \
+                            -DQTC_CLANG_BUILDMODE_MATCH=ON \
+                            -DCLANGTOOLING_LINK_CLANG_DYLIB=ON")
 
 def build():
     qt5.make()
+
 def install():
     qt5.install()
