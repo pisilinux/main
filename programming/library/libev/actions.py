f267b91bd7be9722f7e6ef27b48213d29434041d
4fury-c3440d8 <uglyside@yandex.ru>
2022-11-15 18:13:19 +0300

    libev-4.33.

--- programming/library/libev/actions.py
+++ programming/library/libev/actions.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2018 TUBITAK/UEKAE
-# Licensed under the GNU General Public License, version 2.
-# See the file http://www.gnu.org/copyleft/gpl.txt.
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
-
 
 def setup():
     autotools.autoreconf("-fi")
-    
     autotools.configure()
-    
     pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
@@ -23,5 +18,5 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    
+
     pisitools.remove("/usr/include/event.h")
