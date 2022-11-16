a9dc36aa037341c40fa89d7296919097f9bbb3c2
4fury-c3440d8 <uglyside@yandex.ru>
2022-11-16 10:19:47 +0300

    python3-humanfriendly.

---
+++ programming/language/python3/python3-humanfriendly/actions.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
+
+from pisi.actionsapi import pythonmodules
+
+def build():
+	pythonmodules.compile(pyVer = '3')
+
+def install():
+	pythonmodules.install(pyVer = '3')
