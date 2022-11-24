9744cf71fb842ef6dec74a329c474125388647d4
4fury-c3440d8 <uglyside@yandex.ru>
2022-11-25 00:09:08 +0300

    libopenraw.

--- multimedia/graphics/libopenraw/actions.py
+++ multimedia/graphics/libopenraw/actions.py
@@ -9,7 +9,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-	pisitools.dosed("gnome/libopenraw-gnome-0.3.pc.in", "1", "3")
+#	pisitools.dosed("gnome/libopenraw-gnome-0.3.pc.in", "1", "3")
 	autotools.configure("--enable-gnome --disable-static")
 
 def build():
