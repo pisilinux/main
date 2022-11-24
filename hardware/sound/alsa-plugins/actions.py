8f6263194a79d970efbb7193ada68e0a79f17659
4fury-c3440d8 <uglyside@yandex.ru>
2022-11-24 19:20:28 +0300

    alsa-plugins.

--- hardware/sound/alsa-plugins/actions.py
+++ hardware/sound/alsa-plugins/actions.py
@@ -2,30 +2,32 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
+from pisi.actionsapi import autotools, pisitools, get
+
+i = ''.join([
+    ' -Wno-deprecated-declarations',
+    ' -Wno-restrict',
+    ' -Wno-stringop-truncation',
+    ' -Wno-unused-result '
+    ])
+
+t = "99-pulseaudio-default.conf"
 
 def setup():
-    autotools.configure("--prefix=/usr \
-        --sysconfdir=/etc \
-        --enable-maemo-plugin")
+	pisitools.cflags.add(i)
+	autotools.configure("--enable-maemo-plugin --disable-static --with-speex=lib")
 
 def build():
-    autotools.make()
-    
-    
+	autotools.make()
+
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.remove("/etc/alsa/conf.d/98-maemo.conf")
-    pisitools.domove("/etc/alsa/conf.d/99-pulseaudio-default.conf.example", "/usr/share/alsa/alsa.conf.d", "99-pulseaudio-default.conf")
-    pisitools.dosym("/usr/share/alsa/alsa.conf.d/99-pulseaudio-default.conf", "/etc/alsa/conf.d/99-pulseaudio-default.conf")
-    
-    #shelltools.system("rm -v %s/etc/alsa/conf.d/98-maemo.conf" % get.installDIR())
-    #shelltools.cd("%s" % get.installDIR())
-    #shelltools.system("cp -r /etc/alsa/conf.d/99-pulseaudio-default.conf.example %s/usr/share/alsa/alsa.conf.d/99-pulseaudio-default.conf" % get.installDIR())
-    #shelltools.system("ln -st  %s/etc/alsa/conf.d/ /usr/share/alsa/alsa.conf.d/99-pulseaudio-default.conf" % get.installDIR())
+	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+	pisitools.insinto("/usr/share/alsa/alsa.conf.d", "pulse/%s.example" % t, "%s" % t)
+	pisitools.dosym("/usr/share/alsa/alsa.conf.d/%s" % t, "/etc/alsa/conf.d/%s" % t)
+	pisitools.remove("/etc/alsa/conf.d/98-maemo.conf")
+	pisitools.dosym("/usr/share/alsa/alsa.conf.d/98-maemo.conf", "/etc/alsa/conf.d/98-maemo.conf")
+
+	pisitools.dodoc("COPYING", "COPYING.GPL", "doc/*.txt", "doc/README*")
 
