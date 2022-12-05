1011a34fecb171a8d2cfee738972a9f5673242b5
Rmys <muscnsl@hotmail.com>
2022-12-05 18:50:56 +0300

    tracker-* ver. bump

--- desktop/gnome/base/gnome-control-center/actions.py
+++ desktop/gnome/base/gnome-control-center/actions.py
@@ -11,7 +11,9 @@ from pisi.actionsapi import pisitools
 
 def setup():
     # pisitools.dosed("meson.build", "gcr-base-3", "gcr-4")
-    mesontools.configure("-Dsnap=false")
+    mesontools.configure("-Dsnap=false \
+                                        -Ddistributor_logo=/usr/share/pixmaps/gnome-control-center-pisilinux-logo.svg \
+                                        -Ddark_mode_distributor_logo=/usr/share/pixmaps/gnome-control-center-pisilinux-logo-dark.svg")
 
 def build():
     mesontools.build()
