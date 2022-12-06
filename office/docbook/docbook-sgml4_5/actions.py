dcba72cb04e1f4348c4345382e5d39f6e8839196
Rmys <muscnsl@hotmail.com>
2022-12-06 22:08:23 +0300

    docbook-sgml4_5 rebuild

--- office/docbook/docbook-sgml4_5/actions.py
+++ office/docbook/docbook-sgml4_5/actions.py
@@ -5,11 +5,15 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 WorkDir="."
 
 def install():
+    shelltools.system("sed -i -e '/ISO 8879/d' \
+                                        -e '/gml/d' docbook.cat")
+
     pisitools.insinto("/usr/share/sgml/docbook/sgml-dtd-%s" % get.srcVERSION(), "*.dcl")
     pisitools.insinto("/usr/share/sgml/docbook/sgml-dtd-%s" % get.srcVERSION(), "*.dtd")
     pisitools.insinto("/usr/share/sgml/docbook/sgml-dtd-%s" % get.srcVERSION(), "*.mod")
