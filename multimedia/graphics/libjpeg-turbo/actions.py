2a478f2421fa66d6ac404b77ea27c605e2dc9cc4
Rmys <muscnsl@gmail.com>
2022-12-16 18:39:00 +0300

    libjpeg-turbo-2.1.4

--- multimedia/graphics/libjpeg-turbo/actions.py
+++ multimedia/graphics/libjpeg-turbo/actions.py
@@ -10,7 +10,7 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    options = "-DCMAKE_BUILD_TYPE=Release \
+    options = "-B build -DCMAKE_BUILD_TYPE=Release \
                -DWITH_JPEG8=TRUE \
                -DENABLE_STATIC=FALSE \
                -DCMAKE_INSTALL_DEFAULT_LIBDIR=lib \
@@ -21,18 +21,20 @@ def setup():
         shelltools.export("CXX", "g++ -m32")
         shelltools.export("PKG_CONFIG_PATH","/usr/lib32/pkgconfig")
         
-        options = "-DCMAKE_INSTALL_DEFAULT_LIBDIR=/usr/lib32 \
+        options = "-B build -DCMAKE_INSTALL_DEFAULT_LIBDIR=/usr/lib32 \
                    -DCMAKE_INSTALL_PREFIX=/emul32"
     
     cmaketools.configure(options)
 
 def build():
-    cmaketools.make()
+    cmaketools.make("-C build")
     
 def check():
+    shelltools.cd("build")
     cmaketools.make("test")
 
 def install():
+    shelltools.cd("build")
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
     
     if get.buildTYPE() == "emul32":
@@ -42,4 +44,4 @@ def install():
     
     # provide jpegint.h as it is required by various software
     #pisitools.insinto("/usr/lib/include", "jpegint.h")
-    pisitools.insinto("/usr/include", "jpegint.h")
+    pisitools.insinto("/usr/include", "../jpegint.h")
