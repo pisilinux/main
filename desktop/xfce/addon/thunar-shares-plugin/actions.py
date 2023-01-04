450abe95208a38f101d1087d9e1230de99f30402
Rmys <muscnsl@gmail.com>
2023-01-04 15:15:25 +0300

    thunar-shares-plugin-0.3.2

--- desktop/xfce/addon/thunar-shares-plugin/actions.py
+++ desktop/xfce/addon/thunar-shares-plugin/actions.py
@@ -19,5 +19,5 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README")
+    pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README*")
 
