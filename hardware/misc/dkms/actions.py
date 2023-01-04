2a860e6fcb1518d13a90f5d769c7a5f11653a092
Rmys <muscnsl@hotmail.com>
2023-01-04 19:18:36 +0300

    dkms ver. bump

--- hardware/misc/dkms/actions.py
+++ hardware/misc/dkms/actions.py
@@ -4,13 +4,17 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
+from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
 
 def install():
-    pisitools.dosbin("dkms")
-    pisitools.doman("dkms.8")
-    pisitools.insinto("/usr/share/bash-completion/completions", "dkms.bash-completion", "dkms")
-    pisitools.insinto("/etc/dkms", "dkms_framework.conf", "framework.conf")
-    pisitools.insinto("/var/lib/dkms", "dkms_dbversion")
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    # pisitools.dosbin("dkms")
+    # pisitools.doman("dkms.8")
+    # pisitools.insinto("/usr/share/bash-completion/completions", "dkms.bash-completion", "dkms")
+    # pisitools.insinto("/etc/dkms", "dkms_framework.conf", "framework.conf")
+    # pisitools.insinto("/var/lib/dkms", "dkms_dbversion")
 
-    pisitools.dodoc("AUTHORS", "COPYING", "README*", "TODO")
+    pisitools.dodoc("COPYING", "README*")
