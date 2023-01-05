0e0a22c8d4cbc940573c40ef70781611b6ac5920
4fury-c3440d8 <uglyside@yandex.ru>
2023-01-05 16:55:55 +0300

    fribidi-1.0.12

--- multimedia/misc/fribidi/actions.py
+++ multimedia/misc/fribidi/actions.py
@@ -2,33 +2,29 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
+from pisi.actionsapi import shelltools, autotools, pisitools, get
 
 def setup():
     autotools.autoreconf("-fiv")
-    options = "--disable-static \
-                         --with-pic"
-    
+    options = "--disable-static --with-pic"
+
     if get.buildTYPE()=="emul32":
         shelltools.export("PKG_CONFIG_PATH", "/usr/lib32/pkgconfig")
         options += "--libdir=/usr/lib32"
-    
 
     autotools.configure(options)
-    
+
     pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
-    
+
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    
-    if get.buildTYPE()=="emul32":
+    # unneeded empty manpages.
+    pisitools.removeDir("/usr/share/man")
 
-        pisitools.dodoc("AUTHORS", "NEWS", "README", "ChangeLog", "THANKS", "TODO")
+    if get.buildTYPE()=="emul32":
+        pisitools.dodoc("AUTHORS", "NEWS", "THANKS")
