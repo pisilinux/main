e0ccba274a8b6fdadd80271a2c0553b7064cc152
4fury-c3440d8 <uglyside@yandex.ru>
2023-01-08 04:41:45 +0300

    graphviz-7.0.6.

--- multimedia/graphics/graphviz/actions.py
+++ multimedia/graphics/graphviz/actions.py
@@ -4,30 +4,17 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
+from pisi.actionsapi import shelltools, autotools, pisitools, get
 
 j = ''.join([
-    ' --enable-lefty',
-    ' --disable-r',
-    ' --disable-lua',
-    ' --disable-php',
-    ' --disable-rpath',
     ' --disable-static',
-    ' --disable-dependency-tracking',
-    ' --with-libgd',
-    ' --with-fontconfig',
-    ' --with-pangocairo',
     ' --without-webp',
-    ' --without-devil '
+    ' --disable-dependency-tracking '
     ])
 
 def setup():
     shelltools.export("LIBPOSTFIX", "/")
     shelltools.export("CONFIG_SHELL", "/bin/bash")
-    shelltools.system("NOCONFIGURE=1 ./autogen.sh")
     autotools.configure(j)
 
     pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
