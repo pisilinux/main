b5f99ca1615a7cff45eeedbc25a2466277bf7ed6
4fury-c3440d8 <uglyside@yandex.ru>
2023-03-08 21:08:23 +0300

    tageditor-3.7.8

--- multimedia/editor/tageditor/actions.py
+++ multimedia/editor/tageditor/actions.py
@@ -4,30 +4,22 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import cmaketools
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
+from pisi.actionsapi import mesontools, cmaketools
 
 j = ''.join([
     ' -DCMAKE_BUILD_TYPE=Release',
     ' -DDISABLE_DEPRECATED_QT_FEATURES=ON',
     ' -DQT_PACKAGE_PREFIX=Qt6',
     ' -DJS_PROVIDER=none',
-    ' -DWEBVIEW_PROVIDER=none -L '
+    ' -DWEBVIEW_PROVIDER=none',
+    ' -B_build -G Ninja -L '
     ])
 
 def setup():
-	pisitools.cxxflags.add("-Wno-unused-result")
-	cmaketools.configure("-B_build %s" % j)
+	cmaketools.configure(j)
 
 def build():
-	shelltools.cd("_build")
-	cmaketools.make()
+	mesontools.build("-C _build")
 
 def install():
-	shelltools.cd("_build")
-	cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-	pisitools.dodoc("../LICENSE", "../README.md")
-
+	mesontools.install("-C _build")
