7f796ca520611c2cced6e79086d6fc5c92c5cb47
4fury-c3440d8 <uglyside@yandex.ru>
2023-03-19 10:23:56 +0300

    tini.

--- hardware/virtualization/tini/actions.py
+++ hardware/virtualization/tini/actions.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2018 TUBITAK/UEKAE
-# Licensed under the GNU General Public License, version 2.
-# See the file http://www.gnu.org/copyleft/gpl.txt.
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import cmaketools
-from pisi.actionsapi import get
-
-# WorkDir = ""
-# NoStrip = "/"
+from pisi.actionsapi import cmaketools, pisitools
 
 def setup():
     cmaketools.configure()
@@ -23,8 +15,8 @@ def build():
 def install():
     pisitools.dobin("tini")
     pisitools.dobin("tini-static")
-    
+
     # symlink docker-init (nice integration with docker)
     pisitools.dosym("/usr/bin/tini-static", "/usr/bin/docker-init")
-    
+
     pisitools.dodoc("LICENSE", "README*", "VERSION")
