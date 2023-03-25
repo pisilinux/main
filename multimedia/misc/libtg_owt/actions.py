d549c42ae155e29882404f6887a48bfd05061ce5
Rmys <muscnsl@gmail.com>
2023-03-25 23:24:42 +0300

    libtg_owt ver. bump

--- multimedia/misc/libtg_owt/actions.py
+++ multimedia/misc/libtg_owt/actions.py
@@ -27,7 +27,7 @@ def setup():
     shelltools.move("abseil-cpp/*", "src/third_party/abseil-cpp")
     # shelltools.move("pipewire/*", "src/third_party/pipewire")
     shelltools.system("rm -rf src/third_party/crc32c/src")
-    shelltools.move("crc32c-1.1.2/*", "src/third_party/crc32c")
+    shelltools.move("crc32c-1.1.2/*", "src/third_party/crc32c/src")
 
     pisitools.cflags.add(" -ffat-lto-objects")
     pisitools.cxxflags.add(" -ffat-lto-objects -I/usr/include/libdrm")
