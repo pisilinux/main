8fd2319fdae5cf48d69475aed6cd388998d89505
4fury-c3440d8 <uglyside@yandex.ru>
2023-03-31 07:49:08 +0300

    libchromaprint.

--- multimedia/plugin/libchromaprint/actions.py
+++ multimedia/plugin/libchromaprint/actions.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+#
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/copyleft/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import cmaketools
 from pisi.actionsapi import get
@@ -17,4 +18,3 @@ def install():
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("*txt")
- 
