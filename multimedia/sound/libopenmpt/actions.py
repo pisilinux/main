9ce0576240f40323de7ff15e752e2411dcedf72b
4fury-c3440d8 <uglyside@yandex.ru>
2023-03-31 07:40:53 +0300

    rebuild libopenmpt.

--- multimedia/sound/libopenmpt/actions.py
+++ multimedia/sound/libopenmpt/actions.py
@@ -2,28 +2,20 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/copyleft/gpl.txt 
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import cmaketools
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
+from pisi.actionsapi import autotools, pisitools, get
 
 NoStrip = ["/usr/lib"]
 
 def setup():
 	autotools.autoreconf("-vfi")
-	autotools.configure("--disable-static \
-		                 --without-portaudiocpp")
+	autotools.configure("--disable-static --without-portaudiocpp")
 
-	
 def build():
-	
 	autotools.make()
 
 def install():
 	autotools.rawInstall("DESTDIR=%s" %get.installDIR())
-	
-	pisitools.dodoc("LICENSE", "README.md")
 
+	pisitools.dodoc("LICENSE", "README.md")
