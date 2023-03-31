c7abe595743c8b116cccbb9de23b9cb0cdab6bc5
4fury-c3440d8 <uglyside@yandex.ru>
2023-03-31 07:28:48 +0300

    davs2.

--- multimedia/video/davs2/actions.py
+++ multimedia/video/davs2/actions.py
@@ -4,17 +4,17 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
+from pisi.actionsapi import shelltools, autotools, get
+
+i = ''.join([
+    ' --extra-ldflags="-Wl,-z,noexecstack"',
+    ' --chroma-format="all"',
+    ' --enable-shared '
+    ])
 
 def setup():
-    shelltools.system("patch -p1 --binary < davs2-1.6-gcc8-fix.patch")
     shelltools.cd("build/linux")
-    autotools.configure("--extra-ldflags='-Wl,-z,noexecstack' \
-                         --chroma-format='all' \
-                         --enable-shared ")
+    autotools.configure(i)
 
 def build():
     shelltools.cd("build/linux")
@@ -23,6 +23,3 @@ def build():
 def install():
     shelltools.cd("build/linux")
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    shelltools.cd("../..")
-    pisitools.dodoc("COPYING", "README.md")
-
