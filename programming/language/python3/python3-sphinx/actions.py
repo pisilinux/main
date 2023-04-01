f9c59fca8cea91191f29b286f0a3e1adb56846bd
Rmys <muscnsl@hotmail.com>
2023-04-01 18:06:17 +0300

    python3-sphinx ver. bump

--- programming/language/python3/python3-sphinx/actions.py
+++ programming/language/python3/python3-sphinx/actions.py
@@ -5,11 +5,17 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pythonmodules
-from pisi.actionsapi import shelltools
+from pisi.actionsapi import shelltools, autotools, get
 
+# WorkDir="Sphinx-%s" % get.srcVERSION()
+# t = "dist/sphinx-5.3.0-py3-none-any.whl"
+# shelltools.export("SETUPTOOLS_SCM_PRETEND_VERSION", "5.3.0")
 
 def build():
-    pythonmodules.compile(pyVer="3")
+    shelltools.export("PYTHON", "/usr/bin/python3")
+    pythonmodules.run("-m build --wheel --skip-dependency-check --no-isolation", pyVer="3")
 
 def install():
-    pythonmodules.install(pyVer="3")
+     # py3.run("-m installer -d %s " % get.installDIR(), pyVer = '3')
+    # shelltools.system("python3 -m installer --wheel  DESTDIR=%s" % get.installDIR())
+    pythonmodules.run("-m installer  --destdir='%s'  dist/*.whl" % get.installDIR(), pyVer="3")
