39da3d1471d610f27cd58019b97c94d31ca9c04b
Rmys <muscnsl@gmail.com>
2023-04-05 03:16:18 +0300

    cairo-1.17.8

--- desktop/toolkit/gtk/cairo/actions.py
+++ desktop/toolkit/gtk/cairo/actions.py
@@ -7,34 +7,20 @@
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import mesontools
 from pisi.actionsapi import get
 
 def setup():
     pisitools.flags.add("-flto -ffat-lto-objects")
-    autotools.autoreconf("-vfi")
-    autotools.configure("--disable-static \
-                         --enable-xlib \
-                         --disable-drm \
-                         --enable-xml=yes \
-                         --enable-ft \
-                         --enable-ps \
-                         --enable-pdf \
-                         --enable-svg \
-                         --enable-tee=yes \
-                         --enable-gl=yes \
-                         --enable-gobject \
-                         --enable-xcb \
-                         --disable-gtk-doc")
 
-    pisitools.dosed("libtool", "^(hardcode_libdir_flag_spec=).*", '\\1""')
-    pisitools.dosed("libtool", "^(runpath_var=)LD_RUN_PATH", "\\1DIE_RPATH_DIE")
-    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+    mesontools.configure("-Dtests=disabled \
+                          -Dsymbol-lookup=disabled \
+                          -Dspectre=disabled")
 
 def build():
-    autotools.make()
+    mesontools.build()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    mesontools.install()
 
-    #pisitools.removeDir("/usr/share/gtk-doc")
-    pisitools.dodoc("AUTHORS", "README", "ChangeLog", "NEWS", "COPYING", "COPYING-LGPL-2.1", "COPYING-MPL-1.1")
+    pisitools.dodoc("AUTHORS", "README*", "NEWS", "COPYING*")
