764bffa8ab130c9fe9ffa15b3716031e0db84132
Rmys <muscnsl@gmail.com>
2023-04-18 15:52:56 +0300

    xfce4-settings-4.19.0

--- desktop/xfce/base/xfce4-settings/actions.py
+++ desktop/xfce/base/xfce4-settings/actions.py
@@ -32,5 +32,5 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "TODO")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS")
 
