5928090bc40560d78038c4bc6adaf07266a72009
Rmys <muscnsl@gmail.com>
2023-04-20 23:49:49 +0300

    xfce4-mailwatch-plugin-1.3.1

--- desktop/xfce/addon/xfce4-mailwatch-plugin/actions.py
+++ desktop/xfce/addon/xfce4-mailwatch-plugin/actions.py
@@ -19,5 +19,5 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README*")
 
