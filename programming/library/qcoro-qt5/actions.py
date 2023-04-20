3e1eb35e61edc82b7fdc7aa3f61856ff6170b0bb
Rmys <muscnsl@hotmail.com>
2023-04-20 15:29:18 +0300

    qcoro-qt5 new package

---
+++ programming/library/qcoro-qt5/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    cmaketools.configure("-B build -DCMAKE_INSTALL_PREFIX=/usr \
+                                        -DUSE_QT_VERSION=5 \
+                                        -DBUILD_SHARED_LIBS=ON")
+
+def build():
+    shelltools.cd("build")
+    cmaketools.make()
+
+def install():
+    shelltools.cd("build")
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    shelltools.cd("..")
+    pisitools.dodoc("LICENSE", "README*")
