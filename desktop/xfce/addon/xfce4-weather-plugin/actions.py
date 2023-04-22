1719124e996b0aa045b33efc1faf1ebbc8f3a426
Rmys <muscnsl@gmail.com>
2023-04-23 02:08:24 +0300

    xfce4-weather-plugin

--- desktop/xfce/addon/xfce4-weather-plugin/actions.py
+++ desktop/xfce/addon/xfce4-weather-plugin/actions.py
@@ -19,5 +19,5 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "INSTALL", "NEWS", "README")
+    pisitools.dodoc("AUTHORS", "COPYING",  "NEWS", "README")
 
