3be23862c02d935554cea26e6ddc9d3fda0942f2
4fury-c3440d8 <uglyside@yandex.ru>
2023-04-25 06:27:14 +0300

    monkeys audio codec.

--- multimedia/sound/mac/actions.py
+++ multimedia/sound/mac/actions.py
@@ -4,16 +4,15 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import autotools, get
+from pisi.actionsapi import cmaketools, mesontools
 
 WorkDir = "."
-opts = "prefix=/usr -C Source/Projects/NonWindows"
 
 def setup():
-	pass
+	cmaketools.configure("-DCMAKE_BUILD_TYPE=Release -B_build -G Ninja -L")
 
 def build():
-	autotools.make(opts)
+	mesontools.build("-C _build")
 
 def install():
-	autotools.rawInstall("DESTDIR=%s %s" % (get.installDIR(), opts))
+	mesontools.install("-C _build")
