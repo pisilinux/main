749e81724d667261c68a69808a3f26d160c80f83
Rmys <muscnsl@gmail.com>
2023-06-01 23:11:11 +0300

    timeshift

--- util/admin/timeshift/actions.py
+++ util/admin/timeshift/actions.py
@@ -4,17 +4,16 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import autotools
+from pisi.actionsapi import mesontools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
-#def setup():
-    #autotools.configure()
+def setup():
+    mesontools.configure()
 
 def build():
-    autotools.make()
+    mesontools.build()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    mesontools.install()
 
     pisitools.dodoc("AUTHORS", "LICENSE*", "COPYING", "README*")
