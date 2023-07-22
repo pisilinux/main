6191ab9dff7f5a140fe9ae450652e6bca0802cef
Rmys <muscnsl@gmail.com>
2023-07-22 20:21:35 +0300

    glibmm-2.68

--- desktop/gnome2/glibmm-2.68/actions.py
+++ desktop/gnome2/glibmm-2.68/actions.py
@@ -1,22 +1,23 @@
-#!/usr/bin/env python
+##!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import mesontools
-from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
 
 def setup():
-    mesontools.configure("-Dlibdbus=auto")
+    mesontools.configure()
 
 def build():
     mesontools.build()
-
+    
 def install():
     mesontools.install()
-    
-    pisitools.dodoc("COPYING", "README*")
+
+    #dirs = ["/usr/share/doc", "/usr/share/devhelp"]
+    #for dir in dirs:
+        #pisitools.removeDir(dir)
+
+    pisitools.dodoc("COPYING*", "NEWS", "README*")
