7774bdb086cd684881df79eeadbf347f11a2bba2
Rmys <muscnsl@gmail.com>
2023-09-21 00:07:34 +0300

    xorg-xwayland-23.2.1

--- x11/wayland/xorg-xwayland/actions.py
+++ x11/wayland/xorg-xwayland/actions.py
@@ -14,6 +14,8 @@ def setup():
     mesontools.configure("-Dipv6=true \
                           -Dxcsecurity=true \
                           -Ddri3=true \
+                          -D xvfb=false \
+                          -D xdmcp=false \
                           -Dxwayland_eglstream=true \
                           -Dglamor=true \
                           -Dxkb_dir=/usr/share/X11/xkb \
