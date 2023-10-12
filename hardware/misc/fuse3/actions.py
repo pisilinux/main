5ba14e2d8c3edf5e748b9298a4fca304c833e170
Rmys <muscnsl@hotmail.com>
2023-10-12 14:30:26 +0300

    fuse3, libgusb, ModemManager, libmbim, libqmi, zeromq ver. bump

--- hardware/misc/fuse3/actions.py
+++ hardware/misc/fuse3/actions.py
@@ -28,7 +28,7 @@ def install():
     
     pisitools.dosym("/usr/lib/libfuse3.so", "/lib/libfuse3.so")
     pisitools.dosym("/usr/lib/libfuse3.so", "/lib/libfuse3.so.3")
-    pisitools.dosym("/usr/lib/libfuse3.so", "/lib/libfuse3.so.3.16.1")
+    pisitools.dosym("/usr/lib/libfuse3.so", "/lib/libfuse3.so.3.16.2")
     
     # Move pkgconfig file to /usr/lib
     #pisitools.domove("/lib/pkgconfig", "/usr/lib/")
