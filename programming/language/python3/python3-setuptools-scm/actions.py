6f36e31dfdbf3a19fec14f4ba3a97cf1ded88736
Rmys <muscnsl@hotmail.com>
2023-10-16 20:55:30 +0300

    python3-* rebuild

--- programming/language/python3/python3-setuptools-scm/actions.py
+++ programming/language/python3/python3-setuptools-scm/actions.py
@@ -4,18 +4,21 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import python3modules
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
+
+WorkDir="setuptools_scm-%s" % get.srcVERSION()
 shelltools.export("SETUPTOOLS_SCM_PRETEND_VERSION","7.1.0")
 
 def build():
-    pythonmodules.compile(pyVer="3")
+    python3modules.compile(pyVer="3")
     
-    #pythonmodules.run("setup.py build_sphinx", pyVer="3")
+    #python3modules.run("setup.py build_sphinx", pyVer="3")
     
-def check():
-    pythonmodules.compile("test", pyVer="3")
+# def check():
+    # python3modules.compile("test", pyVer="3")
 
 def install():
-    pythonmodules.install(pyVer="3")
+    python3modules.install(pyVer="3")
