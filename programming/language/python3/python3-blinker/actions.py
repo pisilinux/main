b109b4b11a1e29da5c3978f69cf968216ffd238b
Rmys <muscnsl@hotmail.com>
2023-10-18 12:28:02 +0300

    python3 libffi icu4c boost rebuild list

--- programming/language/python3/python3-blinker/actions.py
+++ programming/language/python3/python3-blinker/actions.py
@@ -4,15 +4,17 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import python3modules
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
+WorkDir="blinker-%s" % get.srcVERSION()
+
 def build():
-    pythonmodules.compile(pyVer="3")
+    python3modules.compile()
     
-def check():
-    shelltools.system("nosetests3")
+# def check():
+    # shelltools.system("nosetests3")
 
 def install():
-    pythonmodules.install(pyVer="3")
+    python3modules.install()
