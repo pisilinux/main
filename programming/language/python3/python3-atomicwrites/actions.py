c9b3412b6e9c10b9628380a795b6e0ee9106d174
Rmys <muscnsl@hotmail.com>
2023-10-19 08:30:21 +0300

    python3 libffi icu4c boost rebuild list

--- programming/language/python3/python3-atomicwrites/actions.py
+++ programming/language/python3/python3-atomicwrites/actions.py
@@ -4,15 +4,15 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import python3modules
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def build():
-    pythonmodules.compile(pyVer="3")
+    python3modules.compile(pyVer="3")
     
 def check():
-    shelltools.system("PYTHONPATH=build:%s/python-atomicwrites-1.4.0/build/lib py.test" % get.workDIR())
+    shelltools.system("PYTHONPATH=build:%s/python-atomicwrites-1.4.1/build/lib py.test" % get.workDIR())
 
 def install():
-    pythonmodules.install(pyVer="3")
+    python3modules.install(pyVer="3")
