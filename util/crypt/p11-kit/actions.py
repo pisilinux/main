58f03fd5bf37de1136bb8a9aa711bb2da3e670ef
Rmys <muscnsl@gmail.com>
2023-10-28 12:01:04 +0300

    p11-kit ver bump

--- util/crypt/p11-kit/actions.py
+++ util/crypt/p11-kit/actions.py
@@ -7,29 +7,30 @@
 from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
+from pisi.actionsapi import shelltools, mesontools
 
 def setup():
-    options = "--with-module-path=/usr/lib/pkcs11 \
-              "
+    options = "-Dmodule_path=/usr/lib/pkcs11 -Dtrust_paths=/etc/ssl/certs/ca-certificates.crt -Dman=true -Dgtk_doc=true\
+                    "
               
     if get.buildTYPE() == "emul32":
         shelltools.export("CC", "%s -m32" % get.CC())
         shelltools.export("CXX", "%s -m32" % get.CXX())
         shelltools.export("PKG_CONFIG_PATH", "/usr/lib32/pkgconfig")
-        options += "--libdir=/usr/lib32 \
+        options += " --libdir=/usr/lib32 \
                     --libexecdir=/_emul32/libexec \
-                    --with-module-path=/usr/lib32/pkcs11"
+                    --bindir=/_emul32/bin \
+                    -Dmodule_path=/usr/lib32/pkcs11"
                  
                  
-    autotools.configure(options)
+    mesontools.configure(options)
  
       
 def build():
-    autotools.make()
+    mesontools.build()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    mesontools.install()
 
     if get.buildTYPE() == "emul32":
         pisitools.removeDir("_emul32")
