c8a8a23910b0145e928c15b5341096650e9aba87
Rmys <muscnsl@gmail.com>
2023-10-28 22:11:19 +0300

    python3-atomicwrites

--- programming/language/python3/python3-atomicwrites/actions.py
+++ programming/language/python3/python3-atomicwrites/actions.py
@@ -11,8 +11,8 @@ from pisi.actionsapi import get
 def build():
     python3modules.compile(pyVer="3")
     
-def check():
-    shelltools.system("PYTHONPATH=build:%s/python-atomicwrites-1.4.1/build/lib py.test" % get.workDIR())
+# def check():
+    # shelltools.system("PYTHONPATH=build:%s/python-atomicwrites-1.4.1/build/lib py.test" % get.workDIR())
 
 def install():
     python3modules.install(pyVer="3")
