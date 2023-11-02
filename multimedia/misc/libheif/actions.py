ed037f58b77d7d13a1e3418ef99d321567eedbcb
Rmys <muscnsl@gmail.com>
2023-11-02 17:29:53 +0300

    libheif ver. bump

--- multimedia/misc/libheif/actions.py
+++ multimedia/misc/libheif/actions.py
@@ -7,16 +7,23 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import cmaketools
 from pisi.actionsapi import get
 
 def setup():
-    shelltools.system("./autogen.sh")
-    autotools.configure("--enable-static=no --disable-go")
+    cmaketools.configure("-B build \
+                                        -DCMAKE_INSTALL_PREFIX=/usr \
+                                        -DWITH_DAV1D=ON \
+                                        -DWITH_RAV1E=ON \
+                                        -DWITH_SvtEnc=ON")
 
 def build():
-    autotools.make()
+    shelltools.cd("build")
+    cmaketools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    shelltools.cd("build")
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
 
+    shelltools.cd("..")
     pisitools.dodoc("COPYING", "README*")
