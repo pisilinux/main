ec2129255ddb0a024f0d768ab4862cb909c2b5cc
Rmys <muscnsl@hotmail.com>
2023-11-05 09:06:00 +0300

    gdal ver. bump

--- science/gis/gdal/actions.py
+++ science/gis/gdal/actions.py
@@ -9,58 +9,60 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import perlmodules
-from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import python3modules, cmaketools
 
 
 def setup():
     options_cfg = ''.join([
-              '--with-png ',
-              '--with-curl ',
-              '--with-geos ',
-              '--with-hdf5 ',
-              '--with-perl ',
-              '--with-expat ',
-              '--with-mysql ',
-              '--with-netcdf ',
-              '--with-geotiff ',
-              '--with-libtiff ',
-              '--with-sqlite3 ',
-              '--enable-shared ',
-              '--with-openjpeg ',
-              '--with-spatialite'])
+              '-DGDAL_USE_PNG=ON ',
+              '-DGDAL_USE_CURL=ON ',
+              '-DGDAL_USE_GEOS=ON ',
+              '-DGDAL_USE_HDF5=ON ',
+              '-DGDAL_USE_EXPAT=ON ',
+              '-DGDAL_USE_MYSQL=ON ',
+              '-DGDAL_USE_NETCDF=ON ',
+              '-DGDAL_USE_GEOTIFF=ON ',
+              '-DGDAL_USE_TIFF=ON ',
+              '-DGDAL_USE_NETCDF=ON ',
+              '-DGDAL_USE_SQLITE3=ON ',
+              '-DGDAL_USE_OPENJPEG=ON ',
+              '-DBUILD_PYTHON_BINDINGS=ON ',
+              '-DGDAL_USE_SPATIALITE=ON'])
+                  # '--with-perl ',
+                  # '--enable-shared ',
 
     pisitools.cflags.add("-fno-strict-aliasing")
-    autotools.configure("%s" % options_cfg)
+    cmaketools.configure("%s" % options_cfg)
 
     # fix unused direct dependency analysis
-    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+    # pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 
 def build():
-    autotools.make()
+    cmaketools.make()
     # FIXME build man pages
     #autotools.make("man")
     
     # build python bindings
-    shelltools.cd("swig/python")
-    pythonmodules.compile()
+    # shelltools.cd("swig/python")
+    # python3modules.compile()
 
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
     
     # install python bindings
-    shelltools.cd("swig/python")
-    pythonmodules.install()
-    shelltools.cd("../..")
+    # shelltools.cd("swig/python")
+    # python3modules.install()
+    # shelltools.cd("../..")
 
-    perlmodules.removePodfiles()
+    # perlmodules.removePodfiles()
     
     # fix rpath
-    shelltools.system("chrpath -d %s/usr/lib/perl5/x86_64-linux-thread-multi/auto/Geo/GDAL/Const/Const.so" % get.installDIR())
-    shelltools.system("chrpath -d %s/usr/lib/perl5/x86_64-linux-thread-multi/auto/Geo/GDAL/GDAL.so" % get.installDIR())
-    shelltools.system("chrpath -d %s/usr/lib/perl5/x86_64-linux-thread-multi/auto/Geo/GNM/GNM.so" % get.installDIR())
-    shelltools.system("chrpath -d %s/usr/lib/perl5/x86_64-linux-thread-multi/auto/Geo/OSR/OSR.so" % get.installDIR())
-    shelltools.system("chrpath -d %s/usr/lib/perl5/x86_64-linux-thread-multi/auto/Geo/OGR/OGR.so" % get.installDIR())
+    # shelltools.system("chrpath -d %s/usr/lib/perl5/x86_64-linux-thread-multi/auto/Geo/GDAL/Const/Const.so" % get.installDIR())
+    # shelltools.system("chrpath -d %s/usr/lib/perl5/x86_64-linux-thread-multi/auto/Geo/GDAL/GDAL.so" % get.installDIR())
+    # shelltools.system("chrpath -d %s/usr/lib/perl5/x86_64-linux-thread-multi/auto/Geo/GNM/GNM.so" % get.installDIR())
+    # shelltools.system("chrpath -d %s/usr/lib/perl5/x86_64-linux-thread-multi/auto/Geo/OSR/OSR.so" % get.installDIR())
+    # shelltools.system("chrpath -d %s/usr/lib/perl5/x86_64-linux-thread-multi/auto/Geo/OGR/OGR.so" % get.installDIR())
     
     pisitools.dodoc("LICENSE*")
