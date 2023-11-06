6def523bc8c68f04e76f10527a26d1e70f5b80c0
Rmys <muscnsl@hotmail.com>
2023-11-06 14:03:46 +0300

    packagekit-qt6 new package

---
+++ desktop/misc/packagekit-qt6/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.system("mkdir build")
+    shelltools.cd("build")
+    cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr \
+                                        -DBUILD_WITH_QT6=ON", sourceDir="..")
+
+def build():
+    cmaketools.make("-C build")
+
+def install():
+    shelltools.cd("build")
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    shelltools.cd("..")
+    pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README*")
