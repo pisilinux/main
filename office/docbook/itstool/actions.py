2e45fc3baa99d591b5e27c30eafa800dd64897b7
Rmys <muscnsl@gmail.com>
2023-11-17 13:54:19 +0300

    itstool-2.0.7 rebuild py3

--- office/docbook/itstool/actions.py
+++ office/docbook/itstool/actions.py
@@ -4,7 +4,10 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
+
+shelltools.export("PYTHON", "/usr/bin/python3")
 
 def setup():
     autotools.configure()
