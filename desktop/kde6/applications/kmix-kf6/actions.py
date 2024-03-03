f63a8292d694bce4a695b101aa15273041560122
Rmys <muscnsl@hotmail.com>
2024-03-03 19:18:39 +0300

    applications-24.02.0

---
+++ desktop/kde6/applications/kmix-kf6/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import kde6
+
+NoStrip=["/usr/share"]
+
+def setup():
+    kde6.configure("-Dkmix-kf6_KF5_BUILD=ON")
+
+def build():
+    kde6.make()
+
+def install():
+    kde6.install()
