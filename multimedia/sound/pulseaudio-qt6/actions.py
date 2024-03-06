b99f16da403f714d2ec462cbdb2209e1cb172b2e
Rmys <muscnsl@hotmail.com>
2024-03-06 17:21:42 +0300

    pulseaudio-qt6

---
+++ multimedia/sound/pulseaudio-qt6/actions.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.makedirs("build")
+    shelltools.cd("build")
+    cmaketools.configure("-DBUILD_QCH=ON \
+                          -DCMAKE_INSTALL_PREFIX=/usr \
+                          -DCMAKE_INSTALL_LIBDIR=lib \
+                          -DQT_MAJOR_VERSION=6 \
+                          -DBUILD_TESTING=OFF", sourceDir="..")
+
+def build():
+    shelltools.cd("build")
+    cmaketools.make()
+    #cmaketools.make("-C build doc")
+
+def install():
+    pisitools.dodoc("README*", "LICENSES/*")
+
+    shelltools.cd("build")
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    #pisitools.dohtml("doc/html/*")
+
+    #pisitools.remove("/usr/bin/lit2epub")
