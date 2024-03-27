4be8f35594f877ab633266489aff4f44214d044f
Rmys <muscnsl@gmail.com>
2024-03-27 15:12:06 +0300

    signond qt5 rebuild

--- network/library/signond/actions.py
+++ network/library/signond/actions.py
@@ -12,15 +12,31 @@ from pisi.actionsapi import get
 
 
 def setup():
-    shelltools.system("qmake6-qt6 PREFIX=/usr LIBDIR=lib")
+    shelltools.cd("%s" % get.workDIR())
+    shelltools.move("signond-*", "signond-%s" % get.srcVERSION())
+    shelltools.copy("signond-*",  "signond-qt6")
+    shelltools.cd("signond-%s" % get.srcVERSION())
+
+    shelltools.system("qmake PREFIX=/usr LIBDIR=lib")
+
     # qt6.configure()
+    shelltools.cd("../signond-qt6")
+    shelltools.system("qmake6-qt6 PREFIX=/usr LIBDIR=lib")
+
+    shelltools.cd("..")
 
 def build():
     autotools.make()
+
     # qt6.make()
+    shelltools.cd("../signond-qt6")
+    autotools.make()
 
 def install():
     autotools.rawInstall("INSTALL_ROOT=%s" % get.installDIR())
+
     # qt6.install()
+    shelltools.cd("../signond-qt6")
+    autotools.rawInstall("INSTALL_ROOT=%s" % get.installDIR())
 
     pisitools.dodoc("COPYING", "TODO", "README*")
