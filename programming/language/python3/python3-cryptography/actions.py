e772ad9296fd7332ec26001c566827030b7da169
Rmys <muscnsl@hotmail.com>
2024-04-10 11:29:57 +0300

    python3-cryptography-42.0.5

--- programming/language/python3/python3-cryptography/actions.py
+++ programming/language/python3/python3-cryptography/actions.py
@@ -7,7 +7,7 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import python3modules
 from pisi.actionsapi import get
 
 
@@ -16,14 +16,14 @@ def build():
     pisitools.cflags.add("-Wno-stringop-truncation -Wno-deprecated-declarations -Wno-incompatible-pointer-types -Wno-discarded-qualifiers -Wno-sign-conversion -Wno-sign-compare -Wno-unused-function")
     # fix unused direct dependency analysis
     shelltools.export("LDSHARED", "x86_64-pc-linux-gnu-gcc -Wl,-O1,--as-needed -shared -lpthread -Wl,-O1 -Wl,-z,relro -Wl,--hash-style=gnu -Wl,--as-needed -Wl,--sort-common")
-    pythonmodules.compile(pyVer="3")
+    python3modules.compile(pyVer="3")
     # build documentation
     shelltools.export("PYTHONPATH", "%s/cryptography-%s/build/lib.linux-x86_64-cpython-311" % (get.workDIR(), get.srcVERSION()))
     shelltools.cd("docs")
     autotools.make("html")
 
 def install():
-    pythonmodules.install(pyVer="3")
+    python3modules.install(pyVer="3")
     pisitools.dohtml("docs/_build/html/*")
     
     #for dirs in ["vectors"]:
