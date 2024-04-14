40fc6a7c125646dc414aa85d61b98f260200f9aa
Rmys <muscnsl@hotmail.com>
2024-04-14 17:23:32 +0300

    dotconf ver. bump

--- programming/misc/dotconf/actions.py
+++ programming/misc/dotconf/actions.py
@@ -6,8 +6,12 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
 
 def setup():
+    pisitools.dosed("src/dotconf.c", "dotconf_continue_line_new", "dotconf_continue_line")
+    shelltools.system("sed -i 's|cp1, tmp|cp1|g' src/dotconf.c")
     autotools.autoreconf("-i")
     autotools.configure("--disable-static")
 
