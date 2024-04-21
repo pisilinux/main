f905c2dfc29b60115ff81a4c5a6774a8c97459f6
suvari <suvarice@gmail.com>
2024-04-21 15:58:14 +0300

    plank: Switched to Ricotz plank

--- util/dock/plank/actions.py
+++ util/dock/plank/actions.py
@@ -1,19 +1,25 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
+# Copyright 2018 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from pisi.actionsapi import mesontools
+from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
 def setup():
-    mesontools.configure("-Denable-apport=false")
+    #shelltools.system("NOCONFIGURE=1 ./autogen.sh")
+    autotools.autoreconf("-fi")
+    autotools.configure("--with-systemduserunitdir=none \
+        --disable-apport")
 
 def build():
-    mesontools.build()
+    autotools.make()
 
 def install():
-    mesontools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "COPYING*", "NEWS", "README.md")
+    pisitools.dodoc("AUTHORS", "COPYING*", "NEWS", "README")
