d035901993e0b17fe2ac559f587928ec3d5e624d
Rmys <muscnsl@hotmail.com>
2024-05-01 16:43:15 +0300

    cups-filters-2.0.0 ibcupsfilters new package

---
+++ hardware/printer/libcupsfilters/actions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.system("./autogen.sh")
+    autotools.configure("--disable-mutool \
+                         --disable-rpath \
+                         --disable-silent-rules \
+                         --disable-static \
+                         --enable-dbus")
+
+
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README*")
