791c9d14d91ea8b01b666ef43a129b47706f7f15
suvari <suvarice@gmail.com>
2024-06-04 21:36:10 +0300

    new packages: freetds, editline, libxkbcommon: version bump to 1.7.0

---
+++ editor/editline/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+def setup():
+    autotools.configure("--prefix=/usr")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("LICENSE", "README.md")
+
+    # rename conflict man file with libedit
+    pisitools.rename("/usr/share/man/man3/editline.3", "editline-troglobit.3.gz")
+
