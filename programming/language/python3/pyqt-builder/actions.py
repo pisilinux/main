05e8129152db2e90133904e33ad62c764e36a005
Rmys <muscnsl@hotmail.com>
2024-06-09 11:51:30 +0300

    pyqt-builder ver. bump

--- programming/language/python3/pyqt-builder/actions.py
+++ programming/language/python3/pyqt-builder/actions.py
@@ -4,13 +4,16 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import pythonmodules
-from pisi.actionsapi import pisitools
+from pisi.actionsapi import python3modules
+from pisi.actionsapi import pisitools, get
+
+
+WorkDir="pyqt_builder-%s" % get.srcVERSION()
 
 def build():
-    pythonmodules.compile(pyVer="3")
+    python3modules.compile()
 
 def install():
-    pythonmodules.install(pyVer="3")
+    python3modules.install()
 
     pisitools.dodoc("ChangeLog", "LICENSE*", "README*")
