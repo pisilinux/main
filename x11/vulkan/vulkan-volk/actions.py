955babfa3a7d2737213c5ce99a0baeeae859a632
Rmys <muscnsl@hotmail.com>
2024-07-02 16:53:18 +0300

    vulkan-* ver. bump

---
+++ x11/vulkan/vulkan-volk/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    cmaketools.configure("-DCMAKE_BUILD_TYPE=Release \
+                          -DCMAKE_C_FLAGS_RELEASE='-DNDEBUG' \
+                          -DCMAKE_INSTALL_PREFIX=/usr \
+                          -DVOLK_INSTALL=ON")
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("LICENSE*", "README*")
