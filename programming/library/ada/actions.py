4a02fedd4b69ffdb52961a5240a5f742d8c698e9
Rmys <muscnsl@hotmail.com>
2024-08-02 12:22:06 +0300

    ada new package

---
+++ programming/library/ada/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    cmaketools.configure("-B build")
+
+def build():
+    cmaketools.make("-C build")
+
+def install():
+    shelltools.cd("build")
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    shelltools.cd("..")
+    pisitools.dodoc("LICENSE-*", "README*")
