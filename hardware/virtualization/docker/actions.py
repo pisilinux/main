4921df0ed05a5b3367bf480fffb22964b9028989
Rmys <muscnsl@hotmail.com>
2024-08-14 12:13:31 +0300

    docker ver. bump

--- hardware/virtualization/docker/actions.py
+++ hardware/virtualization/docker/actions.py
@@ -11,9 +11,9 @@ from pisi.actionsapi import get
 
 shelltools.export("DISABLE_WARN_OUTSIDE_CONTAINER", "1")
 shelltools.export("AUTO_GOPATH", "1")
-shelltools.export("DOCKER_GITCOMMIT","662f78c")
+shelltools.export("DOCKER_GITCOMMIT","f9522e5")
 shelltools.export("IAMSTATIC", "false")
-shelltools.export("VERSION", "27.0.3")
+shelltools.export("VERSION", "27.1.2")
 shelltools.export("GOROOT","/usr/lib/go")
 shelltools.export("GO111MODULE","off")
 
