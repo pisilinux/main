1bf7a5023d558cb50b0368cf7a1275cd25d0e459
Rmys <muscnsl@hotmail.com>
2024-08-21 15:29:45 +0300

    meson-python rebuild

--- programming/build/meson-python/actions.py
+++ programming/build/meson-python/actions.py
@@ -5,16 +5,20 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import mesontools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import python3modules
 from pisi.actionsapi import get
 
 def setup():
-    mesontools.configure("--prefix=/usr")
+    shelltools.system("sed -e '/ninja/d' -i pyproject.toml")
+    # mesontools.configure("--prefix=/usr")
 
 def build():
-    mesontools.build()
+    python3modules.compile()
+    # mesontools.build()
 
 def install():
-    mesontools.install()
+    python3modules.install()
+    # mesontools.install()
 
     pisitools.dodoc("LICENSE", "README*")
