e7b5a89115102f7b146b8c711f3e647365e6f549
Rmys <muscnsl@hotmail.com>
2024-08-30 11:24:40 +0300

    calligra ver. bump

--- office/calligra/calligra/actions.py
+++ office/calligra/calligra/actions.py
@@ -5,21 +5,22 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import kde5
+from pisi.actionsapi import kde6
 from pisi.actionsapi import get
 
 def setup():
-    kde5.configure("-DPYTHON_EXECUTABLE=python3 \
+    kde6.configure("-DPYTHON_EXECUTABLE=python3 \
                     -DBUILD_TESTING=OFF \
                     -DCMAKE_BUILD_TYPE=debugfull \
                     -DCMAKE_INSTALL_LIBDIR=lib \
                     -Wno-dev  \
+                    -DQT_MAJOR_VERSION=6 \
                     -DBUILD_UNMAINTAINED=ON")
 
 def build():
-    kde5.make()
+    kde6.make()
 
 def install():
-    kde5.install("DESTDIR=%s" % get.installDIR())
+    kde6.install("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("ANNOTATIONS-ODF", "AUTHORS", "COPYING*", "OBSOLETE.TXT", "README*", "TODO-ANNOTATIONS", "doc/status.txt")
