e9ad88b93d9e07ac430014acfe04aace8025f123
Bahar Kurt <kurtbahartr@users.noreply.github.com>
2024-10-20 11:04:44 +0300

    Unorphane and uprev GitHub CLI
    This package was left only with its initial packaging on beta, and I was
    wondering what on Earth I got when I installed it myself.

--- programming/scm/github-cli/actions.py
+++ programming/scm/github-cli/actions.py
@@ -11,10 +11,10 @@ from pisi.actionsapi import get
 def build():
     shelltools.system("go build \
         -trimpath \
-        -ldflags '-extldflags \"%s\" -X github.com/cli/cli/command.Version=v0.5.7 -X github.com/cli/cli/command.BuildDate=2020-02-24' \
+        -ldflags '-extldflags \"%s\" -X github.com/cli/cli/command.Version=v2.59.0 -X github.com/cli/cli/command.BuildDate=2024-10-20' \
         -o 'bin/gh' ./cmd/gh" % get.LDFLAGS())
 
 def install():
-    shelltools.cd("../cli-0.5.7")
+    shelltools.cd("../cli-2.59.0")
     pisitools.insinto("/usr/bin", "bin/gh")
     pisitools.dodoc("LICENSE", "README*")
