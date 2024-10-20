8653659d961d2584e6bb7b263807ef8df8aef9b0
Bahar Kurt <kurtbahartr@users.noreply.github.com>
2024-10-20 11:33:59 +0300

    Initial package release for nvidia-prime
    nvidia-prime is a package originating from Arch Linux, containing
    the prime-run script, meant to allow DEs, and users in some cases,
    to run apps on the NVIDIA GPU exclusively.

---
+++ kernel/tools/nvidia-prime/actions.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Licensed under GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import pisitools
+
+def install():
+    pisitools.dobin("prime-run")
