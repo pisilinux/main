359d8a23b8bfbf6ace4c976d732605373f8e3496
uglyside <uglyside@yandex.ru>
2024-10-24 17:56:14 +0300

    zziplib-0.13.78

--- util/archive/zziplib/actions.py
+++ util/archive/zziplib/actions.py
@@ -4,28 +4,24 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import cmaketools
+from pisi.actionsapi import mesontools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
-
-shelltools.export("JOBS", get.makeJOBS().replace("-j5", "-j1"))
 
 j = ''.join([
-    '-DCMAKE_BUILD_TYPE=Release ',
-    '-DCMAKE_INSTALL_LIBDIR=lib ',
+    ' -DCMAKE_BUILD_TYPE=Release',
+    ' -DCMAKE_INSTALL_LIBDIR=lib',
+    ' -Bbuild -G Ninja -L '
     ])
 
 def setup():
-	shelltools.makedirs("build")
-	shelltools.cd("build")
-	cmaketools.configure("%s -G Ninja -L" % j, sourceDir = '..')
+	cmaketools.configure(j)
 
 def build():
-	shelltools.system("ninja -C build")
+	mesontools.build()
 
 def install():
-	shelltools.system("DESTDIR=%s ninja -C build install" % get.installDIR())
+	mesontools.install()
 
 	pisitools.dodoc("ChangeLog", "COPYING.LIB", "README", "TODO", "docs/COPYING*", "docs/README.SDL")
 
