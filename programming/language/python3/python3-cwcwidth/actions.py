5409253156c313bd87ab361d1eb5b6bc12b82d91
uglyside <uglyside@yandex.ru>
2024-12-21 21:14:48 +0300

    add python3-cwcwidth.

---
+++ programming/language/python3/python3-cwcwidth/actions.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
+
+from pisi.actionsapi import python3modules
+
+def build():
+    python3modules.compile()
+
+def install():
+    python3modules.install()
