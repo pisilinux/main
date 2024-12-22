bb5d7aae2aa9d742b55c87ad07fffa50103b4879
Rmys <muscnsl@hotmail.com>
2024-12-22 18:32:03 +0300

    iwd ver. bump

--- programming/library/liburcu/actions.py
+++ programming/library/liburcu/actions.py
@@ -17,4 +17,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("ChangeLog", "LICENSE", "README*")
+    pisitools.dodoc("ChangeLog", "README*")
