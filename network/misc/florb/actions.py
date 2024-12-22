4b57072a0517e1c272affdd519a91e17452568e3
uglyside <uglyside@yandex.ru>
2024-12-22 10:40:56 +0300

    florb rebuild with omake.

--- network/misc/florb/actions.py
+++ network/misc/florb/actions.py
@@ -5,30 +5,25 @@
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-j = "-DCMAKE_BUILD_TYPE=RelWithDebInfo \
-     -DCMAKE_INSTALL_PREFIX=/usr -L \
-    "
-
-def setup():
-	pisitools.flags.add("-pthread -fpermissive -lXpm")
-	pisitools.unlinkDir("src/i18n/de_DE")
-	shelltools.makedirs("build")
-	shelltools.cd("build")
-	cmaketools.configure(j, sourceDir = '..')
-
 def build():
-	shelltools.cd("build")
-	cmaketools.make()
+	# passed with --ignore-sandbox
+	shelltools.cd("src")
+	pisitools.dosed("fluid/dlg_search.fl", "Double\ resizable", "Double resizable modal")
+	pisitools.dosed("OMakefile", "unroll-loops", "unroll-loops -fpermissive")
+	for t in ["", "i18nupdate", "i18ncompile"]:
+		shelltools.system("omake %s" % t)
 
 def install():
-	pisitools.dobin("build/src/florb")
-	pisitools.dopixmaps("src/res/florb.png")
-	shelltools.chmod("src/res/florb.svg", mode = 0644)
-	pisitools.insinto("/usr/share/icons/hicolor/scalable/apps", "src/res/florb.svg")
+	shelltools.cd("src")
+	shelltools.system("omake PREFIX=%s/usr install" % get.installDIR())
 
-#	pisitools.dodoc("LICENSE.txt", "README.txt")
+	for mo in ["de_DE/LC_MESSAGES", "sv_SE/LC_MESSAGES"]:
+		shelltools.chmod("%s/usr/share/locale/%s/florb.mo" % (get.installDIR(), mo), mode = 0644)
 
+	for i in ["florb.xpm", "res/florb.png", "res/florb.svg"]:
+		pisitools.insinto("/usr/share/pixmaps", i)
+
+	pisitools.dodoc("../LICENSE.txt")
