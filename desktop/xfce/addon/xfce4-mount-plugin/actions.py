5cb1f7126028fd2504c1032027a296b7c6c95e10
Rmys <muscnsl@hotmail.com>
2024-12-23 17:21:38 +0300

    xfce4-mount-plugin-1.1.6

--- desktop/xfce/addon/xfce4-mount-plugin/actions.py
+++ desktop/xfce/addon/xfce4-mount-plugin/actions.py
@@ -23,5 +23,5 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README", "TODO")
+    pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README*")
 
