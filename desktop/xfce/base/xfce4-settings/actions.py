e071c60f217e02d8ef00e1b5b7d76b9e0c90d176
Rmys <muscnsl@hotmail.com>
2024-12-25 18:14:37 +0300

    xfce4-* ver. bump

--- desktop/xfce/base/xfce4-settings/actions.py
+++ desktop/xfce/base/xfce4-settings/actions.py
@@ -18,6 +18,7 @@ i = "--enable-xorg-libinput \
      --enable-xrandr \
      --disable-pluggable-dialogs \
      --disable-sound-settings \
+     --enable-x11 \
      --enable-wayland \
      --disable-static \
     "
