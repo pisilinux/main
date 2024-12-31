ec8e3854213fa1d02a514dedeeceb6c7b5cf0edb
Rmys <muscnsl@hotmail.com>
2024-12-31 16:54:56 +0300

    amarok ver. bump

--- multimedia/sound/amarok/actions.py
+++ multimedia/sound/amarok/actions.py
@@ -5,7 +5,7 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import get
-from pisi.actionsapi import kde5
+from pisi.actionsapi import kde6
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 
@@ -13,18 +13,19 @@ shelltools.export("XDG_DATA_DIRS", get.workDIR())
 
 def setup():
     #shelltools.system("sed -i -e 's|PATH_SUFFIXES lastfm5|PATH_SUFFIXES lastfm|' cmake/modules/FindLibLastFm.cmake")
-    kde5.configure("-DCMAKE_BUILD_TYPE=Release \
+    kde6.configure("-DCMAKE_BUILD_TYPE=Release \
                     -DCMAKE_INSTALL_PREFIX=/usr \
                     -DLIB_INSTALL_DIR=lib \
-                    -DPLUGIN_INSTALL_DIR=/usr/lib/qt5/plugins \
+                    -DPLUGIN_INSTALL_DIR=/usr/lib/qt6/plugins \
                     -DKDE_INSTALL_USE_QT_SYS_PATHS=ON \
                     -DBUILD_TESTING=OFF \
+                    -DBUILD_WITH_QT6=ON \
                     -DKDE_DISTRIBUTION_TEXT='PisiLinux'")
 
 def build():
-    kde5.make()
+    kde6.make()
 
 def install():
-    kde5.install()
+    kde6.install()
 
-    pisitools.dodoc("AUTHORS", "README", "COPYING*", "ChangeLog")
+    pisitools.dodoc("AUTHORS", "README", "COPYING*", "ChangeLog", "LICENSES/*")
