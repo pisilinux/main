4c7c2cf494814327279f7cdfda315b3d72529702
Rmys <muscnsl@hotmail.com>
2025-01-06 19:40:21 +0300

    python3-zipp

--- programming/language/python3/python3-zipp/actions.py
+++ programming/language/python3/python3-zipp/actions.py
@@ -4,13 +4,13 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import python3modules
 from pisi.actionsapi import shelltools
 
-shelltools.export("SETUPTOOLS_SCM_PRETEND_VERSION","3.1.0")
+# shelltools.export("SETUPTOOLS_SCM_PRETEND_VERSION","3.1.0")
 
 def build():
-    pythonmodules.compile(pyVer="3")
+    python3modules.compile(pyVer="3")
 
 def install():
-    pythonmodules.install(pyVer="3")
+    python3modules.install(pyVer="3")
