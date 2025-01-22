38baea282869c4df8d097dbfff854311587865d7
Rmys <muscnsl@hotmail.com>
2025-01-22 23:19:28 +0300

    kde-applications

--- desktop/kde/applications/okular/actions.py
+++ desktop/kde/applications/okular/actions.py
@@ -4,16 +4,19 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import kde5
+from pisi.actionsapi import kde6
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
 
 def setup():
-    kde5.configure("-DKDEPIM_NO_TEXTTOSPEECH=TRUE")
+    # shelltools.system("sed -i 's|5.92.0|5.245.0|g' CMakeLists.txt")
+    kde6.configure("-DKDEPIM_NO_TEXTTOSPEECH=TRUE")
 
 def build():
-    kde5.make()
+    kde6.make()
 
 def install():
-    kde5.install()
+    kde6.install()
 
     pisitools.dodoc("LICENSES/*")
