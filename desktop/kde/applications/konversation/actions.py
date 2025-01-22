db068d58157451fdc9faaf920642848fde816019
Rmys <muscnsl@hotmail.com>
2025-01-22 22:53:56 +0300

    kde-applications

--- desktop/kde/applications/konversation/actions.py
+++ desktop/kde/applications/konversation/actions.py
@@ -5,18 +5,18 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import kde5
+from pisi.actionsapi import kde6
 
 def setup():
-    kde5.configure("-DCMAKE_BUILD_TYPE=Release \
+    kde6.configure("-DCMAKE_BUILD_TYPE=Release \
                     -DCMAKE_INSTALL_PREFIX=/usr \
                     -DLIB_INSTALL_DIR=lib \
                     -DKDE_INSTALL_USE_QT_SYS_PATHS=ON")
 
 def build():
-    kde5.make()
+    kde6.make()
 
 def install():
-    kde5.install()
+    kde6.install()
 
-    pisitools.dodoc("COPYING",  "ChangeLog", "AUTHORS", "README", "COPYING-DOCS", "NEWS")
+    pisitools.dodoc("LICENSES/*", "AUTHORS", "README", "NEWS")
