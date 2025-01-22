144aa472f567c4836b30d8ab3ea97b900d5095e3
Rmys <muscnsl@hotmail.com>
2025-01-22 22:36:28 +0300

    kde-applications

--- desktop/kde/applications/kcolorchooser/actions.py
+++ desktop/kde/applications/kcolorchooser/actions.py
@@ -5,15 +5,15 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import kde5
+from pisi.actionsapi import kde6
 
 def setup():
-    kde5.configure()
+    kde6.configure()
 
 def build():
-    kde5.make()
+    kde6.make()
 
 def install():
-    kde5.install()
+    kde6.install()
     
     pisitools.dodoc("COPYING")
