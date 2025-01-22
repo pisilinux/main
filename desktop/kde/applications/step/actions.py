9f5d6e3331f1ca0a014304e69d930994d7b0baef
Rmys <muscnsl@hotmail.com>
2025-01-22 23:02:54 +0300

    kde-applications

--- desktop/kde/applications/step/actions.py
+++ desktop/kde/applications/step/actions.py
@@ -5,16 +5,16 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import kde5, shelltools
+from pisi.actionsapi import kde6, shelltools
 
 
 def setup():
-    kde5.configure("-DPYTHON_EXECUTABLE:FILEPATH=/usr/bin/python3")
+    kde6.configure("-DPYTHON_EXECUTABLE:FILEPATH=/usr/bin/python3")
 
 def build():
-    kde5.make()
+    kde6.make()
 
 def install():
-    kde5.install()
+    kde6.install()
 
     pisitools.dodoc("LICENSES/*",  "ChangeLog", "AUTHORS", "README*", "DESIGN")
