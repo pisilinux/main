5bb0bfe7dc7d923a0ecb31997fff64b885aad411
Rmys <muscnsl@hotmail.com>
2025-01-22 22:59:49 +0300

    kde-applications

--- desktop/kde/applications/kteatime/actions.py
+++ desktop/kde/applications/kteatime/actions.py
@@ -5,18 +5,18 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import kde5
+from pisi.actionsapi import kde6
 
 NoStrip=["/usr/share/icons"]
 
 def setup():
     pisitools.dosed("CMakeLists.txt", "5.92.0", "5.110.0")
-    kde5.configure()
+    kde6.configure()
 
 def build():
-    kde5.make()
+    kde6.make()
 
 def install():
-    kde5.install()
+    kde6.install()
 
     pisitools.dodoc("LICENSES/*")
