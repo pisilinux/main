355b6f7a5d6c9682741fdfb7a1a017f36daf41f1
Rmys <muscnsl@hotmail.com>
2025-01-22 22:30:07 +0300

    kde-applications

--- desktop/kde/applications/akonadi/actions.py
+++ desktop/kde/applications/akonadi/actions.py
@@ -4,18 +4,18 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import kde5
+from pisi.actionsapi import kde6
 from pisi.actionsapi import pisitools
 
 def setup():
-    kde5.configure("-DCMAKE_BUILD_TYPE=Release \
+    kde6.configure("-DCMAKE_BUILD_TYPE=Release \
                     -DINSTALL_QSQLITE_IN_QT_PREFIX=TRUE \
                     -DMYSQLD_EXECUTABLE:FILEPATH=/usr/bin/mysqld \
                     -DWITH_SOPRANO=OFF")
 
 def build():
-    kde5.make()
+    kde6.make()
 
 def install():
-    kde5.install()
+    kde6.install()
     pisitools.dodoc("AUTHORS", "NEWS", "README.md")
