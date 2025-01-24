4619bcadb162fd7f005492166eac65ddd491a976
Rmys <muscnsl@hotmail.com>
2025-01-24 23:54:07 +0300

    kde5-framework

---
+++ desktop/kde5/framework/plasma-framework5/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import kde5
+from pisi.actionsapi import pisitools, get
+
+def setup():
+    kde5.configure("-DBUILD_DESKTOPTHEMES=OFF")
+
+def build():
+    kde5.make()
+
+def install():
+    kde5.install()
+    #set pisi-crocus-ancyrensis photos as default wallpapers
+    # pisitools.dosed("%s/usr/share/plasma/desktoptheme/default/plasmarc" % get.installDIR(), "Next", "pisi-crocus-ancyrensis")
+
+    pisitools.dodoc("README.md")
