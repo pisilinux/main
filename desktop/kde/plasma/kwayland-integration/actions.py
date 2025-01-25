cbd7431f31162516c9c579d88a379d565550d6fe
Rmys <muscnsl@hotmail.com>
2025-01-25 11:25:16 +0300

    kwayland-integration rebuild

--- desktop/kde/plasma/kwayland-integration/actions.py
+++ desktop/kde/plasma/kwayland-integration/actions.py
@@ -4,18 +4,18 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import kde6
+from pisi.actionsapi import kde5
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 
 def setup():
     #shelltools.system("sed -i 's|5.11.0|5.10.0|g' CMakeLists.txt")
-    kde6.configure("-DQT_MAJOR_VERSION=6")
+    kde5.configure("-DQT_MAJOR_VERSION=6")
 
 def build():
-    kde6.make()
+    kde5.make()
 
 def install():
-    kde6.install()
+    kde5.install()
     
     pisitools.dodoc("LICENSES/*")
