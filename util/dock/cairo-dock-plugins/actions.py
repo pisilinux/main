13620a380fd624cd4c5436871dc884bfebbac65b
uglyside <uglyside@yandex.ru>
2025-01-31 11:40:06 +0300

    cairo-dock-plugins.

--- util/dock/cairo-dock-plugins/actions.py
+++ util/dock/cairo-dock-plugins/actions.py
@@ -4,7 +4,7 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import cmaketools, mesontools
+from pisi.actionsapi import cmaketools, mesontools, pisitools
 
 i = ''.join([
     ' -DCMAKE_BUILD_TYPE=RelWithDebInfo',
@@ -20,3 +20,5 @@ def build():
 
 def install():
     mesontools.install()
+
+    pisitools.dodoc("LICENSE", "LGPL-2", "copyright")
