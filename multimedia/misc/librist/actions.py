b1354c412d8b64f1b7653b86f9dd51b76d8eb198
uglyside <uglyside@yandex.ru>
2025-02-06 23:36:39 +0300

    librist-0.2.11

--- multimedia/misc/librist/actions.py
+++ multimedia/misc/librist/actions.py
@@ -4,14 +4,16 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import mesontools, shelltools, get
+from pisi.actionsapi import mesontools, shelltools, pisitools
 
 def setup():
-	mesontools.configure("-Dbuiltin_cjson=true")
+    mesontools.configure("--prefix=/usr --buildtype=release")
 
 def build():
-	mesontools.build()
+    mesontools.build()
 
 def install():
-	mesontools.install()
-	shelltools.chmod("%s/usr/include/librist/librist.h" % get.installDIR(), mode = 0644)
+    shelltools.chmod("include/librist/librist.h", mode = 0644)
+    mesontools.install()
+
+    pisitools.dodoc("COPYING", "THANKS.md")
