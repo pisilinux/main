d2031f415b855f0d33a5aac7fd0363f12cd903cf
uglyside <uglyside@yandex.ru>
2025-02-06 23:37:56 +0300

    libsrtp-2.6.0

--- network/library/libsrtp/actions.py
+++ network/library/libsrtp/actions.py
@@ -1,28 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    pisitools.cflags.add("-fPIC")
-    #autotools.autoreconf("-fiv")
-    autotools.configure("--enable-openssl \
-                         --enable-nss")
+    autotools.configure("--prefix=/usr --enable-nss --enable-openssl")
 
 def build():
     autotools.make("all")
     autotools.make("shared_library")
-    
 
 def check():
-    autotools.make("test")
+    autotools.make("runtest")
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("LICENSE", "CHANGES", "README*")
+    pisitools.dodoc("LICENSE")
