8151f9253790fd05a31036d5ff2746bd340a778a
uglyside <uglyside@yandex.ru>
2025-02-08 20:04:28 +0300

    klavaro-3.14

--- multimedia/misc/klavaro/actions.py
+++ multimedia/misc/klavaro/actions.py
@@ -2,16 +2,14 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/copyleft/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    #pisitools.dosed("configure.in", "uk ur", "uk tr ur")
-    pisitools.dosed("configure", "uk ur", "uk tr ur")
-    autotools.configure()
+    autotools.configure("--prefix=/usr --disable-static")
 
 def build():
     autotools.make()
@@ -19,4 +17,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
+    pisitools.dodoc("AUTHORS", "COPYING")
