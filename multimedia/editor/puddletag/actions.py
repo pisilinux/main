8ce2ca4fab0ae5cfba3c9216331026905132075f
uglyside <uglyside@yandex.ru>
2025-02-09 12:19:11 +0300

    puddletag-2.4.0

--- multimedia/editor/puddletag/actions.py
+++ multimedia/editor/puddletag/actions.py
@@ -5,13 +5,9 @@
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import pythonmodules
-from pisi.actionsapi import get
-
-def setup():
-	pass
 
 def build():
-	pythonmodules.compile(pyVer = '3')
+    pythonmodules.compile(pyVer = '3')
 
 def install():
-	pythonmodules.run("setup.py install --root=%s --no-compile -O0" % get.installDIR(), pyVer = '3')
+    pythonmodules.install(pyVer = "3")
