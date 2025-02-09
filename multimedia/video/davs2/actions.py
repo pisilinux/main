8d91d7b1327bc646234945a3472bcef20d86e07f
uglyside <uglyside@yandex.ru>
2025-02-09 12:19:37 +0300

    davs2-1.7.0

--- multimedia/video/davs2/actions.py
+++ multimedia/video/davs2/actions.py
@@ -4,25 +4,27 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
+WorkDir = "%s-%s/build/linux" % (get.srcNAME(), get.srcVERSION())
+
+y = ''.join([
+    ' --prefix=/usr',
+    ' --chroma-format=\'all\'',
+    ' --extra-ldflags=\'-Wl,-z,noexecstack\'',
+    ' --enable-shared',
+    ' --disable-static '
+    ])
+
 def setup():
-    shelltools.system("patch -p1 --binary < davs2-1.6-gcc8-fix.patch")
-    shelltools.cd("build/linux")
-    autotools.configure("--extra-ldflags='-Wl,-z,noexecstack' \
-                         --chroma-format='all' \
-                         --enable-shared ")
+    autotools.rawConfigure(y)
 
 def build():
-    shelltools.cd("build/linux")
     autotools.make()
 
 def install():
-    shelltools.cd("build/linux")
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    shelltools.cd("../..")
-    pisitools.dodoc("COPYING", "README.md")
 
+    pisitools.dodoc("../../COPYING")
