5516f7c1d0a24432e35301674924bbec0deb5760
uglyside <uglyside@yandex.ru>
2025-02-10 10:41:29 +0300

    soundconverter-4.0.6

--- multimedia/converter/soundconverter/actions.py
+++ multimedia/converter/soundconverter/actions.py
@@ -5,13 +5,9 @@
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import pythonmodules
-from pisi.actionsapi import pisitools
 
 def build():
-	pythonmodules.compile(pyVer="3")
+    pythonmodules.compile(pyVer = "3")
 
 def install():
-	pythonmodules.install(pyVer="3")
-
-	pisitools.dodoc("AUTHORS", "PKG-INFO", "README.md")
-
+    pythonmodules.install(pyVer = "3")
