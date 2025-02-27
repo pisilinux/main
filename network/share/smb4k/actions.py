762cc9e5ae8df22564fa502e6537dbd99a31d8e3
Rmys <muscnsl@hotmail.com>
2025-02-27 09:09:07 +0300

    k3b smb4k ver. bump

--- network/share/smb4k/actions.py
+++ network/share/smb4k/actions.py
@@ -6,17 +6,17 @@
 
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import kde5
+from pisi.actionsapi import kde6
 from pisi.actionsapi import get
 
 
 def setup():
-    kde5.configure()
+    kde6.configure("-DSMB4K_WITH_WS_DISCOVERY=ON")
 
 def build():
-    kde5.make()
+    kde6.make()
 
 def install():
-    kde5.install()
+    kde6.install()
 
     pisitools.dodoc("ChangeLog", "README*", "AUTHORS", "LICENSES/*")
