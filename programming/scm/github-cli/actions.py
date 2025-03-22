fd3abad969120db899a03f838fcae26257be97f2
Kamil ATLI <suvarice@gmail.com>
2025-03-22 14:56:01 +0300

    github-cli

--- programming/scm/github-cli/actions.py
+++ programming/scm/github-cli/actions.py
@@ -15,6 +15,6 @@ def build():
         -o 'bin/gh' ./cmd/gh" % get.LDFLAGS())
 
 def install():
-    shelltools.cd("../cli-2.68.0")
+    shelltools.cd("../cli-2.69.0")
     pisitools.insinto("/usr/bin", "bin/gh")
     pisitools.dodoc("LICENSE", "README*")
