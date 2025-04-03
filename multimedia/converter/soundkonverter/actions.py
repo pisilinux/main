dd67208be280c4ae897a54a2378102c42dd3aec9
Rmys <muscnsl@gmail.com>
2025-04-03 19:59:24 +0300

    Update actions.py

--- multimedia/converter/soundkonverter/actions.py
+++ multimedia/converter/soundkonverter/actions.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import kde5
+from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import pisitools
 
-def setup():
-    kde5.configure()
-
 def build():
-    kde5.make()
+	pythonmodules.compile(pyVer="3")
 
 def install():
-    kde5.install()
+	pythonmodules.install(pyVer="3")
 
-    pisitools.dodoc("CHANGELOG", "INSTALL", "README")
+	pisitools.dodoc("AUTHORS", "PKG-INFO", "README.md")
+
