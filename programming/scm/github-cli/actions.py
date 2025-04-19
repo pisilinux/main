3f80d0dc47598f4d5fa9986bb7befa8f8153bccf
Bahar KURT <168406273+kurtbahartr@users.noreply.github.com>
2025-04-19 19:10:42 +0000

    Bump github-cli

--- programming/scm/github-cli/actions.py
+++ programming/scm/github-cli/actions.py
@@ -11,10 +11,10 @@ from pisi.actionsapi import get
 def build():
     shelltools.system("go build \
         -trimpath \
-        -ldflags '-extldflags \"%s\" -X github.com/cli/cli/command.Version=v2.69.0 -X github.com/cli/cli/command.BuildDate=2025-03-22' \
+        -ldflags '-extldflags \"%s\" -X github.com/cli/cli/command.Version=v2.70.0 -X github.com/cli/cli/command.BuildDate=2025-04-19' \
         -o 'bin/gh' ./cmd/gh" % get.LDFLAGS())
 
 def install():
-    shelltools.cd("../cli-2.69.0")
+    shelltools.cd("../cli-2.70.0")
     pisitools.insinto("/usr/bin", "bin/gh")
     pisitools.dodoc("LICENSE", "README*")
