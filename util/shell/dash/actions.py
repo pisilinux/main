5f5538b19d879c6fff3382888be87593c908dc19
uglyside <uglyside@yandex.ru>
2025-05-16 13:48:09 +0300

    dash-0.5.12

--- util/shell/dash/actions.py
+++ util/shell/dash/actions.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--prefix=/")
+    autotools.configure("--prefix=/usr")
 
 def build():
     autotools.make()
@@ -17,4 +17,5 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
+    pisitools.dosym("/usr/bin/dash", "/bin/dash")
     pisitools.dodoc("COPYING")
