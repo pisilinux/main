1bf17470b4ef06154565a1008d7c306ebf186454
Rmys <muscnsl@hotmail.com>
2025-05-26 17:10:32 +0300

    converseen new package

---
+++ multimedia/converter/converseen/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr \
+                          -DUSE_QT6=ON")
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("COPYING*", "README*")
