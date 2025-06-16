6af7f2da68343a74b0809ddb91c86285030795b6
Rmys <muscnsl@hotmail.com>
2025-06-16 19:06:54 +0300

    gpgme ver. bump

--- util/crypt/gpgme/actions.py
+++ util/crypt/gpgme/actions.py
@@ -10,34 +10,16 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    shelltools.cd("%s" % get.workDIR())
-    shelltools.copy("gpgme-*",  "gpgme-qt6")
-    shelltools.cd("gpgme-%s" % get.srcVERSION())
-
     autotools.configure("--disable-fd-passing \
                           --disable-static \
                           --disable-gpg-test \
                           --disable-gpgsm-test \
-                          --enable-languages=cpp,qt,python")
+                          ")
      
     pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
-    # qt6
-    shelltools.cd("../gpgme-qt6")
-    autotools.configure("--disable-fd-passing \
-                          --disable-static \
-                          --disable-gpg-test \
-                          --disable-gpgsm-test \
-                          --enable-languages=cpp,qt6")
-
-    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
-
-    shelltools.cd("..")
 
 def build():
-    autotools.make()
-
-    shelltools.cd("../gpgme-qt6")
     autotools.make()
 
 #def check():
@@ -46,10 +28,7 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
     
-    pisitools.insinto("/usr/lib/cmake/Qgpgme", "lang/qt/src/QGpgmeConfig.cmake")
-    pisitools.insinto("/usr/lib/cmake/Qgpgme", "lang/qt/src/QGpgmeConfigVersion.cmake")
+    # pisitools.insinto("/usr/lib/cmake/Qgpgme", "lang/qt/src/QGpgmeConfig.cmake")
+    # pisitools.insinto("/usr/lib/cmake/Qgpgme", "lang/qt/src/QGpgmeConfigVersion.cmake")
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README", "THANKS", "TODO")
-
-    shelltools.cd("../gpgme-qt6/lang/qt")
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
