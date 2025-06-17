bb580ab1a2dcaf8edfcae1dc450b6f1d9fda5c22
uglyside <uglyside@yandex.ru>
2025-06-17 07:36:48 +0300

    add libXaw3dxft.

---
+++ x11/library/libXaw3dXft/actions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
+
+from pisi.actionsapi import autotools, get
+
+def setup():
+    autotools.configure("--prefix=/usr --disable-static")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
