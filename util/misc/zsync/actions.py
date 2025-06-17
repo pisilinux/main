6b3250206ab7f167f49b89c4d2d33dc1f2515b82
uglyside <uglyside@yandex.ru>
2025-06-17 07:39:17 +0300

    zsync-0.6.3

--- util/misc/zsync/actions.py
+++ util/misc/zsync/actions.py
@@ -2,10 +2,9 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
@@ -15,5 +14,5 @@ def build():
     autotools.make()
 
 def install():
-    autotools.rawInstall("PREFIX=/%s DESTDIR=%s" % (get.defaultprefixDIR(), get.installDIR()))
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
