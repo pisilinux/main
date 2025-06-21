0a3e38a6a52a7fbf5b8226441a57313fe27b8de8
uglyside <uglyside@yandex.ru>
2025-06-21 22:44:13 +0300

    cmark v0.31.1

--- multimedia/misc/cmark/actions.py
+++ multimedia/misc/cmark/actions.py
@@ -1,28 +1,23 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import cmaketools
+from pisi.actionsapi import mesontools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
 
 def setup():
-    shelltools.system("mkdir build")
-    shelltools.cd("build")
     cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr \
                           -DCMARK_STATIC=OFF \
-                          -DCMAKE_INSTALL_LIBDIR=lib", sourceDir = '..')
+                          -DCMAKE_INSTALL_LIBDIR=lib \
+                          -Bbuild -G Ninja")
 
 def build():
-    shelltools.cd("build")
-    cmaketools.make()
+    mesontools.build()
 
 def install():
-    shelltools.cd("build")
-    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
-    
-    shelltools.cd("..")
-    pisitools.dodoc("COPYING", "README*")
+    mesontools.install()
+
+    pisitools.dodoc("COPYING")
