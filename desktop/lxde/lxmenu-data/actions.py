bf32b6e15e191732101ec1d86244d132bda7deaa
uglyside <uglyside@yandex.ru>
2025-06-21 10:48:45 +0300

    lxmenu-data v0.1.6

--- desktop/lxde/lxmenu-data/actions.py
+++ desktop/lxde/lxmenu-data/actions.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+#
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/copyleft/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import get
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import autotools
-from pisi.actionsapi import libtools
-from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools, pisitools, get
 
 def setup():
-    pisitools.dosed("configure.ac", "AM_GLIB", deleteLine=True)
-    shelltools.system("./autogen.sh")
     autotools.configure()
 
 def build():
@@ -19,4 +14,5 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.dodoc("AUTHORS", "COPYING", "README")
+
+    pisitools.dodoc("AUTHORS", "COPYING")
