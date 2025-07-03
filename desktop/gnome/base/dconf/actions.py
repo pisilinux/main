ca05a3d30695bd3ef969110c852dbdf5ff035602
uglyside <uglyside@yandex.ru>
2025-07-03 05:32:48 +0300

    dconf fix tarball link.

--- desktop/gnome/base/dconf/actions.py
+++ desktop/gnome/base/dconf/actions.py
@@ -1,13 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import mesontools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
 def setup():
     shelltools.system("sed -i 's/install_dir: systemd_userunitdir,//' service/meson.build")
@@ -15,9 +14,8 @@ def setup():
 
 def build():
     mesontools.build()
-    
+
 def install():
     mesontools.install()
 
     pisitools.dodoc("COPYING", "NEWS", "README")
-
