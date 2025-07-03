d8a4ca3aa1d0db237f0a837f2cb5e19d2ab721a6
uglyside <uglyside@yandex.ru>
2025-07-03 06:53:32 +0300

    menu-cache.

--- desktop/lxqt/menu-cache/actions.py
+++ desktop/lxqt/menu-cache/actions.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+#
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/copyleft/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
 def setup():
-    shelltools.system("./autogen.sh")
     autotools.configure("--disable-static \
                          --libexecdir=/usr/lib \
                          --sysconfdir=/etc")
@@ -20,4 +18,5 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
     pisitools.dodoc("AUTHORS", "COPYING")
