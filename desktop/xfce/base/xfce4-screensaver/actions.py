7a8693f83dd65e9abb36492d486af81232e734d4
Rmys <muscnsl@hotmail.com>
2025-07-17 12:21:01 +0300

    xfce4-screensaver ver. bump

--- desktop/xfce/base/xfce4-screensaver/actions.py
+++ desktop/xfce/base/xfce4-screensaver/actions.py
@@ -7,30 +7,25 @@
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import mesontools
 from pisi.actionsapi import get
 
 j = ''.join([
-    '--enable-pam',
-    '--enable-locking',
-    '--enable-authentication-scheme=pam',
-    '--disable-static',
-    '--with-libgl',
-    '--with-shadow',
-    '--with-mit-ext',
-    '--with-elogind',
-    '--without-console-kit',
-    '--with-pam-auth-type=system',
-    '--without-systemd'])
+    '-Dsession-manager=elogind',
+    ' -Dlocking=true',
+    ' -Dauthentication-scheme=pam',
+    ' -Dshadow=true',
+    ' -Dpam-auth-type=system'])
 
 def setup():
 #    shelltools.system("patch -Rp1 < 0001-Catch-gs_listener_dbus_init-failures.patch")
-    autotools.configure(j)
+    mesontools.configure(j)
 
 def build():
-    autotools.make()
+    mesontools.build()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    mesontools.install()
 
     pisitools.dodoc("COPYING*", "INSTALL", "NEWS", "README.md")
 
