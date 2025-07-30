00e7f3323c0fccd6dfa224ef5a7fde28e08908f2
Rmys <muscnsl@hotmail.com>
2025-07-30 12:02:26 +0300

    libICE rebuild

--- x11/library/libICE/actions.py
+++ x11/library/libICE/actions.py
@@ -8,15 +8,23 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
+    options = "--enable-ipv6 \
+               --disable-static \
+               --without-fop"
+
+    if get.buildTYPE() == "emul32":
+        options += " --disable-static"
+
     autotools.autoreconf("-vif")
-    autotools.configure("--disable-static \
-                         --enable-ipv6 \
-                         --without-fop")
+    autotools.configure(options)
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    if get.buildTYPE() == "emul32":
+        return
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README*")
