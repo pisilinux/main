d7c9924d94f867d4da9610c8e83ad89f2a4359da
Rmys <muscnsl@hotmail.com>
2025-08-26 22:20:49 +0300

    libb2 new package

---
+++ programming/library/libb2/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.autoreconf("-vif")
+    autotools.configure("--disable-static \
+                         --enable-shared \
+                         --disable-native")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("COPYING")
