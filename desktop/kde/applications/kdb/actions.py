025ee6e7345d6557801567224c969953477587ea
Rmys <muscnsl@hotmail.com>
2025-10-10 18:48:22 +0300

    applications-25.08.2

--- desktop/kde/applications/kdb/actions.py
+++ desktop/kde/applications/kdb/actions.py
@@ -7,7 +7,8 @@
 from pisi.actionsapi import kde5
 
 def setup():
-    kde5.configure("-DCMAKE_CXX_STANDARD=17")
+    kde5.configure("-DCMAKE_CXX_STANDARD=17 \
+                    -DCMAKE_POLICY_VERSION_MINIMUM=3.5")
 
 def build():
     kde5.make()
