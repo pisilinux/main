7c170ecd0995cf9366d1e4b123a470b41654a674
Rmys <muscnsl@gmail.com>
2025-10-12 11:09:24 +0300

    kde-cli-tools

--- desktop/kde/plasma/kde-cli-tools/comar/package.py
+++ desktop/kde/plasma/kde-cli-tools/comar/package.py
@@ -4,4 +4,8 @@
 import os
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
+    # Eski link'i her zaman sil
+    if os.path.exists("/usr/bin/kdesu"):
+        os.remove("/usr/bin/kdesu")
+    # Yeni link'i oluştur
     os.system("ln -s /usr/lib/kf6/kdesu /usr/bin/kdesu")
