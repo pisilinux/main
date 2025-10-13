12e86464720822bb766b156d3fa97f991d0c2ace
Rmys <muscnsl@hotmail.com>
2025-10-13 11:48:15 +0300

    gnome ver. bump

--- desktop/gnome/base/mozjs/actions.py
+++ desktop/gnome/base/mozjs/actions.py
@@ -9,66 +9,33 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 
 #WorkDir = "mozjs%s/js/src" % get.srcVERSION()
-
+ObjDir = "obj"
 shelltools.export("SHELL","/bin/sh")
 WorkDir = "firefox-%s" %get.srcVERSION()
 
-def setup():
-   shelltools.system("sed -i 's|/usr/local|/usr|g' nsprpub/configure")
-   #shelltools.cd("js/src")
-   #shelltools.system("sed -i 's/(defined\((@TEMPLATE_FILE)\))/\1/' config/milestone.pl ")
-   shelltools.export("CC", "gcc")
-   shelltools.export("CXX", "g++")
 
-   shelltools.system("mkdir -p build-js")
-   shelltools.cd("build-js")
-   shelltools.export("ac_default_prefix", "/usr")
-   shelltools.system("sh ../js/src/configure \
-                      --enable-readline \
-                      --with-intl-api \
-                      --disable-debug \
-                      --disable-debug-symbols \
-                      --disable-jemalloc \
-                      --disable-strip  \
-                      --enable-hardening \
-                      --enable-linker=gold \
-                      --enable-optimize \
-                      --enable-readline \
-                      --enable-release \
-                      --enable-shared-js \
-                      --enable-tests \
-                      --with-intl-api \
-                      --with-system-zlib \
-                      --with-system-nspr \
-                      --without-system-icu")
-                     # --prefix=/usr \
+shelltools.system("export MACH_BUILD_PYTHON_NATIVE_PACKAGE_SOURCE=pip")
+shelltools.export("MOZBUILD_STATE_PATH", "mozbuild")
+shelltools.system("export MOZ_NOSPAM=1")
+
+def setup():
+   # shelltools.export("CC", "gcc")
+   # shelltools.export("CXX", "g++")
+   # shelltools.system("cp mozconfig .mozconfig")
+   shelltools.system("cp mozconfig js/src/.mozconfig")
+
 
 def build():
-    shelltools.cd("build-js")
-    autotools.make("prefix=/usr")
-
-# def check():
     # shelltools.cd("build-js")
-
-    #autotools.make("-C js/src check-jstests")
-    #autotools.make("-C js/src check-jit-test")
+    shelltools.system("./mach build")
 
 
 def install():
-    shelltools.cd("build-js")
-    autotools.rawInstall("DESTDIR=%s prefix=/usr" % get.installDIR())
+    shelltools.system("DESTDIR=%s ./mach install " % get.installDIR())
     pisitools.remove("/usr/lib/*.ajs")
 
-    pisitools.dosym("/usr/lib/libmozjs-143.so", "/usr/lib/libmozjs-143.so.0")
+    pisitools.dosym("/usr/lib/libmozjs-140.so", "/usr/lib/libmozjs-140.so.0")
 
-    pisitools.dosed("%s/usr/bin/js128-config" % get.installDIR(), "usr/local", "usr")
-    pisitools.dosed("%s/usr/lib/pkgconfig/mozjs-143.pc" % get.installDIR(), "usr/local", "usr")
-
-    #for polkit
-    #pisitools.rename("/usr/lib/pkgconfig/mozjs-..pc", "mozjs-17.0.pc")
-    #pisitools.remove("usr/lib/libmozjs-..a")
-
-    shelltools.cd("..")
     pisitools.dodoc("README*")
 
     # add link for polkit
