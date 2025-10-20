b73de4b1469ccd8aa491f37e9a0b0abf7883e19b
uglyside <uglyside@yandex.ru>
2025-10-20 06:05:43 +0300

    rnnoise v0.2

--- programming/misc/rnnoise/actions.py
+++ programming/misc/rnnoise/actions.py
@@ -1,22 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    shelltools.system("./autogen.sh")
-    autotools.configure()
+    autotools.autoreconf("-isf")
+    autotools.configure("--disable-doc --disable-examples --disable-static")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-    pisitools.dodoc("AUTHORS", "COPYING", "README")
