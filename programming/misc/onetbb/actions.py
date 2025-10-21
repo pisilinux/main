a344bb01ba6624e34975deb47a3607a9bb3eb3db
uglyside <uglyside@yandex.ru>
2025-10-21 06:18:19 +0300

    oneTBB-v2022.2.0

--- programming/misc/onetbb/actions.py
+++ programming/misc/onetbb/actions.py
@@ -2,25 +2,27 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import cmaketools
+from pisi.actionsapi import mesontools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
+
+j = ''.join([
+    ' -DCMAKE_INSTALL_PREFIX=/usr',
+    ' -DCMAKE_INSTALL_DOCDIR=share/doc/onetbb',
+    ' -DTBB4PY_BUILD=ON',
+    ' -DTBB_TEST=OFF',
+    ' -Bbuild -G Ninja -L '
+    ])
 
 def setup():
-    shelltools.makedirs("build")
-    shelltools.cd("build")
-
-    cmaketools.configure("CMAKE_INSTALL_PREFIX=/usr -D TBB_STRICT=OFF -D TBB4PY_BUILD=ON", sourceDir="..")
+    cmaketools.configure(j)
 
 def build():
-    cmaketools.make("-B build")
+    mesontools.build()
 
 def install():
-    shelltools.cd("build")
-    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    mesontools.install()
 
-    # pisitools.dodoc("COPYING", "README")
-
+    pisitools.dodoc("LICENSE.txt")
