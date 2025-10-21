40d6f50d90c33686f6578140bd7a8fcdb467a97b
Rmys <muscnsl@hotmail.com>
2025-10-21 13:52:34 +0300

    plasma-6.5.0

--- desktop/kde/plasma/kde-cli-tools/comar/package.py
+++ desktop/kde/plasma/kde-cli-tools/comar/package.py
@@ -4,4 +4,8 @@
 import os
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-    os.system("ln -s /usr/lib/kf6/kdesu /usr/bin/kdesu")
+    # Eski link'i her zaman sil
+    if os.path.exists("/usr/bin/kdesu"):
+        os.remove("/usr/bin/kdesu")
+    # Yeni link'i oluştur
+    os.symlink("/usr/lib/kf6/kdesu", "/usr/bin/kdesu")
