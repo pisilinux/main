818d963e8bb5a0ddac9bb7be57ec90c4e1f7d7ba
Rmys <muscnsl@gmail.com>
2025-10-24 15:31:17 +0300

    gnome-shell-extensions

--- desktop/gnome/base/gnome-shell-extensions/actions.py
+++ desktop/gnome/base/gnome-shell-extensions/actions.py
@@ -8,7 +8,7 @@ from pisi.actionsapi import mesontools
 from pisi.actionsapi import pisitools
 
 def setup():
-    mesontools.configure()
+    mesontools.configure("-Dx11=true")
 
 def build():
     mesontools.build()
