6a4ac0d133ae4f746e44b18e2e35ff4b853ae530
Rmys <muscnsl@gmail.com>
2025-10-27 16:07:28 +0300

    grilo-plugins

--- desktop/gnome/base/grilo-plugins/actions.py
+++ desktop/gnome/base/grilo-plugins/actions.py
@@ -7,22 +7,7 @@
 from pisi.actionsapi import mesontools, pisitools
 
 def setup():
-    mesontools.configure("-Denable-shoutcast=no \
-                            -Denable-bookmarks=yes \
-                            -Denable-dleyna=yes \
-                            -Denable-dmap=yes \
-                            -Denable-filesystem=yes \
-                            -Denable-flickr=yes \
-                            -Denable-freebox=yes \
-                            -Denable-gravatar=yes \
-                            -Denable-lua-factory=yes \
-                            -Denable-metadata-store=yes \
-                            -Denable-podcasts=yes \
-                            -Denable-tmdb=yes \
-                            -Denable-youtube=yes \
-                            -Denable-tracker=no \
-                            -Denable-youtube=no \
-                            -Denable-tracker3=yes")
+    mesontools.configure()
 
 def build():
     mesontools.build()
