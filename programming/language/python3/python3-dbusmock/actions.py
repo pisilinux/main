be4404cc5ff2603d3a9e202144cc80bbe094daa1
Rmys <muscnsl@hotmail.com>
2025-10-27 09:15:31 +0300

    python3-dbusmock  rebuild

--- programming/language/python3/python3-dbusmock/actions.py
+++ programming/language/python3/python3-dbusmock/actions.py
@@ -4,12 +4,18 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import python3modules
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 
+
+
+
+shelltools.export("SETUPTOOLS_SCM_PRETEND_VERSION","0.37.1")
+
 def build():
-    pythonmodules.compile(pyVer = '3')
+    python3modules.compile(pyVer = '3')
 
 def install():
-    pythonmodules.install(pyVer = '3')
+    python3modules.install(pyVer = '3')
 
