fedd6ed9d518287a985a53f6a2772b158670a94b
Rmys <muscnsl@gmail.com>
2025-12-21 20:18:51 +0300

    mate-notification-daemon

--- desktop/mate/mate-notification-daemon/actions.py
+++ desktop/mate/mate-notification-daemon/actions.py
@@ -11,6 +11,7 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
+    shelltools.system("sh ./autogen.sh")
     autotools.configure("--disable-schemas-compile \
                          --libexecdir=/usr/lib/mate-notification-daemon")
 
