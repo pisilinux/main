1e4446d33fd582b24abb35ffcd0365cfcc7f0211
Rmys <muscnsl@gmail.com>
2025-12-28 12:48:59 +0300

    kshutdown ver/ bump

--- desktop/kde/addon/kshutdown/actions.py
+++ desktop/kde/addon/kshutdown/actions.py
@@ -3,20 +3,20 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
-from pisi.actionsapi import kde5
+from pisi.actionsapi import kde6
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 
 
 def setup():
-    shelltools.system("./Setup-kf5.sh")
-    kde5.configure("-DKS_KF5=ON")
+    shelltools.system("./Setup-kf6.sh")
+    kde6.configure()
 
 def build():
-    kde5.make()
+    kde6.make()
 
 def install():
-    kde5.install()
+    kde6.install()
 
     pisitools.dodoc("ChangeLog", "LICENSE")
 
