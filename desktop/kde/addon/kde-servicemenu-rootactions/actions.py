ad34c065fd441cbfa3e1e7ad15a0f0979647c3df
Rmys <muscnsl@gmail.com>
2025-12-28 13:35:52 +0300

    kde-servicemenu-rootactions

--- desktop/kde/addon/kde-servicemenu-rootactions/actions.py
+++ desktop/kde/addon/kde-servicemenu-rootactions/actions.py
@@ -4,13 +4,22 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "rootactions_servicemenu_%s" % get.srcVERSION()
+# WorkDir = "rootactions_servicemenu_%s" % get.srcVERSION()
+
+def setup():
+    autotools.autoreconf("-iv")
+    autotools.configure()
+
+def build():
+    autotools.make()
 
 def install():
-    pisitools.insinto("/usr/share/kservices5/ServiceMenus", "Root_Actions_%s/dolphin-KDE4/*.desktop" % get.srcVERSION())
-    pisitools.dobin("Root_Actions_%s/rootactions-servicemenu.pl" % get.srcVERSION())
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    # pisitools.insinto("/usr/share/kservices5/ServiceMenus", "Root_Actions_%s/dolphin-KDE4/*.desktop" % get.srcVERSION())
+    # pisitools.dobin("Root_Actions_%s/rootactions-servicemenu.pl" % get.srcVERSION())
 
-    pisitools.dodoc("README")
+    pisitools.dodoc("README*")
