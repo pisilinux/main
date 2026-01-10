b055cbc78c03c0e1735e51c5ddecf7bf32382bd8
Rmys <muscnsl@hotmail.com>
2026-01-10 11:20:26 +0300

    calligra-plan ver. bump

--- office/calligra/calligra-plan/actions.py
+++ office/calligra/calligra-plan/actions.py
@@ -5,19 +5,19 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import kde5
+from pisi.actionsapi import kde6
 
 def setup():
-    kde5.configure()
+    kde6.configure()
 
 def build():
-    kde5.configure("-DCMAKE_BUILD_TYPE=Release \
+    kde6.configure("-DCMAKE_BUILD_TYPE=Release \
                     -DCMAKE_INSTALL_PREFIX=/usr \
                     -DKDE_INSTALL_LIBDIR=lib \
                     -DBUILD_TESTING=OFF \
                     -DBUILD_UNMAINTAINED=ON")
 
 def install():
-    kde5.install()
+    kde6.install()
 
     pisitools.dodoc("COPYING*", "TODO")
