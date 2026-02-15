4bd005865f50838c2035345c4d7323b72f82a70b
4fury-c3440d8 <uglyside@yandex.ru>
2026-02-16 03:23:39 +0400

    xsel.

--- x11/misc/xsel/actions.py
+++ x11/misc/xsel/actions.py
@@ -1,23 +1,23 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    shelltools.system("touch {AUTHORS,NEWS,README,COPYING}")
-    autotools.autoreconf("-vi")
+#    shelltools.system("sh autogen.sh")
+    autotools.autoreconf("-vif")
     autotools.configure("--prefix=/usr")
-    
+
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("COPYING", "README", "AUTHORS")
+    pisitools.dodoc("AUTHORS", "COPYING")
