2d06b6de1b70dd5aac1d75e1e089ff083c052813
4fury-c3440d8 <uglyside@yandex.ru>
2026-02-15 23:56:30 +0400

    enca-1.21

--- office/misc/enca/actions.py
+++ office/misc/enca/actions.py
@@ -2,15 +2,13 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/copyleft/gpl.txt.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-
 def setup():
-    autotools.autoreconf("-vfi")
     autotools.configure("--enable-external \
                          --with-librecode=/usr \
                          --disable-static")
@@ -21,4 +19,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "FAQ", "README*", "THANKS", "TODO")
+    pisitools.dodoc("AUTHORS", "COPYING", "THANKS")
