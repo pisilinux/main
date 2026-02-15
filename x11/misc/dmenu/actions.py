d2092718730b25e8b409f68a9426a4a7b5c74058
4fury-c3440d8 <uglyside@yandex.ru>
2026-02-16 01:14:01 +0400

    dmenu-5.4

--- x11/misc/dmenu/actions.py
+++ x11/misc/dmenu/actions.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2018 TUBITAK/UEKAE
-# Licensed under the GNU General Public License, version 2.
-# See the file http://www.gnu.org/copyleft/gpl.txt.
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def build():
