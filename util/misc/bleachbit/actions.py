02171a5ff5f9bd1be155237b6c9e8824552c1656
4fury-c3440d8 <uglyside@yandex.ru>
2026-02-19 20:44:33 +0400

    bleachbit-v5.1.0

--- util/misc/bleachbit/actions.py
+++ util/misc/bleachbit/actions.py
@@ -2,17 +2,10 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 
-def setup():
-	pass
-
-def build():
-	pass
-
 def install():
-	shelltools.system("make prefix=/usr DESTDIR=%s install" % get.installDIR())
-
+    autotools.rawInstall("DESTDIR=%s prefix=/usr" % get.installDIR())
