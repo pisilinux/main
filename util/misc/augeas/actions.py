3fc20d9f091245fb07abe50491b721a5fc78b25c
4fury-c3440d8 <uglyside@yandex.ru>
2026-02-19 21:19:49 +0400

    augeas-v1.14.1

--- util/misc/augeas/actions.py
+++ util/misc/augeas/actions.py
@@ -2,18 +2,29 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
+j = ''.join([
+    ' --prefix=/usr',
+    ' --with-pdfdocs=no',
+    ' --with-threads=posix',
+    ' --without-selinux',
+    ' --disable-static '
+    ])
+
 def setup():
-    autotools.configure("--disable-static")
+    shelltools.system("sh autogen.sh")
+    autotools.configure(j)
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README*")
+
+    pisitools.dodoc("AUTHORS", "COPYING")
