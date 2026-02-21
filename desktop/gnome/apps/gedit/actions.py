6377ad4dc58ff740b31630c1741a50ba25108a64
Rmys <muscnsl@gmail.com>
2026-02-21 11:52:38 +0300

    gedit

--- desktop/gnome/apps/gedit/actions.py
+++ desktop/gnome/apps/gedit/actions.py
@@ -1,12 +1,18 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 2
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
-from pisi.actionsapi import mesontools, pisitools
+from pisi.actionsapi import mesontools, pisitools, shelltools, get
+import os
+
 
 def setup():
+    shelltools.cd("subprojects")
+    shelltools.system("git clone https://gitlab.gnome.org/GNOME/libgd.git")
+    shelltools.cd("..")
+
     mesontools.configure()
 
 def build():
