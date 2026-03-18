2d3b6a1136c8cd9ae430535b9a1833087723f201
Rmys <muscnsl@hotmail.com>
2026-03-18 17:19:29 +0300

    vulkan-lunarg-tools

--- x11/vulkan/vulkan-lunarg-tools/actions.py
+++ x11/vulkan/vulkan-lunarg-tools/actions.py
@@ -13,15 +13,16 @@ from pisi.actionsapi import get
 pisitools.cxxflags.add("-std=gnu++11")
 
 ver = get.srcVERSION()
-indir = "%s/VulkanTools-%s" % (get.workDIR(), ver)
+indir = "%s/VulkanTools-vulkan-sdk-%s" % (get.workDIR(), ver)
 
 def setup():
-	shelltools.system("./update_external_sources.sh")
+	# shelltools.system("./update_external_sources.sh")
 	shelltools.system("mkdir -p build")
 	shelltools.cd("build")	
-	shelltools.system("../scripts/update_deps.py")
+	# shelltools.system("../scripts/update_deps.py")
 	
-	shelltools.system("cmake .. -C helper.cmake \
+	shelltools.system("cmake .. \
+						-DCMAKE_SKIP_INSTALL_RPATH=ON \
 		               -DCMAKE_INSTALL_PREFIX=/usr \
 		               -DCMAKE_INSTALL_SYSCONFDIR=/etc \
 		               -DCMAKE_INSTALL_DATADIR=/usr/share \
@@ -32,11 +33,11 @@ def setup():
 		               -DBUILD_WSI_WAYLAND_SUPPORT=On \
 		               -DBUILD_TESTS=OFF \
 		               -DBUILD_VIA=OFF \
-		               -DCMAKE_BUILD_TYPE=Release \
-		               -DVULKAN_HEADERS_INSTALL_DIR=%s/build/Vulkan-Headers/build/install \
-		               -DVULKAN_LOADER_INSTALL_DIR=%s/build/Vulkan-Loader/build/install  \
-		               -DVULKAN_VALIDATIONLAYERS_INSTALL_DIR=%s/build/Vulkan-ValidationLayers/build/install \
-		               " % (indir, indir, indir ) )
+		               -DCMAKE_BUILD_TYPE=Release" )
+					   # -DVULKAN_HEADERS_INSTALL_DIR=%s/build/Vulkan-Headers/build/install \
+		               # -DVULKAN_LOADER_INSTALL_DIR=%s/build/Vulkan-Loader/build/install  \
+		               # -DVULKAN_VALIDATIONLAYERS_INSTALL_DIR=%s/build/Vulkan-ValidationLayers/build/install \
+		               # % (indir, indir, indir ) )
 	
   
 
@@ -49,7 +50,7 @@ def install():
 	shelltools.cd("build")
 	autotools.rawInstall("DESTDIR=%s" %get.installDIR()) 
 	shelltools.cd("..")
-	pisitools.insinto("/usr/share/doc/vulkan-trace", "vktrace/LICENSE")
+	# pisitools.insinto("/usr/share/doc/vulkan-trace", "vktrace/LICENSE")
 	pisitools.insinto("/usr/share/doc/vulkan-extra-layers", "CONTRIBUTING.md")
 	pisitools.insinto("/usr/share/doc/vulkan-extra-layers", "GOVERNANCE.md")
 	pisitools.insinto("/usr/share/doc/vulkan-extra-layers", "LICENSE.txt")
