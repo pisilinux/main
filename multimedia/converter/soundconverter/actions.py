8e52fbbcdc63dae3a01853863f1b3f85c6a748c7
suvari <suvarice@gmail.com>
2026-04-12 13:40:05 +0300

    soundconverter: version bump to 4.1.3

--- multimedia/converter/soundconverter/actions.py
+++ multimedia/converter/soundconverter/actions.py
@@ -4,10 +4,12 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pythonmodules, pisitools
 
 def build():
     pythonmodules.compile(pyVer = "3")
 
 def install():
     pythonmodules.install(pyVer = "3")
+
+    pisitools.dodoc("COPYING")
