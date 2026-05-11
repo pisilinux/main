1cc64140a50fc0afb0e6a6a137517a3a3ce25540
Erkan IŞIK <erkanisik@yahoo.com>
2026-05-11 15:40:33 +0300

    Update actions.py

--- desktop/font/carlito-fonts/actions.py
+++ desktop/font/carlito-fonts/actions.py
@@ -4,15 +4,7 @@
 from pisi.actionsapi import shelltools, pisitools
 
 def install():
-    # Arşivden çıkan klasörün içine gir (GitHub varsayılanı)
-    # Eğer klasör adı farklıysa 'ls' ile kontrol edip burayı güncelleyebilirsin
-    #shelltools.cd("carlito-main")
-    
-    # Yazı tipi dosyalarını (fonts/ttf/ dizini altındaysa orayı belirt)
-    # Carlito reposunda fontlar genellikle fonts/ttf/ altındadır
     pisitools.insinto("/usr/share/fonts/carlito", "fonts/ttf/*.ttf")
-    
-    # Fontconfig ayarları (Öncekiyle aynı)
     conf_content = """<?xml version="1.0"?>
 <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
 <fontconfig>
@@ -27,6 +19,4 @@ def install():
     shelltools.echo("30-calibri-to-carlito.conf", conf_content)
     pisitools.insinto("/etc/fonts/conf.avail", "30-calibri-to-carlito.conf")
     pisitools.dosym("/etc/fonts/conf.avail/30-calibri-to-carlito.conf", "/etc/fonts/conf.d/30-calibri-to-carlito.conf")
-
-    # Belgeler
     pisitools.dodoc("OFL.txt", "README.md")
