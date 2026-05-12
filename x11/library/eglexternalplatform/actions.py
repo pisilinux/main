60f1b2db4a186f0f7dfff038fa02df5fd39713cf
suvari <suvarice@gmail.com>
2026-05-12 17:05:21 +0300

    new packages: egl-gbm, egl-wayland2, composer

--- x11/library/eglexternalplatform/actions.py
+++ x11/library/eglexternalplatform/actions.py
@@ -1,26 +1,26 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import mesontools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    pisitools.dosed("meson.build", "datadir", "libdir")
-    #autotools.configure()
+    mesontools.configure()
 
 def build():
-    shelltools.system("meson build --libdir=/usr/lib --includedir=/usr/include/EGL")
+    mesontools.build()
+
+def check():
+    mesontools.build("test")
 
 def install():
     mesontools.install()
-    # pisitools.insinto("/usr/include/EGL", "interface/*")
-    # pisitools.insinto("/usr/lib/pkgconfig", "*.pc")
-    #autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
+    shelltools.copytree("%s/usr/share/pkgconfig" % get.installDIR(), "%s/usr/lib/pkgconfig" % get.installDIR())
+    pisitools.removeDir("/usr/share/pkgconfig")
     pisitools.dodoc("COPYING", "README*")
