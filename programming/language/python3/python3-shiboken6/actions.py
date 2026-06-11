275a93b6222f9f68d94b582e5e170c2d14ffa36e
Rmys <muscnsl@hotmail.com>
2026-06-11 21:49:01 +0300

    python3-shiboken6 6.11.1

--- programming/language/python3/python3-shiboken6/actions.py
+++ programming/language/python3/python3-shiboken6/actions.py
@@ -9,14 +9,27 @@ from pisi.actionsapi import cmaketools
 from pisi.actionsapi import shelltools
 
 
-# WorkDir="pyside-setup-everywhere-src-%s/sources/shiboken6" % get.srcVERSION()
+# WorkDir="pyside-setup-everywhere-src-%s/sources" % get.srcVERSION()
 
 def setup():
     shelltools.cd("sources/shiboken6")
     pisitools.dosed("libshiboken/CMakeLists.txt", 'LIBRARY DESTINATION "shiboken6"', 'LIBRARY DESTINATION "${LIB_INSTALL_DIR}"')
+    pisitools.dosed("libshiboken/CMakeLists.txt", 'wheels/cmake/Shiboken6', '/cmake/Shiboken6')
     cmaketools.configure("-B build -DCMAKE_INSTALL_PREFIX=/usr \
                           -DCMAKE_BUILD_TYPE=Release \
                           -DBUILD_TESTS=OFF \
+                          -DPYTHON_EXECUTABLE=/usr/bin/python3 \
+                          -DSHIBOKEN_PYTHON_LIBRARIES=`pkgconf python3-embed --libs` \
+                          -DUSE_PYTHON_VERSION=3")
+
+    shelltools.cd("../shiboken6_generator")
+    # pisitools.dosed("generator/CMakeLists.txt", 'EXPORT "${package_name}Targets"', 'EXPORT "/usr/bin/Targets"')
+    pisitools.dosed("generator/CMakeLists.txt", 'wheels/cmake', '/cmake')
+    cmaketools.configure("-B build -DCMAKE_INSTALL_PREFIX=/usr \
+                          -DCMAKE_BUILD_TYPE=Release \
+                          -DBUILD_TESTS=OFF \
+                          -DCMAKE_BIN_INSTALL_DIR:PATH=/usr/bin \
+                          -DPYTHON_EXECUTABLE=/usr/bin/python3 \
                           -DSHIBOKEN_PYTHON_LIBRARIES=`pkgconf python3-embed --libs` \
                           -DUSE_PYTHON_VERSION=3")
 
@@ -25,9 +38,18 @@ def build():
     shelltools.cd("sources/shiboken6/build")
     cmaketools.make()
 
+    shelltools.cd("../../shiboken6_generator/build")
+    cmaketools.make()
+
 def install():
     shelltools.cd("sources/shiboken6/build")
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
     # pisitools.removeDir("/usr/shiboken6")
+
+    shelltools.cd("../../shiboken6_generator/build")
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.removeDir("/usr/shiboken6_generator")
+
+    pisitools.dosed("%s/usr/lib/cmake/Shiboken6Tools/Shiboken6ToolsTargets-release.cmake" % get.installDIR(), "shiboken6_generator", "bin")
 
     # pisitools.dosed("%s/usr/lib/cmake/Shiboken6/Shiboken6Targets-release.cmake" % get.installDIR(), "/shiboken6/", "/lib/")
