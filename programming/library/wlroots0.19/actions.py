b591572ab416fe0590e54ab3ed58c3fed1db3462
suvari <suvarice@gmail.com>
2026-07-13 14:49:44 +0300

    new packages: wlroots0.19, libcamera

---
+++ programming/library/wlroots0.19/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import mesontools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    mesontools.configure("-Dsession=enabled \
+        -Dbackends=drm,libinput,x11 \
+            -Drenderers='gles2, vulkan'")
+
+
+def build():
+    mesontools.build()
+
+def install():
+    mesontools.install()
+
+    pisitools.dodoc("LICENSE", "README.md")
