776863c0c146d807821326235fed82844ef26598
suvari <suvarice@gmail.com>
2026-07-22 13:23:20 +0300

    new package: erofs-utils

---
+++ hardware/misc/erofs-utils/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
+
+from pisi.actionsapi import autotools, pisitools, get, shelltools
+
+def setup():
+    autotools.autoreconf("-vif")
+
+    autotools.configure("--enable-fuse \
+        --enable-lzma \
+        --enable-multithreading \
+        --prefix=/usr \
+        --sbindir=/usr/bin  \
+        --with-libdeflate")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("COPYING")
