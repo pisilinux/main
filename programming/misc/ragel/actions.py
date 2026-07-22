de0cb31c5d5d28655d4d89b8dd494b397a8e06fb
suvari <suvarice@gmail.com>
2026-07-22 11:29:28 +0300

    ragel: fix build

--- programming/misc/ragel/actions.py
+++ programming/misc/ragel/actions.py
@@ -4,16 +4,24 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import autotools, pisitools, get
+from pisi.actionsapi import autotools, pisitools, get, shelltools
 
 def setup():
-    autotools.configure("--prefix=/usr")
+    autotools.autoreconf("-vif")
+
+    autotools.configure("./configure \
+                        --prefix=/usr \
+                        --docdir=/usr/share/doc/ragel \
+                        --bindir=/usr/bin \
+                        --mandir=/usr/share/man \
+                        --libdir=/usr/lib \
+                        --disable-static")
 
 def build():
     autotools.make()
 
 def install():
-    autotools.install("DESTDIR=%s" % get.installDIR())
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.insinto("/usr/share/vim/vimfiles/syntax/", "ragel.vim")
 
