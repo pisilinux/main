b2273d82eee33f8e5418eda346ea3bef509fcf13
Rimru01 <136918667+Rimru01@users.noreply.github.com>
2026-07-30 18:19:49 +0300

    Switch from qt5 to autotools for build process

--- util/admin/pgModeler/actions.py
+++ util/admin/pgModeler/actions.py
@@ -7,22 +7,22 @@
 
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import qt5
-
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
 
 def setup():
     #qt5.configure("pgmodeler.pro")
-    shelltools.system("qmake -r pgmodeler.pro PREFIX+=/usr BINDIR+=/usr/bin LIBDIR+=/usr/lib RESDIR+=/usr/share")
+    shelltools.system("/usr/lib/qt6/bin/qmake -r pgmodeler.pro PREFIX+=/usr BINDIR+=/usr/bin LIBDIR+=/usr/lib RESDIR+=/usr/share")
 
 def build():
-    qt5.make()
+    autotools.make()
 
 def install():
-    qt5.install()
+    autotools.rawInstall("INSTALL_ROOT=%s" % get.installDIR())
 
     pisitools.domove("/usr/lib/pgmodeler/lib*", "/usr/lib")
 
-    pisitools.insinto("usr/share/icons/hicolor/64x64/apps", "conf/pgmodeler_logo.png")
+    pisitools.insinto("usr/share/icons/hicolor/64x64/apps", "assets/conf/pgmodeler_logo.png", "pgmodeler.png")
     pisitools.dosym("/usr/share/pgmodeler/schemas", "/etc/pgmodeler/schemas")
 
     pisitools.dodir("/usr/lib/pgmodeler/plugins")
