d84c29badd68efa2a47a5864f6398ad05f66e828
Rimru01 <136918667+Rimru01@users.noreply.github.com>
2026-08-01 16:13:37 +0300

    Refactor setup and install functions for clarity

--- accessibility/liblouis/actions.py
+++ accessibility/liblouis/actions.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-#
-# Licensed under the GNU General Public License, version 3.
-# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import pythonmodules
+
 
 def setup():
-    autotools.configure("--prefix=/usr --enable-ucs4 --disable-static")
+    autotools.configure(
+        "--prefix=/usr "
+        "--enable-ucs4 "
+        "--disable-static"
+    )
+
 
 def build():
     autotools.make()
 
+
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    autotools.rawInstall(
+        "DESTDIR=%s" % get.installDIR()
+    )
 
-    pisitools.dodoc("AUTHORS", "COPYING", "COPYING.LESSER")
-
-    shelltools.cd("python")
-    pythonmodules.install(pyVer = "3")
+    pisitools.dodoc(
+        "AUTHORS",
+        "COPYING",
+        "COPYING.LESSER"
+    )
