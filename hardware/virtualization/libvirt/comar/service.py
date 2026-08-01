8e2667a60f6ef74f97bfc76f208f29272280eee9
Erkan IŞIK <erkanisik@yahoo.com>
2026-08-02 00:05:46 +0300

    libvirt 11.7.0: run modular daemons instead of monolithic libvirtd
    virt-manager 5.x connects to qemu:///system via /run/libvirt/virtqemud-sock,
    which only exists when the modular daemons run. The comar service now
    starts virtqemud, virtnetworkd, virtnodedevd and virtstoraged with
    detach=True (and --timeout 0 to prevent idle shutdown), instead of the
    deprecated monolithic libvirtd. Add dnsmasq runtime dependency for the
    default NAT network.

--- hardware/virtualization/libvirt/comar/service.py
+++ hardware/virtualization/libvirt/comar/service.py
@@ -7,37 +7,42 @@ serviceDesc = _({"en": "libvirt virtualization API daemon",
                  "tr": "libvirt sanallaştırma hizmeti"})
 serviceDefault = "on"
 
-PIDFILE         = "/run/libvirt/libvirtd.pid"
-KRB5_KTNAME     = "/etc/libvirt/krb5.tab"
-LIBVIRTD_CONFIG = "/etc/libvirt/libvirtd.conf"
+DAEMONS = [
+    ("/usr/sbin/virtqemud",    "/run/libvirt/virtqemud.pid"),
+    ("/usr/sbin/virtnetworkd", "/run/libvirt/virtnetworkd.pid"),
+    ("/usr/sbin/virtnodedevd", "/run/libvirt/virtnodedevd.pid"),
+    ("/usr/sbin/virtstoraged", "/run/libvirt/virtstoraged.pid"),
+]
 
 @synchronized
 def start():
-    os.environ["KRB5_KTNAME"] = config.get("KRB5_KTNAME", KRB5_KTNAME)
     os.environ["PATH"] = "/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/sbin:/usr/local/bin"
-
-    if os.path.exists(PIDFILE):
-        os.unlink(PIDFILE)
-
-    startService(command="/usr/sbin/libvirtd",
-                 args="--daemon --config %s %s --pid-file %s" % (
-                     config.get("LIBVIRTD_ARGS", ""),
-                     config.get("LIBVIRTD_CONFIG", LIBVIRTD_CONFIG),
-                     PIDFILE,
-                     ),
-                 donotify=True)
+    for daemon, pidfile in DAEMONS:
+        if os.path.exists(pidfile):
+            os.unlink(pidfile)
+        startService(command=daemon,
+                     args="--timeout 0 --pid-file %s" % pidfile,
+                     detach=True,
+                     donotify=True)
+    if not os.path.isdir("/run/libvirt"):
+        os.makedirs("/run/libvirt", 0755)
+    else:
+        os.chmod("/run/libvirt", 0755)
 
 @synchronized
 def stop():
-    stopService(pidfile="/run/libvirt/libvirtd.pid",
-                donotify=True)
-    if os.path.exists(PIDFILE):
-        os.unlink(PIDFILE)
+    for daemon, pidfile in DAEMONS:
+        stopService(pidfile=pidfile,
+                    command=daemon,
+                    donotify=True)
+        if os.path.exists(pidfile):
+            os.unlink(pidfile)
 
 @synchronized
 def reload():
-    if os.path.exists(PIDFILE):
-        os.kill(int(open(PIDFILE, "r").read().strip()), 1)
+    for daemon, pidfile in DAEMONS:
+        if os.path.exists(pidfile):
+            os.kill(int(open(pidfile, "r").read().strip()), 1)
 
 def status():
-    return isServiceRunning(pidfile=PIDFILE)
+    return isServiceRunning(pidfile="/run/libvirt/virtqemud.pid")
