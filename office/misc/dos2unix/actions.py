7c0cde88df36a8e1e0458f61292573f4c0c3b214
Erkan IŞIK <erkanisik@yahoo.com>
2026-08-01 22:03:27 +0300

    dos2unix 7.5.6

--- office/misc/dos2unix/actions.py
+++ office/misc/dos2unix/actions.py
@@ -5,17 +5,10 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
 def build():
     autotools.make()
 
 def install():
-    for binary in ["dos2unix", "mac2unix", "unix2dos", "unix2mac"]:
-        pisitools.dobin(binary)
-
-    pisitools.dodoc("NEWS.txt", "README.txt" ,"TODO.txt", "ChangeLog.txt", "COPYING.txt")
-    pisitools.doman("man/man1/dos2unix.1")
-
-
-
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
