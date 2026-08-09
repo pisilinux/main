8ce062703cc60a2f22be98f5e43cdac80b4df675
Rmys <muscnsl@gmail.com>
2026-08-09 10:22:09 +0300

    liblangtag

--- editor/liblangtag/actions.py
+++ editor/liblangtag/actions.py
@@ -5,6 +5,7 @@
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 y = ''.join([
