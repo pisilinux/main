22cf6e8c76f5e70f802d4b2c6282b55534af5ab0
Rmys <muscnsl@gmail.com>
2026-09-13 18:17:15 +0300

    java8-openjdk ver. bump

--- programming/language/java/java8-openjdk/actions.py
+++ programming/language/java/java8-openjdk/actions.py
@@ -52,7 +52,7 @@ def setup():
                             --with-jdk-home=/usr/lib/jvm/java-8-openjdk \
                             --with-ecj-jar=/usr/share/java/ecj.jar \
                             --with-vendor-name='PisiLinux' \
-                            --with-pkgversion='PisiLinux build 8u.502_3.40.0' \
+                            --with-pkgversion='PisiLinux build 8u.512_3.40.1' \
                            " % jobs.replace("-j", ""))
 
 def build():
