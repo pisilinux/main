06c5fc7deafc80bc251bbebaf410dd97c6788efd
Rmys <muscnsl@gmail.com>
2026-09-21 12:41:53 +0300

    python3-installer ver. bump

--- programming/language/python3/python3-installer/actions.py
+++ programming/language/python3/python3-installer/actions.py
@@ -4,10 +4,12 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
-from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import python3modules
 
 def build():
-	pythonmodules.compile(pyVer = '3')
+	python3modules.compile(pyVer = '3')
 
 def install():
-	pythonmodules.install(pyVer = '3')
+	python3modules.install(pyVer = '3')
+
+	# pisitools.remove("/usr/lib/python3.11/site-packages/installer/_scripts/*.exe")
